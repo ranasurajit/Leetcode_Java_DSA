df13e0c24115c4a0f6285be12fb2dc14afd9ad3d
Surajit Rana <surajit1602@gmail.com>
2024-02-11 00:03:40 +0530

    Time: 6 ms (26.10%) | Memory: 52.6 MB (6.50%) - LeetSync

--- Graphs/200-number-of-islands/number-of-islands.java
+++ Graphs/200-number-of-islands/number-of-islands.java
@@ -1,27 +1,52 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-		int count = 0;
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				if (grid[i][j] == '1') {
-					dfs(grid, rows, cols, i, j);
-					count++;
-				}
-			}
-		}
-		return count;
+        int m = grid.length; // rows
+        int n = grid[0].length; // cols
+        int[][] visited = new int[m][n];
+        int islands = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && visited[i][j] == 0) {
+                    bfsGraph(grid, visited, i, j, m, n);
+                    islands++;
+                }
+            }
+        }
+        return islands;
     }
 
-    private void dfs(char[][] grid, int rows, int cols, int i, int j) {
-		if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] == '0') {
-            return;
+    private void bfsGraph(char[][] grid, int[][] visited, int row, int col, int m, int n) {
+        Queue<Pair> queue = new LinkedList<Pair>();
+        queue.offer(new Pair(row, col));
+        visited[row][col] = 1;
+        while (!queue.isEmpty()) {
+            Pair current = queue.poll();
+            int directions = 4;
+            int[] delRow = { -1, 0, 1, 0 };
+            int[] delCol = { 0, 1, 0, -1 };
+
+            for (int i = 0; i < directions; i++) {
+                int effRow = current.row + delRow[i];
+                int effCol = current.col + delCol[i];
+
+                if (effRow >= 0 && effRow < m &&
+                    effCol >= 0 && effCol < n &&
+                    grid[effRow][effCol] == '1' && visited[effRow][effCol] == 0) {
+                    visited[effRow][effCol] = 1;
+                    queue.offer(new Pair(effRow, effCol));
+                }
+            }
         }
-        grid[i][j] = '0';
-        dfs(grid, rows, cols, i + 1, j);
-        dfs(grid, rows, cols, i, j + 1);
-        dfs(grid, rows, cols, i - 1, j);
-        dfs(grid, rows, cols, i, j - 1);
-	}
+    }
+
+    class Pair {
+        int row;
+        int col;
+
+        public Pair(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
 }
