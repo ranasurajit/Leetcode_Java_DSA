3763327369482c0a61c9088b2fa148653c7d8fe2
Surajit Rana <surajit1602@gmail.com>
2024-02-11 23:30:21 +0530

    Time: 1 ms (93.39%) | Memory: 47.5 MB (55.32%) - LeetSync

--- Graphs/547-number-of-provinces/number-of-provinces.java
+++ Graphs/547-number-of-provinces/number-of-provinces.java
@@ -7,24 +7,35 @@ class Solution {
             if (!visited[i]) {
                 provinces++;
                 // Using BFS
-                bfsGraph(isConnected, visited, n, i);
+                // bfsGraph(isConnected, visited, n, i);
+                dfsGraph(isConnected, visited, n, i);
             }
         }
         return provinces;
     }
 
-    private void bfsGraph(int[][] isConnected, boolean[] visited, int n, int current) {
-        Queue<Integer> queue = new LinkedList<Integer>();
-        queue.offer(current);
+    private void dfsGraph(int[][] isConnected, boolean[] visited, int n, int current) {
         visited[current] = true;
-        while (!queue.isEmpty()) {
-            Integer node = queue.poll();
-            for (int i = 0; i < n; i++) {
-                if (isConnected[node][i] == 1 && !visited[i]) {
-                    visited[i] = true;
-                    queue.offer(i);
-                }
+        for (int i = 0; i < n; i++) {
+            if (isConnected[current][i] == 1 && !visited[i]) {
+                visited[i] = true;
+                dfsGraph(isConnected, visited, n, i);
             }
         }
     }
+
+    // private void bfsGraph(int[][] isConnected, boolean[] visited, int n, int current) {
+    //     Queue<Integer> queue = new LinkedList<Integer>();
+    //     queue.offer(current);
+    //     visited[current] = true;
+    //     while (!queue.isEmpty()) {
+    //         Integer node = queue.poll();
+    //         for (int i = 0; i < n; i++) {
+    //             if (isConnected[node][i] == 1 && !visited[i]) {
+    //                 visited[i] = true;
+    //                 queue.offer(i);
+    //             }
+    //         }
+    //     }
+    // }
 }
