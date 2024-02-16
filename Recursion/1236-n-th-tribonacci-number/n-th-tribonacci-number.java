d430c353132b00cfea056574af47b99425df935d
Surajit Rana <surajit1602@gmail.com>
2024-02-16 23:49:13 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (7.34%) - LeetSync

--- Recursion/1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ Recursion/1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,28 +1,17 @@
 class Solution {
     public int tribonacci(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return helper(n, dp);
-    }
-
-    private int helper(int n, int[] dp) {
         if (n == 0) {
-            dp[n] = 0;
-            return dp[n];
+            return 0;
         }
-        if (n <= 2) {
-            dp[n] = 1;
-            return dp[n];
-        }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
+        int prev3 = 0;
+        int prev2 = 1;
+        int prev1 = 1;
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            int prev = prev1 + prev2 + prev3;
+            prev3 = prev2;
+            prev2 = prev1;
+            prev1 = prev;
         }
-        return dp[n];
+        return prev1;
     }
 }
