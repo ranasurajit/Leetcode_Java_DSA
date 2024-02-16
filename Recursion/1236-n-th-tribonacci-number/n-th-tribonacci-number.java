dd7dd17a01667c28aec6ae71e6f0eb4465387617
Surajit Rana <surajit1602@gmail.com>
2024-02-16 23:41:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (54.07%) - LeetSync

--- Recursion/1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ Recursion/1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -14,12 +14,12 @@ class Solution {
             dp[n] = 1;
             return dp[n];
         }
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
         if (dp[n] != -1) {
             return dp[n];
         }
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
         for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
