aa0931f9e106de3a31caab9a1f5ec460f63a77d2
Surajit Rana <surajit1602@gmail.com>
2024-02-28 23:53:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (78.24%) - LeetSync

--- BinaryTrees/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ BinaryTrees/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -21,12 +21,8 @@ class Solution {
         if (root.left == null && root.right == null) {
             return 1;
         }
-        if (root.left == null) {
-            return maxDepth(root.right) + 1;
-        }
-        if (root.right == null) {
-            return maxDepth(root.left) + 1;
-        }
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+        return 1 + Math.max(lh, rh);
     }
 }
