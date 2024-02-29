81b1d7af891f394d4737e358b4ea65c3d83e9fc3
Surajit Rana <surajit1602@gmail.com>
2024-02-29 23:25:08 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (91.69%) - LeetSync

--- LinkedList/206-reverse-linked-list/reverse-linked-list.java
+++ LinkedList/206-reverse-linked-list/reverse-linked-list.java
@@ -10,9 +10,6 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
         ListNode prev = null;
         ListNode current = head;
         while (current != null) {
