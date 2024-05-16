a954aa2970650bf12afcb79783543f7fc4452922
Surajit Rana <surajit1602@gmail.com>
2024-05-17 00:17:26 +0530

    Time: 2 ms (80.79%) | Memory: 45.2 MB (92.97%) - LeetSync

--- GreedyAlgorithms/55-jump-game/jump-game.java
+++ GreedyAlgorithms/55-jump-game/jump-game.java
@@ -1,13 +1,28 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int reachable = 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            if (i > reachable) {
-                return false;
+        // int reachable = 0;
+        // int n = nums.length;
+        // for (int i = 0; i < n; i++) {
+        //     if (i > reachable) {
+        //         return false;
+        //     }
+        //     if (reachable < nums[i] + i) {
+        //         reachable = nums[i] + i;
+        //     }
+        // }
+        // return true;
+
+        int left = 0;
+        int right = 0;
+        int farthest = 0;
+        while (right < nums.length - 1) {
+            for (int i = left; i <= right; i++) {
+                farthest = Math.max(farthest, nums[i] + i);
             }
-            if (reachable < nums[i] + i) {
-                reachable = nums[i] + i;
+            left = right + 1;
+            right = farthest;
+            if (left > right) {
+                return false;
             }
         }
         return true;
