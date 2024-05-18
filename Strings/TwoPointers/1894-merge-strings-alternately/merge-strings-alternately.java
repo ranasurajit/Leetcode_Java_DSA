be1ce90eb18e3963a3ca993a1ff1e28b3eee0e1a
Surajit Rana <surajit1602@gmail.com>
2024-05-18 21:26:11 +0530

    Time: 1 ms (85.56%) | Memory: 41.6 MB (67.65%) - LeetSync

--- Strings/TwoPointers/1894-merge-strings-alternately/merge-strings-alternately.java
+++ Strings/TwoPointers/1894-merge-strings-alternately/merge-strings-alternately.java
@@ -5,21 +5,15 @@ class Solution {
         int wLen1 = word1.length();
         int wLen2 = word2.length();
         StringBuilder builder = new StringBuilder();
-        while (p < wLen1 && q < wLen2) {
-            builder.append(word1.charAt(p));
-            builder.append(word2.charAt(q));
-            p++;
-            q++;
-        }
-        // if word1 is still left untraversed
-        while (p < wLen1) {
-            builder.append(word1.charAt(p));
-            p++;
-        }
-        // if word2 is still left untraversed
-        while (q < wLen2) {
-            builder.append(word2.charAt(q));
-            q++;
+        while (p < wLen1 || q < wLen2) {
+            if (p < wLen1) {
+                builder.append(word1.charAt(p));
+                p++;
+            }
+            if (q < wLen2) {
+                builder.append(word2.charAt(q));
+                q++;
+            }
         }
         return builder.toString();
     }
