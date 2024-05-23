7ff16dedaa7b34c3ad6c74d76caec8d0826674b8
Surajit Rana <surajit1602@gmail.com>
2024-05-23 23:59:15 +0530

    Time: 2 ms (72.58%) | Memory: 42.9 MB (9.86%) - LeetSync

--- Graphs/1036-rotting-oranges/rotting-oranges.java
+++ Graphs/1036-rotting-oranges/rotting-oranges.java
@@ -1,47 +1,48 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m = grid.length; // rows
-        int n = grid[0].length; // cols
+        int m = grid.length;
+        int n = grid[0].length;
         int[][] visited = new int[m][n];
         int freshOranges = 0;
-
+        int convertedOranges = 0;
+        int minTime = 0;
         Queue<Pair> queue = new LinkedList<Pair>();
-
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    freshOranges++;
-                } else if (grid[i][j] == 2 && visited[i][j] == 0) {
-                    queue.offer(new Pair(i, j, 0));
+                if (grid[i][j] == 2) {
                     visited[i][j] = 1;
+                    queue.offer(new Pair(i, j, 0));
+                } else if (grid[i][j] == 1) {
+                    freshOranges++;
                 }
             }
         }
-
-        int convertedOranges = 0;
-        int time = 0;
-
-        while(!queue.isEmpty()) {
-            Pair current = queue.poll();
+        while (!queue.isEmpty()) {
+            Pair currentPair = queue.poll();
+            int row = currentPair.row;
+            int col = currentPair.col;
+            int time = currentPair.time;
+            if (time > minTime) {
+                minTime = time;
+            }
+            int[] deltaRow = { -1, 0, 1, 0 };
+            int[] deltaCol = { 0, 1, 0, -1 };
             int directions = 4;
-            int[] delRow = { -1, 0, 1, 0 };
-            int[] delCol = { 0, 1, 0, -1 };
-            time = Math.max(time, current.time);
 
-            for (int i = 0; i < directions; i++) {
-                int effRow = current.row + delRow[i];
-                int effCol = current.col + delCol[i];
-                if (effRow >= 0 && effRow < m &&
-                    effCol >= 0 && effCol < n &&
-                    visited[effRow][effCol] == 0 &&
-                    grid[effRow][effCol] == 1) {
-                    convertedOranges++;
+            for (int k = 0; k < directions; k++) {
+                int effRow = row + deltaRow[k];
+                int effCol = col + deltaCol[k];
+                if (effRow >= 0 && effRow < m && 
+                    effCol >= 0 && effCol < n && 
+                    visited[effRow][effCol] == 0 && grid[effRow][effCol] == 1) {
                     visited[effRow][effCol] = 1;
-                    queue.offer(new Pair(effRow, effCol, current.time + 1));
+                    grid[effRow][effCol] = 2;
+                    queue.offer(new Pair(effRow, effCol, time + 1));
+                    convertedOranges++;
                 }
             }
         }
-        return (freshOranges == convertedOranges) ? time : -1;
+        return convertedOranges == freshOranges ? minTime : -1;
     }
 
     class Pair {
@@ -49,7 +50,7 @@ class Solution {
         int col;
         int time;
 
-        public Pair (int row, int col, int time) {
+        public Pair(int row, int col, int time) {
             this.row = row;
             this.col = col;
             this.time = time;
