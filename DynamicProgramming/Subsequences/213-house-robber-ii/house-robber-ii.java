c645c69f66321ac0bd1b9ae4ff8f92051e1a014d
Surajit Rana <surajit1602@gmail.com>
2024-05-27 23:28:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (8.89%) - LeetSync

---
+++ DynamicProgramming/Subsequences/213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        int[] nums1 = new int[n - 1];
+        int[] nums2 = new int[n - 1];
+        for (int i = 0; i < n; i++) {
+            if (i > 0) {
+                nums2[i - 1] = nums[i]; 
+            }
+            if (i < n - 1) {
+                nums1[i] = nums[i];
+            }
+        }
+        // With recursion
+        // return Math.max(maxSumRobbedRecursion(n - 2, nums1), maxSumRobbedRecursion(n - 2, nums2));
+        int[] dp1 = new int[n - 1];
+        int[] dp2 = new int[n - 1];
+        Arrays.fill(dp1, -1);
+        Arrays.fill(dp2, -1);
+        return Math.max(maxSumRobbedMemoization(n - 2, nums1, dp1), 
+            maxSumRobbedMemoization(n - 2, nums2, dp2));
+    }
+
+    /**
+        With Memoization - TC: O(2^N), SC: O(2^N)
+     */
+    private int maxSumRobbedMemoization(int index, int[] nums, int[] dp) {
+        if (index == 0) {
+            return nums[index];
+        }
+        if (index < 0) {
+            return 0;
+        }
+        if (dp[index] != -1) {
+            return dp[index];
+        }
+        int pick = nums[index] + maxSumRobbedMemoization(index - 2, nums, dp);
+        int notpick = 0 + maxSumRobbedMemoization(index - 1, nums, dp);
+        dp[index] = Math.max(pick, notpick);
+        return dp[index];
+    }
+
+    /**
+        With Recursion - TC: O(2^N), SC: O(2^N)
+     */
+    private int maxSumRobbedRecursion(int index, int[] nums) {
+        if (index == 0) {
+            return nums[index];
+        }
+        if (index < 0) {
+            return 0;
+        }
+        int pick = nums[index] + maxSumRobbedRecursion(index - 2, nums);
+        int notpick = 0 + maxSumRobbedRecursion(index - 1, nums);
+        return Math.max(pick, notpick);
+    }
+}
