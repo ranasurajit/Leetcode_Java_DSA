3a463bb41179cb578f51c6dcc1572c70560d4873
Surajit Rana <surajit1602@gmail.com>
2024-06-14 22:43:56 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (51.12%) - LeetSync

--- BinaryTrees/DFS/199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ BinaryTrees/DFS/199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -14,21 +14,20 @@
  * }
  */
 class Solution {
-    // DFS Solution
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> view = new ArrayList<Integer>();
-        dfsTree(root, 0, view);
+        dfsTree(root, view, 0);
         return view;
     }
 
-    private void dfsTree(TreeNode node, int level, List<Integer> view) {
+    private void dfsTree(TreeNode node, List<Integer> view, int level) {
         if (node == null) {
             return;
         }
-        if (view.size() == level) {
+        if (level == view.size()) {
             view.add(node.val);
         }
-        dfsTree(node.right, level + 1, view);
-        dfsTree(node.left, level + 1, view);
+        dfsTree(node.right, view, level + 1);
+        dfsTree(node.left, view, level + 1);
     }
 }
