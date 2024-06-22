f4f10508a4c15961b807188b26b34cd59d4710cd
Surajit Rana <surajit1602@gmail.com>
2024-06-22 17:06:37 +0530

    Time: 3 ms (100.00%) | Memory: 53.1 MB (94.39%) - LeetSync

--- Arrays/SubArrays/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ Arrays/SubArrays/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -2,16 +2,16 @@ class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
         int subArraysCount = 0;
         int n = nums.length;
-        // HashMap to store the count of odd numbers till ith element and frequency of that
-        Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
-        hm.put(0, 1);
+        int[] prefixOdds = new int[n + 1];
+        prefixOdds[0] = 1;
         int countOdds = 0;
         for (int i = 0; i < n; i++) {
             // if num is odd then returns 1 else 0
             countOdds += (nums[i] & 1);
-            // checks if (countOdds - k) exists in HashMap and returns the count
-            subArraysCount += hm.getOrDefault(countOdds - k, 0);
-            hm.put(countOdds, hm.getOrDefault(countOdds, 0) + 1);
+            if (countOdds - k >= 0) {
+                subArraysCount += prefixOdds[countOdds - k];
+            }
+            prefixOdds[countOdds]++;
         }
         return subArraysCount;
     }
