9f9f103d6039ecc00f6120fd6e70e676832e3726
Surajit Rana <surajit1602@gmail.com>
2024-06-24 20:39:17 +0530

    Time: 5 ms (88.39%) | Memory: 55.2 MB (89.68%) - LeetSync

--- Arrays/SlidingWindow/1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.java
+++ Arrays/SlidingWindow/1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.java
@@ -3,16 +3,15 @@ class Solution {
         int n = nums.length;
         int flips = 0;
         int flipsTillIndex = 0;
-        boolean[] isFlipped = new boolean[n];
         for (int i = 0; i < n; i++) {
-            if (i >= k && isFlipped[i - k]) {
+            if (i >= k && nums[i - k] == -1) {
                 flipsTillIndex--;
             }
             if (flipsTillIndex % 2 == nums[i]) {
                 if (i + k > n) {
                     return -1;
                 }
-                isFlipped[i] = true;
+                nums[i] = -1;
                 flipsTillIndex++;
                 flips++;
             }
