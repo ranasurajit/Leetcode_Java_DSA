998668cd1c6b3ac85639d28d462788ed2f06c5cc
Surajit Rana <surajit1602@gmail.com>
2024-07-07 18:02:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (56.21%) - LeetSync

---
+++ BinaryTrees/DFS/2384-root-equals-sum-of-children/root-equals-sum-of-children.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return root.val == dfsTree(root);
+    }
+
+    private int dfsTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val;
+        }
+        int left = dfsTree(root.left);
+        int right = dfsTree(root.right);
+        return left + right;
+    }
+}
