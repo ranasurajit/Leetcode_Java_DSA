1bf47bafdebf60af720306a6f9f04b525b520ea3
Surajit Rana <surajit1602@gmail.com>
2024-07-09 00:23:42 +0530

    Time: 12 ms (50.58%) | Memory: 58.5 MB (42.72%) - LeetSync

---
+++ HashTable/217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(nums[i])) {
+                return true;
+            }
+            hm.put(nums[i], i);
+        }
+        return false;
+    }
+}
