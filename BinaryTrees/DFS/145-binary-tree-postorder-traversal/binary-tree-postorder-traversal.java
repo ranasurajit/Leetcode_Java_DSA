b3d04d45c75d8f71fda85aa9950227eb24f2e610
Surajit Rana <surajit1602@gmail.com>
2024-07-14 22:41:04 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (98.23%) - LeetSync

---
+++ BinaryTrees/DFS/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> path = new ArrayList<Integer>();
+        dfsTree(root, path);
+        return path;
+    }
+
+    private void dfsTree(TreeNode node, List<Integer> path) {
+        if (node == null) {
+            return;
+        }
+        dfsTree(node.left, path);
+        dfsTree(node.right, path);
+        path.add(node.val);
+    }
+}
