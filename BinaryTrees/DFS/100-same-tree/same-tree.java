f6afd88565a008e0e708493076ffaff248372aec
Surajit Rana <surajit1602@gmail.com>
2024-07-19 23:28:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (21.59%) - LeetSync

--- BinaryTrees/DFS/100-same-tree/same-tree.java
+++ BinaryTrees/DFS/100-same-tree/same-tree.java
@@ -18,6 +18,8 @@ class Solution {
         if (p == null || q == null) {
             return p == q;
         }
-        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return p.val == q.val && 
+            isSameTree(p.left, q.left) && 
+            isSameTree(p.right, q.right);
     }
 }
