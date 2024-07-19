5bb481c3d0f6939d36381a323e4351bea40ab2c6
Surajit Rana <surajit1602@gmail.com>
2024-07-19 23:46:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (13.28%) - LeetSync

--- BinaryTrees/DFS/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ BinaryTrees/DFS/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -14,17 +14,12 @@
  * }
  */
 class Solution {
-    // DFS Solution
     public int maxDepth(TreeNode root) {
-        return dfsTree(root);
-    }
-
-    private int dfsTree(TreeNode node) {
-        if (node == null) {
+        if (root == null) {
             return 0;
         }
-        int left = dfsTree(node.left);
-        int right = dfsTree(node.right);
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
         return 1 + Math.max(left, right);
     }
 }
