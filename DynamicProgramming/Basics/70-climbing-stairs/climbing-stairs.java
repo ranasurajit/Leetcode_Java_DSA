27a9e535455cfb99713663ec27dfe3f934808381
Surajit Rana <surajit1602@gmail.com>
2024-07-20 01:34:12 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (8.11%) - LeetSync

--- DynamicProgramming/Basics/70-climbing-stairs/climbing-stairs.java
+++ DynamicProgramming/Basics/70-climbing-stairs/climbing-stairs.java
@@ -12,10 +12,7 @@ class Solution {
     }
 
     private int helper(int n, int[] dp) {
-        if (n == 0) {
-            return 0;
-        }
-        if (n == 1 || n == 2) {
+        if (n <= 2) {
             return n;
         }
         if (dp[n] != -1) {
