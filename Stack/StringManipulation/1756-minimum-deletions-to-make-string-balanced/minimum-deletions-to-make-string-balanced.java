8c1f22190655ab620a36041dc2cc86df898b5d8a
Surajit Rana <surajit1602@gmail.com>
2024-07-30 17:51:12 +0530

    Time: 44 ms (25.07%) | Memory: 45.5 MB (23.12%) - LeetSync

--- Stack/StringManipulation/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ Stack/StringManipulation/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,5 +1,34 @@
 class Solution {
+
+    /**
+     * Using Two Pointers - TC: O(N), SC: O(1)
+     */
     public int minimumDeletions(String s) {
+        int n = s.length();
+        int minCount = Integer.MAX_VALUE;
+        int leftB = 0;
+        int rightA = 0;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'a') {
+                rightA++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'a') {
+                rightA--;
+            }
+            minCount = Math.min(minCount, rightA + leftB);
+            if (s.charAt(i) == 'b') {
+                leftB++;
+            }
+        }
+        return minCount;
+    }
+
+    /**
+     * Using Stack - TC: O(N), SC: O(N)
+     */
+    public int minimumDeletionsUsingStack(String s) {
         int n = s.length();
         int count = 0;
         Stack<Character> st = new Stack<Character>();
