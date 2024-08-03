cc0cec2ed70a49166fec5d53395ab7620a86b58d
Surajit Rana <surajit1602@gmail.com>
2024-08-03 15:56:59 +0530

    Time: 3 ms (85.96%) | Memory: 44.3 MB (42.83%) - LeetSync

--- Arrays/Sorting/1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
+++ Arrays/Sorting/1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,5 +1,36 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
+        // return canBeEqualByHashing(target, arr);
+        return canBeEqualBySorting(target, arr);
+    }
+
+    /**
+     * TC: O(2N) ~ O(N)
+     * SC: O(N)
+     */
+    private boolean canBeEqualByHashing(int[] target, int[] arr) {
+        int n = target.length;
+        Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n; i++) {
+            hm.put(target[i], hm.getOrDefault(target[i], 0) + 1);
+        }
+        for (int i = 0; i < n; i++) {
+            int count = hm.getOrDefault(arr[i], 0);
+            if (count == 1) {
+                hm.remove(arr[i]);
+            } else {
+                count = count - 1;
+                hm.put(arr[i], count);
+            }
+        }
+        return hm.isEmpty();
+    }
+
+    /**
+     * TC: O(NlogN + N) ~ O(NlogN)
+     * SC: O(1)
+     */
+    private boolean canBeEqualBySorting(int[] target, int[] arr) {
         int n = target.length;
         Arrays.sort(target);
         Arrays.sort(arr);
