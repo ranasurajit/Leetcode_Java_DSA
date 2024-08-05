20c75b2ee115f9baae4b8dd86ed9f42456e72793
Surajit Rana <surajit1602@gmail.com>
2024-08-05 16:21:13 +0530

    Time: 7 ms (55.29%) | Memory: 44.1 MB (51.23%) - LeetSync

--- HashTable/2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
+++ HashTable/2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
@@ -1,43 +1,18 @@
 class Solution {
     public String kthDistinct(String[] arr, int k) {
         Map<String, Integer> hm = new HashMap<String, Integer>();
-        Set<String> hs = new HashSet<String>();
         for (int i = 0; i < arr.length; i++) {
-            if (hm.containsKey(arr[i])) {
-                hs.add(arr[i]);
-                hm.remove(arr[i]);
-            } else {
-                if (!hs.contains(arr[i])) {
-                    hm.put(arr[i], i);
+            hm.put(arr[i], hm.getOrDefault(arr[i], 0) + 1);
+        }
+        String result = "";
+        for (int i = 0; i < arr.length; i++) {
+            if (hm.get(arr[i]) == 1) {
+                k--;
+                if (k == 0) {
+                    result = arr[i];
                 }
             }
         }
-        PriorityQueue<Pair> pq = new PriorityQueue<Pair>((p, q) -> p.index - q.index);
-        for (String key : hm.keySet()) {
-            pq.offer(new Pair(key, hm.get(key)));
-        }
-        String result = "";
-        if (pq.size() < k) {
-            return "";
-        }
-        int count = 1;
-        while (!pq.isEmpty()) {
-            Pair current = pq.poll();
-            if (count == k) {
-                result = current.str;
-            }
-            count++;
-        }
         return result;
-    }
-
-    class Pair {
-        String str;
-        int index;
-
-        public Pair(String str, int index) {
-            this.str = str;
-            this.index = index;
-        }
     }
 }
