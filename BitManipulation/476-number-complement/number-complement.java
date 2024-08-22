1eb06a013c655195f02201a25a72db8497e00259
Surajit Rana <surajit1602@gmail.com>
2024-08-22 18:43:19 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (80.96%) - LeetSync

--- BitManipulation/476-number-complement/number-complement.java
+++ BitManipulation/476-number-complement/number-complement.java
@@ -1,20 +1,35 @@
 class Solution {
     public int findComplement(int num) {
-        return solve(num);
+        int bits = countBits(num);
+        int index = 0;
+        while (index < bits) {
+            num = num ^ (1 << index);
+            index++;
+        }
+        return num;
     }
 
-    private int solve(int num) {
-        String s = "";
-        int ans = 0;
+    private int countBits(int n) {
         int count = 0;
-        while (num > 0) {
-            int rem = num % 2;
-            s = rem + s;
-            num = num / 2;
-            int bit = rem == 0 ? 1 : 0;
-            ans = ans + (int) (Math.pow(2, count) * bit);
+        while (n > 0) {
+            n = n >> 1;
             count++;
         }
-        return ans;
+        return count;
     }
+
+    // private int solve(int num) {
+    //     String s = "";
+    //     int ans = 0;
+    //     int count = 0;
+    //     while (num > 0) {
+    //         int rem = num % 2;
+    //         s = rem + s;
+    //         num = num / 2;
+    //         int bit = rem == 0 ? 1 : 0;
+    //         ans = ans + (int) (Math.pow(2, count) * bit);
+    //         count++;
+    //     }
+    //     return ans;
+    // }
 }
