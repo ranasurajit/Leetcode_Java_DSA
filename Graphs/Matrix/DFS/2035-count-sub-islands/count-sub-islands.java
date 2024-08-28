e21511e2d21e0f6ec5121c7e6b6b67b0e8c65e4a
Surajit Rana <surajit1602@gmail.com>
2024-08-28 18:47:06 +0530

    Time: 19 ms (89.93%) | Memory: 88.4 MB (14.18%) - LeetSync

--- Graphs/Matrix/DFS/2035-count-sub-islands/count-sub-islands.java
+++ Graphs/Matrix/DFS/2035-count-sub-islands/count-sub-islands.java
@@ -5,27 +5,27 @@ class Solution {
         int subIslands = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid2[i][j] == 1 && isSubIslandsDFS(grid1, grid2, i, j)) {
-                    subIslands++;
+                if (grid2[i][j] == 1) {
+                    int[] flag = {1};
+                    isSubIslandsDFS(grid1, grid2, i, j, flag);
+                    subIslands += flag[0];
                 }
             }
         }
         return subIslands;
     }
 
-    private boolean isSubIslandsDFS(int[][] grid1, int[][] grid2, int i, int j) {
-        if (i < 0 || i >= grid1.length || j < 0 || j >= grid1[0].length) {
-            return true;
+    private void isSubIslandsDFS(int[][] grid1, int[][] grid2, int i, int j, int[] flag) {
+        if (i < 0 || i >= grid1.length || j < 0 || j >= grid1[0].length || grid2[i][j] == 0) {
+            return;
         }
-        if (grid2[i][j] != 1) {
-            return true;
+        if (grid1[i][j] == 0) {
+            flag[0] = 0;
         }
-        grid2[i][j] = -1; // marking visited
-        boolean isSubIsland = (grid1[i][j] == 1);
-        isSubIsland = isSubIsland & isSubIslandsDFS(grid1, grid2, i - 1, j); // top
-        isSubIsland = isSubIsland & isSubIslandsDFS(grid1, grid2, i + 1, j); // bottom
-        isSubIsland = isSubIsland & isSubIslandsDFS(grid1, grid2, i, j - 1); // left
-        isSubIsland = isSubIsland & isSubIslandsDFS(grid1, grid2, i, j + 1); // right
-        return isSubIsland;
+        grid2[i][j] = 0; // marking visited
+        isSubIslandsDFS(grid1, grid2, i - 1, j, flag); // top
+        isSubIslandsDFS(grid1, grid2, i + 1, j, flag); // bottom
+        isSubIslandsDFS(grid1, grid2, i, j - 1, flag); // left
+        isSubIslandsDFS(grid1, grid2, i, j + 1, flag); // right
     }
 }
