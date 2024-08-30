6306867e80816387d4f7fa36b118db9f75daa98d
Surajit Rana <surajit1602@gmail.com>
2024-08-30 16:16:37 +0530

    Time: 262 ms (31.11%) | Memory: 48 MB (98.89%) - LeetSync

--- Graphs/DijkstraAlgorithm/2803-modify-graph-edge-weights/modify-graph-edge-weights.java
+++ Graphs/DijkstraAlgorithm/2803-modify-graph-edge-weights/modify-graph-edge-weights.java
@@ -2,7 +2,8 @@ class Solution {
     private static final int LARGE_VAL = (int) 2e9;
 
     public int[][] modifiedGraphEdges(int n, int[][] edges, int source, int destination, int target) {
-        long minDist = dijkstra(edges, n, source, destination);
+        HashMap<Integer, ArrayList<long[]>> adj = createGraph(n, edges);
+        long minDist = dijkstra(adj, n, source, destination);
         if (minDist < target) {
             return new int[][]{ };
         }
@@ -11,7 +12,9 @@ class Solution {
             if (edge[2] == -1) {
                 edge[2] = isTargetEqual ? LARGE_VAL : 1;
                 if (!isTargetEqual) {
-                    long minCalc = dijkstra(edges, n, source, destination);
+                    adj.get(edge[0]).add(new long[] { edge[1], edge[2] });
+                    adj.get(edge[1]).add(new long[] { edge[0], edge[2] });
+                    long minCalc = dijkstra(adj, n, source, destination);
                     if (minCalc <= target) {
                         isTargetEqual = true;
                         edge[2] += target - minCalc;
@@ -22,8 +25,7 @@ class Solution {
         return isTargetEqual ? edges : new int[][]{};
     }
 
-    private long dijkstra(int[][] edges, int n, int src, int dest) {
-        HashMap<Integer, ArrayList<long[]>> adj = createGraph(n, edges);
+    private long dijkstra(HashMap<Integer, ArrayList<long[]>> adj, int n, int src, int dest) {
         PriorityQueue<long[]> pq = new PriorityQueue<long[]>(Comparator.comparingLong(a -> a[1]));
         long[] minDist = new long[n];
         Arrays.fill(minDist, Long.MAX_VALUE);
