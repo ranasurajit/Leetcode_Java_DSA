8f3c5b4ca8f533969b8bb842bdf6e4a45edcd44b
Surajit Rana <surajit1602@gmail.com>
2024-09-02 00:10:17 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (53.11%) - LeetSync

--- BinarySearch/1D-Arrays/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ BinarySearch/1D-Arrays/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,44 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        return new int[] { searchLeft(nums, target), searchRight(nums, target) };
+        int[] positions = { getFirstPosition(nums, target), getLastPosition(nums, target) };
+        return positions;
     }
 
-    public int searchLeft(int[] nums, int k) {
+    private int getFirstPosition(int[] nums, int target) {
+        int n = nums.length;
         int low = 0;
-        int high = nums.length - 1;
-        int minIndex = Integer.MAX_VALUE;
+        int high = n - 1;
+        int index = Integer.MAX_VALUE;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] < k) {
-                low = mid + 1;
-            } else if (nums[mid] > k) {
+            if (nums[mid] == target) {
+                index = Math.min(index, mid);
                 high = mid - 1;
-            } else {
-                // nums[mid] == k
-                minIndex = mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
                 high = mid - 1;
             }
         }
-        return minIndex == Integer.MAX_VALUE ? -1 : minIndex;
+        return index == Integer.MAX_VALUE ? -1 : index;
     }
-    
-    public int searchRight(int[] nums, int k) {
+
+    private int getLastPosition(int[] nums, int target) {
+        int n = nums.length;
         int low = 0;
-        int high = nums.length - 1;
-        int maxIndex = Integer.MIN_VALUE;
+        int high = n - 1;
+        int index = Integer.MIN_VALUE;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] < k) {
+            if (nums[mid] == target) {
+                index = Math.max(index, mid);
                 low = mid + 1;
-            } else if (nums[mid] > k) {
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
                 high = mid - 1;
-            } else {
-                // nums[mid] == k
-                maxIndex = mid;
-                low = mid + 1;
             }
         }
-        return maxIndex == Integer.MIN_VALUE ? -1 : maxIndex;
+        return index == Integer.MIN_VALUE ? -1 : index;
     }
 }
