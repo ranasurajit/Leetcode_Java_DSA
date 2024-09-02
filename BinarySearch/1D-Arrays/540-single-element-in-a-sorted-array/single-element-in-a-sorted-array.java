9616fa70058a76caa902c25f8162e8e1a1f53220
Surajit Rana <surajit1602@gmail.com>
2024-09-02 23:55:52 +0530

    Time: 0 ms (100.00%) | Memory: 49.9 MB (93.89%) - LeetSync

--- BinarySearch/1D-Arrays/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ BinarySearch/1D-Arrays/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,4 +1,9 @@
 class Solution {
+
+    /**
+     * TC: O(log(N))
+     * SC: O(1)
+     */
     public int singleNonDuplicate(int[] nums) {
         int n = nums.length;
         if (n == 1) {
@@ -7,27 +12,30 @@ class Solution {
         if (nums[0] != nums[1]) {
             return nums[0];
         }
-        if (nums[n-1] != nums[n -2]) {
+        if (nums[n - 2] != nums[n - 1]) {
             return nums[n - 1];
         }
         int low = 2;
-        int high = n - 3;
-        // the duplicate elements starts at even position unless sequence is broken by a single element
+        int high = n - 2;
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (nums[mid] == nums[mid - 1]) {
                 mid = mid - 1;
             }
+            /**
+                * Non-repeating element would be found in even index only
+                * any duplicate elements starts from even index unless single
+                * element breaks this. After single element, the repeating 
+                * element starts from odd index
+                */
             if (mid % 2 == 0) {
-                // even single element will fall here
-                if (nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1]) {
+                if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
                     return nums[mid];
+                } else {
+                    low = mid + 2;
                 }
-                low = mid + 2;
             } else {
-                if (nums[mid] == nums[mid + 1]) {
-                    high = mid - 1;
-                }
+                high = mid - 1;
             }
         }
         return -1;
