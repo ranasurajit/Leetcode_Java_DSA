a1e5766272a7102c6a48c1bf44daff3df36c45f5
Surajit Rana <surajit1602@gmail.com>
2024-09-13 09:49:16 +0530

    Time: 2 ms (100.00%) | Memory: 58.5 MB (19.45%) - LeetSync

--- Arrays/PrefixSuffix/1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ Arrays/PrefixSuffix/1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -1,24 +1,22 @@
 class Solution {
     /**
      * TC: O(N + M), where M is the number of queries
-     * SC: O(N)
+     * SC: O(1)
      */
     public int[] xorQueries(int[] arr, int[][] queries) {
         int n = arr.length;
         int m = queries.length;
-        int[] prefixXOR = new int[n]; // SC: O(N)
-        prefixXOR[0] = arr[0];
         for (int i = 1; i < n; i++) { // TC: O(N)
-            prefixXOR[i] = prefixXOR[i - 1] ^ arr[i];
+            arr[i] = arr[i - 1] ^ arr[i];
         }
         int[] result = new int[m];
         for (int i = 0; i < m; i++) { // TC: O(M)
             int[] q = queries[i];
             if (q[0] > 0) {
                 // using the property of XOR which cancels the XOR of same numbers
-                result[i] = prefixXOR[q[1]] ^ prefixXOR[q[0] - 1];
+                result[i] = arr[q[1]] ^ arr[q[0] - 1];
             } else {
-                result[i] = prefixXOR[q[1]];
+                result[i] = arr[q[1]];
             }
         }
         return result;
