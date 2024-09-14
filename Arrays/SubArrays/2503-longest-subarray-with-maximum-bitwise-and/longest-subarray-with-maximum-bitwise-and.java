3249853934b217216b8f640c5d06ad98b6070ec9
Surajit Rana <surajit1602@gmail.com>
2024-09-14 17:02:50 +0530

    Time: 3 ms (89.89%) | Memory: 59.8 MB (28.65%) - LeetSync

---
+++ Arrays/SubArrays/2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int longestSubarray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int streak = 0;
+        int result = 0;
+        /**
+         * bitwise AND of any two numbers cannot exceed the max of two
+         * so we need to find the maximum number and maximum length of
+         * contiguous occurence of max number
+         */
+        for (int num : nums) {
+            if (max < num) {
+                max = num;
+                streak = 0;
+                result = 0;
+            }
+            if (num == max) {
+                streak++;
+            } else {
+                streak = 0;
+            }
+            result = Math.max(result, streak);
+        }
+        return result;
+    }
+}
