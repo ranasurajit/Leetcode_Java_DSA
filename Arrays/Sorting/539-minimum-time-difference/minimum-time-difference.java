8cfe9d9164912e3a36b93c39328dbaaec1e5c735
Surajit Rana <surajit1602@gmail.com>
2024-09-16 14:48:28 +0530

    Time: 5 ms (67.04%) | Memory: 45.7 MB (46.04%) - LeetSync

--- Arrays/Sorting/539-minimum-time-difference/minimum-time-difference.java
+++ Arrays/Sorting/539-minimum-time-difference/minimum-time-difference.java
@@ -6,18 +6,20 @@ class Solution {
     public int findMinDifference(List<String> timePoints) {
         int n = timePoints.size();
         int currentDiff = 0;
-        List<Integer> minutes = new ArrayList<Integer>(); // SC: O(N)
-        for (String s : timePoints) { // TC: O(N)
-            minutes.add(Integer.parseInt(s.substring(0, 2)) * 60 + Integer.parseInt(s.substring(3, 5)));
+        int[] minutes = new int[n]; // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            String s = timePoints.get(i);
+            minutes[i] = Integer.parseInt(s.substring(0, 2)) * 60 + 
+                Integer.parseInt(s.substring(3, 5));
         }
-        Collections.sort(minutes); // TC: O(Nlog(N))
+        Arrays.sort(minutes); // TC: O(Nlog(N))
         int min = Integer.MAX_VALUE;
         for (int i = 1; i < n; i++) { // TC: O(N)
-            min = Math.min(min, minutes.get(i) - minutes.get(i - 1));
+            min = Math.min(min, minutes[i] - minutes[i - 1]);
         }
         // calculating offsets for hours around 00:00 i.e. 24:00
-        int endOffset = 24 * 60 - minutes.get(n - 1);
-        int startOffset = minutes.get(0);
+        int endOffset = 24 * 60 - minutes[n - 1];
+        int startOffset = minutes[0];
         return Math.min(min, endOffset + startOffset);
     }
 }
