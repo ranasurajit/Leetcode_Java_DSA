e3667d17637da99b24ba8096d2a81ce17b5c0b84
Surajit Rana <surajit1602@gmail.com>
2024-09-16 14:45:33 +0530

    Time: 4 ms (72.96%) | Memory: 44.7 MB (95.85%) - LeetSync

--- Arrays/Sorting/539-minimum-time-difference/minimum-time-difference.java
+++ Arrays/Sorting/539-minimum-time-difference/minimum-time-difference.java
@@ -9,8 +9,8 @@ class Solution {
         int[] minutes = new int[n]; // SC: O(N)
         for (int i = 0; i < n; i++) { // TC: O(N)
             String s = timePoints.get(i);
-            minutes[i] = Integer.valueOf(s.substring(0, 2)) * 60 + 
-                Integer.valueOf(s.substring(3, 5));
+            minutes[i] = Integer.parseInt(s.substring(0, 2)) * 60 + 
+                Integer.parseInt(s.substring(3, 5));
         }
         Arrays.sort(minutes); // TC: O(Nlog(N))
         int min = Integer.MAX_VALUE;
