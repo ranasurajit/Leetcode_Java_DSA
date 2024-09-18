daf3d3e7af503dda4f448538b7a64aca1d55976e
Surajit Rana <surajit1602@gmail.com>
2024-09-19 00:50:30 +0530

    Time: 7 ms (52.81%) | Memory: 44.2 MB (15.42%) - LeetSync

--- HashTable/2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
+++ HashTable/2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
@@ -1,5 +1,25 @@
 class Solution {
     /**
+     * Optimal Approach
+     * 
+     * TC: O(N)
+     * SC: O(N)
+     * 
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int countKDifference(int[] nums, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>(); // SC: O(N)
+        int count = 0;
+        for (int num : nums) { // TC: O(N)
+            count += hm.getOrDefault(num + k, 0) + hm.getOrDefault(num - k, 0);
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
+        }
+        return count;
+    }
+
+    /**
      * Brute-Force Approach
      * 
      * TC: O(N^2)
@@ -9,7 +29,7 @@ class Solution {
      * @param k
      * @return
      */
-    public int countKDifference(int[] nums, int k) {
+    public int countKDifferenceBruteForce(int[] nums, int k) {
         int n = nums.length;
         int count = 0;
         for (int i = 0; i < n; i++) { // TC: O(N)
