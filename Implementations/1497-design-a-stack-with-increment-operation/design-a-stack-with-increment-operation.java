150b0dc26c8a8980dfbec06266fde007c0d70d17
Surajit Rana <surajit1602@gmail.com>
2024-09-30 15:01:19 +0530

    Time: 5 ms (93.47%) | Memory: 45.1 MB (90.93%) - LeetSync

--- Implementations/1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
+++ Implementations/1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
@@ -1,20 +1,36 @@
+/**
+ * TC: O(N)
+ * SC: O(N)
+ */
 class CustomStack {
     int[] st;
     int size;
     int count;
 
+    /**
+     * TC: O(1)
+     * SC: O(N)
+     */
     public CustomStack(int maxSize) {
         st = new int[maxSize];
         size = maxSize;
         count = 0;
     }
     
+    /**
+     * TC: O(1)
+     * SC: O(1)
+     */
     public void push(int x) {
         if (count < size) {
             st[count++] = x;
         }
     }
     
+    /**
+     * TC: O(1)
+     * SC: O(1)
+     */
     public int pop() {
         if (count == 0) {
             return -1;
@@ -25,6 +41,10 @@ class CustomStack {
         return popped;
     }
     
+    /**
+     * TC: O(Max(K, N))
+     * SC: O(1)
+     */
     public void increment(int k, int val) {
         for (int i = 0; i < Math.min(k, size); i++) {
             st[i] += val;
