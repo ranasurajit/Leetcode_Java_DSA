a2ca65df34fca032ad8ad6ae894bc07536035a49
Surajit Rana <surajit1602@gmail.com>
2024-10-02 14:34:34 +0530

    Time: 41 ms (24.30%) | Memory: 65.4 MB (9.01%) - LeetSync

--- HashTable/1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ HashTable/1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,22 +1,23 @@
 class Solution {
     /**
-     * TC: O(N)
-     * SC: O(N)
+     * TC: O(2 x Nlog(N)) ~ O(Nlog(N))
+     * SC: O(2K) ~ O(2N) ~ O(N)
      */
     public int[] arrayRankTransform(int[] arr) {
         int n = arr.length;
         int[] ranks = new int[n];
-        TreeMap<Integer, Integer> tm = new TreeMap<Integer, Integer>(); // SC: O(N)
-        for (int i = 0; i < n; i++) { // TC: O(N)
-            tm.put(arr[i], tm.getOrDefault(arr[i], 0) + 1);
+        TreeSet<Integer> ts = new TreeSet<Integer>(); // SC: O(K)
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>(); // SC: O(K)
+        for (int num : arr) { // TC: O(Nlog(N))
+            ts.add(num);
         }
-        int index = 0;
         int rank = 1;
-        for (Integer key : tm.keySet()) { // TC: O(N)
-            tm.put(key, rank++);
+        for (Integer key : ts) { // TC: O(Nlog(N))
+            hm.put(key, rank);
+            rank++;
         }
         for (int i = 0; i < n; i++) { // TC: O(N)
-            ranks[i] = tm.get(arr[i]);
+            ranks[i] = hm.get(arr[i]);
         }
         return ranks;
     }
