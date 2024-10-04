fdc5d01fb74b93a3a496bfb5ca2f7c570b512bbc
Surajit Rana <surajit1602@gmail.com>
2024-10-04 13:33:10 +0530

    Time: 2 ms (100.00%) | Memory: 55.2 MB (97.23%) - LeetSync

--- Arrays/TwoPointers/2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ Arrays/TwoPointers/2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -33,4 +33,32 @@ class Solution {
         }
         return chemistry / 2;
     }
+
+    /**
+     * Using Two Pointers and Sorting
+     * TC: O(Nlog(N) + N / 2) ~ O(Nlog(N))
+     * SC: O(1)
+     */
+    public long dividePlayersBetter(int[] skill) {
+        int n = skill.length; // n is always even
+        if (n == 2) {
+            return (long) skill[0] * (long) skill[1];
+        }
+        Arrays.sort(skill); // TC: O(Nlog(N))
+        long sum = 0L;
+        int p = 0;
+        int q = n - 1;
+        long totalSkill = skill[p] + skill[q];
+        while (p <= q) { // TC: O(N / 2)
+            long currentSkill = skill[p] + skill[q];
+            if (totalSkill == currentSkill) {
+                sum += (long) skill[p] * (long) skill[q];
+            } else {
+                return -1L;
+            }
+            p++;
+            q--;
+        }
+        return sum;
+    }
 }
