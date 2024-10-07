f37b68d540e9cd29bddcec941296225f4d5e602b
Surajit Rana <surajit1602@gmail.com>
2024-10-07 13:36:03 +0530

    Time: 4 ms (85.00%) | Memory: 43.2 MB (71.14%) - LeetSync

--- Stack/Basics/2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ Stack/Basics/2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -8,19 +8,12 @@ class Solution {
         Stack<Character> st = new Stack<Character>(); // SC: O(N)
         for (int i = n - 1; i >= 0; i--) { // TC: O(N)
             char current = s.charAt(i);
-            if (st.isEmpty()) {
-                st.push(current);
+            if (current == 'A' && !st.isEmpty() && st.peek() == 'B') {
+                st.pop();
+            } else if (current == 'C' && !st.isEmpty() && st.peek() == 'D') {
+                st.pop();
             } else {
-                if (current == 'A' || current == 'C') {
-                    char previous = st.peek();
-                    if ((current == 'A' && previous == 'B') || (current == 'C' && previous == 'D')) {
-                        st.pop();
-                    } else {
-                        st.push(current);
-                    }
-                } else {
-                    st.push(current);
-                }
+                st.push(current);
             }
         }
         return st.size();
