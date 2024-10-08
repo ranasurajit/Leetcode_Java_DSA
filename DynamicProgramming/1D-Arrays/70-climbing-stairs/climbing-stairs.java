113160531a2978194b29f8cde4a5f5b42fd1ee6d
Surajit Rana <surajit1602@gmail.com>
2024-10-09 00:10:50 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (39.67%) - LeetSync

---
+++ DynamicProgramming/1D-Arrays/70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1]; // SC: O(N)
+        dp[0] = 1; // number of ways to reach 0th step (already present at 0th step)
+        dp[1] = 1; // number of ways to reach 1st step
+        // dp[2] = dp[0] + dp[1]; // number of ways to reach 2nd step 
+        // if I am at 1st step I can take 1 step else if I am at 0th step I can take 2 steps
+        for (int i = 2; i <= n; i++) { // TC: O(N)
+            dp[i] = dp[i - 2] + dp[i - 1]; // number of ways to reach ith step
+        }
+        return dp[n];
+    }
+}
