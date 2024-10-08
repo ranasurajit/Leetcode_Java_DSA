dbc404bbab811b142ae6413eec5531d30978def5
Surajit Rana <surajit1602@gmail.com>
2024-10-08 14:35:26 +0530

    Time: 15 ms (65.33%) | Memory: 53.9 MB (84.30%) - LeetSync

--- Stack/Basics/2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
+++ Stack/Basics/2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -25,4 +25,31 @@ class Solution {
          */
         return (open + 1) / 2;
     }
+
+    /**
+     * Using Stack
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public int minSwapsWithStack(String s) {
+        int n = s.length();
+        Stack<Character> st = new Stack<Character>(); // SC: O(N / 2)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            char ch = s.charAt(i);
+            if (!st.isEmpty() && ch == ']' && st.peek() == '[') {
+                st.pop(); // negating balanced brackets
+            } else {
+                if (ch == '[') {
+                    st.push(ch);
+                }
+            }
+        }
+        /*
+         * For any best swaps made creates a pair of
+         * balanced brackets so we need number of swaps 
+         * = (num open brackets + 1) / 2 to make all brackets 
+         * balanced
+         */
+        return (st.size() + 1) / 2;
+    }
 }
