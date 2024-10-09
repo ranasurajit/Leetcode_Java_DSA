3046293a035be9c7e1c2f08d786d68756385bf6f
Surajit Rana <surajit1602@gmail.com>
2024-10-09 17:40:23 +0530

    Time: 1 ms (58.43%) | Memory: 41.4 MB (64.62%) - LeetSync

---
+++ Stack/Basics/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * TC: O(N)
+     * SC: O(1)
+     */
+    // public int minAddToMakeValid(String s) {
+    //     int n = s.length();
+    //     int open = 0;
+    //     int close = 0;
+    //     for (int i = 0; i < n; i++) { // TC: O(N)
+    //         char ch = s.charAt(i);
+    //         if (ch == '(') {
+    //             open++;
+    //         } else if (ch == ')') {
+    //             if (open > 0) {
+    //                 open--;
+    //             } else {
+    //                 close++;
+    //             }
+    //         }
+    //     }
+    //     return Math.max(open, close);
+    // }
+
+    /**
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int minAddToMakeValid(String s) {
+        int n = s.length();
+        Stack<Character> st = new Stack<Character>();
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            char ch = s.charAt(i);
+            if (!st.isEmpty() && ch == ')' && st.peek() == '(') {
+                st.pop(); // negating the balanced parentheses
+            } else {
+                st.push(ch);
+            }
+        }
+        return st.size();
+    }
+}
