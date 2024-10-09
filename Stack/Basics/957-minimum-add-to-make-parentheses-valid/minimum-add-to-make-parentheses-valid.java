087c2eb625d734142612fac987b0bc29654acc16
Surajit Rana <surajit1602@gmail.com>
2024-10-09 17:44:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (79.36%) - LeetSync

--- Stack/Basics/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ Stack/Basics/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,33 +1,34 @@
 class Solution {
     /**
+     * Without using Stack (Memory Efficient)
      * TC: O(N)
      * SC: O(1)
      */
-    // public int minAddToMakeValid(String s) {
-    //     int n = s.length();
-    //     int open = 0;
-    //     int close = 0;
-    //     for (int i = 0; i < n; i++) { // TC: O(N)
-    //         char ch = s.charAt(i);
-    //         if (ch == '(') {
-    //             open++;
-    //         } else if (ch == ')') {
-    //             if (open > 0) {
-    //                 open--;
-    //             } else {
-    //                 close++;
-    //             }
-    //         }
-    //     }
-    //     return Math.max(open, close);
-    // }
+    public int minAddToMakeValid(String s) {
+        int n = s.length();
+        int open = 0;
+        int close = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                open++;
+            } else if (ch == ')') {
+                if (open > 0) {
+                    open--;
+                } else {
+                    close++;
+                }
+            }
+        }
+        return open + close;
+    }
 
     /**
      * Using Stack
      * TC: O(N)
      * SC: O(N)
      */
-    public int minAddToMakeValid(String s) {
+    public int minAddToMakeValidBetter(String s) {
         int n = s.length();
         Stack<Character> st = new Stack<Character>(); // SC: O(N)
         for (int i = 0; i < n; i++) { // TC: O(N)
