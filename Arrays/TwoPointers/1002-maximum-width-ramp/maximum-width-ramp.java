8d3b0b092d49637c153f7b08c8d736c435e86101
Surajit Rana <surajit1602@gmail.com>
2024-10-10 19:21:04 +0530

    Time: 3 ms (97.76%) | Memory: 55 MB (33.58%) - LeetSync

--- Arrays/TwoPointers/1002-maximum-width-ramp/maximum-width-ramp.java
+++ Arrays/TwoPointers/1002-maximum-width-ramp/maximum-width-ramp.java
@@ -1,22 +1,22 @@
 class Solution {
     /**
-     * TC: O(N/2) ~ O(N)
-     * SC: O(1)
+     * TC: O(2N) ~ O(N)
+     * SC: O(N)
      */
     public int maxWidthRamp(int[] nums) {
         int n = nums.length;
         // pre-process max from right
-        int[] maxRight = new int[n];
+        int[] maxRight = new int[n]; // SC: O(N)
         maxRight[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
+        for (int i = n - 2; i >= 0; i--) { // TC: O(N)
             maxRight[i] = Math.max(maxRight[i + 1], nums[i]);
         }
         int maxDiff = 0; // maxDiff will capture the width of ramp (j - i)
 
         // Using two pointers
         int p = 0;              // pointer for nums
-        int q = 0;          // pointer for maxRight
-        while (q < n) {
+        int q = 0;              // pointer for maxRight
+        while (q < n) { // TC: O(N)
             while (p < q && nums[p] > maxRight[q]) {
                 p++;
             }
