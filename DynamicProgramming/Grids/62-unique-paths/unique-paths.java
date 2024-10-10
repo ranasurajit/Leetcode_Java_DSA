1fb2e59c25f4bdfa48ff78305db8f5730d2392c9
Surajit Rana <surajit1602@gmail.com>
2024-10-11 01:50:03 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (19.10%) - LeetSync

--- DynamicProgramming/Grids/62-unique-paths/unique-paths.java
+++ DynamicProgramming/Grids/62-unique-paths/unique-paths.java
@@ -1,13 +1,37 @@
 class Solution {
+    /**
+     * Bottom-Up approach
+     * TC: O(M x N)
+     * SC: O(M x N) 
+     */
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for (int[] row : dp) {
-            Arrays.fill(row, - 1);
+        int[][] dp = new int[m][n]; // SC: O(M x N)
+        dp[0][0] = 1;
+        for (int[] row : dp) { // TC: O(M x N)
+            Arrays.fill(row, 1);
         }
-        return helper(m - 1, n - 1, dp);
+        for (int i = 1; i < m; i++) { // TC: O(M)
+            for (int j = 1; j < n; j++) { // TC: O(N)
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m -1][n - 1];
     }
 
-    private int helper(int m, int n, int[][] dp) {
+    /**
+     * Top-Down approach
+     * TC: O(M x N)
+     * SC: O(M x N) + O(M x N) ~ O(M x N)
+     */
+    public int uniquePathsRecursive(int m, int n) {
+        int[][] dp = new int[m][n]; // SC: O(M x N)
+        for (int[] row : dp) { // TC: O(M x N)
+            Arrays.fill(row, -1);
+        }
+        return solveRecursively(dp, m - 1, n - 1); // TC and SC: O(M x N) 
+    }
+
+    private int solveRecursively(int[][] dp, int m, int n) {
         if (m < 0 || n < 0) {
             return 0;
         }
@@ -17,9 +41,12 @@ class Solution {
         if (dp[m][n] != -1) {
             return dp[m][n];
         }
-        int xways = helper(m - 1, n, dp); // no of ways to reach [m - 2, n - 1]th cell
-        int yways = helper(m, n - 1, dp); // no of ways to reach [m - 1, n - 2]th cell
-        dp[m][n] = xways + yways;
-        return dp[m][n];
+        /*
+         * to reach grid[m - 1][n - 1] we need to count unique paths 
+         * from grid[m - 1][n-2] + grid[m - 2][n - 1]
+         */
+        int x = solveRecursively(dp, m, n - 1);
+        int y = solveRecursively(dp, m - 1, n);
+        return dp[m][n] = x + y;
     }
 }
