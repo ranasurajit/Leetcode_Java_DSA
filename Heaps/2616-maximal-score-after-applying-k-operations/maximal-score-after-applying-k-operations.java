95dea38b88be6c69d018ac7916b7f8d0531fe61e
Surajit Rana <surajit1602@gmail.com>
2024-10-14 17:12:03 +0530

    Time: 158 ms (26.07%) | Memory: 62.4 MB (92.42%) - LeetSync

---
+++ Heaps/2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * TC: O(Nlog(N) + Klog(N))
+     * SC: O(N)
+     */
+    public long maxKelements(int[] nums, int k) {
+        if (k == 0) {
+            return 0L;
+        }
+        int n = nums.length;
+        // storing in max-heap TC: O(Nlog(N)), SC: O(N)
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder()); 
+        for (int it : nums) { // TC: O(N)
+            pq.offer(it);
+        }
+        long score = 0L;
+        while (!pq.isEmpty() && k > 0) { // TC: O(K)
+            int top = pq.poll(); // TC: O(log(N))
+            score += top;
+            int deno = (int) Math.ceil(top / 3.0);
+            pq.offer(deno); // TC: O(log(N))
+            k--;
+        }
+        return score;
+    }
+}
