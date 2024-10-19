33c07855867d3829b9ca80047283d0a6159dfba2
Surajit Rana <surajit1602@gmail.com>
2024-10-19 12:24:20 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (73.93%) - LeetSync

---
+++ Recursion/1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public char findKthBit(int n, int k) {
+        // it is understood that the length of the binary string would be 2^n - 1
+        int length = (int) Math.pow(2, n) - 1;
+        return solve(length, k);
+    }
+
+    private char solve(int n, int k) {
+        if (n == 1) {
+            return '0';
+        }
+        int half = n / 2;
+        int mid = half + 1;
+        if (mid == k) {
+            return '1';
+        } else if (k < mid) {
+            // fall back to previous binary string
+            return solve(half, k);
+        } else {
+            // find the inverted from previous binary string
+            char c = solve(half, n - k + 1);
+            return c == '0' ? '1' : '0';
+        }
+    }
+}
