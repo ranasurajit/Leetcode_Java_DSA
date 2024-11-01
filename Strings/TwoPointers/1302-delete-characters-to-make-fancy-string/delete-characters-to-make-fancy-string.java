c809296cce34ebb42dfd78df89c2472fe3daec5d
Surajit Rana <surajit1602@gmail.com>
2024-11-01 13:16:46 +0530

    Time: 45 ms (37.21%) | Memory: 45.8 MB (21.25%) - LeetSync

--- Strings/TwoPointers/1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
+++ Strings/TwoPointers/1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
@@ -14,30 +14,4 @@ class Solution {
         }
         return sb.toString();
     }
-
-    /**
-     * Using Two Pointers Approach
-     * TC: O(3N) ~ O(N)
-     * SC: O(N)
-     */
-    public String makeFancyStringTwoPointers(String s) {
-        StringBuilder sb = new StringBuilder();
-        char[] ch = s.toCharArray(); // TC: O(N), SC: O(N)
-        int n = ch.length;
-        int p = 0, q = 1, r = 2;
-        while (r < n) {     // TC: O(N)
-            if (ch[p] == ch[q] && ch[q] == ch[r]) {
-                ch[p] = '0';
-            }
-            p++;
-            q++;
-            r++;
-        }
-        for (char c : ch) { // TC: O(N)
-            if (c != '0') {
-                sb.append(c); // TC: O(1)
-            }
-        }
-        return sb.toString();
-    }
 }
