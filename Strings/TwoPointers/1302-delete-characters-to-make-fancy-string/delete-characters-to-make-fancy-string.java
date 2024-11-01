5b752c636f172358a54b8be3555aa344826e36e3
Surajit Rana <surajit1602@gmail.com>
2024-11-01 13:16:19 +0530

    Time: 45 ms (37.21%) | Memory: 45.8 MB (34.07%) - LeetSync

--- Strings/TwoPointers/1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
+++ Strings/TwoPointers/1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
@@ -1,13 +1,30 @@
 class Solution {
     /**
+     * Optimized Approach
      * TC: O(N)
-     * SC: O(1)
+     * SC: O(N)
      */
     public String makeFancyString(String s) {
-        char[] ch = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) { // TC: O(N), SC: O(N)
+            int n = sb.length();
+            if (n < 2 || !(sb.charAt(n - 1) == ch && sb.charAt(n - 2) == ch)) {
+                sb.append(ch); // TC: O(1)
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Using Two Pointers Approach
+     * TC: O(3N) ~ O(N)
+     * SC: O(N)
+     */
+    public String makeFancyStringTwoPointers(String s) {
+        StringBuilder sb = new StringBuilder();
+        char[] ch = s.toCharArray(); // TC: O(N), SC: O(N)
         int n = ch.length;
         int p = 0, q = 1, r = 2;
-        StringBuilder sb = new StringBuilder();
         while (r < n) {     // TC: O(N)
             if (ch[p] == ch[q] && ch[q] == ch[r]) {
                 ch[p] = '0';
@@ -18,7 +35,7 @@ class Solution {
         }
         for (char c : ch) { // TC: O(N)
             if (c != '0') {
-                sb.append(c);
+                sb.append(c); // TC: O(1)
             }
         }
         return sb.toString();
