b24194be51fd028a077c776b01f28183b42df487
Surajit Rana <surajit1602@gmail.com>
2024-11-05 10:26:24 +0530

    Time: 3 ms (97.78%) | Memory: 44.7 MB (51.56%) - LeetSync

--- Strings/Basics/3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.java
+++ Strings/Basics/3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.java
@@ -1,8 +1,27 @@
 class Solution {
+    /**
+     * TC: O(N / 2) ~ O(N)
+     * SC: O(1)
+     */
     public int minChanges(String s) {
         int n = s.length();
-        char[] chars = s.toCharArray();
-        for (int i = 1; i < n; i = i + 2) {
+        int changes = 0;
+        for (int i = 1; i < n; i = i + 2) {                // TC: O(N / 2)
+            if (s.charAt(i) != s.charAt(i - 1)) {
+                changes++;
+            }
+        }
+        return changes;
+    }
+
+    /**
+     * TC: O(3N / 2) ~ O(N)
+     * SC: O(N)
+     */
+    public int minChangesNotEfficient(String s) {
+        int n = s.length();
+        char[] chars = s.toCharArray();                   // SC: O(N)
+        for (int i = 1; i < n; i = i + 2) {               // TC: O(N / 2)
             String evenSub = chars[i - 1] + "" + chars[i];
             if (evenSub.equals("00") || evenSub.equals("11")) {
                 chars[i - 1] = 'x';
@@ -11,7 +30,7 @@ class Solution {
         }
         int count0s = 0;
         int count1s = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {                     // TC: O(N)
             if (chars[i] == '0') {
                 count0s++;
             } else if (chars[i] == '1') {
