35ad3586a9a6843a93260828082b08772e4c8bb6
Surajit Rana <surajit1602@gmail.com>
2024-11-07 13:59:14 +0530

    Time: 38 ms (11.11%) | Memory: 56.6 MB (43.03%) - LeetSync

--- BitManipulation/2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.java
+++ BitManipulation/2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.java
@@ -1,15 +1,30 @@
 class Solution {
     /**
      * TC: O(32 x N) ~ O(N)
-     * SC: O(32) ~ O(1)
+     * SC: O(1)
      */
     public int largestCombination(int[] candidates) {
+        int countMax = 0;
+        for (int i = 0; i < 32; i++) {
+            int countIthBit = 0;
+            for (int it : candidates) { // TC: O(N)
+                countIthBit += (it & (1 << i)) != 0 ? 1 : 0;
+            }
+            countMax = Math.max(countMax, countIthBit);
+        }
+        return countMax;
+    }
+
+    /**
+     * TC: O(32 x N) ~ O(N)
+     * SC: O(32) ~ O(1)
+     */
+    public int largestCombinationWithSpace(int[] candidates) {
         int countMax = 0;
         int n = candidates.length;
         // for 32 bits we need this placeholder - SC: O(32)
         int[] bitData = new int[32];
         for (int i = 0; i < 32; i++) { // TC: O(32)
-            int countIthBit = 0;
             for (int it : candidates) { // TC: O(N)
                 if ((it & (1 << i)) != 0) {
                     bitData[i]++;
