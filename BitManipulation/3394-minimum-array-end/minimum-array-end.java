4a67acfbba42d71910b2b24acbc65939adaec7e9
Surajit Rana <surajit1602@gmail.com>
2024-11-09 11:12:31 +0530

    Time: 1741 ms (14.29%) | Memory: 40.6 MB (71.15%) - LeetSync

--- BitManipulation/3394-minimum-array-end/minimum-array-end.java
+++ BitManipulation/3394-minimum-array-end/minimum-array-end.java
@@ -5,13 +5,10 @@ class Solution {
      */
     public long minEnd(int n, int x) {
         // for AND operation of all elements = x, x has to be the minimum of nums
-        long lastNum = (long) x;
-        int count = 0;
-        // as we already have x as the first element so we need to find n - 2 more elements
-        while (count < n - 1) {          // TC : O(N)
-            lastNum = (x | (lastNum + 1));
-            count++;
+        long num = x;
+        for (int i = 1; i < n; i++) {
+            num = (x | (num + 1));
         }
-        return lastNum;
+        return num;
     }
 }
