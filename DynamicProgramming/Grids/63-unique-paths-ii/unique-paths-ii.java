86c128f20b533f639a77c7f1e3417d429c1f3b70
Surajit Rana <surajit1602@gmail.com>
2024-11-11 23:45:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (54.35%) - LeetSync

--- DynamicProgramming/Grids/63-unique-paths-ii/unique-paths-ii.java
+++ DynamicProgramming/Grids/63-unique-paths-ii/unique-paths-ii.java
@@ -1,39 +1,66 @@
 class Solution {
-    /**
-     * Top-Down approach
-     * TC: O(M x N)
-     * SC: O(M x N) + O(M x N) ~ O(M x N)
-     */
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        if (obstacleGrid[0][0] == 1) {
-            return 0;
+        //  return solveRecursion(m - 1, n - 1, obstacleGrid);
+        int[][] dp = new int[m][n];
+        for (int[] dp1D : dp) {
+            Arrays.fill(dp1D, -1);
         }
-        int[][] dp = new int[m][n]; // SC: O(M x N)
-        for (int[] row : dp) { // TC: O(M x N)
-            Arrays.fill(row, -1);
-        }
-        return solve(m - 1, n - 1, dp, obstacleGrid); // TC: O(M x N) and SC: O(M x N)
+        return solveMemoization(m - 1, n - 1, obstacleGrid, dp);
     }
 
-    private int solve(int m, int n, int[][] dp, int[][] obstacleGrid) {
-        // base case
-        if (m < 0 || n < 0) {
+    /**
+     * Using Memoization
+     * 
+     * TC: O(M x N)
+     * SC: O((M - 1) + (N - 1) + (M x N))
+     * 
+     * @param obstacleGrid
+     * @return
+     */
+    private static int solveMemoization(int m, int n, int[][] grid, int[][] dp) {
+        // Base case
+        if (m >= 0 && n >= 0 && grid[m][n] == 1) {
             return 0;
         }
         if (m == 0 && n == 0) {
             return 1;
         }
-        if (obstacleGrid[m][n] == 1) {
+        if (m < 0 || n < 0) {
             return 0;
         }
         if (dp[m][n] != -1) {
             return dp[m][n];
         }
-        int x = solve(m - 1, n, dp, obstacleGrid);
-        int y = solve(m , n - 1, dp, obstacleGrid);
-        dp[m][n] = x + y;
+        int up = solveMemoization(m - 1, n, grid, dp);
+        int left = solveMemoization(m, n - 1, grid, dp);
+        dp[m][n] = up + left;
         return dp[m][n];
+    }
+
+    /**
+     * Using Recursion
+     * 
+     * TC: O(2 ^ (M x N))
+     * SC: O((M - 1) + (N - 1))
+     * 
+     * @param obstacleGrid
+     * @return
+     */
+    private int solveRecursion(int m, int n, int[][] grid) {
+        // Base case
+        if (m >= 0 && n >= 0 && grid[m][n] == 1) {
+            return 0;
+        }
+        if (m == 0 && n == 0) {
+            return 1;
+        }
+        if (m < 0 || n < 0) {
+            return 0;
+        }
+        int up = solveRecursion(m - 1, n, grid);
+        int left = solveRecursion(m, n - 1, grid);
+        return up + left;
     }
 }
