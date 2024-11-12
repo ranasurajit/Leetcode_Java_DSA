8ba7650ff7b4b7fe9c21ba47551f81c5313cbdec
Surajit Rana <surajit1602@gmail.com>
2024-11-12 21:49:02 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (6.97%) - LeetSync

---
+++ Arrays/PrefixSuffix/58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int lengthOfLastWord(String s) {
+        int n = s.length();
+        int p = n - 1;
+        while (p > 0 && s.charAt(p) == ' ') {
+            p--;
+        }
+        int q = p;
+        while (q >= 0 && s.charAt(q) != ' ') {
+            q--;
+        }
+        return p - q;
+    }
+}
