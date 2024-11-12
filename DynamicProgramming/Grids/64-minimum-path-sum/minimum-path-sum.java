29d272fe135c53692e92e24e0a4988010a8bfc67
Surajit Rana <surajit1602@gmail.com>
2024-11-12 08:48:39 +0530

    Time: 1 ms (99.74%) | Memory: 47.7 MB (23.57%) - LeetSync

--- DynamicProgramming/Grids/64-minimum-path-sum/minimum-path-sum.java
+++ DynamicProgramming/Grids/64-minimum-path-sum/minimum-path-sum.java
@@ -1,24 +1,65 @@
 class Solution {
-    /**
-     * TC: O((M x N) + (M + N)) ~ O(M x N)
-     * SC: O(M x N)
-     */
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] dp = new int[m][n]; // SC: O(M x N)
-        dp[0][0] = grid[0][0];
-        for (int i = 1; i < m; i++) { // TC: O(M)
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        // return solveRecursion(m - 1, n - 1, grid);
+
+        int[][] dp = new int[m][n];
+        for (int[] dp1D : dp) {
+            Arrays.fill(dp1D, -1);
         }
-        for (int i = 1; i < n; i++) { // TC: O(N)
-            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        return solveMemoization(m - 1, n - 1, grid, dp);
+    }
+
+    /**
+     * Using Memoization
+     * 
+     * TC: O(M x N)
+     * SC: O((M - 1) + (N - 1) + (M x N))
+     * 
+     * @param m
+     * @param n
+     * @param grid
+     * @return
+     */
+    private static int solveMemoization(int m, int n, int[][] grid, int[][] dp) {
+        // Base case
+        if (m == 0 && n == 0) {
+            return grid[0][0];
         }
-        for (int i = 1; i < m; i++) { // TC: O(M)
-            for (int j = 1; j < n; j++) { // TC: O(N)
-                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
-            }
+        if (m < 0 || n < 0) {
+            return (int) 1e9;
         }
-        return dp[m - 1][n - 1];
+        if (dp[m][n] != -1) {
+            return dp[m][n];
+        }
+        int up = grid[m][n] + solveMemoization(m - 1, n, grid, dp);
+        int left = grid[m][n] + solveMemoization(m, n - 1, grid, dp);
+        dp[m][n] = Math.min(up, left);
+        return dp[m][n];
+    }
+
+    /**
+     * Using Recursion
+     * 
+     * TC: O(2^ (M x N))
+     * SC: O((M - 1) + (N - 1))
+     * 
+     * @param m
+     * @param n
+     * @param grid
+     * @return
+     */
+    private static int solveRecursion(int m, int n, int[][] grid) {
+        // Base case
+        if (m == 0 && n == 0) {
+            return grid[m][n];
+        }
+        if (m < 0 || n < 0) {
+            return (int) 1e9;
+        }
+        int up = grid[m][n] + solveRecursion(m - 1, n, grid);
+        int left = grid[m][n] + solveRecursion(m, n - 1, grid);
+        return Math.min(up, left);
     }
 }
