5417cecf81e9dd71ceeb21c315831ac512597620
Surajit Rana <surajit1602@gmail.com>
2024-11-17 14:25:00 +0530

    Time: 10 ms (5.09%) | Memory: 55.6 MB (92.22%) - LeetSync

--- Queue/ArrayDeque/209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ Queue/ArrayDeque/209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,9 +1,45 @@
 class Solution {
     /**
+     * Using Deque
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        ArrayDeque<Pair> deque = new ArrayDeque<Pair>(); // SC: O(N)
+        int minLength = Integer.MAX_VALUE;
+        long currentSum = 0L;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            currentSum += nums[i];
+            if (currentSum >= target) {
+                minLength = Math.min(minLength, i + 1);
+            }
+            // shrink the window
+            while (!deque.isEmpty() && currentSum - deque.peekFirst().sum >= target) {
+                minLength = Math.min(minLength, i - deque.pollFirst().index);
+            }
+            deque.addLast(new Pair(i, currentSum));
+        }
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+
+    class Pair {
+        int index;
+        long sum;
+        public Pair (int index, long sum) {
+            this.index = index;
+            this.sum = sum;
+        }
+    }
+
+    /**
+     * Using Two Pointers
+     *
      * TC: O(N)
      * SC: O(1)
      */
-    public int minSubArrayLen(int target, int[] nums) {
+    public int minSubArrayLenTwoPointers(int target, int[] nums) {
         int n = nums.length;
         int minLength = Integer.MAX_VALUE;
         long currentSum = 0L;
