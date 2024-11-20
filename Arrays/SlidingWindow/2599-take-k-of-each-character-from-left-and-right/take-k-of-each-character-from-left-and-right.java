9c23c6fd50e9ab1c167cf30ae0104f7601fa6cd9
Surajit Rana <surajit1602@gmail.com>
2024-11-20 10:50:58 +0530

    Time: 15 ms (49.09%) | Memory: 45.6 MB (15.98%) - LeetSync

---
+++ Arrays/SlidingWindow/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.java
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(1)
+     */
+    public int takeCharacters(String s, int k) {
+        int n = s.length();
+        int[] chars = new int[3];
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            chars[s.charAt(i) - 'a']++;
+        }
+        if (!isCharsValid(chars, k)) {
+            return -1;
+        }
+        int maxLength = -1;
+        for (int i = 0, j = 0; j < n; j++) { // TC: O(N)
+            chars[s.charAt(j) - 'a']--;
+            while (i <= j && !isCharsValid(chars, k)) {
+                chars[s.charAt(i) - 'a']++;
+                i++;
+            }
+            maxLength = Math.max(maxLength, j - i + 1);
+        }
+        return n - maxLength;
+    }
+
+    /**
+     * TC: O(3) ~ O(1)
+     * SC: O(1)
+     */
+    private boolean isCharsValid(int[] chars, int k) {
+        for (int i = 0; i < 3; i++) {
+            if (chars[i] < k) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
