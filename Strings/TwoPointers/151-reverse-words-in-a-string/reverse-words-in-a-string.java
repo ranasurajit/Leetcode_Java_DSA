02cdeb9c0d84abbe6f8ffd721b3a7934ca6f40b3
Surajit Rana <surajit1602@gmail.com>
2024-11-23 02:22:04 +0530

    Time: 34 ms (5.24%) | Memory: 45.4 MB (9.17%) - LeetSync

--- Strings/TwoPointers/151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ Strings/TwoPointers/151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,11 +1,60 @@
 class Solution {
     /**
+     * Using Two Pointers
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public String reverseWords(String s) {
+        char[] chars = s.toCharArray(); // SC: O(N)
+        int n = chars.length;
+        // reverse the entire String
+        reverse(chars, 0, n - 1); // TC: O(N)
+        int i = 0;
+        int left = 0;
+        int right = 0;
+        while (i < n) { // TC: O(2 x N)
+            while (i < n && chars[i] != ' ') {
+                chars[right] = chars[i];
+                right++;
+                i++;
+            }
+            // reverse the word between left and right
+            if (left < right) {
+                reverse(chars, left, right - 1);
+                if (right < n) {
+                    chars[right] = ' ';
+                }
+                right++;
+                left = right;
+            }
+            i++;
+        }
+        return String.valueOf(chars).substring(0, right - 1);
+    }
+
+    /**
+     * TC: O(end - start)
+     * SC: O(1)
+     */
+    private String reverse(char[] chars, int start, int end) {
+        while (start < end) {
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+            start++;
+            end--;
+        }
+        return String.valueOf(chars);
+    }
+
+    /**
      * Using Stack
      *
      * TC: O(2 x N) ~ O(N)
      * SC: O(2 x N) ~ O(N)
      */
-    public String reverseWords(String s) {
+    public String reverseWordsUsingStack(String s) {
         String[] words = s.split(" ");
         Stack<String> st = new Stack<String>(); // SC: O(N)
         StringBuilder sb = new StringBuilder(); // SC: O(N)
