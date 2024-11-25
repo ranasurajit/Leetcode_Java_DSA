6325f807b348606968da78fecf18324429037cc3
Surajit Rana <surajit1602@gmail.com>
2024-11-26 02:27:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (60.69%) - LeetSync

--- BinarySearch/BinarySearchOnAnswers/1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ BinarySearch/BinarySearchOnAnswers/1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,16 +1,40 @@
 class Solution {
+    /**
+     * Using Shifting
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
     public int findKthPositive(int[] arr, int k) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (arr[i] <= k) {
+                k++;
+            } else {
+                break;
+            }
+        }
+        return k;
+    }
+
+    /**
+     * Using HashSet
+     *
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(N)
+     */
+    public int findKthPositiveUsingHashSet(int[] arr, int k) {
         int n = arr.length;
         int low = 1;
         int high = arr[n - 1];
-        HashSet<Integer> hs = new HashSet<Integer>();
-        HashSet<Integer> missing = new HashSet<Integer>();
-        for (int i = 0; i < n; i++) {
+        HashSet<Integer> hs = new HashSet<Integer>(); // SC: O(X) - (X + Y) = N
+        HashSet<Integer> missing = new HashSet<Integer>(); // SC: O(Y) - (X + Y) = N
+        for (int i = 0; i < n; i++) { // TC: O(N)
             hs.add(arr[i]);
         }
         int missed = -1;
         int count = 0;
-        for (int i = low; i <= high; i++) {
+        for (int i = low; i <= high; i++) { // TC: O(N)
             count = countOfMissing(hs, missing, i);
             if (count == k) {
                 missed = i;
