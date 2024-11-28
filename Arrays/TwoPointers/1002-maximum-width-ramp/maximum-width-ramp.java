1842d214aaf9a9c571620083a78e5bb5081f4a8c
Surajit Rana <surajit1602@gmail.com>
2024-11-28 11:13:54 +0530

    Time: 3 ms (99.87%) | Memory: 55.1 MB (71.24%) - LeetSync

--- Arrays/TwoPointers/1002-maximum-width-ramp/maximum-width-ramp.java
+++ Arrays/TwoPointers/1002-maximum-width-ramp/maximum-width-ramp.java
@@ -1,28 +1,76 @@
 class Solution {
     /**
-     * TC: O(2N) ~ O(N)
+     * Optimal Approach
+     * 
+     * TC: O(2 x N) ~ O(N)
      * SC: O(N)
+     * 
+     * @param nums
+     * @return
      */
     public int maxWidthRamp(int[] nums) {
         int n = nums.length;
         // pre-process max from right
-        int[] maxRight = new int[n]; // SC: O(N)
-        maxRight[n - 1] = nums[n - 1];
+        int[] rightMax = new int[n]; // SC: O(N)
+        rightMax[n - 1] = nums[n - 1];
         for (int i = n - 2; i >= 0; i--) { // TC: O(N)
-            maxRight[i] = Math.max(maxRight[i + 1], nums[i]);
+            rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
         }
-        int maxDiff = 0; // maxDiff will capture the width of ramp (j - i)
-
-        // Using two pointers
-        int p = 0;              // pointer for nums
-        int q = 0;              // pointer for maxRight
-        while (q < n) { // TC: O(N)
-            while (p < q && nums[p] > maxRight[q]) {
-                p++;
+        int max = 0;
+        int i = 0; // pointer for 'nums' array from left
+        int j = 0; // pointer for 'rightMax' array from left
+        while (j < n) { // TC: O(N)
+            if (i < j && nums[i] > rightMax[j]) {
+                i++;
             }
-            maxDiff = Math.max(maxDiff, q - p);
-            q++;
+            max = Math.max(max, j - i);
+            j++;
         }
-        return maxDiff;
+        return max;
+    }
+
+    /**
+     * Better Approach
+     * 
+     * TC: O(N ^ 2)
+     * SC: O(1)
+     * 
+     * @param nums
+     * @return
+     */
+    public int maxWidthRampBetter(int[] nums) {
+        int n = nums.length;
+        int max = 0;
+        for (int i = 0; i < n; i++) { // O(N)
+            for (int j = n - 1; j >= i; j--) { // O(N)
+                if (nums[i] <= nums[j]) {
+                    max = Math.max(max, j - i);
+                    break;
+                }
+            }
+        }
+        return max;
+    }
+
+    /**
+     * Brute-Force Approach
+     * 
+     * TC: O(N ^ 2)
+     * SC: O(1)
+     * 
+     * @param nums
+     * @return
+     */
+    public int maxWidthRampBruteForce(int[] nums) {
+        int n = nums.length;
+        int max = 0;
+        for (int i = 0; i < n; i++) { // O(N)
+            for (int j = i + 1; j < n; j++) { // O(N)
+                if (nums[i] <= nums[j]) {
+                    max = Math.max(max, j - i);
+                }
+            }
+        }
+        return max;
     }
 }
