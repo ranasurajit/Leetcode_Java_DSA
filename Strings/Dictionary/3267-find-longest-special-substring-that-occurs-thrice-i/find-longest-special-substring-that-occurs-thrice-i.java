4652dd8d774c70875c06c6e869ed6d10bfff6a44
Surajit Rana <surajit1602@gmail.com>
2024-12-10 23:29:28 +0530

    Time: 10 ms (68.40%) | Memory: 44.8 MB (56.13%) - LeetSync

--- Strings/Dictionary/3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
+++ Strings/Dictionary/3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
@@ -1,11 +1,48 @@
 class Solution {
     /**
+     * Better Approach
+     *
+     * TC: O(N ^ 2)
+     * SC: O(N ^ 2)
+     */
+    public int maximumLength(String s) {
+        int n = s.length();
+        HashMap<Pair<Character, Integer>, Integer> hm =
+            new HashMap<Pair<Character, Integer>, Integer>();
+        int len = 0;
+        for (int i = 0; i < n; i++) {     // TC: O(N)
+            char current = s.charAt(i);
+            len = 0;
+            for (int j = i; j < n; j++) { // TC: O(N)
+                if (current == s.charAt(j)) {
+                    len++;
+                    Pair<Character, Integer> pair = 
+                        new Pair<Character, Integer>(current, len);
+                    hm.put(pair, hm.getOrDefault(pair, 0) + 1);
+                } else {
+                    break;
+                }
+            }
+        }
+        int maxLength = -1;
+        // TC: O(N)
+        for (Map.Entry<Pair<Character, Integer>, Integer> entry : hm.entrySet()) {
+            Pair<Character, Integer> currentKey = entry.getKey();
+            if (hm.get(currentKey) >= 3) {
+                int currentLength = currentKey.getValue(); // access length from Pair
+                maxLength = Math.max(maxLength, currentLength);
+            }
+        }
+        return maxLength;
+    }
+
+    /**
      * Brute-Force Approach
      *
      * TC: O(N ^ 3)
      * SC: O(N ^ 2)
      */
-    public int maximumLength(String s) {
+    public int maximumLengthBruteForce(String s) {
         int n = s.length();
         HashMap<String, Integer> hm = new HashMap<String, Integer>();
         for (int i = 0; i < n; i++) {     // TC: O(N)
