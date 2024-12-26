d081ae296b6201e162d31191478394e4b0295d6b
Surajit Rana <surajit1602@gmail.com>
2024-12-26 16:38:40 +0530

    Time: 641 ms (14.12%) | Memory: 45 MB (24.62%) - LeetSync

--- DynamicProgramming/1D-Arrays/494-target-sum/target-sum.java
+++ DynamicProgramming/1D-Arrays/494-target-sum/target-sum.java
@@ -1,24 +1,55 @@
 class Solution {
     /**
-     * Using Recursion
+     * Using Recursion + Memoization
      * 
      * TC: O(2 ^ N)
      * SC: O(N)
      */
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-        return solve(0, nums, 0, target);
+        int[][] dp = new int[n][1001];
+        for (int[] dp1D: dp) {
+            Arrays.fill(dp1D, -1);
+        }
+        return solveMemoization(0, nums, 0, target, dp);
     }
 
-    private int solve(int index, int[] nums, int sum, int target) {
+    private int solveMemoization(int index, int[] nums, int sum, int target, int[][] dp) {
         if (index == nums.length) {
             if (sum == target) {
                 return 1;
             }
             return 0;
         }
-        int positiveSelection = solve(index + 1, nums, sum + nums[index], target);
-        int negativeSelection = solve(index + 1, nums, sum - nums[index], target);
+        if (dp[index][sum] != -1) {
+            return dp[index][sum];
+        }
+        int positiveSelection = solveRecursion(index + 1, nums, sum + nums[index], target);
+        int negativeSelection = solveRecursion(index + 1, nums, sum - nums[index], target);
+        dp[index][sum] = positiveSelection + negativeSelection;
+        return dp[index][sum];
+    }
+
+    /**
+     * Using Recursion
+     * 
+     * TC: O(2 ^ N)
+     * SC: O(N)
+     */
+    public int findTargetSumWaysRecursion(int[] nums, int target) {
+        int n = nums.length;
+        return solveRecursion(0, nums, 0, target);
+    }
+
+    private int solveRecursion(int index, int[] nums, int sum, int target) {
+        if (index == nums.length) {
+            if (sum == target) {
+                return 1;
+            }
+            return 0;
+        }
+        int positiveSelection = solveRecursion(index + 1, nums, sum + nums[index], target);
+        int negativeSelection = solveRecursion(index + 1, nums, sum - nums[index], target);
         return positiveSelection + negativeSelection; 
     }
 }
