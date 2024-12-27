5884b46752eb7e57cd3dbeff497037098c39f705
Surajit Rana <surajit1602@gmail.com>
2024-12-28 02:24:22 +0530

    Time: 4 ms (21.05%) | Memory: 47.3 MB (69.62%) - LeetSync

--- Graphs/DFS/547-number-of-provinces/number-of-provinces.java
+++ Graphs/DFS/547-number-of-provinces/number-of-provinces.java
@@ -1,33 +1,47 @@
 class Solution {
+    /**
+     * TC: O(V x V + V x E) ~ O(V x (V + E))
+     * SC: O(2 x V) ~ O(V)
+     */
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        HashMap<Integer, ArrayList<Integer>> adj = createGraph(isConnected, n);
-        boolean[] visited = new boolean[n];
+        // TC: O(V ^ 2), SC: O(V)
+        Map<Integer, ArrayList<Integer>> adj = createGraph(isConnected, n);
+        boolean[] visited = new boolean[n]; // SC: O(V)
         int provinces = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {       // TC: O(V)
             if (!visited[i]) {
-                dfsGraph(adj, i, visited);
+                dfsGraph(i, visited, adj);  // TC: O(E)
                 provinces++;
             }
         }
         return provinces;
     }
 
-    private void dfsGraph(HashMap<Integer, ArrayList<Integer>> adj, int u, boolean[] visited) {
+    /**
+     * TC: O(E)
+     * SC: O(E)
+     */
+    private void dfsGraph(int u, boolean[] visited, Map<Integer, ArrayList<Integer>> adj) {
         visited[u] = true;
         for (Integer v : adj.get(u)) {
             if (!visited[v]) {
-                dfsGraph(adj, v, visited);
+                dfsGraph(v, visited, adj);
             }
         }
     }
 
-    private HashMap<Integer, ArrayList<Integer>> createGraph(int[][] isConnected, int n) {
-        HashMap<Integer, ArrayList<Integer>> adj = new HashMap<Integer, ArrayList<Integer>>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                adj.put(i, adj.getOrDefault(i, new ArrayList<Integer>()));
-                adj.put(j, adj.getOrDefault(j, new ArrayList<Integer>()));
+    /**
+     * TC: O(V ^ 2 + V) ~ O(V ^ 2)
+     * SC: O(V)
+     */
+    private Map<Integer, ArrayList<Integer>> createGraph(int[][] isConnected, int n) {
+        Map<Integer, ArrayList<Integer>> adj = new HashMap<Integer, ArrayList<Integer>>();
+        for (int i = 0; i < n; i++) { // TC: O(V)
+            adj.put(i, new ArrayList<Integer>());
+        }
+        for (int i = 0; i < n; i++) {     // TC: O(V)
+            for (int j = 0; j < n; j++) { // TC: O(V)
                 if (i != j && isConnected[i][j] == 1) {
                     adj.get(i).add(j);
                 }
