30f9b7a9660bbbf8f19ad2088dd1bf5548561c93
Surajit Rana <surajit1602@gmail.com>
2024-12-30 13:31:42 +0530

    Time: 7 ms (93.62%) | Memory: 43.5 MB (63.42%) - LeetSync

--- DynamicProgramming/Strings/2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
+++ DynamicProgramming/Strings/2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
@@ -1,8 +1,35 @@
 class Solution {
     private int MOD = 1000000007;
 
+    /**
+     * Using Tabulation
+     *
+     * TC: O(High)
+     * SC: O(High + Max(Zero, One))
+     */
     public int countGoodStrings(int low, int high, int zero, int one) {
-        int[] dp = new int[high + 1];
+        int maxLength = high + Math.max(zero, one);
+        int[] dp = new int[maxLength + 1];
+        for (int currentLength = high; currentLength >= 0; currentLength--) {
+            int zeroLen = zero + currentLength;
+            int oneLen = one + currentLength;
+            int zeroCount = zeroLen >= low && zeroLen <= high ? 1 : 0;
+            int oneCount = oneLen >= low && oneLen <= high ? 1 : 0;
+            int result = (zeroCount + dp[zeroLen] + oneCount + dp[oneLen]) % MOD;
+            dp[currentLength] = result;
+        }
+        return dp[0];
+    }
+
+    /**
+     * Using Memoization
+     *
+     * TC: O(2 x High)
+     * SC: O(High + Max(Zero, One))
+     */
+    public int countGoodStringsMemoization(int low, int high, int zero, int one) {
+        int maxLength = high + Math.max(zero, one);
+        int[] dp = new int[maxLength + 1];
         Arrays.fill(dp, -1);
         return solve(low, high, zero, one, 0, dp);
     }
