01f443996ae310f05d23417b944d5fa2ecb7926d
Surajit Rana <surajit1602@gmail.com>
2025-01-03 11:40:05 +0530

    Time: 2 ms (100.00%) | Memory: 58.1 MB (79.25%) - LeetSync

--- Arrays/PrefixSuffix/2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
+++ Arrays/PrefixSuffix/2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -1,9 +1,31 @@
 class Solution {
     /**
      * TC: O(2 x N) ~ O(N)
-     * SC: O(N)
+     * SC: O(1)
      */
     public int waysToSplitArray(int[] nums) {
+        int n = nums.length;
+        long leftSum = 0L;
+        long rightSum = 0L;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            rightSum += nums[i];
+        }
+        int count = 0;
+        for (int i = 0; i < n - 1; i++) { // TC: O(N)
+            leftSum += nums[i];
+            rightSum -= nums[i];
+            if (leftSum >= rightSum) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(N)
+     */
+    public int waysToSplitArrayBetter(int[] nums) {
         int n = nums.length;
         long[] prefixSum = new long[n]; // SC: O(N)
         prefixSum[0] = (long) nums[0];
