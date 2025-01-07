b06a3460131743be6ea8417ef6bf8e1bac7c9f96
Surajit Rana <surajit1602@gmail.com>
2025-01-07 22:58:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (65.38%) - LeetSync

--- LinkedList/TwoPointers/908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ LinkedList/TwoPointers/908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -10,16 +10,25 @@
  */
 class Solution {
     /**
-     * TC: O(N)
+     * Using Two Pointers (Slow and Fast pointers)
+     * 
+     * TC: O(N / 2) ~ O(N)
      * SC: O(1)
+     * 
+     * @param head
+     * @return
      */
     public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
         ListNode slow = head;
         ListNode fast = head;
-        while (fast != null && fast.next != null) {     // TC: O(N)
+        while (fast != null && fast.next != null) { // TC: O(N / 2)
             slow = slow.next;
             fast = fast.next.next;
         }
+        // slow pointer will point to mid node of Linked List
         return slow;
     }
 }
