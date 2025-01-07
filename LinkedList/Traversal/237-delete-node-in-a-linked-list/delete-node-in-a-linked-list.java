e0ac689b24233b6ae6d1df2ffc1e00ad33f1f4d0
Surajit Rana <surajit1602@gmail.com>
2025-01-07 21:57:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (8.68%) - LeetSync

--- LinkedList/Traversal/237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ LinkedList/Traversal/237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -10,10 +10,14 @@ class Solution {
     /**
      * TC: O(N)
      * SC: O(1)
+     * 
+     * @param node
      */
     public void deleteNode(ListNode node) {
         ListNode prev = null;
         while (node != null && node.next != null) { // TC: O(N)
+            // setting the node value to node's next value so that target node value is
+            // deleted
             node.val = node.next.val;
             prev = node;
             node = node.next;
