695d2af828440f0ed452ac291a8fe940c0e51f1b
Surajit Rana <surajit1602@gmail.com>
2025-01-07 22:16:41 +0530

    Time: 3 ms (99.83%) | Memory: 65.2 MB (20.56%) - LeetSync

--- LinkedList/TwoPointers/2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ LinkedList/TwoPointers/2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -10,23 +10,30 @@
  */
 class Solution {
     /**
-     * Using Fast and Slow Pointers approach
-     *
+     * Using Two Pointers (Slow and Fast pointers)
+     * 
      * TC: O(N / 2) ~ O(N)
      * SC: O(1)
+     * 
+     * @param head
+     * @return
      */
     public ListNode deleteMiddle(ListNode head) {
         if (head == null || head.next == null) {
             return null;
         }
+        ListNode prev = null;
         ListNode slow = head;
         ListNode fast = head;
-        ListNode prev = null;
         while (fast != null && fast.next != null) { // TC: O(N / 2)
             prev = slow;
             slow = slow.next;
             fast = fast.next.next;
         }
+        /**
+         * here slow will be the mid of Linked List and prev node's
+         * next will be the middle node so move it
+         */
         prev.next = prev.next.next;
         return head;
     }
