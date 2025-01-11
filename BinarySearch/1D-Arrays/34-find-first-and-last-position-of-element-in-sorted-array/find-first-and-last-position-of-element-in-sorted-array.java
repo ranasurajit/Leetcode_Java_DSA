0929caa7540fa7493276f45a6cacd85ff3020fa2
Surajit Rana <surajit1602@gmail.com>
2025-01-11 21:35:39 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (14.30%) - LeetSync

--- BinarySearch/1D-Arrays/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ BinarySearch/1D-Arrays/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -8,7 +8,86 @@ class Solution {
      * @return
      */
     public int[] searchRange(int[] nums, int target) {
-        return new int[] { searchMinRange(nums, target), searchMaxRange(nums, target) };
+        if (nums.length == 0) {
+            return new int[] { -1, -1 };
+        }
+        int lBound = lowerBound(nums, target);
+        if (lBound == -1) {
+            return new int[] { -1, -1 };
+        }
+        int rBound = upperBound(nums, target);
+        return new int[] {
+            lBound,
+            rBound - 1
+        };
+    }
+
+    /**
+     * TC: O(log(N))
+     * SC: O(1)
+     * 
+     * Lower Bound => nums[i] >= target
+     * 
+     * @param nums
+     * @param target
+     * @return
+     */
+    private int lowerBound(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] >= target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        if (low >= n || nums[low] != target) {
+            return -1;
+        }
+        return low;
+    }
+
+    /**
+     * TC: O(log(N))
+     * SC: O(1)
+     * 
+     * Upper Bound => nums[i] > target
+     * 
+     * @param nums
+     * @param target
+     * @return
+     */
+    private int upperBound(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    /**
+     * TC: O(2 x log(N)) ~ O(log(N))
+     * SC: O(1)
+     * 
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] searchRangeWithoutBound(int[] nums, int target) {
+        return new int[] {
+            searchMinRange(nums, target),
+            searchMaxRange(nums, target)
+        };
     }
 
     /**
