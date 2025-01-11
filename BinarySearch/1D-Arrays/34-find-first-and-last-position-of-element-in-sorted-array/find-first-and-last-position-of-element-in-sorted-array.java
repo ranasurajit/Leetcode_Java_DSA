712eb71fb7150e2eb1073da9a1e30016853d4ea9
Surajit Rana <surajit1602@gmail.com>
2025-01-11 21:01:35 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (25.48%) - LeetSync

--- BinarySearch/1D-Arrays/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ BinarySearch/1D-Arrays/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,44 +1,69 @@
 class Solution {
+    /**
+     * TC: O(2 x log(N)) ~ O(log(N))
+     * SC: O(1)
+     * 
+     * @param nums
+     * @param target
+     * @return
+     */
     public int[] searchRange(int[] nums, int target) {
-        int[] positions = { getFirstPosition(nums, target), getLastPosition(nums, target) };
-        return positions;
+        return new int[] { searchMinRange(nums, target), searchMaxRange(nums, target) };
     }
 
-    private int getFirstPosition(int[] nums, int target) {
+    /**
+     * TC: O(log(N))
+     * SC: O(1)
+     * 
+     * @param nums
+     * @param target
+     * @return
+     */
+    private int searchMinRange(int[] nums, int target) {
         int n = nums.length;
         int low = 0;
         int high = n - 1;
-        int index = Integer.MAX_VALUE;
+        int minRange = -1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                index = Math.min(index, mid);
+            if (nums[mid] > target) {
                 high = mid - 1;
             } else if (nums[mid] < target) {
                 low = mid + 1;
-            } else if (nums[mid] > target) {
+            } else {
+                // nums[mid] = target
+                minRange = mid;
                 high = mid - 1;
             }
         }
-        return index == Integer.MAX_VALUE ? -1 : index;
+        return minRange;
     }
 
-    private int getLastPosition(int[] nums, int target) {
+    /**
+     * TC: O(log(N))
+     * SC: O(1)
+     * 
+     * @param nums
+     * @param target
+     * @return
+     */
+    private int searchMaxRange(int[] nums, int target) {
         int n = nums.length;
         int low = 0;
         int high = n - 1;
-        int index = Integer.MIN_VALUE;
+        int maxRange = -1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                index = Math.max(index, mid);
-                low = mid + 1;
+            if (nums[mid] > target) {
+                high = mid - 1;
             } else if (nums[mid] < target) {
                 low = mid + 1;
-            } else if (nums[mid] > target) {
-                high = mid - 1;
+            } else {
+                // nums[mid] = target
+                maxRange = mid;
+                low = mid + 1;
             }
         }
-        return index == Integer.MIN_VALUE ? -1 : index;
+        return maxRange;
     }
 }
