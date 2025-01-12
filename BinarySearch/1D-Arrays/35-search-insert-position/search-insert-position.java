3c3f9dedcda07affe6bdadcdbb8175899746425b
Surajit Rana <surajit1602@gmail.com>
2025-01-13 00:32:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (5.22%) - LeetSync

--- BinarySearch/1D-Arrays/35-search-insert-position/search-insert-position.java
+++ BinarySearch/1D-Arrays/35-search-insert-position/search-insert-position.java
@@ -1,28 +1,32 @@
 class Solution {
     /**
+     * Using Binary Search Approach
+     * 
      * TC: O(log(N))
      * SC: O(1)
      */
     public int searchInsert(int[] nums, int target) {
+        return lowerbound(nums, target);
+    }
+
+    /**
+     * Lower bound => nums[i] >= x
+     * 
+     * TC: O(log(N))
+     * SC: O(1)
+     */
+    private int lowerbound(int[] nums, int target) {
         int n = nums.length;
         int low = 0;
         int high = n - 1;
-        int pos = Integer.MAX_VALUE;
-        if (nums[0] > target) {
-            return 0;
-        }
-        if (nums[n - 1] < target) {
-            return n;
-        }
-        while (low <= high) {
+        while (low <= high) { // TC: O(log(N))
             int mid = low + (high - low) / 2;
             if (nums[mid] >= target) {
-                pos = Math.min(pos, mid);
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        return pos;
+        return low;
     }
 }
