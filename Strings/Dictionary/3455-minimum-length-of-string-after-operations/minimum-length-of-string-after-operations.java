d954b3b2df7a82b34076c0e8a6d9aa8836828369
Surajit Rana <surajit1602@gmail.com>
2025-01-13 11:06:03 +0530

    Time: 87 ms (29.80%) | Memory: 46.6 MB (33.11%) - LeetSync

--- Strings/Dictionary/3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
+++ Strings/Dictionary/3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
@@ -4,8 +4,7 @@ class Solution {
         Map<Character, Integer> map = 
             new HashMap<Character, Integer>();
         for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
         }
         int minLength = 0;
         for (Character key : map.keySet()) {
