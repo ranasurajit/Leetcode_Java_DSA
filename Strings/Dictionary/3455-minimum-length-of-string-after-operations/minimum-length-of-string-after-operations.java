32827115b12b8d16a4dc03d907b3c112bb02ce94
Surajit Rana <surajit1602@gmail.com>
2025-01-13 11:10:07 +0530

    Time: 9 ms (92.05%) | Memory: 46.7 MB (24.50%) - LeetSync

--- Strings/Dictionary/3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
+++ Strings/Dictionary/3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
@@ -1,18 +1,20 @@
 class Solution {
+    /**
+     * TC: O(N)
+     * SC: O(1)
+     */
     public int minimumLength(String s) {
         int n = s.length();
-        Map<Character, Integer> map = 
-            new HashMap<Character, Integer>();
-        for (int i = 0; i < n; i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        int[] alpha = new int[26];    // SC: O(26) ~ O(1) 
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            alpha[s.charAt(i) - 'a']++;
         }
         int minLength = 0;
-        for (Character key : map.keySet()) {
-            int size = map.get(key);
-            if (size > 2) {
-                minLength += size % 2 == 0 ? 2 : 1;
+        for (int i = 0; i < 26; i++) { // TC: O(26) ~ O(1)
+            if (alpha[i] > 2) {
+                minLength += alpha[i] % 2 == 0 ? 2 : 1;
             } else {
-                minLength += size;
+                minLength += alpha[i];
             }
         }
         return minLength;
