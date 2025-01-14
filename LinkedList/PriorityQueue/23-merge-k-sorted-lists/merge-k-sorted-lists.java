b589a52b21aca963065848dba3a7c4e5c04a7d6e
Surajit Rana <surajit1602@gmail.com>
2025-01-14 21:22:44 +0530

    Time: 172 ms (5.10%) | Memory: 44.5 MB (47.09%) - LeetSync

--- LinkedList/PriorityQueue/23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ LinkedList/PriorityQueue/23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,23 +9,45 @@
  * }
  */
 class Solution {
+    /**
+     * TC: O(K x (M + N))
+     * SC: O(M + N)
+     *
+     * @param lists
+     * @return
+     */
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>((p, q) -> p.val - q.val);
-        for (ListNode node : lists) {
-            if (node != null) {
-                pq.offer(node);
-            }
+        int k = lists.length;
+        if (k == 0) {
+            return null;
         }
-        ListNode dummy = new ListNode(-1);
-        ListNode temp = dummy;
-        while (!pq.isEmpty()) {
-            ListNode node = pq.poll();
-            temp.next = node;
-            if (node.next != null) {
-                pq.offer(node.next);
-            }
-            temp = temp.next;
+        for (int i = 1; i < k; i++) { // TC: O(K)
+            lists[i] = mergeTwoLists(lists[i - 1], lists[i]); // TC: O(M + N)
         }
-        return dummy.next;
+        return lists[k - 1];
+    }
+
+    /**
+     * TC: O(M + N)
+     * SC: O(M + N)
+     * 
+     * @param list1
+     * @param list2
+     * @return
+     */
+    private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
     }
 }
