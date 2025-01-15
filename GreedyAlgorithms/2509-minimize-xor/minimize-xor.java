97598ba121ce5fb3d2dd378af16c4f363ae76969
Surajit Rana <surajit1602@gmail.com>
2025-01-15 11:30:17 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (32.08%) - LeetSync

---
+++ GreedyAlgorithms/2509-minimize-xor/minimize-xor.java
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * Using Property of XOR and using Greedy Algorithm
+     *
+     * TC: O(1)
+     * SC: O(1)
+     */
+    public int minimizeXor(int num1, int num2) {
+        int setBits = 0;
+        int result = 0;
+        for (int i = 0; i < 32; i++) { // TC: O(32) ~ O(1)
+            if (((num2 >> i) & 1) == 1) {
+                setBits++;
+            }
+        }
+
+        // Cancel set bits from highest to lowest significant bit from right to left
+        for (int i = 31; i >= 0 && setBits > 0; i--) { // TC: O(32) ~ O(1)
+            if ((num1 & (1 << i)) != 0) {
+                setBits--;
+                result += (1 << i);
+            }
+        }
+
+        // set the bits starting from lowest significant bit for left over setBits
+        for (int i = 0; i < 32 && setBits > 0; i++) { // TC: O(32) ~ O(1)
+            if ((num1 & (1 << i)) == 0) {
+                setBits--;
+                result += (1 << i);
+            }
+        }
+        return result;
+    }
+}
