315c1a934ac395841ec9d66413a6e96c74d067f6
Surajit Rana <surajit1602@gmail.com>
2025-01-16 21:14:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (49.30%) - LeetSync

--- LinkedList/Recursion/206-reverse-linked-list/reverse-linked-list.java
+++ LinkedList/Recursion/206-reverse-linked-list/reverse-linked-list.java
@@ -10,17 +10,41 @@
  */
 class Solution {
     /**
+     * Using Iterative Approach
+     * 
+     * TC: O(N)
+     * Sc: O(1)
+     * 
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+        }
+        return prev;
+    }
+
+    /**
      * Using Recursion
      * 
      * TC: O(N)
      * SC: O(N)
+     * 
+     * @param head
+     * @return
      */
-    public ListNode reverseList(ListNode head) {
+    public ListNode reverseListRecursive(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
         ListNode nextNode = head.next;
-        ListNode newHead = reverseList(nextNode);
+        ListNode newHead = reverseListRecursive(nextNode);
         head.next = null;
         nextNode.next = head;
         return newHead;
