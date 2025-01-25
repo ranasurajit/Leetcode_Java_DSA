bd7156fc0c8b53e1ecc2bd7e506d0e2d8ee8d0c9
Surajit Rana <surajit1602@gmail.com>
2025-01-25 19:44:13 +0530

    Time: 705 ms (12.48%) | Memory: 46.6 MB (11.90%) - LeetSync

--- SweepLineAlgorithm/731-my-calendar-ii/my-calendar-ii.java
+++ SweepLineAlgorithm/731-my-calendar-ii/my-calendar-ii.java
@@ -6,10 +6,10 @@
  */
 class MyCalendarTwo {
 
-    TreeMap<Integer, Integer> map = null;
+    private TreeMap<Integer, Integer> events = null;
 
     public MyCalendarTwo() {
-        map = new TreeMap<Integer, Integer>();
+        events = new TreeMap<Integer, Integer>();
     }
     
     /**
@@ -22,24 +22,24 @@ class MyCalendarTwo {
      */
     public boolean book(int startTime, int endTime) {
         // increment start time by +1 and decrement end time (excluded) by -1
-        map.put(startTime, map.getOrDefault(startTime, 0) + 1);
-        map.put(endTime, map.getOrDefault(endTime, 0) - 1);
+        events.put(startTime, events.getOrDefault(startTime, 0) + 1);
+        events.put(endTime, events.getOrDefault(endTime, 0) - 1);
 
-        int sum = 0; // calculate cumulative sum for all keys
-        for (Integer key : map.keySet()) { // TC: O(N)
-            sum += map.get(key);
-            if (sum > 2) {
+        int activeEvents = 0; // calculate cumulative sum for all keys
+        for (Integer key : events.keySet()) { // TC: O(N)
+            activeEvents += events.get(key);
+            if (activeEvents > 2) {
                 /**
                     * there is a triple booking then undo the
                     * last operation done for start and end time
                     */
-                map.put(startTime, map.get(startTime) - 1); // TC: O(log(N))
-                if (map.get(startTime) == 0) {
-                    map.remove(startTime);
+                events.put(startTime, events.get(startTime) - 1); // TC: O(log(N))
+                if (events.get(startTime) == 0) {
+                    events.remove(startTime);
                 }
-                map.put(endTime, map.get(endTime) + 1); // TC: O(log(N))
-                if (map.get(endTime) == 0) {
-                    map.remove(endTime);
+                events.put(endTime, events.get(endTime) + 1); // TC: O(log(N))
+                if (events.get(endTime) == 0) {
+                    events.remove(endTime);
                 }
                 return false;
             }
