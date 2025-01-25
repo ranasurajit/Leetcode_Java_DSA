29968aba33ebca28a80d08dcb48a0ca12e524159
Surajit Rana <surajit1602@gmail.com>
2025-01-25 19:40:09 +0530

    Time: 1318 ms (5.01%) | Memory: 46.2 MB (5.91%) - LeetSync

--- SweepLineAlgorithm/729-my-calendar-i/my-calendar-i.java
+++ SweepLineAlgorithm/729-my-calendar-i/my-calendar-i.java
@@ -6,10 +6,10 @@
  */
 class MyCalendar {
 
-    TreeMap<Integer, Integer> map = null;
+    TreeMap<Integer, Integer> events = null;
 
     public MyCalendar() {
-        map = new TreeMap<Integer, Integer>();
+        events = new TreeMap<Integer, Integer>();
     }
     
     /**
@@ -22,19 +22,25 @@ class MyCalendar {
      */
     public boolean book(int startTime, int endTime) {
         // increment start time by +1 and decrement end time (excluded) by -1
-        map.put(startTime, map.getOrDefault(startTime, 0) + 1);
-        map.put(endTime, map.getOrDefault(endTime, 0) - 1);
+        events.put(startTime, events.getOrDefault(startTime, 0) + 1);
+        events.put(endTime, events.getOrDefault(endTime, 0) - 1);
 
-        int sum = 0; // calculate cumulative sum for all keys
-        for (Integer key : map.keySet()) { // TC: O(N)
-            sum += map.get(key);
-            if (sum > 1) {
+        int activeEvents = 0; // calculate cumulative sum for all keys
+        for (Integer key : events.keySet()) { // TC: O(N)
+            activeEvents += events.get(key);
+            if (activeEvents > 1) {
                 /**
                     * there is a double booking then undo the
                     * last operation done for start and end time
                     */
-                map.put(startTime, map.get(startTime) - 1); // TC: O(log(N))
-                map.put(endTime, map.get(endTime) + 1); // TC: O(log(N))
+                events.put(startTime, events.get(startTime) - 1); // TC: O(log(N))
+                if (events.get(startTime) == 0) {
+                    events.remove(startTime);
+                }
+                events.put(endTime, events.get(endTime) + 1); // TC: O(log(N))
+                if (events.get(endTime) == 0) {
+                    events.remove(endTime);
+                }
                 return false;
             }
         }
