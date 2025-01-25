1b005334deca2ab9b90123474bef37756a5b3820
Surajit Rana <surajit1602@gmail.com>
2025-01-25 20:57:07 +0530

    Time: 299 ms (5.15%) | Memory: 102.4 MB (5.48%) - LeetSync

---
+++ SweepLineAlgorithm/2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.java
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * Using Sweep-Line Algorithm
+     *
+     * TC: O(N x log(N))
+     * SC: O(N)
+     */
+    public int minGroups(int[][] intervals) {
+        TreeMap<Integer, Integer> events = new TreeMap<Integer, Integer>();
+        // minimum number of groups is the maximum number of overlapping intervals
+        return getMaximumOverlaps(intervals, events);
+    }
+
+    /**
+     * TC: O(N x log(N))
+     * SC: O(1)
+     */
+    private int getMaximumOverlaps(int[][] intervals, 
+        TreeMap<Integer, Integer> events) {
+        for (int[] interval : intervals) {                    // TC: O(N)
+            events.put(interval[0],
+                events.getOrDefault(interval[0], 0) + 1);     // TC: O(log(N))
+            events.put(interval[1] + 1, 
+                events.getOrDefault(interval[1] + 1, 0) - 1); // TC: O(log(N))
+        }
+        int maxGroups = 0;
+        int currentGroups = 0;
+        for (Integer key : events.keySet()) {                 // TC: O(2 x N)
+            currentGroups += events.get(key);                 // TC: O(log(N))
+            maxGroups = Math.max(maxGroups, currentGroups);
+        }
+        return maxGroups;
+    }
+}
