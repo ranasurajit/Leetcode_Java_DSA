2c226baab07d57fa51c93443243145d15b60b73c
Surajit Rana <surajit1602@gmail.com>
2025-01-25 18:30:23 +0530

    Time: 1917 ms (5.01%) | Memory: 45.7 MB (37.25%) - LeetSync

---
+++ SweepLineAlgorithm/729-my-calendar-i/my-calendar-i.java
@@ -0,0 +1,34 @@
+class MyCalendar {
+
+    TreeMap<Integer, Integer> map;
+
+    public MyCalendar() {
+        map = new TreeMap<Integer, Integer>();
+    }
+    
+    /**
+     * TC: O(N x log(N))
+     * SC: O(1)
+     */
+    public boolean book(int startTime, int endTime) {
+        map.put(startTime, map.getOrDefault(startTime, 0) + 1); // TC: O(log(N))
+        map.put(endTime, map.getOrDefault(endTime, 0) - 1);     // TC: O(log(N))
+        int sum = 0;
+        for (Integer key : map.keySet()) { // TC: O(N)
+            sum += map.get(key);
+            if (sum > 1) {
+                // undo the book operation due to double booking
+                map.put(startTime, map.get(startTime) - 1); // TC: O(log(N))
+                map.put(endTime, map.get(endTime) + 1);     // TC: O(log(N))
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(startTime,endTime);
+ */
