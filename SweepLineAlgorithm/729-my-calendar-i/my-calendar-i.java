29eefabdbc061770342b8856daa448c777c6478a
Surajit Rana <surajit1602@gmail.com>
2025-01-25 18:53:40 +0530

    Time: 2035 ms (5.01%) | Memory: 45.8 MB (27.17%) - LeetSync

--- SweepLineAlgorithm/729-my-calendar-i/my-calendar-i.java
+++ SweepLineAlgorithm/729-my-calendar-i/my-calendar-i.java
@@ -1,6 +1,12 @@
+/**
+ * Using Sweep-Line Algorithm
+ * 
+ * TC: O(N x log(N))
+ * SC: O(N)
+ */
 class MyCalendar {
 
-    TreeMap<Integer, Integer> map;
+    TreeMap<Integer, Integer> map = null;
 
     public MyCalendar() {
         map = new TreeMap<Integer, Integer>();
@@ -8,18 +14,27 @@ class MyCalendar {
     
     /**
      * TC: O(N x log(N))
-     * SC: O(N)
+     * SC: O(1)
+     * 
+     * @param startTime
+     * @param endTime
+     * @return
      */
     public boolean book(int startTime, int endTime) {
-        map.put(startTime, map.getOrDefault(startTime, 0) + 1); // TC: O(log(N))
-        map.put(endTime, map.getOrDefault(endTime, 0) - 1);     // TC: O(log(N))
-        int sum = 0;
+        // increment start time by +1 and decrement end time (excluded) by -1
+        map.put(startTime, map.getOrDefault(startTime, 0) + 1);
+        map.put(endTime, map.getOrDefault(endTime, 0) - 1);
+
+        int sum = 0; // calculate cumulative sum for all keys
         for (Integer key : map.keySet()) { // TC: O(N)
             sum += map.get(key);
             if (sum > 1) {
-                // undo the book operation due to double booking
+                /**
+                    * there is a double booking then undo the
+                    * last operation done for start and end time
+                    */
                 map.put(startTime, map.get(startTime) - 1); // TC: O(log(N))
-                map.put(endTime, map.get(endTime) + 1);     // TC: O(log(N))
+                map.put(endTime, map.get(endTime) + 1); // TC: O(log(N))
                 return false;
             }
         }
