44e8521f61656283e418615ab02255df527c4f45
Surajit Rana <surajit1602@gmail.com>
2025-01-30 13:56:13 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (65.73%) - LeetSync

--- Graphs/Bipartite/801-is-graph-bipartite/is-graph-bipartite.java
+++ Graphs/Bipartite/801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,6 +1,6 @@
 class Solution {
     /**
-     * Using BFS Approach
+     * Using DFS Approach
      * 
      * TC: O(2 x V + 2 x E) ~ O(V + E)
      * SC: O(2 x V) ~ O(V)
@@ -9,6 +9,55 @@ class Solution {
      * @return
      */
     public boolean isBipartite(int[][] graph) {
+        int v = graph.length;
+        int[] colors = new int[v]; // SC: O(V) this will act as visited array
+        Arrays.fill(colors, -1);
+
+        // visiting all nodes having color as -1
+        for (int i = 0; i < v; i++) { // TC: O(V)
+            if (colors[i] == -1 && !isDFSBipartiteGraph(i, graph, colors, 1)) {
+                return false; // TC: O(V + 2 x E), SC: O(V)
+            }
+        }
+        return true;
+    }
+
+    /**
+     * TC: O(V + 2 x E)
+     * SC: O(V)
+     * 
+     * @param u
+     * @param adj
+     * @param colors
+     * @param currentColor
+     * @return
+     */
+    private boolean isDFSBipartiteGraph(int u, int[][] adj,
+            int[] colors, int currentColor) {
+        colors[u] = currentColor;
+        for (int v : adj[u]) {
+            if (colors[v] == colors[u]) {
+                return false;
+            }
+            int alternateColor = 1 - currentColor;
+            if (colors[v] == -1 && 
+                !isDFSBipartiteGraph(v, adj, colors, alternateColor)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Using BFS Approach
+     * 
+     * TC: O(2 x V + 2 x E) ~ O(V + E)
+     * SC: O(2 x V) ~ O(V)
+     * 
+     * @param graph
+     * @return
+     */
+    public boolean isBipartiteBFS(int[][] graph) {
         int v = graph.length;
         int[] colors = new int[v]; // SC: O(V) this will act as visited array
         Arrays.fill(colors, -1);
@@ -46,55 +95,6 @@ class Solution {
                     colors[v] = 1 - colors[u];
                     queue.offer(v);
                 }
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Using DFS Approach
-     * 
-     * TC: O(2 x V + 2 x E) ~ O(V + E)
-     * SC: O(2 x V) ~ O(V)
-     * 
-     * @param graph
-     * @return
-     */
-    public boolean isBipartiteDFS(int[][] graph) {
-        int v = graph.length;
-        int[] colors = new int[v]; // SC: O(V) this will act as visited array
-        Arrays.fill(colors, -1);
-
-        // visiting all nodes having color as -1
-        for (int i = 0; i < v; i++) { // TC: O(V)
-            if (colors[i] == -1 && !isDFSBipartiteGraph(i, graph, colors, 1)) {
-                return false; // TC: O(V + 2 x E), SC: O(V)
-            }
-        }
-        return true;
-    }
-
-    /**
-     * TC: O(V + 2 x E)
-     * SC: O(V)
-     * 
-     * @param u
-     * @param adj
-     * @param colors
-     * @param currentColor
-     * @return
-     */
-    private boolean isDFSBipartiteGraph(int u, int[][] adj,
-            int[] colors, int currentColor) {
-        colors[u] = currentColor;
-        for (int v : adj[u]) {
-            if (colors[v] == colors[u]) {
-                return false;
-            }
-            int alternateColor = 1 - currentColor;
-            if (colors[v] == -1 && 
-                !isDFSBipartiteGraph(v, adj, colors, alternateColor)) {
-                return false;
             }
         }
         return true;
