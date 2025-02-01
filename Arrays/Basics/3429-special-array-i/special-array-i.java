8fb6e08155223d8696857db6dbaa13cd5e193fd3
Surajit Rana <surajit1602@gmail.com>
2025-02-01 10:55:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (84.31%) - LeetSync

--- Arrays/Basics/3429-special-array-i/special-array-i.java
+++ Arrays/Basics/3429-special-array-i/special-array-i.java
@@ -10,8 +10,7 @@ class Solution {
         }
         for (int i = 1; i < n; i++) { // TC: O(N)
             // sum of different parity (odd and even) will always be odd
-            int sum = nums[i] + nums[i - 1];
-            if ((sum & 1) == 0) {
+            if (((nums[i] + nums[i - 1]) & 1) == 0) {
                 return false;
             }
         }
