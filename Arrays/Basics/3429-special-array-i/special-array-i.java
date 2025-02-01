bdfd4fd4ac4b0993951f368aefbad4d0f8e639c3
Surajit Rana <surajit1602@gmail.com>
2025-02-01 11:08:43 +0530

    Time: 1 ms (37.58%) | Memory: 43.3 MB (56.64%) - LeetSync

--- Arrays/Basics/3429-special-array-i/special-array-i.java
+++ Arrays/Basics/3429-special-array-i/special-array-i.java
@@ -1,11 +1,34 @@
 class Solution {
     /**
+     * Approach II
+     *
+     * XOR of Odd and Even numbers is 1 for different parity
+     * XOR of Odd/Odd or Even/Even numbers is 0 for same parity
+     * 
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public boolean isArraySpecial(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return true;
+        }
+        for (int i = 1; i < n; i++) { // TC: O(N)
+            // sum of different parity (odd and even) will always be odd
+            if (((nums[i] ^ nums[i - 1]) & 1) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
      * Approach I - Sum of Odd and Even numbers is Odd
      *
      * TC: O(N)
      * SC: O(1)
      */
-    public boolean isArraySpecial(int[] nums) {
+    public boolean isArraySpecialApproachI(int[] nums) {
         int n = nums.length;
         if (n < 2) {
             return true;
