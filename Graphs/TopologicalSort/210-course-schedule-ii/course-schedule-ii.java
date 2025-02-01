974f4e377dae1ff70ca37d359e19f6499dbc4665
Surajit Rana <surajit1602@gmail.com>
2025-02-02 00:37:00 +0530

    Time: 6 ms (52.88%) | Memory: 45.8 MB (70.23%) - LeetSync

--- Graphs/TopologicalSort/210-course-schedule-ii/course-schedule-ii.java
+++ Graphs/TopologicalSort/210-course-schedule-ii/course-schedule-ii.java
@@ -1,47 +1,83 @@
 class Solution {
+    /**
+     * Using Topological Sort Approach
+     *
+     * TC: O(V + E)
+     * SC: O(V + E)
+     */
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        HashMap<Integer, ArrayList<Integer>> adj = new HashMap<Integer, ArrayList<Integer>>();
-        createGraph(prerequisites, adj, numCourses);
-        int[] indegrees = getIndegrees(prerequisites, numCourses);
-        Queue<Integer> queue = new LinkedList<Integer>();
-        int[] order = new int[numCourses];
-        for (int i = 0; i < indegrees.length; i++) {
-            if (indegrees[i] == 0) {
-                queue.offer(i);
+        // create adjacency list
+        Map<Integer, ArrayList<Integer>> adj =
+            new HashMap<Integer, ArrayList<Integer>>(); // SC: O(V + E)
+        for (int[] edge : prerequisites) {
+            adj.computeIfAbsent(edge[1], k -> new ArrayList<Integer>()).add(edge[0]);
+        }
+        boolean hasCycle = doesGraphHasCycle(adj, numCourses); // TC: O(V + E), SC: O(V)
+        if (hasCycle) {
+            return new int[] {};
+        }
+        Stack<Integer> st = new Stack<Integer>();       // SC: O(V)
+        boolean[] visited = new boolean[numCourses];    // SC: O(V)
+        for (int i = 0; i < numCourses; i++) {          // TC: O(V)
+            if (!visited[i]) {
+                dfsGraph(i, adj, visited, st);          // TC: O(V + E), SC: O(V)
             }
         }
+        int[] topoSort = new int[numCourses];
         int index = 0;
-        while (!queue.isEmpty()) {
-            Integer u = queue.poll();
-            order[index++] = u;
-            for (Integer v : adj.get(u)) {
-                indegrees[v]--;
-                if (indegrees[v] == 0) {
-                    queue.offer(v);
-                }
+        System.out.println(st.size());
+        while (!st.isEmpty()) {                         // TC: O(V)
+            topoSort[index++] = st.pop();
+        }
+        return topoSort;
+    }
+
+    /**
+     * TC: O(2 x V + E) ~ O(V + E)
+     * SC: O(3 x V) ~ O(V)
+     */
+    private boolean doesGraphHasCycle(Map<Integer, ArrayList<Integer>> adj, int n) {
+        boolean[] visited = new boolean[n];             // SC: O(V)
+        boolean[] inRecursion = new boolean[n];         // SC: O(V)
+        for (int i = 0; i < n; i++) {                   // TC: O(V)
+            if (!visited[i] && dfsGraphCycle(i, adj, visited, inRecursion)) {
+                return true;                            // TC: O(V + E), SC: O(V)
             }
         }
-        if (index != numCourses) {
-            return new int[]{};
-        }
-        return order;
+        return false;
     }
 
-    private void createGraph(int[][] prerequisites,
-        HashMap<Integer, ArrayList<Integer>> adj, int numCourses) {
-        for (int i = 0; i < numCourses; i++) {
-            adj.put(i, new ArrayList<Integer>());
+    /**
+     * TC: O(V + E)
+     * SC: O(V)
+     */
+    private boolean dfsGraphCycle(int u, Map<Integer, ArrayList<Integer>> adj,
+        boolean[] visited, boolean[] inRecursion) {
+        visited[u] = true;
+        inRecursion[u] = true;
+        for (Integer v : adj.getOrDefault(u, new ArrayList<Integer>())) {
+            if (!visited[v] && dfsGraphCycle(v, adj, visited, inRecursion)) {
+                return true;
+            } else if (inRecursion[v]) {
+                return true;
+            }
         }
-        for (int i = 0; i < prerequisites.length; i++) {
-            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
-        }
+        inRecursion[u] = false;
+        return false;
     }
 
-    private int[] getIndegrees(int[][] prerequisites, int numCourses) {
-        int[] indegrees = new int[numCourses];
-        for (int i = 0; i < prerequisites.length; i++) {
-            indegrees[prerequisites[i][0]]++;
+    /**
+     * TC: O(V + E)
+     * SC: O(V)
+     */
+    private void dfsGraph(int u, Map<Integer, ArrayList<Integer>> adj,
+        boolean[] visited, Stack<Integer> st) {
+        visited[u] = true;
+        for (Integer v : adj.getOrDefault(u, new ArrayList<Integer>())) {
+            if (!visited[v]) {
+                dfsGraph(v, adj, visited, st);
+            }
         }
-        return indegrees;
+        st.push(u);
     }
 }
