3aa70a42a1e5f0e6d93151639dec2f5e84a8bbed
Surajit Rana <surajit1602@gmail.com>
2025-02-04 00:01:12 +0530

    Time: 33 ms (51.27%) | Memory: 45.3 MB (83.47%) - LeetSync

--- Stack/Basics/1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ Stack/Basics/1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,21 +1,28 @@
 class Solution {
+    /**
+     * Using Stack Approach
+     *
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(N)
+     * 
+     * @param s
+     * @return
+     */
     public String removeDuplicates(String s) {
-        Stack<Character> st = new Stack<Character>();
-        for (int i = 0; i < s.length(); i++) {
-            if (st.isEmpty()) {
-                st.push(s.charAt(i));
+        int n = s.length();
+        Stack<Character> st = new Stack<Character>(); // SC: O(N)
+        for (int i = n - 1; i >= 0; i--) {            // TC: O(N)
+            char ch = s.charAt(i);
+            if (!st.isEmpty() && ch == st.peek()) {
+                st.pop();
             } else {
-                if (st.peek() == s.charAt(i)) {
-                    st.pop();
-                } else {
-                    st.push(s.charAt(i));
-                }
+                st.push(ch);
             }
         }
         StringBuilder sb = new StringBuilder();
-        while (!st.isEmpty()) {
+        while (!st.isEmpty()) {                      // TC: O(N)
             sb.append(st.pop());
         }
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
