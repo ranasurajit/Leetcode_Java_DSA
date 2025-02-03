f6b17945b8547b490e8ba266ea417932466bc365
Surajit Rana <surajit1602@gmail.com>
2025-02-03 22:51:49 +0530

    Time: 30 ms (66.22%) | Memory: 55.6 MB (28.82%) - LeetSync

---
+++ Stack/MonotonicStack/937-online-stock-span/online-stock-span.java
@@ -0,0 +1,33 @@
+/**
+ * Using Stack Approach
+ *
+ * TC: O(N)
+ * SC: O(1)
+ */
+class StockSpanner {
+
+    Stack<int[]> st;
+
+    public StockSpanner() {
+        st = new Stack<int[]>();
+    }
+    
+    /**
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int next(int price) {
+        int span = 1;
+        while (!st.isEmpty() && price >= st.peek()[0]) { // TC: O(N)
+            span += st.pop()[1];
+        }
+        st.push(new int[] { price, span });
+        return span;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
