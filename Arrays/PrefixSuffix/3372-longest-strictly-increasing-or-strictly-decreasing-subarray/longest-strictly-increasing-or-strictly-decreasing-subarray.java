ecc8716e8cefe86fcca0c5c106b8a7dc03c20e9a
Surajit Rana <surajit1602@gmail.com>
2025-02-03 10:26:18 +0530

    Time: 1 ms (93.94%) | Memory: 42.9 MB (33.33%) - LeetSync

--- Arrays/PrefixSuffix/3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ Arrays/PrefixSuffix/3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -1,11 +1,40 @@
 class Solution {
     /**
-     * Using Array Pre-processing
+     * Approach II : Without using space
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int longestMonotonicSubarray(int[] nums) {
+        int n = nums.length;
+        int maxInc = 1;
+        int maxDec = 1;
+        int currentInc = 1;
+        int currentDec = 1;
+        for (int i = 1; i < n; i++) { // TC: O(N)
+            if (nums[i] > nums[i - 1]) {
+                currentInc++;
+                currentDec = 1;
+            } else if (nums[i] < nums[i - 1]) {
+                currentDec++;
+                currentInc = 1;
+            } else {
+                currentInc = 1;
+                currentDec = 1;
+            }
+            maxInc = Math.max(maxInc, currentInc);
+            maxDec = Math.max(maxDec, currentDec);
+        }
+        return Math.max(maxInc, maxDec);
+    }
+
+    /**
+     * Approach I : Using Array Pre-processing
      *
      * TC: O(N)
      * SC: O(2 x N) ~ O(N)
      */
-    public int longestMonotonicSubarray(int[] nums) {
+    public int longestMonotonicSubarrayApproachI(int[] nums) {
         int n = nums.length;
         int[] incArr = new int[n]; // SC: O(N)
         int[] decArr = new int[n]; // SC: O(N)
