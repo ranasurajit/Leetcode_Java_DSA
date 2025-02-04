33485628de32e1792dbdc38f06618ae49c38c281
Surajit Rana <surajit1602@gmail.com>
2025-02-04 09:52:10 +0530

    Time: 8 ms (19.56%) | Memory: 44.6 MB (83.98%) - LeetSync

--- BinaryTrees/DFS/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ BinaryTrees/DFS/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -15,10 +15,13 @@ class Solution {
      * SC: O(N)
      */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // Base case
         if (root == null || root == p || root == q) {
             return root;
         }
+        // compute LCA node from left sub-tree
         TreeNode leftLCA = lowestCommonAncestor(root.left, p, q);
+        // compute LCA node from right sub-tree
         TreeNode rightLCA = lowestCommonAncestor(root.right, p, q);
         if (leftLCA == null) {
             return rightLCA;
