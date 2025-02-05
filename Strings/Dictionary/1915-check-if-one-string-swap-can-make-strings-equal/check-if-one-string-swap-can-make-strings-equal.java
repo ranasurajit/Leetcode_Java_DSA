90a265ea482ba92fb535c8979ca610c22a32bf22
Surajit Rana <surajit1602@gmail.com>
2025-02-05 10:31:54 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (76.92%) - LeetSync

--- Strings/Dictionary/1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
+++ Strings/Dictionary/1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
@@ -1,11 +1,39 @@
 class Solution {
     /**
-     * Using Hashing and Two Pointers Approach
+     * Approach 2
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public boolean areAlmostEqual(String s1, String s2) {
+        int n = s1.length();
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        for (int p = 0; p < n; p++) { // TC: O(N)
+            if (s1.charAt(p) != s2.charAt(p)) {
+                count++;
+                if (count > 2) {
+                    return false;
+                } else {
+                    if (count == 1) {
+                        i = p;
+                    } else {
+                        j = p;
+                    }
+                }
+            }
+        }
+        return (s1.charAt(i) == s2.charAt(j)) && (s2.charAt(i) == s1.charAt(j));
+    }
+
+    /**
+     * Approach 1 : Using Hashing and Two Pointers Approach
      *
      * TC: O(2 x N) ~ O(N)
      * SC: O(1)
      */
-    public boolean areAlmostEqual(String s1, String s2) {
+    public boolean areAlmostEqualApproach1(String s1, String s2) {
         int n = s1.length();
         int p = 0; // pointer for String 's1'
         int q = 0; // pointer for String 's2'
