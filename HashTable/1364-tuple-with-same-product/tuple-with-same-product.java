ee0e3898afc439d9be39ff7940c0961e9c7482d8
Surajit Rana <surajit1602@gmail.com>
2025-02-06 10:43:34 +0530

    Time: 219 ms (55.77%) | Memory: 68.6 MB (66.03%) - LeetSync

--- HashTable/1364-tuple-with-same-product/tuple-with-same-product.java
+++ HashTable/1364-tuple-with-same-product/tuple-with-same-product.java
@@ -2,22 +2,26 @@ class Solution {
     /**
      * Using Hashing Approach
      *
-     * TC: O(N ^ 2) ~ O(N ^ 2)
+     * TC: O(N ^ 2 + N) ~ O(N ^ 2)
      * SC: O(N)
      */
     public int tupleSameProduct(int[] nums) {
         int n = nums.length;
         Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N)
-        int pairs = 0;
         for (int i = 0; i < n; i++) { // TC: O(N)
             for (int j = i + 1; j < n; j++) { // TC: O(N)
                 int prod = nums[i] * nums[j];
-                if (map.getOrDefault(prod, 0) > 0) {
-                    pairs += 8 * map.getOrDefault(prod, 0);
-                }
                 map.put(prod, map.getOrDefault(prod, 0) + 1);
             }
         }
-        return pairs;
+        int pairs = 0;
+        for (Integer key : map.keySet()) { // TC: O(N)
+            if (map.get(key) > 1) {
+                int p = map.get(key);
+                pairs += (p * (p - 1)) / 2;
+            }
+        }
+        // for each pair products, 8 tuples can be created
+        return 8 * pairs;
     }
 }
