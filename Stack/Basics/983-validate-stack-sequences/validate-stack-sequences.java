4c10a644e6bc0a63e92d09ba05bf3ae36869b1b9
Surajit Rana <surajit1602@gmail.com>
2025-02-06 21:59:51 +0530

    Time: 2 ms (87.59%) | Memory: 44.4 MB (60.54%) - LeetSync

--- Stack/Basics/983-validate-stack-sequences/validate-stack-sequences.java
+++ Stack/Basics/983-validate-stack-sequences/validate-stack-sequences.java
@@ -1,27 +1,24 @@
 class Solution {
+    /**
+     * TC: O(N)
+     * SC: O(N)
+     * 
+     * @param pushed
+     * @param popped
+     * @return
+     */
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        int p = 0; // pointer for pushed array
-        int q = 0; // pointer for popped array
-        Stack<Integer> st = new Stack<Integer>();
-        while (p < pushed.length && q < popped.length) {
-            if (st.isEmpty()) {
-                st.push(pushed[p]);
-                p++;
-            } else {
-                if (st.peek() == popped[q]) {
-                    st.pop();
-                    q++;
-                } else {
-                    st.push(pushed[p]);
-                    p++;
-                }
-            }
-        }
-        while (q < popped.length) {
-            if (st.peek() == popped[q]) {
+        int n = pushed.length; // pushed and popped lengths are same
+        Stack<Integer> st = new Stack<Integer>(); // SC: O(N)
+        int p = 0; // pointer for array 'pushed'
+        int q = 0; // pointer for array 'popped'
+        while (p < n && q < n) { // TC: O(N)
+            // intially stack is empty
+            st.push(pushed[p++]);
+            while (!st.isEmpty() && q < n && popped[q] == st.peek()) {
                 st.pop();
+                q++;
             }
-            q++;
         }
         return st.isEmpty();
     }
