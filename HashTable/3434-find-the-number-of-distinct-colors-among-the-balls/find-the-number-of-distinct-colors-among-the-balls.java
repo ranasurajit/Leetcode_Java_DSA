0c9e331f0cfe313b135f0dd9fe1d7bb29e5c3223
Surajit Rana <surajit1602@gmail.com>
2025-02-07 10:52:58 +0530

    Time: 37 ms (96.51%) | Memory: 79.7 MB (93.89%) - LeetSync

---
+++ HashTable/3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * Using Hashing Approach
+     *
+     * TC: O(N)
+     * SC: O(2 x N) ~ O(N)
+     */
+    public int[] queryResults(int limit, int[][] queries) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N)
+        Map<Integer, Integer> colorMap = new HashMap<Integer, Integer>(); // SC: O(N)
+        int q = queries.length;
+        int[] result = new int[q];
+        int count = 0;
+        for (int[] query : queries) { // TC: O(Q)
+            if (colorMap.containsKey(query[0])) {
+                int color = colorMap.get(query[0]);
+                int freq = map.get(color);
+                if (freq == 1) {
+                    map.remove(color);
+                } else {
+                    map.put(color, freq - 1);
+                }
+            }
+            map.put(query[1], map.getOrDefault(query[1], 0) + 1);
+            colorMap.put(query[0], query[1]);
+            result[count++] = map.size();
+        }
+        return result;
+    }
+}
