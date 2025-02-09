c9e61ba13d69fc4cc0166b1782fba2115033ab9a
Surajit Rana <surajit1602@gmail.com>
2025-02-09 13:13:18 +0530

    Time: 40 ms (55.00%) | Memory: 58.2 MB (51.47%) - LeetSync

--- HashTable/2448-count-number-of-bad-pairs/count-number-of-bad-pairs.java
+++ HashTable/2448-count-number-of-bad-pairs/count-number-of-bad-pairs.java
@@ -1,11 +1,32 @@
 class Solution {
     /**
+     * Using Optimization Hashing Approach
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public long countBadPairs(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N)
+        long badPairs = 0L;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            // we will deduce (nums[i] - i) in same loop
+            int diff = nums[i] - i;
+            long goodPairs = map.getOrDefault(diff, 0);
+            long totalPairs = i;
+            badPairs += totalPairs - goodPairs;
+            map.put(diff, map.getOrDefault(diff, 0) + 1);
+        }
+        return badPairs;
+    }
+
+    /**
      * Using Hashing Approach
      *
      * TC: O(2 x N) ~ O(N)
      * SC: O(N)
      */
-    public long countBadPairs(int[] nums) {
+    public long countBadPairsHashing(int[] nums) {
         int n = nums.length;
         /**
          * deduce this problem of j - i != nums[j] - nums[i] 
