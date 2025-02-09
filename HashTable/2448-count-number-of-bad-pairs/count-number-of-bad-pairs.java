211720c4f69a0e0832165f2b86f1e9372c2f4365
Surajit Rana <surajit1602@gmail.com>
2025-02-09 13:17:08 +0530

    Time: 34 ms (81.18%) | Memory: 60.5 MB (16.18%) - LeetSync

--- HashTable/2448-count-number-of-bad-pairs/count-number-of-bad-pairs.java
+++ HashTable/2448-count-number-of-bad-pairs/count-number-of-bad-pairs.java
@@ -7,16 +7,70 @@ class Solution {
      */
     public long countBadPairs(int[] nums) {
         int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N)
+        Map<Integer, Long> map = new HashMap<Integer, Long>(); // SC: O(N)
         long badPairs = 0L;
         for (int i = 0; i < n; i++) { // TC: O(N)
             // we will deduce (nums[i] - i) in same loop
             int diff = nums[i] - i;
-            long goodPairs = map.getOrDefault(diff, 0);
-            long totalPairs = i;
-            badPairs += totalPairs - goodPairs;
-            map.put(diff, map.getOrDefault(diff, 0) + 1);
+            long goodPairs = map.getOrDefault(diff, 0L);
+            badPairs += i - goodPairs;
+            map.put(diff, goodPairs + 1);
         }
         return badPairs;
+    }
+
+    /**
+     * Using Hashing Approach
+     *
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(N)
+     */
+    public long countBadPairsHashing(int[] nums) {
+        int n = nums.length;
+        /**
+         * deduce this problem of j - i != nums[j] - nums[i] 
+         * as 
+         * (nums[i] - i) != (nums[j] - j)
+         * so, pre-process this in the same array 'nums'
+         */
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            nums[i] = nums[i] - i;
+        }
+        /**
+         * store the frequencies of each nums[i] to check how many
+         * different numbers found in the left of a particular 
+         * index so that it makes a bad pair
+         */
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N)
+        long badPairsCount = 0L;
+        long badPairs = 0L;
+        long goodPairs = 0L;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            badPairs = i;
+            goodPairs = map.getOrDefault(nums[i], 0);
+            // match count will make good pairs
+            badPairsCount += (badPairs - goodPairs);
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        return badPairsCount;
+    }
+
+    /**
+     * Using Brute-Force Approach
+     *
+     * TC: O(N ^ 2)
+     * SC: O(1)
+     */
+    public long countBadPairsBruteForce(int[] nums) {
+        int n = nums.length;
+        long count = 0L;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            for (int j = i + 1; j < n; j++) { // TC: O(N)
+                if (j - i != nums[j] - nums[i]) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
