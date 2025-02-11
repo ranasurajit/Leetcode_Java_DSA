d1319436887c39040f2007d3402da5b828fe488c
Surajit Rana <surajit1602@gmail.com>
2025-02-11 10:36:21 +0530

    Time: 5 ms (47.23%) | Memory: 44.7 MB (32.20%) - LeetSync

--- Stack/SubStringProblems/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ Stack/SubStringProblems/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,11 +1,33 @@
 class Solution {
     /**
-     * Approch I : Using Stacks
+     * Approch I : Using StringBuilder
+     *
+     * TC: O(M x N)
+     * SC: O(M)
+     */
+    public String removeOccurrences(String s, String part) {
+        int m = s.length();
+        int n = part.length();
+        StringBuilder sb = new StringBuilder(); // SC: O(M)
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            sb.append(s.charAt(i));
+            int startIndex = sb.length() - n;
+            int len = sb.length();
+            if (len >= n && 
+                sb.substring(startIndex, len).toString().equals(part)) { // TC: O(N)
+                sb.setLength(len - n);
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Approch II : Using Stacks
      *
      * TC: O(M x N)
      * SC: O(M + N)
      */
-    public String removeOccurrences(String s, String part) {
+    public String removeOccurrencesApproachII(String s, String part) {
         int m = s.length();
         int n = part.length();
         Stack<Character> st = new Stack<Character>(); // SC: O(M)
@@ -42,27 +64,5 @@ class Solution {
                 st.pop();
             }
         }
-    }
-
-    /**
-     * Approch II : Using StringBuilder
-     *
-     * TC: O(M x N)
-     * SC: O(M)
-     */
-    public String removeOccurrencesApproachII(String s, String part) {
-        int m = s.length();
-        int n = part.length();
-        StringBuilder sb = new StringBuilder(); // SC: O(M)
-        for (int i = 0; i < m; i++) { // TC: O(M)
-            sb.append(s.charAt(i));
-            int startIndex = sb.length() - n;
-            int len = sb.length();
-            if (len >= n && 
-                sb.substring(startIndex, len).toString().equals(part)) { // TC: O(N)
-                sb.setLength(len - n);
-            }
-        }
-        return sb.toString();
     }
 }
