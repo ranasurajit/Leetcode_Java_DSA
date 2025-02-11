ad5739becfe2d94babc674426bd067ec099544f4
Surajit Rana <surajit1602@gmail.com>
2025-02-11 10:37:24 +0530

    Time: 5 ms (47.23%) | Memory: 44.6 MB (32.20%) - LeetSync

--- Strings/Simulation/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ Strings/Simulation/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,6 +1,6 @@
 class Solution {
     /**
-     * Using StringBuilder
+     * Approch I : Using StringBuilder
      *
      * TC: O(M x N)
      * SC: O(M)
@@ -19,5 +19,50 @@ class Solution {
             }
         }
         return sb.toString();
+    }
+
+    /**
+     * Approch II : Using Stacks
+     *
+     * TC: O(M x N)
+     * SC: O(M + N)
+     */
+    public String removeOccurrencesApproachII(String s, String part) {
+        int m = s.length();
+        int n = part.length();
+        Stack<Character> st = new Stack<Character>(); // SC: O(M)
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            st.push(s.charAt(i));
+            if (st.size() >= n) {
+                modifyIfPresent(st, part, n); // TC: O(N), SC: O(N)
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.insert(0, st.pop());
+        }
+        return sb.toString();
+    }
+
+    /**
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(N)
+     */
+    private void modifyIfPresent(Stack<Character> st, String part, int n) {
+        Stack<Character> compareSt = new Stack<Character>(); // SC: O(N)
+        boolean isValid = true;
+        compareSt.addAll(st);
+        for (int i = n - 1; i >= 0; i--) { // TC: O(N)
+            if (part.charAt(i) != compareSt.peek()) {
+                isValid = false;
+                break;
+            }
+            compareSt.pop();
+        }
+        if (isValid) {
+            for (int i = 0; i < n; i++) { // TC: O(N)
+                st.pop();
+            }
+        }
     }
 }
