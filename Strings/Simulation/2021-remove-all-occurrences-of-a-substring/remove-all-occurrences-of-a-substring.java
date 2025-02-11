8539d60533bfca451a8136cfa7746fd4316365aa
Surajit Rana <surajit1602@gmail.com>
2025-02-11 10:11:27 +0530

    Time: 5 ms (47.23%) | Memory: 44.8 MB (19.65%) - LeetSync

--- Strings/Simulation/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ Strings/Simulation/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,13 +1,20 @@
 class Solution {
+    /**
+     * Using StringBuilder
+     *
+     * TC: O(M x N)
+     * SC: O(M)
+     */
     public String removeOccurrences(String s, String part) {
         int m = s.length();
         int n = part.length();
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(); // SC: O(M)
         for (int i = 0; i < m; i++) { // TC: O(M)
             sb.append(s.charAt(i));
             int startIndex = sb.length() - n;
             int len = sb.length();
-            if (len >= n && sb.substring(startIndex, len).toString().equals(part)) {
+            if (len >= n && 
+                sb.substring(startIndex, len).toString().equals(part)) { // TC: O(N)
                 sb.setLength(sb.length() - n);
             }
         }
