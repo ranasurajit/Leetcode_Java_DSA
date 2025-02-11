1e4c809bfc1a2461d4fb3a0207b413b00991e2d4
Surajit Rana <surajit1602@gmail.com>
2025-02-11 23:13:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (10.30%) - LeetSync

--- BinaryTrees/DFS/129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ BinaryTrees/DFS/129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -19,32 +19,43 @@ class Solution {
      *
      * TC: O(N + K)
      * SC: O(N + K)
+     * where K = number of possible paths
+     * 
+     * @param root
+     * @return
      */
     public int sumNumbers(TreeNode root) {
         List<Integer> list = new ArrayList<Integer>(); // SC: O(K)
-        solve(root, 0, list);     // TC: O(N), SC: O(N)
-        int sum = 0;
+        solve(root, 0, list);
+        int sumTree = 0;
         for (Integer it : list) { // TC: O(K)
-            sum += it;
+            sumTree += it;
         }
-        return sum;
+        return sumTree;
     }
 
     /**
-     * Using DFS Approach
-     *
      * TC: O(N)
      * SC: O(N)
+     * 
+     * @param root
+     * @param sum
+     * @param list
      */
     private void solve(TreeNode root, int sum, List<Integer> list) {
         if (root == null) {
             return;
         }
         sum = sum * 10 + root.val;
+        // if node is a leaft node
         if (root.left == null && root.right == null) {
             list.add(sum);
         }
+        // call recursion for left sub-tree
         solve(root.left, sum, list);
+        // call recursion for right sub-tree
         solve(root.right, sum, list);
+        // backtrack
+        sum -= root.val;
     }
 }
