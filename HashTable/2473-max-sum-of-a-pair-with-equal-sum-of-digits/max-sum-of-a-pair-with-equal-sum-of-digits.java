37f9858e9ecaed765bbaa5a97b083739b4667285
Surajit Rana <surajit1602@gmail.com>
2025-02-12 10:52:48 +0530

    Time: 53 ms (35.85%) | Memory: 57.8 MB (68.54%) - LeetSync

---
+++ HashTable/2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -0,0 +1,61 @@
+class Solution {
+    /**
+     * Better Approach: Using HashMap and Max-Heap(PriorityQueue)
+     *
+     * TC: O(2 x N x log(N)) ~ O(N x log(N))
+     * SC: O(N)
+     */
+    public int maximumSum(int[] nums) {
+        int n = nums.length;
+        Map<Integer, PriorityQueue<Integer>> sumMap =
+            new HashMap<Integer, PriorityQueue<Integer>>(); // SC: O(N)
+        for (int i = 0; i < n; i++) {   // TC: O(N)
+            int key = getSumOfDigits(nums[i]);
+            sumMap.computeIfAbsent(key, // TC: O(log(N))
+                k -> new PriorityQueue<Integer>((p, q) -> q - p)).offer(nums[i]);
+        }
+        int maxSum = Integer.MIN_VALUE;
+        for (Integer key : sumMap.keySet()) { // TC: O(N)
+            PriorityQueue pq = sumMap.get(key);
+            if (pq.size() >= 2) {
+                int first = (int) pq.poll();  // TC: O(log(N))
+                int second = (int) pq.poll(); // TC: O(log(N))
+                maxSum = Math.max(maxSum, first + second);
+            }
+        }
+        return maxSum == Integer.MIN_VALUE ? -1 : maxSum;
+    }
+
+    /**
+     * Brute-Force Approach
+     *
+     * TC: O(N ^ 2 + N) ~ O(N ^ 2)
+     * SC: O(N)
+     */
+    public int maximumSumApproachII(int[] nums) {
+        int n = nums.length;
+        int[] numSum = new int[n]; // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            numSum[i] = getSumOfDigits(nums[i]);
+        }
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            for (int j = i + 1; j < n; j++) { // TC: O(N)
+                if (numSum[i] == numSum[j]) {
+                    maxSum = Math.max(maxSum, nums[i] + nums[j]);
+                }
+            }
+        }
+        return maxSum == Integer.MIN_VALUE ? -1 : maxSum;
+    }
+
+    private int getSumOfDigits(int num) {
+        int sum = 0;
+        while (num > 0) {
+            int rem = num % 10;
+            num = num / 10;
+            sum += rem;
+        }
+        return sum;
+    }
+}
