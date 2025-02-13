706a333053621552fcc0e27e5bf609f5015ceccf
Surajit Rana <surajit1602@gmail.com>
2025-02-13 10:09:20 +0530

    Time: 151 ms (45.27%) | Memory: 70.9 MB (17.28%) - LeetSync

---
+++ Heaps/3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * Using Greedy Approach - Min-Heap (PriorityQueue)
+     *
+     * TC: O(4 x N x log(N)) ~ O(N x log(N))
+     * SC: O(N)
+     */
+    public int minOperations(int[] nums, int k) {
+        int n = nums.length;
+        if (n <= 1) {
+            return 0;
+        }
+        // min-heap needed to sort the elements during insertion
+        PriorityQueue<Long> pq = new PriorityQueue<Long>(); // SC: O(N)
+        for (int i = 0; i < n; i++) {             // TC: O(N)
+            pq.offer((long) nums[i]);
+        }
+        int operations = 0;
+        while (pq.size() >= 2 && pq.peek() < k) { // TC: O(N)
+            long x = pq.poll();                    // TC: O(log(N))
+            long y = pq.poll();                    // TC: O(log(N))
+            pq.offer(x * 2 + y);                  // TC: O(log(N))
+            operations++;
+        }
+        return operations;
+    }
+}
