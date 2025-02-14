d799d4d8d57c5aaa9eef15c8750c8aa51c278593
Surajit Rana <surajit1602@gmail.com>
2025-02-14 21:36:39 +0530

    Time: 15 ms (91.34%) | Memory: 69 MB (42.05%) - LeetSync

--- Design_DataStructures/1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
+++ Design_DataStructures/1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
@@ -1,13 +1,59 @@
 class ProductOfNumbers {
 
     private List<Integer> added = null;
-    private int n = 0;
+    private int product = 1;
 
     /**
      * TC: O(1)
      * SC: O(1)
      */
     public ProductOfNumbers() {
+        added = new ArrayList<Integer>();
+        product = 1;
+    }
+    
+    /**
+     * TC: O(1)
+     * SC: O(1)
+     */
+    public void add(int num) {
+        if (num == 0) {
+            // reset the added array to zero
+            added = new ArrayList<Integer>();
+            product = 1;
+            return;
+        }
+        product = product * num;
+        added.add(product);
+    }
+    
+    /**
+     * TC: O(1)
+     * SC: O(1)
+     */
+    public int getProduct(int k) {
+        int n = added.size();
+        if (n < k) {
+            return 0;
+        }
+        int result = added.get(n - 1);
+        if (n == k) {
+            return result;
+        }
+        return result / added.get(n - 1 - k);
+    }
+}
+
+class ProductOfNumbersBruteForce {
+
+    private List<Integer> added = null;
+    private int n = 0;
+
+    /**
+     * TC: O(1)
+     * SC: O(1)
+     */
+    public ProductOfNumbersBruteForce() {
         added = new ArrayList<Integer>();
         n = 0;
     }
