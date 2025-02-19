5eb99500b3a5b04eaa59eed1b73c7365e6b28490
Surajit Rana <surajit1602@gmail.com>
2025-02-19 22:05:59 +0530

    Time: 7 ms (68.56%) | Memory: 41.2 MB (94.08%) - LeetSync

--- Recursion/Backtracking/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ Recursion/Backtracking/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,11 +1,46 @@
 class Solution {
     /**
-     * Using Backtracking Approach
+     * Approach II - Using Backtracking Approach - Optimal Approach
+     *
+     * TC : O(N x 3 x 2 ^ (N - 1)) ~ O(N * 2 ^ N)
+     * SC: O(N)
+     */
+    public String getHappyString(int n, int k) {
+        StringBuilder sb = new StringBuilder();
+        int[] count = { 0 };
+        String[] result = { "" };
+        solveOptimal(n, sb, count, result, k);
+        return result[0];
+    }
+
+    private void solveOptimal(int n, StringBuilder sb, int[] count, 
+        String[] result, int k) {
+        // Base case
+        if (sb.length() == n) {
+            count[0]++;
+            if (count[0] == k) {
+                result[0] = sb.toString();
+            }
+            return;
+        }
+        // explore all possibilities
+        for (char ch = 'a'; ch <= 'c'; ch++) {
+            if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ch) {
+                continue;
+            }
+            sb.append(ch); // do
+            solveOptimal(n, sb, count, result, k); // explore
+            sb.setLength(sb.length() - 1); // undo - backtrack
+        }
+    }
+
+    /**
+     * Approach I - Using Backtracking Approach
      *
      * TC : O(N x 3 x 2 ^ (N - 1)) ~ O(N * 2 ^ N)
      * SC: O(N * 2 ^ N)
      */
-    public String getHappyString(int n, int k) {
+    public String getHappyStringApproachII(int n, int k) {
         StringBuilder sb = new StringBuilder();
         List<String> result = new ArrayList<String>();
         solve(n, sb, result);
