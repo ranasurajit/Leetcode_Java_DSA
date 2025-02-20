7d77aed6228d4aae0457da9284be553745783bc6
Surajit Rana <surajit1602@gmail.com>
2025-02-20 17:38:59 +0530

    Time: 3 ms (36.03%) | Memory: 41.8 MB (40.73%) - LeetSync

--- Recursion/Backtracking/2107-find-unique-binary-string/find-unique-binary-string.java
+++ Recursion/Backtracking/2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,11 +1,37 @@
 class Solution {
     /**
-     * Using Backtracking Approach
+     * Approach II : Using Hashing Approach
+     *
+     * TC: O(N ^ 2 + N) ~ O(N ^ 2)
+     * SC: O(N)
+     */
+    public String findDifferentBinaryString(String[] nums) {
+        int n = nums.length;
+        HashSet<Integer> hs = new HashSet<Integer>(); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            hs.add(Integer.parseInt(nums[i], 2));
+        }
+        String result = "";
+        // we would loop from 0 to n
+        for (int i = 0; i <= n; i++) { // TC: O(N)
+            if (!hs.contains(i)) {
+                result = Integer.toBinaryString(i); // TC: O(N)
+                while (result.length() < n) {
+                    result = "0" + result;
+                }
+                return result;
+            }
+        }
+        return "";
+    }
+
+    /**
+     * Approach I : Using Backtracking Approach
      *
      * TC: O(N x 2 ^ N)
      * SC: O(N)
      */
-    public String findDifferentBinaryString(String[] nums) {
+    public String findDifferentBinaryStringUsingBacktracking(String[] nums) {
         int n = nums.length;
         HashSet<String> hs = new HashSet<String>();
         for (int i = 0; i < n; i++) { // TC: O(N)
