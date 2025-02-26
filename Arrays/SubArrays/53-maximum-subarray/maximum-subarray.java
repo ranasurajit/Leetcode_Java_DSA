34662b70e27d7f4defe50831e2ca12a24ec3bb53
Surajit Rana <surajit1602@gmail.com>
2025-02-26 13:11:24 +0530

    Time: 1 ms (99.53%) | Memory: 57.2 MB (42.67%) - LeetSync

--- Arrays/SubArrays/53-maximum-subarray/maximum-subarray.java
+++ Arrays/SubArrays/53-maximum-subarray/maximum-subarray.java
@@ -1,13 +1,38 @@
 class Solution {
+    /**
+     * Optimal Approach (Kadane's Algorithm)
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
     public int maxSubArray(int[] nums) {
         int n = nums.length;
         int maxSum = Integer.MIN_VALUE;
         int currentSum = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {     // TC: O(N)
             currentSum += nums[i];
             maxSum = Math.max(maxSum, currentSum);
             if (currentSum < 0) {
                 currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+
+    /**
+     * Brute-Force Approach
+     *
+     * TC: O(N ^ 2)
+     * SC: O(1)
+     */
+    public int maxSubArrayBruteForceApproach(int[] nums) {
+        int n = nums.length;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {     // TC: O(N)
+            int currentSum = 0;
+            for (int j = i; j < n; j++) { // TC: O(N)
+                currentSum += nums[j];
+                maxSum = Math.max(maxSum, currentSum);
             }
         }
         return maxSum;
