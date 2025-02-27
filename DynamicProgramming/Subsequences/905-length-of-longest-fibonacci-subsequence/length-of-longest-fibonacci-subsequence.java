18d360b9c89d052f4fc76f9cab38284b2ebb4767
Surajit Rana <surajit1602@gmail.com>
2025-02-27 19:13:58 +0530

    Time: 223 ms (20.41%) | Memory: 45 MB (88.46%) - LeetSync

--- DynamicProgramming/Subsequences/905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
+++ DynamicProgramming/Subsequences/905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
@@ -14,7 +14,7 @@ class Solution {
         int maxLength = 0;
         for (int j = 1; j < n; j++) {
             for (int k = j + 1; k < n; k++) {
-                int length = solve(j, k, arr, indexMap);
+                int length = solveRecursion(j, k, arr, indexMap);
                 if (length >= 3) {
                     maxLength = Math.max(maxLength, length);
                 }
@@ -23,11 +23,12 @@ class Solution {
         return maxLength;
     }
 
-    private int solve(int j, int k, int[] arr, Map<Integer, Integer> indexMap) {
+    private int solveRecursion(int j, int k, int[] arr,
+        Map<Integer, Integer> indexMap) {
         int target = arr[k] - arr[j]; // strictly increasing
         if (indexMap.containsKey(target) && indexMap.get(target) < j) {
             int i = indexMap.get(target);
-            return 1 + solve(i, j, arr, indexMap);
+            return 1 + solveRecursion(i, j, arr, indexMap);
         }
         return 2;
     }
