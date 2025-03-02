080df7cd865679674eb9a76f29c4cb22bb0f8230
Surajit Rana <surajit1602@gmail.com>
2025-03-03 00:23:23 +0530

    Time: 41 ms (57.45%) | Memory: 55.6 MB (13.43%) - LeetSync

---
+++ DynamicProgramming/Strings/516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -0,0 +1,86 @@
+class Solution {
+    /**
+     * Approach II : Using Memoization Approach
+     *
+     * TC: O(N + 2 ^ (2 x N)) ~ O(4 ^ N)
+     * SC: O(3 x N) ~ O(N)
+     *
+     * @param s
+     * @return
+     */
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        String s1 = reverse(s, n);
+        int[][] memo = new int[n + 1][n + 1];
+        for (int[] memoItem : memo) {
+            Arrays.fill(memoItem, -1);
+        }
+        return lcsMemoization(s, s1, n, n, memo);
+    }
+
+    private int lcsMemoization(String s1, String s2, int m, int n, int[][] memo) {
+        // Base Case
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        // Memoization Check
+        if (memo[m][n] != -1) {
+            return memo[m][n];
+        }
+        // Recursive Calls
+        if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
+            return memo[m][n] = 1 + lcsMemoization(s1, s2, m - 1, n - 1, memo);
+        } else {
+            return memo[m][n] =
+                 Math.max(lcsMemoization(s1, s2, m - 1, n, memo), 
+                          lcsMemoization(s1, s2, m, n - 1, memo));
+        }
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(N + 2 ^ (2 x N)) ~ O(4 ^ N)
+     * SC: O(3 x N) ~ O(N)
+     *
+     * @param s
+     * @return
+     */
+    public int longestPalindromeSubseqRecursion(String s) {
+        int n = s.length();
+        String s1 = reverse(s, n);
+        return lcsRecursion(s, s1, n, n);
+    }
+
+    private int lcsRecursion(String s1, String s2, int m, int n) {
+        // Base Case
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        // Recursive Calls
+        if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
+            return 1 + lcsRecursion(s1, s2, m - 1, n - 1);
+        } else {
+            return Math.max(lcsRecursion(s1, s2, m - 1, n), 
+                lcsRecursion(s1, s2, m, n - 1));
+        }
+    }
+
+    /**
+     * TC: O(N / 2) ~ O(N)
+     * SC: O(N)
+     */
+    private String reverse(String s, int n) {
+        int start = 0;
+        int end = n - 1;
+        char[] ch = s.toCharArray();
+        while (start < end) {
+            char temp = ch[end];
+            ch[end] = ch[start];
+            ch[start] = temp;
+            start++;
+            end--;
+        }
+        return String.valueOf(ch);
+    }
+}
