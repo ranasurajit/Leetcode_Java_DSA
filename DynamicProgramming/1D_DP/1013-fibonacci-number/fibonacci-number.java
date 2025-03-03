893bd486d0a7e49abb177048eeddd41eb999530a
Surajit Rana <surajit1602@gmail.com>
2025-03-04 00:47:19 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (79.03%) - LeetSync

--- DynamicProgramming/1D_DP/1013-fibonacci-number/fibonacci-number.java
+++ DynamicProgramming/1D_DP/1013-fibonacci-number/fibonacci-number.java
@@ -1,17 +1,38 @@
 class Solution {
     /**
+     * Approach III : Using Tabulation Approach
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public int fib(int n) {
+        int[] dp = new int[n + 1]; // SC: O(N)
+        // initialization
+        dp[0] = 0;
+        if (n > 0) {
+            dp[1] = 1;
+        }
+        // iterative call
+        for (int i = 2; i < n + 1; i++) { // TC: O(N)
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    /**
      * Approach II : Using Memoization Approach
      *
      * TC: O(N)
      * SC: O(N + N) ~ O(N)
      */
-    public int fib(int n) {
+    public int fibMemoization(int n) {
         int[] memo = new int[n + 1];
         Arrays.fill(memo, -1);
         return solveMemoization(n, memo);
     }
 
     private int solveMemoization(int n, int[] memo) {
+        // Base Case
         if (n == 0 || n == 1) {
             return n;
         }
@@ -34,6 +55,7 @@ class Solution {
     }
 
     private int solveRecursion(int n) {
+        // Base Case
         if (n == 0 || n == 1) {
             return n;
         }
