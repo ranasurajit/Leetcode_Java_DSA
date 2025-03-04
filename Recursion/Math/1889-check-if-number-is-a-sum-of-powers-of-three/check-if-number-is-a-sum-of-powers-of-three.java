a545ccaeb1375d072faa83728de617a5594f70ec
Surajit Rana <surajit1602@gmail.com>
2025-03-04 12:11:16 +0530

    Time: 147 ms (6.98%) | Memory: 55 MB (9.30%) - LeetSync

--- Recursion/Math/1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
+++ Recursion/Math/1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,8 +1,8 @@
 class Solution {
     /**
-     * Approach I : Using Math + Recursion Approach
+     * Approach II : Using Math + Memoization Approach
      * 
-     * TC: O(2 x K + 2 ^ K) ~ O(2 ^ K)
+     * TC: O(3 x K) ~ O(K)
      * SC: O(2 x K) ~ O(K)
      */
     public boolean checkPowersOfThree(int n) {
@@ -14,8 +14,58 @@ class Solution {
         for (int i = 0; i < k; i++) { // TC: O(K)
             pow[i] = (int) Math.pow(3, i);
         }
-        int[] sum = { 0 };
-        return hasSumSubSequence(0, pow, sum, n);
+        int sum = 0;
+        Map<String, Boolean> memo = new HashMap<String, Boolean>(); // SC: O(K)
+        return hasSumSubSequenceMemoization(0, pow, sum, n, memo);
+    }
+
+    /**
+     * TC: O(K)
+     * SC: O(K)
+     * where K is such that log(N) Base 3
+     */
+    private boolean hasSumSubSequenceMemoization(int index, int[] pow, 
+        int sum, int n, Map<String, Boolean> memo) {
+        // base case
+        if (sum > n) { // pruning un-necessary calls
+            return false;
+        }
+        if (index == pow.length) {
+            return sum == n;
+        }
+        String key = index + "-" + sum;
+        // memoization check
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        // include
+        boolean include = 
+            hasSumSubSequenceMemoization(index + 1, pow, sum + pow[index], n, memo);
+        // exclude
+        boolean exclude =
+            hasSumSubSequenceMemoization(index + 1, pow, sum, n, memo);
+        boolean result = include || exclude;
+        memo.put(key, result);
+        return result;
+    }
+
+    /**
+     * Approach I : Using Math + Recursion Approach
+     * 
+     * TC: O(2 x K + 2 ^ K) ~ O(2 ^ K)
+     * SC: O(2 x K) ~ O(K)
+     */
+    public boolean checkPowersOfThreeRecursion(int n) {
+        int k = 0;
+        while ((int) Math.pow(3, k) <= n) { // TC: O(K)
+            k++;
+        }
+        int[] pow = new int[k]; // SC: O(K)
+        for (int i = 0; i < k; i++) { // TC: O(K)
+            pow[i] = (int) Math.pow(3, i);
+        }
+        int sum = 0;
+        return hasSumSubSequenceRecursion(0, pow, sum, n);
     }
 
     /**
@@ -23,16 +73,18 @@ class Solution {
      * SC: O(K)
      * where K is such that log(N) Base 3
      */
-    private boolean hasSumSubSequence(int index, int[] pow, int[] sum, int n) {
+    private boolean hasSumSubSequenceRecursion(int index, int[] pow, int sum, int n) {
+        if (sum > n) { // pruning un-necessary calls
+            return false;
+        }
         if (index == pow.length) {
-            return sum[0] == n;
+            return sum == n;
         }
         // include
-        sum[0] += pow[index];
-        boolean include = hasSumSubSequence(index + 1, pow, sum, n);
+        boolean include = 
+            hasSumSubSequenceRecursion(index + 1, pow, sum + pow[index], n);
         // exclude
-        sum[0] -= pow[index];
-        boolean exclude = hasSumSubSequence(index + 1, pow, sum, n);
+        boolean exclude = hasSumSubSequenceRecursion(index + 1, pow, sum, n);
         return include || exclude;
     }
 }
