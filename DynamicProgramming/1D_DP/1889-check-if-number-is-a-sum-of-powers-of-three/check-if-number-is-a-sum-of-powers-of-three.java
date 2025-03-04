e6e44a3ff3860aea0e837ffc8247dae80fe5affa
Surajit Rana <surajit1602@gmail.com>
2025-03-04 13:46:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (78.49%) - LeetSync

--- DynamicProgramming/1D_DP/1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
+++ DynamicProgramming/1D_DP/1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,11 +1,95 @@
 class Solution {
     /**
+     * Approach V : Using Math + Greedy + Bit Manipulation Approach
+     * 
+     * TC: O(log(N) Base 3)
+     * SC: O(1)
+     */
+    public boolean checkPowersOfThree(int n) {
+        while (n > 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n = n / 3;
+        }
+        return true;
+    }
+
+    /**
+     * Approach IV : Using Math + Space Optimization Approach
+     * 
+     * TC: O(2 x K + K x N) ~ O(K x N)
+     * SC: O(2 x N + K) ~ O(N + K)
+     */
+    public boolean checkPowersOfThreeOptimization(int n) {
+        int k = 0;
+        while ((int) Math.pow(3, k) <= n) { // TC: O(K)
+            k++;
+        }
+        int[] pow = new int[k]; // SC: O(K)
+        for (int i = 0; i < k; i++) { // TC: O(K)
+            pow[i] = (int) Math.pow(3, i);
+        }
+        // Initialization
+        boolean[] current = new boolean[n + 1]; // SC: O(N)
+        boolean[] prev = new boolean[n + 1];    // SC: O(N)
+        current[0] = true;
+        prev[0] = true;
+        // Iterative Calls - replace dp[i] with current and dp[i - 1] with prev
+        for (int i = 1; i < k + 1; i++) { // TC: O(K)
+            for (int j = 0; j < n + 1; j++) { // TC: O(N)
+                current[j] = prev[j];
+                if (pow[i - 1] <= j) {
+                    current[j] = current[j] || prev[j - pow[i - 1]];
+                }
+            }
+            prev = current.clone();
+        }
+        return prev[n];
+    }
+
+    /**
+     * Approach III : Using Math + Tabulation Approach
+     * 
+     * TC: O(2 x K + K x N) ~ O(K x N)
+     * SC: O(K x N + K) ~ O(K x N)
+     */
+    public boolean checkPowersOfThreeTabulation(int n) {
+        int k = 0;
+        while ((int) Math.pow(3, k) <= n) { // TC: O(K)
+            k++;
+        }
+        int[] pow = new int[k]; // SC: O(K)
+        for (int i = 0; i < k; i++) { // TC: O(K)
+            pow[i] = (int) Math.pow(3, i);
+        }
+        // Initialization
+        boolean[][] dp = new boolean[k + 1][n + 1];
+        for (int i = 0; i < n + 1; i++) { // TC: O(N)
+            dp[0][i] = false;
+        }
+        for (int i = 0; i < k + 1; i++) { // TC: O(K)
+            dp[i][0] = true;
+        }
+        // Iterative Calls
+        for (int i = 1; i < k + 1; i++) { // TC: O(K)
+            for (int j = 0; j < n + 1; j++) { // TC: O(N)
+                dp[i][j] = dp[i - 1][j];
+                if (pow[i - 1] <= j) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j - pow[i - 1]];
+                }
+            }
+        }
+        return dp[k][n];
+    }
+
+    /**
      * Approach II : Using Math + Memoization Approach
      * 
      * TC: O(2 x K + K x N) ~ O(K x N)
      * SC: O(K x N + K) ~ O(K x N)
      */
-    public boolean checkPowersOfThree(int n) {
+    public boolean checkPowersOfThreeMemoization(int n) {
         int k = 0;
         while ((int) Math.pow(3, k) <= n) { // TC: O(K)
             k++;
@@ -23,8 +107,8 @@ class Solution {
      * SC: O(K)
      * where K is such that log(N) Base 3
      */
-    private boolean hasSumSubSequenceMemoization(int k, int[] pow, int sum,             
-        HashMap<String, Boolean> memo) {
+    private boolean hasSumSubSequenceMemoization(int k, int[] pow, 
+        int sum, HashMap<String, Boolean> memo) {
         // Base Case
         if (sum < 0) { // pruning un-necessary calls
             return false;
