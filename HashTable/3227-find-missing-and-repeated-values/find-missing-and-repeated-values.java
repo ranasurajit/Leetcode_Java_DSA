01ad420edfe2ea149e40fcd63278a265f285faa2
Surajit Rana <surajit1602@gmail.com>
2025-03-06 12:06:07 +0530

    Time: 1 ms (100.00%) | Memory: 45.6 MB (41.99%) - LeetSync

--- HashTable/3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ HashTable/3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,11 +1,36 @@
 class Solution {
     /**
-     * Using Hashing Approach
+     * Approach II : Using Array as Hashing Approach
      *
      * TC: O(2 x N x N) ~ O(N x N)
      * SC: O(N x N)
      */
     public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid.length;
+        int[] freq = new int[n * n + 1];   // SC: O(N x N)
+        for (int i = 0; i < n; i++) {      // TC: O(N)
+            for (int j = 0; j < n; j++) {  // TC: O(N)
+                freq[grid[i][j]]++;
+            }
+        }
+        int[] result = new int[2];
+        for (int i = 1; i <= n * n; i++) { // TC: O(N x N)
+            if (freq[i] == 2) {
+                result[0] = i;
+            } else if (freq[i] == 0) {
+                result[1] = i;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Approach I : Using Hashing Approach
+     *
+     * TC: O(2 x N x N) ~ O(N x N)
+     * SC: O(N x N)
+     */
+    public int[] findMissingAndRepeatedValuesApproachI(int[][] grid) {
         int n = grid.length;
         Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N x N)
         for (int i = 0; i < n; i++) { // TC: O(N)
@@ -14,7 +39,7 @@ class Solution {
             }
         }
         int[] result = new int[2];
-        for (int i = 1; i <= n * n; i++) { // TC: O(N ^ 2)
+        for (int i = 1; i <= n * n; i++) { // TC: O(N x N)
             if (map.containsKey(i)) {
                 if (map.get(i) == 2) {
                     result[0] = i;
