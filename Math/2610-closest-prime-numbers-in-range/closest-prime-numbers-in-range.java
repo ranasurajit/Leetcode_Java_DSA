e95b3068193d5d2f0783261407005bfdcc4bf4a6
Surajit Rana <surajit1602@gmail.com>
2025-03-07 14:47:48 +0530

    Time: 110 ms (37.20%) | Memory: 56.5 MB (6.40%) - LeetSync

--- Math/2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
+++ Math/2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
@@ -1,9 +1,10 @@
 class Solution {
     /**
-     * Using Sieve of Eratosthenes Approach
+     * Approach II : Using Sieve of Eratosthenes Approach
      *
-     * TC: (R + R x log(log(R)) + R - L) ~ O(2 x R - L + R x log(log(R)))
-     * SC: O(R)
+     * TC: (R + R x log(log(R)) + R - L + K) ~ O(2 x R - L + K + R x log(log(R)))
+     * SC: O(R + K)
+     * where K = number of prime numbers between left and right
      */
     public int[] closestPrimes(int left, int right) {
         // Sieve of Eratosthenes
@@ -24,8 +25,8 @@ class Solution {
          * data of all prime numbers till right
          * Count number of prime numbers in the range [left, right]
          */
-        List<Integer> primeNumbers = new ArrayList<Integer>();
-        for (int i = left; i <= right; i++) {
+        List<Integer> primeNumbers = new ArrayList<Integer>(); // SC: O(K)
+        for (int i = left; i <= right; i++) { // TC: O(R - L)
             if (primes[i] == 1) {
                 primeNumbers.add(i);
             }
@@ -47,5 +48,53 @@ class Solution {
             }
         }
         return new int[] { first, second };
+    }
+
+    /**
+     * Approach I : Using Sieve of Eratosthenes and Two Pointers Approach
+     *
+     * TC: (2 x R + R x log(log(R))) ~ O(R + R x log(log(R)))
+     * SC: O(R)
+     */
+    public int[] closestPrimesApproachI(int left, int right) {
+        int[] primes = new int[right + 1];     // SC: O(R)
+        Arrays.fill(primes, 1);
+        // as 0 and 1 are non-primes
+        primes[0] = 0;
+        primes[1] = 0;
+        for (int i = 2; i * i <= right; i++) { // TC: O(R)
+            if (primes[i] == 1) {
+                for (int j = i * i; j <= right; j += i) { // TC: O(log(log(R)))
+                    primes[j] = 0; // marking the multiples of prime numbers zero
+                }
+            }
+        }
+        /**
+         * at the end of this we have a pre-computed 
+         * data of all prime numbers till right
+         */
+        int[] result = { -1, -1 };
+        int i = left;
+        int j = left;
+        int minDiff = Integer.MAX_VALUE;
+        // Using Two Pointers
+        while (i <= right && j <= right) { // TC: O(R)
+            while (i <= right && primes[i] != 1) {
+                i++;
+            }
+            j = i + 1;
+            while (j <= right && primes[j] != 1) {
+                j++;
+            }
+            int diff = j - i;
+            if (diff < minDiff) {
+                minDiff = diff;
+                result[0] = i;
+                result[1] = j;
+            }
+            i++;
+            j++;
+        }
+        return result[0] < left || result[1] > right ? new int[] { -1, -1 } : result;
     }
 }
