36faeb18ccbe6e8c9936ebed6b097090b96ba93a
Surajit Rana <surajit1602@gmail.com>
2025-03-07 14:41:00 +0530

    Time: 107 ms (40.00%) | Memory: 56.3 MB (10.40%) - LeetSync

--- Math/2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
+++ Math/2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
@@ -1,11 +1,12 @@
 class Solution {
     /**
-     * Using Sieve of Eratosthenes and Two Pointers Approach
+     * Using Sieve of Eratosthenes Approach
      *
      * TC: (R + R x log(log(R)) + R - L) ~ O(2 x R - L + R x log(log(R)))
      * SC: O(R)
      */
     public int[] closestPrimes(int left, int right) {
+        // Sieve of Eratosthenes
         int[] primes = new int[right + 1];  // SC: O(R)
         Arrays.fill(primes, 1);
         // as 0 and 1 are non-primes
@@ -21,29 +22,30 @@ class Solution {
         /**
          * at the end of this we have a pre-computed 
          * data of all prime numbers till right
+         * Count number of prime numbers in the range [left, right]
          */
-        int[] result = { -1, -1 };
-        int i = left;
-        int j = left;
-        int minDiff = Integer.MAX_VALUE;
-        // Using Two Pointers
-        while (i <= right && j <= right) {
-            while (i <= right && primes[i] != 1) {
-                i++;
+        List<Integer> primeNumbers = new ArrayList<Integer>();
+        for (int i = left; i <= right; i++) {
+            if (primes[i] == 1) {
+                primeNumbers.add(i);
             }
-            j = i + 1;
-            while (j <= right && primes[j] != 1) {
-                j++;
-            }
-            int diff = j - i;
-            if (diff < minDiff) {
-                minDiff = diff;
-                result[0] = i;
-                result[1] = j;
-            }
-            i++;
-            j++;
         }
-        return result[0] < left || result[1] > right ? new int[] { -1, -1 } : result;
+        // default return if we have no pair
+        if (primeNumbers.size() < 2) {
+            return new int[] { -1, -1 };
+        }
+        // from here it is guaranted that we have atleast two prime numbers
+        int minDiff = right - left + 1;
+        int first = -1;
+        int second = -1;
+        for (int i = 1; i < primeNumbers.size(); i++) { // TC: O(K)
+            int currentDiff = primeNumbers.get(i) - primeNumbers.get(i - 1);
+            if (currentDiff < minDiff) {
+                minDiff = currentDiff;
+                first = primeNumbers.get(i - 1);
+                second = primeNumbers.get(i);
+            }
+        }
+        return new int[] { first, second };
     }
 }
