8f8e25b8dbfbf2d2e475eff370aeb20b5be09e86
Surajit Rana <surajit1602@gmail.com>
2025-03-08 01:00:58 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (76.40%) - LeetSync

--- DynamicProgramming/1D_DP/747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ DynamicProgramming/1D_DP/747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -3,17 +3,23 @@ class Solution {
      * Using Tabulation
      *
      * TC: O(N)
-     * TC: O(1)
+     * TC: O(N)
      */
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        if (n == 2) {
-            return Math.min(cost[0], cost[1]);
+        if (n == 0) {
+            return 0;
         }
+        if (n == 1) {
+            return cost[0];
+        }
+        int[] dp = new int[n]; // SC: O(N)
+        dp[0] = cost[0];
+        dp[1] = cost[1];
         for (int i = 2; i < n; i++) { // TC: O(N)
-            cost[i] = cost[i] + Math.min(cost[i - 1], cost[i - 2]);
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
         }
-        return Math.min(cost[n - 1], cost[n - 2]);
+        return Math.min(dp[n - 1], dp[n - 2]);
     }
 
     /**
