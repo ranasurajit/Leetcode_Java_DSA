54a8d7580979667695d1316742d67d8d711a132b
Surajit Rana <surajit1602@gmail.com>
2025-03-08 00:23:27 +0530

    Time: 1 ms (39.06%) | Memory: 43.3 MB (48.02%) - LeetSync

---
+++ DynamicProgramming/1D_DP/747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -0,0 +1,64 @@
+class Solution {
+    /**
+     * Using Memoization
+     *
+     * TC: O(N)
+     * TC: O(2 x N)
+     */
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+        return Math.min(solveMemoization(0, cost, n, memo),
+            solveMemoization(1, cost, n, memo));
+    }
+
+    /**
+     * Using Memoization
+     *
+     * TC: O(N)
+     * TC: O(N)
+     */
+    private int solveMemoization(int index, int[] cost, int n, int[] memo) {
+        // Base Case
+        if (index >= n) {
+            return 0;
+        }
+        // Memoization Check
+        if (memo[index] != -1) {
+            return memo[index];
+        }
+        // Recursion Calls
+        int oneStepCost = cost[index] + solveMemoization(index + 1, cost, n, memo);
+        int twoStepCost = cost[index] + solveMemoization(index + 2, cost, n, memo);
+        return memo[index] = Math.min(oneStepCost, twoStepCost);
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(2 x 2 ^ N) ~ O(2 ^ (N + 1))
+     * TC: O(2 x N) ~ O(N)
+     */
+    public int minCostClimbingStairsRecursion(int[] cost) {
+        int n = cost.length;
+        return Math.min(solveRecursion(0, cost, n), solveRecursion(1, cost, n));
+    }
+
+    /**
+     * Using Recursion
+     *
+     * TC: O(2 ^ N)
+     * TC: O(N)
+     */
+    private int solveRecursion(int index, int[] cost, int n) {
+        // Base Case
+        if (index >= n) {
+            return 0;
+        }
+        // Recursion Calls
+        int oneStepCost = cost[index] + solveRecursion(index + 1, cost, n);
+        int twoStepCost = cost[index] + solveRecursion(index + 2, cost, n);
+        return Math.min(oneStepCost, twoStepCost);
+    }
+}
