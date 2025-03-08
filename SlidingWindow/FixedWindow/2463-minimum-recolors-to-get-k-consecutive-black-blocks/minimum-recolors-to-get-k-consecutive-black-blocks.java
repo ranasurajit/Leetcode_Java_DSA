1e3f6baecf7296b502126e9f04e6e2b3b7a70494
Surajit Rana <surajit1602@gmail.com>
2025-03-08 11:16:58 +0530

    Time: 1 ms (92.10%) | Memory: 41.3 MB (94.69%) - LeetSync

--- SlidingWindow/FixedWindow/2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ SlidingWindow/FixedWindow/2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -33,24 +33,4 @@ class Solution {
         // minimum recolors needed to minimum white blocks in continuity of k items
         return minCountWhites;
     }
-
-    /**
-     * Approach I : Using Brute-Force Approach
-     *
-     * TC: O(K x (N - K))
-     * SC: O(1)
-     */
-    public int minimumRecolorsApproachI(String blocks, int k) {
-        int n = blocks.length();
-        int minCountWhites = n;
-        for (int i = 0; i < n - k + 1; i++) { // TC: O(N - K)
-            int countWhites = 0;
-            for (int j = i; j < i + k; j++) { // TC: O(K)
-                countWhites += blocks.charAt(j) == 'W' ? 1 : 0; 
-            }
-            minCountWhites = Math.min(minCountWhites, countWhites);
-        }
-        // minimum recolors needed to minimum white blocks in continuity of k items
-        return minCountWhites;
-    }
 }
