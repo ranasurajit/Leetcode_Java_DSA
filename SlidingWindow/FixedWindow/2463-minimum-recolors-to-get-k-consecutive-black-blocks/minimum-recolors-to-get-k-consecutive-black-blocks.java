8de80ef98e93215fead08eb6fa1d19a672161f9b
Surajit Rana <surajit1602@gmail.com>
2025-03-08 11:14:22 +0530

    Time: 1 ms (92.10%) | Memory: 41.5 MB (58.45%) - LeetSync

--- SlidingWindow/FixedWindow/2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ SlidingWindow/FixedWindow/2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,11 +1,46 @@
 class Solution {
     /**
-     * Approach I : Brute-Force Approach
+     * Approach II : Using Sliding Window (Fixed Size) Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int minimumRecolors(String blocks, int k) {
+        int n = blocks.length();
+        int i = 0; // start pointer of sliding window
+        int j = 0; // end pointer of sliding window
+        int minCountWhites = n;
+        int countWhites = 0;
+        while (j < n) { // TC: O(N)
+            if (blocks.charAt(j) == 'W') {
+                countWhites++;
+            }
+            if (j - i + 1 < k) {
+                // sliding window size is not met
+                j++;
+            } else if (j - i + 1 == k) {
+                // capture the minimum whites encountered
+                minCountWhites = Math.min(minCountWhites, countWhites);
+                // remove the calculation before sliding to next window
+                if (blocks.charAt(i) == 'W') {
+                    countWhites--;
+                }
+                // slide the window maintaining the k size window
+                i++;
+                j++;
+            }
+        }
+        // minimum recolors needed to minimum white blocks in continuity of k items
+        return minCountWhites;
+    }
+
+    /**
+     * Approach I : Using Brute-Force Approach
      *
      * TC: O(K x (N - K))
      * SC: O(1)
      */
-    public int minimumRecolors(String blocks, int k) {
+    public int minimumRecolorsApproachI(String blocks, int k) {
         int n = blocks.length();
         int minCountWhites = n;
         for (int i = 0; i < n - k + 1; i++) { // TC: O(N - K)
