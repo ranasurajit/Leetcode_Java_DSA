95d4b8bd7e606dd77ac463747660dd5ea56651cb
Surajit Rana <surajit1602@gmail.com>
2025-03-09 18:15:09 +0530

    Time: 4 ms (68.16%) | Memory: 56.6 MB (88.55%) - LeetSync

--- SlidingWindow/FixedWindow/3483-alternating-groups-ii/alternating-groups-ii.java
+++ SlidingWindow/FixedWindow/3483-alternating-groups-ii/alternating-groups-ii.java
@@ -1,11 +1,49 @@
 class Solution {
     /**
-     * Approach II : Using Sliding Window (Fixed Size) Approach
+     * Approach III : Using Sliding Window (Fixed Size) Approach (Optimal)
+     *
+     * TC: O(N + K - 1) ~ O(N + K)
+     * SC: O(N + K - 1) ~ O(N + K)
+     */
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int n = colors.length;
+        // imitating the 'colors' array to form like a circular tile system
+        int[] circularColors = new int[n + k - 1]; // SC: O(N + K - 1)
+        for (int i = 0; i < n + k - 1; i++) {      // TC: O(N + K - 1)
+            circularColors[i] = colors[i % n];
+        }
+        int i = 0; // start pointer of the sliding window
+        int j = 1; // end pointer of the sliding window (to check with previous index)
+        int groups = 0;
+        while (j < n + k - 1) { // TC: O(N + K - 1)
+            if (circularColors[j] == circularColors[j - 1]) {
+                i = j;
+                j++;
+                continue;
+            }
+            if (j - i + 1 < k) {
+                // sliding window size is not yet met
+                j++;
+            } else if (j - i + 1 == k) {
+                /**
+                 * here it is guaranted that sliding window has alternative colors
+                 */
+                groups++;
+                // now slide to next window maintaining its size k
+                i++;
+                j++;
+            }
+        }
+        return groups;
+    }
+
+    /**
+     * Approach II : Using Sliding Window (Fixed Size) and ArrayDeque Approach
      *
      * TC: O(N + K - 1) ~ O(N + K)
      * SC: O(N + 2 x K - 1) ~ O(N + K)
      */
-    public int numberOfAlternatingGroups(int[] colors, int k) {
+    public int numberOfAlternatingGroupsApproachII(int[] colors, int k) {
         int n = colors.length;
         // imitating the 'colors' array to form like a circular tile system
         int[] circularColors = new int[n + k - 1]; // SC: O(N + K - 1)
@@ -27,6 +65,10 @@ class Solution {
                 j++;
             } else if (j - i + 1 == k) {
                 if (changes == k - 1) {
+                    /**
+                     * number of changes = k guarantees
+                     * that our sliding window has alternative colors
+                     */
                     groups++;
                 }
                 // remove calculation before moving to next sliding window
