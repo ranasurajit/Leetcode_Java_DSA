69bbfb6774e6a68c4854bdb098a3804503dd5753
Surajit Rana <surajit1602@gmail.com>
2025-03-16 11:29:23 +0530

    Time: 59 ms (42.47%) | Memory: 54.9 MB (94.35%) - LeetSync

--- BinarySearch/BinarySearchOnAnswers/2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.java
+++ BinarySearch/BinarySearchOnAnswers/2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.java
@@ -9,12 +9,14 @@ class Solution {
      */
     public long repairCars(int[] ranks, int cars) {
         int n = ranks.length;
-        long low = 1;
-        long high = 0;
+        long maxTimeNeeded = 0;
         long minTime = 0;
         for (int i = 0; i < n; i++) { // TC: O(N)
-            high = Math.max(high, (long) ranks[i] * cars * cars);
+            maxTimeNeeded = Math.max(maxTimeNeeded, (long) ranks[i] * cars * cars);
         }
+        // Using Binary Search on Answers
+        long low = 1;
+        long high = maxTimeNeeded;
         while (low <= high) { // TC: O(log(K))
             long mid = low + (high - low) / 2;
             if (isRepairPossible(ranks, n, cars, mid)) { // TC: O(N)
