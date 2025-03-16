533e960004731392de5cd4984853c06ec5daad48
Surajit Rana <surajit1602@gmail.com>
2025-03-16 14:26:38 +0530

    Time: 1 ms (99.43%) | Memory: 45.5 MB (32.02%) - LeetSync

--- GreedyAlgorithms/45-jump-game-ii/jump-game-ii.java
+++ GreedyAlgorithms/45-jump-game-ii/jump-game-ii.java
@@ -1,15 +1,23 @@
 class Solution {
+    /**
+     * Approach : Using Greedy Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     * 
+     */
     public int jump(int[] nums) {
+        int n = nums.length;
+        int low = 0;
+        int high = 0;
         int jumps = 0;
-        int left = 0;
-        int right = 0;
-        int farthest = 0;
-        while (right < nums.length - 1) {
-            for (int i = left; i <= right; i++) {
+        while (high < n - 1) { // TC: O(N)
+            int farthest = 0;
+            for (int i = low; i <= high; i++) {
                 farthest = Math.max(farthest, nums[i] + i);
             }
-            left = right + 1;
-            right = farthest;
+            low = high + 1;
+            high = farthest;
             jumps++;
         }
         return jumps;
