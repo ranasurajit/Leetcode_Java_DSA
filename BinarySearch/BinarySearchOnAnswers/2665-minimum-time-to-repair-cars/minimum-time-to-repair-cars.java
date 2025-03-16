16b87deb1bd94b3b75b21ecf1cd3bd503c9dd20f
Surajit Rana <surajit1602@gmail.com>
2025-03-16 11:52:51 +0530

    Time: 58 ms (44.09%) | Memory: 57 MB (9.41%) - LeetSync

--- BinarySearch/BinarySearchOnAnswers/2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.java
+++ BinarySearch/BinarySearchOnAnswers/2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.java
@@ -11,10 +11,13 @@ class Solution {
         int n = ranks.length;
         long minRank = Integer.MAX_VALUE;
         long minTime = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            minRank = Math.min(minRank, ranks[i]);
+        }
         // Using Binary Search on Answers
         long low = 1;
         // as the mechanic with best rank can repair all cars
-        long high = (long) Arrays.stream(ranks).min().getAsInt() * cars * cars;
+        long high = minRank * cars * cars;
         while (low <= high) { // TC: O(log(K))
             long mid = low + (high - low) / 2;
             if (isRepairPossible(ranks, n, cars, mid)) { // TC: O(N)
@@ -22,6 +25,32 @@ class Solution {
                 high = mid - 1; // shrink the size to get best answer
             } else {
                 low = mid + 1;
+            }
+        }
+        return minTime;
+    }
+
+    /**
+     * Approach I : Brute-Force (Using Linear Search on Answers)
+     *
+     * TC: O(N + K x N)
+     * SC: O(1)
+     *
+     * where K = Min(ranks) x cars = 100 * 10^6 (TLE - 47 / 1072 testcases passed) 
+     */
+    public long repairCarsApproachI(int[] ranks, int cars) {
+        int n = ranks.length;
+        long minRank = Integer.MAX_VALUE;
+        long minTime = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            minRank = Math.min(minRank, ranks[i]);
+        }
+        // time max value is minTimeNeeded as the mechanic with best rank can repair all cars
+        long maxTime = minRank * cars * cars;
+        for (long time = 1; time <= maxTime; time++) { // TC: O(K)
+            if (isRepairPossible(ranks, n, cars, time)) { // TC: O(N)
+                minTime = time;
+                break;
             }
         }
         return minTime;
