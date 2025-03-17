070c4906d27f8833aeccc3d9d21c747b9ff080a4
Surajit Rana <surajit1602@gmail.com>
2025-03-17 09:10:42 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (71.91%) - LeetSync

--- HashTable/2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
+++ HashTable/2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -1,11 +1,31 @@
 class Solution {
     /**
+     * Approach II : Using Constant Space Array Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public boolean divideArray(int[] nums) {
+        int n = nums.length;
+        int[] freq = new int[1001]; // SC: O(1001) ~ O(1) (as per constraints 1 <= n <= 500)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            freq[nums[i]]++;
+        }
+        for (int i = 0; i < 1001; i++) { // TC: O(1001) ~ O(1)
+            if (freq[i] % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
      * Approach I : Using HashMap Approach
      *
      * TC: O(2 x N) ~ O(N)
      * SC: O(N)
      */
-    public boolean divideArray(int[] nums) {
+    public boolean divideArrayApproachI(int[] nums) {
         int n = nums.length;
         Map<Integer, Integer> freq = new HashMap<Integer, Integer>(); // SC: O(N)
         for (int i = 0; i < n; i++) {
@@ -19,5 +39,3 @@ class Solution {
         return true;
     }
 }
-
-// int[] freq = new int[1001]; // SC: O(1001) (as per constraints 1 <= n <= 500)
