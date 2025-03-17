3fb1eed88aa76d3c3918e083c8b2356bf5c0bbf0
Surajit Rana <surajit1602@gmail.com>
2025-03-17 09:53:29 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (58.82%) - LeetSync

--- HashTable/2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
+++ HashTable/2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -1,11 +1,37 @@
 class Solution {
     /**
-     * Approach II : Using Constant Space Array Approach
+     * Approach III : Using Bit-Manipulation Approach
      *
      * TC: O(N)
      * SC: O(1)
      */
     public boolean divideArray(int[] nums) {
+        int n = nums.length;
+        int xor1 = 0;
+        int xor2 = 0;
+        /**
+         * In xor1 we do XOR operation for nums[i] + 1
+         * to track edge cases where number appears odd
+         * number of times and do not cancel out
+         *
+         * In xor2 we do XOR operation for nums[i] and 
+         * as pairs needs to be of equal values so XOR
+         * operation would cancel out to zero
+         */
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            xor1 = xor1 ^ (nums[i] + 1);
+            xor2 = xor2 ^ nums[i];
+        }
+        return xor1 == 0 && xor2 == 0;
+    }
+
+    /**
+     * Approach II : Using Constant Space Array Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public boolean divideArrayApproachII(int[] nums) {
         int n = nums.length;
         int[] freq = new int[1001]; // SC: O(1001) ~ O(1) (as per constraints 1 <= n <= 500)
         for (int i = 0; i < n; i++) { // TC: O(N)
