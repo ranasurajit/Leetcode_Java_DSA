a50849a42a496660864af10d11b453a1e33b6ebe
Surajit Rana <surajit1602@gmail.com>
2025-03-17 14:10:05 +0530

    Time: 24 ms (32.54%) | Memory: 51 MB (55.81%) - LeetSync

--- DynamicProgramming/Subsequences/1250-longest-common-subsequence/longest-common-subsequence.java
+++ DynamicProgramming/Subsequences/1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,22 +1,95 @@
 class Solution {
+    /**
+     * Approach II : Using Memoization Approach
+     *
+     * TC: O(2 ^ (M + N))
+     * SC: O(M + N)
+     *
+     * (17 / 47 testcases passed)
+     */
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                int x = 0;
-                int y = 0;
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    //  took 1 character and continued
-                    x = 1 + dp[i - 1][j - 1];
-                } else {
-                    //  did not take any character and continued
-                    y = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-                dp[i][j] = Math.max(x, y);
-            }
+        int[][] memo = new int[m + 1][n + 1];
+        for (int[] memoItem : memo) {
+            Arrays.fill(memoItem, -1);
         }
-        return dp[m][n];
+        return solveMemoization(text1, text2, m, n, memo);
+    }
+
+    /**
+     * Using Memoization
+     *
+     * TC: O(2 ^ (M + N))
+     * SC: O(M + N)
+     */
+    private int solveMemoization(String text1, String text2,
+        int m, int n, int[][] memo) {
+        // Base Case
+        if (m == 0 || n == 0) {
+            // if any String text1 or text2 exhausts we don't find anything common
+            return 0;
+        }
+        // Memoization Check
+        if (memo[m][n] != -1) {
+            return memo[m][n];
+        }
+        // Recursion Calls
+        if (text1.charAt(m - 1) == text2.charAt(n - 1)) {
+            // we got a common character so add 1 and shrink both m and n
+            return memo[m][n] = 
+                1 + solveMemoization(text1, text2, m - 1, n - 1, memo);
+        } else {
+            /**
+             * we did not get common character yet so try out
+             * shrinking anyone and get the maximum out of it
+             */
+            return memo[m][n] = Math.max(
+                solveMemoization(text1, text2, m, n - 1, memo),
+                solveMemoization(text1, text2, m - 1, n, memo)
+            );
+        }
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(2 ^ (M + N))
+     * SC: O(M + N)
+     *
+     * (17 / 47 testcases passed)
+     */
+    public int longestCommonSubsequenceRecursion(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        return solveRecursion(text1, text2, m, n);
+    }
+
+    /**
+     * Using Recursion
+     *
+     * TC: O(2 ^ (M + N))
+     * SC: O(M + N)
+     */
+    private int solveRecursion(String text1, String text2, int m, int n) {
+        // Base Case
+        if (m == 0 || n == 0) {
+            // if any String text1 or text2 exhausts we don't find anything common
+            return 0;
+        }
+        // Recursion Calls
+        if (text1.charAt(m - 1) == text2.charAt(n - 1)) {
+            // we got a common character so add 1 and shrink both m and n
+            return 1 + solveRecursion(text1, text2, m - 1, n - 1);
+        } else {
+            /**
+             * we did not get common character yet so try out
+             * shrinking anyone and get the maximum out of it
+             */
+            return Math.max(
+                solveRecursion(text1, text2, m, n - 1),
+                solveRecursion(text1, text2, m - 1, n)
+            );
+        }
     }
 }
