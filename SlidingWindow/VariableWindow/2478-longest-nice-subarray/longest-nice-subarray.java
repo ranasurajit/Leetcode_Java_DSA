88c99d3ade137dfa4ae1d1b0e0e3484aae60a090
Surajit Rana <surajit1602@gmail.com>
2025-03-18 10:19:29 +0530

    Time: 3 ms (98.64%) | Memory: 56.3 MB (8.45%) - LeetSync

--- SlidingWindow/VariableWindow/2478-longest-nice-subarray/longest-nice-subarray.java
+++ SlidingWindow/VariableWindow/2478-longest-nice-subarray/longest-nice-subarray.java
@@ -1,11 +1,40 @@
 class Solution {
     /**
+     * Approach III : Sliding Window Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     *
+     * Testcases passed (65 / 65) - Runtime 4ms - Beats 28.34%
+     */
+    public int longestNiceSubarray(int[] nums) {
+        int n = nums.length;
+        int maxLength = 1;
+        int i = 0;
+        int j = 0;
+        int bitmask = 0;
+        while (j < n) { // TC: O(N)
+            while ((bitmask & nums[j]) != 0) {
+                // remove the previous calculation from nums[i]
+                bitmask = bitmask ^ nums[i];
+                i++;
+            }
+            maxLength = Math.max(maxLength, j - i + 1);
+            bitmask = (bitmask | nums[j]);
+            j++;
+        }
+        return maxLength;
+    }
+
+    /**
      * Approach II : Better Approach
      *
      * TC: O(N ^ 2)
      * SC: O(1)
+     *
+     * Testcases passed (65 / 65) - Runtime 4ms - Beats 28.34%
      */
-    public int longestNiceSubarray(int[] nums) {
+    public int longestNiceSubarrayApproachII(int[] nums) {
         int n = nums.length;
         int maxLength = 1;
         for (int i = 0; i < n; i++) { // TC: O(N)
@@ -26,6 +55,8 @@ class Solution {
      *
      * TC: O(N ^ 3)
      * SC: O(1)
+     *
+     * Testcases passed (65 / 65) - Runtime 7ms - Beats 15.80%
      */
     public int longestNiceSubarrayApproachI(int[] nums) {
         int n = nums.length;
