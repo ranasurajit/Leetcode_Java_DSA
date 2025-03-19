7466fe241bcfe43124812c5b207e4354976c5332
Surajit Rana <surajit1602@gmail.com>
2025-03-19 10:29:14 +0530

    Time: 7 ms (66.53%) | Memory: 57.2 MB (84.19%) - LeetSync

--- SlidingWindow/FixedWindow/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ SlidingWindow/FixedWindow/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -1,5 +1,29 @@
 class Solution {
     /**
+     * Approach II : Sliding Window (Fixed Size) Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     *
+     * Accepted (689 / 689 testcases passed), Runtime ~ 9ms, Beats 16.02%
+     */
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+        int operations = 0;
+        for (int i = 0; i < n - 2; i++) { // TC: O(N)
+            if (nums[i] == 1) {
+                continue;
+            }
+            nums[i] = 1 - nums[i];
+            nums[i + 1] = 1 - nums[i + 1];
+            nums[i + 2] = 1 - nums[i + 2];
+            operations++;
+        }
+        // check if last two elements are 1 else return -1
+        return nums[n - 2] == 1 && nums[n - 1] == 1 ? operations : -1;
+    }
+
+    /**
      * Approach I : Brute-Force Approach
      *
      * TC: O(6 x N) ~ O(N)
@@ -7,7 +31,7 @@ class Solution {
      *
      * Accepted (689 / 689 testcases passed), Runtime ~ 9ms, Beats 16.02%
      */
-    public int minOperations(int[] nums) {
+    public int minOperationsApproachI(int[] nums) {
         int n = nums.length;
         int operations = 0;
         for (int i = 0; i < n - 2; i++) { // TC: O(N)
