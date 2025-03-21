fc811d7563d234de2e143dc50cb8e0b2d944b471
Surajit Rana <surajit1602@gmail.com>
2025-03-21 21:40:27 +0530

    Time: 65 ms (32.86%) | Memory: 58.8 MB (93.09%) - LeetSync

--- DynamicProgramming/Stocks/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ DynamicProgramming/Stocks/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -3,7 +3,7 @@ class Solution {
      * Approach IV : Using Space Optimization Approach
      *
      * TC: O(6 x N) ~ O(N)
-     * SC: O(6 x N) ~ O(N)
+     * SC: O(2) ~ O(1)
      */
     public int maxProfit(int[] prices) {
         int n = prices.length;
