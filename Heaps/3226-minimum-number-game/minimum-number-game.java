efb33d81105b6b3fe314ce776ffe41e535eb9d64
Surajit Rana <surajit1602@gmail.com>
2025-03-22 01:40:51 +0530

    Time: 3 ms (41.33%) | Memory: 45 MB (58.32%) - LeetSync

--- Heaps/3226-minimum-number-game/minimum-number-game.java
+++ Heaps/3226-minimum-number-game/minimum-number-game.java
@@ -1,11 +1,29 @@
 class Solution {
     /**
-     * Approach : Using PriorityQueue (Min-Heap) Approach
+     * Approach II : In place operation using Array Sorting Approach
+     *
+     * TC: O(N x log(N) + N) ~ O(N x log(N))
+     * SC: O(1)
+     */
+    public int[] numberGame(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums); // TC: O(N x log(N))
+        for (int i = 0; i < n - 1; i += 2) { // TC: O(N)
+            // swap nums at index i and (i + 1)
+            int temp = nums[i + 1];
+            nums[i + 1] = nums[i];
+            nums[i] = temp;
+        }
+        return nums;
+    }
+
+    /**
+     * Approach I : Using PriorityQueue (Min-Heap) Approach
      *
      * TC: O(3 x N x log(N)) ~ O(N x log(N))
      * SC: O(N)
      */
-    public int[] numberGame(int[] nums) {
+    public int[] numberGameApproachI(int[] nums) {
         int n = nums.length;
         int[] result = new int[n];
         PriorityQueue<Integer> pqMin = new PriorityQueue<Integer>((p, q) -> p - q); // SC: O(N)
