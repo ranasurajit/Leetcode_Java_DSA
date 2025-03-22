4115c0557b885427cd7b897916b909a9c36f3a83
Surajit Rana <surajit1602@gmail.com>
2025-03-22 11:37:33 +0530

    Time: 20 ms (21.75%) | Memory: 45.3 MB (56.68%) - LeetSync

--- Graphs/DFS/2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ Graphs/DFS/2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -1,25 +1,89 @@
 class Solution {
+    /**
+     * Approach II : Using DFS Approach
+     *
+     * TC: O(4 x V + 2 x E) ~ O(V + E)
+     * SC: O(2 x V) ~ O(V)
+     */
     public int countCompleteComponents(int n, int[][] edges) {
-        Map<Integer, ArrayList<Integer>> adj = new HashMap<Integer, ArrayList<Integer>>();
-        int[] indegrees = new int[n];
-        int[] componentIndices = new int[n];
-        for (int[] edge : edges) {
+        Map<Integer, ArrayList<Integer>> adj =
+            new HashMap<Integer, ArrayList<Integer>>(); // SC: O(V)
+        for (int[] edge : edges) { // TC: O(V + E)
+            adj.computeIfAbsent(edge[0], k -> new ArrayList<Integer>()).add(edge[1]);
+            adj.computeIfAbsent(edge[1], k -> new ArrayList<Integer>()).add(edge[0]);
+        }
+        boolean[] visited = new boolean[n]; // SC: O(V)
+        int count = 0;
+        for (int i = 0; i < n; i++) { // TC: O(V)
+            if (!visited[i]) {
+                Set<Integer> compSet = new HashSet<Integer>();
+                dfsGraphComponent(i, adj, visited, compSet); // TC: O(E), SC: O(V)
+                if (isCompleteComponent(compSet, adj)) { // TC: O(V), SC: O(1)
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * TC: O(V)
+     * SC: O(1)
+     */
+    private boolean isCompleteComponent(Set<Integer> compSet,
+        Map<Integer, ArrayList<Integer>> adj) {
+        for (Integer node : compSet) {
+            if (adj.getOrDefault(node, new ArrayList<Integer>()).size() 
+                != compSet.size() - 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * TC: O(E)
+     * SC: O(V)
+     */
+    private void dfsGraphComponent(int u, Map<Integer, ArrayList<Integer>> adj,
+        boolean[] visited, Set<Integer> compSet) {
+        visited[u] = true;
+        compSet.add(u);
+        for (Integer v : adj.getOrDefault(u, new ArrayList<Integer>())) {
+            if (!visited[v]) {
+                dfsGraphComponent(v, adj, visited, compSet);
+            }
+        }
+    }
+
+    /**
+     * Approach I : Using Indegrees and DFS Approach
+     *
+     * TC: O(4 x V + 2 x E) ~ O(V + E)
+     * SC: O(5 x V) ~ O(V)
+     */
+    public int countCompleteComponentsApproachI(int n, int[][] edges) {
+        Map<Integer, ArrayList<Integer>> adj =
+            new HashMap<Integer, ArrayList<Integer>>(); // SC: O(V)
+        int[] indegrees = new int[n]; // SC: O(V)
+        int[] componentIndices = new int[n]; // SC: O(V)
+        for (int[] edge : edges) { // TC: O(V + E)
             adj.computeIfAbsent(edge[0], k -> new ArrayList<Integer>()).add(edge[1]);
             adj.computeIfAbsent(edge[1], k -> new ArrayList<Integer>()).add(edge[0]);
             indegrees[edge[1]]++;
             indegrees[edge[0]]++;
         }
-        boolean[] visited = new boolean[n];
+        boolean[] visited = new boolean[n]; // SC: O(V)
         int count = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { // TC: O(V + E)
             if (!visited[i]) {
                 dfsGraph(i, adj, visited, componentIndices, count);
                 count++;
             }
         }
         int incomplete = 0;
-        Map<Integer, List<Integer>> compMap = new HashMap<Integer, List<Integer>>();
-        for (int i = 0; i < n; i++) {
+        Map<Integer, List<Integer>> compMap = new HashMap<Integer, List<Integer>>(); // SC: O(V)
+        for (int i = 0; i < n; i++) { // TC: O(V)
             if (!compMap.containsKey(componentIndices[i])) {
                 compMap.put(componentIndices[i], 
                     Arrays.asList(0, Integer.MAX_VALUE));
@@ -29,8 +93,9 @@ class Solution {
             compMap.get(componentIndices[i]).
                 set(1, Math.min(compMap.get(componentIndices[i]).get(1), indegrees[i]));
         }
-        for (Integer key : compMap.keySet()) {
+        for (Integer key : compMap.keySet()) { // TC: O(V)
             List<Integer> comp = compMap.get(key);
+            // If minimum indegree < number of vertices per component - 1 then it is incomplete
             if (comp.get(1) < comp.get(0) - 1) {
                 incomplete++;
             }
@@ -38,6 +103,10 @@ class Solution {
         return count - incomplete;
     }
 
+    /**
+     * TC: O(E)
+     * SC: O(V)
+     */
     private void dfsGraph(int u, Map<Integer, ArrayList<Integer>> adj,
         boolean[] visited, int[] componentIndices, int count) {
         visited[u] = true;
