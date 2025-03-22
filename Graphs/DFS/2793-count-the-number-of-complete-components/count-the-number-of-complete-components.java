86124312f4f030d308e8edf6b6af2c7b3c76bf53
Surajit Rana <surajit1602@gmail.com>
2025-03-22 13:34:15 +0530

    Time: 8 ms (90.92%) | Memory: 45.7 MB (7.19%) - LeetSync

--- Graphs/DFS/2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ Graphs/DFS/2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -1,11 +1,89 @@
 class Solution {
     /**
+     * Approach V : Using Disjoint Set Union Approach (Optimal Approach)
+     *
+     * TC: O(2 x V + E + E x α(V)) ~ O(E x α(V))
+     * SC: O(3 x V) ~ O(V)
+     *
+     * where α(V) denotes Inverse Ackermann time complexity which is
+     * much smaller than other commonly used time complexity of
+     * O(1), O(log n), and O(n)
+     */
+    public int countCompleteComponents(int n, int[][] edges) {
+        int[] parent = new int[n]; // SC: O(V)
+        for (int i = 0; i < n; i++) { // TC: O(V)
+            parent[i] = i;
+        }
+        int[] vSize = new int[n]; // SC: O(V)
+        Arrays.fill(vSize, 1);
+        // creating connections by DSU and pre-computing size of each component
+        for (int[] edge : edges) { // TC: O(E)
+            unionBySize(edge[0], edge[1], parent, vSize);
+        }
+        Map<Integer, Integer> verEdgeMap = new HashMap<Integer, Integer>(); // SC: O(V)
+        // looping over edges to find number of edges for each disconnected components
+        for (int[] edge : edges) { // TC: O(E)
+            int root = find(edge[0], parent); // TC: O(α(V))
+            verEdgeMap.put(root, verEdgeMap.getOrDefault(root, 0) + 1);
+        }
+        // checking for complete connected components
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) {
+                int eCount = verEdgeMap.getOrDefault(i, 0);
+                int vCount = vSize[i];
+                if (eCount == (vCount * (vCount - 1)) / 2) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Using DSU : Find by Path Compression Approach
+     *
+     * TC: O(V)
+     * SC: O(V)
+     */
+    private int find(int x, int[] parent) {
+        if (x == parent[x]) {
+            return x;
+        }
+        return parent[x] = find(parent[x], parent);
+    }
+
+    /**
+     * Using DSU : Union by Size Approach
+     *
+     * TC: O(2 x V) ~ O(V)
+     * SC: O(1)
+     */
+    private void unionBySize(int x, int y, int[] parent, int[] vSize) {
+        int xParent = find(x, parent); // TC: O(V)
+        int yParent = find(y, parent); // TC: O(V)
+        if (xParent == yParent) {
+            return;
+        }
+        if (vSize[xParent] > vSize[yParent]) {
+            parent[yParent] = xParent;
+            vSize[xParent] += vSize[yParent];
+        } else if (vSize[yParent] > vSize[xParent]) {
+            parent[xParent] = yParent;
+            vSize[yParent] += vSize[xParent];
+        } else {
+            parent[yParent] = xParent;
+            vSize[xParent] += vSize[yParent];
+        }
+    }
+
+    /**
      * Approach IV : Using BFS Approach (Better Approach)
      *
      * TC: O(3 x V + 2 x E) ~ O(V + E)
      * SC: O(2 x V + E) ~ O(V + E)
      */
-    public int countCompleteComponents(int n, int[][] edges) {
+    public int countCompleteComponentsApproachIV(int n, int[][] edges) {
         Map<Integer, ArrayList<Integer>> adj =
             new HashMap<Integer, ArrayList<Integer>>(); // SC: O(V + E)
         for (int[] edge : edges) { // TC: O(V + E)
