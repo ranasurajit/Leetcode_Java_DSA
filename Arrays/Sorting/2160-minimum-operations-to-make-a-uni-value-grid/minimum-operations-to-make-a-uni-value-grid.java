c6503bb354f52e7709433186a82b9f3fea39f109
Surajit Rana <surajit1602@gmail.com>
2025-03-26 13:45:15 +0530

    Time: 37 ms (76.38%) | Memory: 77.8 MB (28.64%) - LeetSync

--- Arrays/Sorting/2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
+++ Arrays/Sorting/2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -5,7 +5,7 @@ class Solution {
      * TC: O((2 + (M x N)) x Log(M x N)) ~ O((M x N) x log(M x N))
      * SC: O(M x N)
      *
-     * Runtime - 36 ms, Beats 86.43%
+     * Runtime - 34 ms, Beats 90.45%
      */
     public int minOperations(int[][] grid, int x) {
         int m = grid.length;
