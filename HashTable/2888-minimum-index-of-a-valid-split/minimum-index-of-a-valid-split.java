d369beb61ab24eb87ab0f33781cea2536a32cc57
Surajit Rana <surajit1602@gmail.com>
2025-03-27 11:48:22 +0530

    Time: 9 ms (98.08%) | Memory: 56.8 MB (78.21%) - LeetSync

--- HashTable/2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
+++ HashTable/2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -1,11 +1,49 @@
 class Solution {
     /**
-     * Approach : Using Hashing Approach
+     * Approach II : Using Boyer-Moore Majority Voting Algorithm (Constant Space)
+     *
+     * TC: O(3 x N) ~ O(N)
+     * SC: O(1) - Constant Space
+     */
+    public int minimumIndex(List<Integer> nums) {
+        int n = nums.size();
+        int dominant = -1;
+        int count = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (count == 0) {
+                dominant = nums.get(i);
+                count = 1;
+            } else if (nums.get(i) == dominant) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        int domFreq = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (nums.get(i) == dominant) {
+                domFreq++;
+            }
+        }
+        int countLeftDominant = 0;
+        int countRightDominant = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            countLeftDominant += nums.get(i) == dominant ? 1 : 0;
+            countRightDominant = domFreq - countLeftDominant;
+            if (countLeftDominant > (i + 1) / 2 && countRightDominant > (n - i - 1) / 2) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Approach I : Using Hashing Approach
      *
      * TC: O(2 x N) ~ O(N)
      * SC: O(N)
      */
-    public int minimumIndex(List<Integer> nums) {
+    public int minimumIndexApproachI(List<Integer> nums) {
         int n = nums.size();
         Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N)
         for (int i = 0; i < n; i++) { // TC: O(N)
