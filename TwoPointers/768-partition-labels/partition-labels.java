73c6c1aa07a014ce23d039079902123c03c99c18
Surajit Rana <surajit1602@gmail.com>
2025-03-30 14:04:29 +0530

    Time: 4 ms (92.83%) | Memory: 42.3 MB (51.26%) - LeetSync

--- TwoPointers/768-partition-labels/partition-labels.java
+++ TwoPointers/768-partition-labels/partition-labels.java
@@ -1,11 +1,39 @@
 class Solution {
     /**
-     * Approach I : Using Two Pointers Approach
+     * Approach II : Using Two Pointers Approach (Cleaner Approach)
      *
      * TC: O(2 x N) ~ O(N)
      * SC: O(26) ~ O(1)
      */
     public List<Integer> partitionLabels(String s) {
+        int n = s.length();
+        List<Integer> partitions = new ArrayList<Integer>();
+        int[] lastOccur = new int[26]; // SC: O(26)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            lastOccur[s.charAt(i) - 'a'] = i;
+        }
+        int i = 0;
+        int start = 0;
+        int end = 0;
+        while (i < n) { // TC: O(N)
+            end = Math.max(end, lastOccur[s.charAt(i) - 'a']);
+            if (i == end) {
+                // we got a partition here
+                partitions.add(end - start + 1);
+                start = end + 1;
+            }
+            i++;
+        }
+        return partitions;
+    }
+
+    /**
+     * Approach I : Using Two Pointers Approach
+     *
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(26) ~ O(1)
+     */
+    public List<Integer> partitionLabelsApproachI(String s) {
         int n = s.length();
         List<Integer> partitions = new ArrayList<Integer>();
         int[] lastOccur = new int[26]; // SC: O(26)
