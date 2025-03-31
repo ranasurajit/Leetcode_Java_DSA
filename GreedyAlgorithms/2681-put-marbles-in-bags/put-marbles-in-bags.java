394f9146bbc99199b26bee3b0baced75d9cb2802
Surajit Rana <surajit1602@gmail.com>
2025-03-31 20:43:41 +0530

    Time: 35 ms (47.76%) | Memory: 61.4 MB (70.90%) - LeetSync

--- GreedyAlgorithms/2681-put-marbles-in-bags/put-marbles-in-bags.java
+++ GreedyAlgorithms/2681-put-marbles-in-bags/put-marbles-in-bags.java
@@ -7,17 +7,17 @@ class Solution {
      */
     public long putMarbles(int[] weights, int k) {
         int n = weights.length;
-        List<Long> pairSum = new ArrayList<Long>(); // SC: O(N)
+        long[] pairSum = new long[n - 1]; // SC: O(N)
         for (int i = 1; i < n; i++) { // TC: O(N)
-            pairSum.add((long) weights[i - 1] + (long) weights[i]);
+            pairSum[i - 1] = (long) weights[i - 1] + (long) weights[i];
         }
-        Collections.sort(pairSum, Comparator.comparingLong(a -> a)); // TC: O(N x Log(N))
+        Arrays.sort(pairSum); // TC: O(N x Log(N))
         // we need to pick the (k - 1) pairs for k bags
         long minimumScores = 0;
         long maximumScores = 0;
         for (int i = 0; i < k - 1; i++) { // TC: O(K)
-            minimumScores += pairSum.get(i);
-            maximumScores += pairSum.get(n - i - 2);
+            minimumScores += pairSum[i];
+            maximumScores += pairSum[n - i - 2];
         }
         return maximumScores - minimumScores;
     }
