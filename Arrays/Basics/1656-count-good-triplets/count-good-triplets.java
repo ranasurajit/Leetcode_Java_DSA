aad6e588d0bcccf00230e90994f8de19e38aa965
Surajit Rana <surajit1602@gmail.com>
2025-04-14 10:53:42 +0530

    Time: 11 ms (61.76%) | Memory: 41.4 MB (59.44%) - LeetSync

--- Arrays/Basics/1656-count-good-triplets/count-good-triplets.java
+++ Arrays/Basics/1656-count-good-triplets/count-good-triplets.java
@@ -1,11 +1,36 @@
 class Solution {
     /**
-     * Approach I : Brute-Force Approach
+     * Approach II : Better Brute-Force Approach
      *
      * TC: O(N ^ 3)
      * SC: O(1)
      */
     public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int n = arr.length;
+        int count = 0;
+        for (int i = 0; i < n - 2; i++) {         // TC: O(N)
+            for (int j = i + 1; j < n - 1; j++) { // TC: O(N)
+                if (Math.abs(arr[i] - arr[j]) <= a) {
+                    for (int k = j + 1; k < n; k++) { // TC: O(N)
+                        int y = Math.abs(arr[j] - arr[k]);
+                        int z = Math.abs(arr[i] - arr[k]);
+                        if (y <= b && z <= c) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Approach I : Brute-Force Approach
+     *
+     * TC: O(N ^ 3)
+     * SC: O(1)
+     */
+    public int countGoodTripletsApproachI(int[] arr, int a, int b, int c) {
         int n = arr.length;
         int count = 0;
         for (int i = 0; i < n - 2; i++) {         // TC: O(N)
