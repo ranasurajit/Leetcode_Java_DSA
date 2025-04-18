287b4d57a8c869eb0263c321551c3c05ab7a2fbf
Surajit Rana <surajit1602@gmail.com>
2025-04-18 17:28:09 +0530

    Time: 3 ms (56.86%) | Memory: 43.6 MB (26.59%) - LeetSync

---
+++ Strings/Simulation/38-count-and-say/count-and-say.java
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * Approach : Using Recursion Approach
+     *
+     * TC: O(N)
+     * SC: O(2 x N) ~ O(N)
+     */
+    public String countAndSay(int n) {
+        // Base Case
+        if (n == 1) {
+            return "1";
+        }
+        // Recursion Calls
+        String say = countAndSay(n - 1);
+        // process Run-length encoding
+        return process(say); // TC: O(N), SC: O(N)
+    }
+
+    /**
+     * Using a StringBuilder
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    private String process(String s) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder(); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            int count = 1;
+            while (i < n - 1 && s.charAt(i) == s.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            sb.append(String.valueOf(count));
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
