baa75e6982da2cac406df7362ef5c1ac4a07f80b
Surajit Rana <surajit1602@gmail.com>
2025-04-20 16:12:06 +0530

    Time: 1 ms (93.98%) | Memory: 42 MB (93.98%) - LeetSync

--- HashTable/797-rabbits-in-forest/rabbits-in-forest.java
+++ HashTable/797-rabbits-in-forest/rabbits-in-forest.java
@@ -1,11 +1,37 @@
 class Solution {
     /**
-     * Approach : Using Hashing Approach
+     * Approach II : Using Hashing Array Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int numRabbits(int[] answers) {
+        int n = answers.length;
+        int minRabbits = 0;
+        int[] count = new int[1001]; // SC: O(1001) ~ O(1) as 0 <= answers[i] < 1000
+        for (int i = 0; i < n; i++) {
+            int countRabbits = answers[i] + 1;
+            count[countRabbits]++;
+            if (count[countRabbits] == countRabbits) {
+                minRabbits += countRabbits;
+                count[countRabbits] = 0;
+            }
+        }
+        for (int i = 1; i < 1001; i++) { // TC: O(1000) ~ O(1)
+            if (count[i] > 0) {
+                minRabbits += i;
+            }
+        }
+        return minRabbits;
+    }
+
+    /**
+     * Approach I : Using Hashing (HashMap) Approach
      *
      * TC: O(2 x N) ~ O(N)
      * SC: O(N)
      */
-    public int numRabbits(int[] answers) {
+    public int numRabbitsApproachI(int[] answers) {
         int n = answers.length;
         HashMap<Integer, Integer> rabbitMap = new HashMap<Integer, Integer>(); // SC: O(N)
         for (int i = 0; i < n; i++) { // TC: O(N)
