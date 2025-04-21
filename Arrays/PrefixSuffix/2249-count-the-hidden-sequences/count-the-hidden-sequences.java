22e6dd6683c53f6374e9112aa541839ec4edc19e
Surajit Rana <surajit1602@gmail.com>
2025-04-21 11:31:29 +0530

    Time: 8 ms (15.87%) | Memory: 61.9 MB (25.40%) - LeetSync

--- Arrays/PrefixSuffix/2249-count-the-hidden-sequences/count-the-hidden-sequences.java
+++ Arrays/PrefixSuffix/2249-count-the-hidden-sequences/count-the-hidden-sequences.java
@@ -1,6 +1,6 @@
 class Solution {
     /**
-     * Approach: Using Array Pre-Processing Approach
+     * Approach II : Using Array Pre-Processing Approach
      *
      * TC: O(N + R)
      * SC: O(N)
@@ -8,6 +8,34 @@ class Solution {
      * where R = Range (upper - lower + 1)
      */
     public int numberOfArrays(int[] differences, int lower, int upper) {
+        int n = differences.length;
+        int hidden = 0;
+        int minValue = 0;
+        int maxValue = 0;
+        for (int i = 1; i <= n; i++) { // TC: O(N)
+            hidden = differences[i - 1] + hidden;
+            minValue = Math.min(minValue, hidden);
+            maxValue = Math.max(maxValue, hidden);
+        }
+        // now storing min and max values in array 'hidden'
+        int countSeq = 0;
+        for (int i = lower; i <= upper; i++) { // TC: O(upper - lower + 1) ~ O(R)
+            if (minValue + i >= lower && maxValue + i <= upper) {
+                countSeq++;
+            }
+        }
+        return countSeq;
+    }
+
+    /**
+     * Approach I : Using Array Pre-Processing Approach
+     *
+     * TC: O(N + R)
+     * SC: O(N)
+     *
+     * where R = Range (upper - lower + 1)
+     */
+    public int numberOfArraysApproachI(int[] differences, int lower, int upper) {
         int n = differences.length;
         int[] hidden = new int[n + 1]; // SC: O(N + 1)
         hidden[0] = 0; // setting offset as 0 by setting hidden[0] = 0
