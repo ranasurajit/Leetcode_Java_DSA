a839d5508fb2f75db839472bc8618e662f14771c
Surajit Rana <surajit1602@gmail.com>
2025-04-24 21:58:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (39.77%) - LeetSync

--- DynamicProgramming/Grids/63-unique-paths-ii/unique-paths-ii.java
+++ DynamicProgramming/Grids/63-unique-paths-ii/unique-paths-ii.java
@@ -1,5 +1,47 @@
 class Solution {
     /**
+     * Approach III : Using Tabulation Approach
+     *
+     * TC: O((M x N) + O(M + N))
+     * SC: O(M x N)
+     *
+     * Time Limit Exceeded (42 / 42 testcases passed)
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        // Initialization
+        int[][] dp = new int[m][n]; // SC: O(M x N)
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                // Base Case
+                if ((i >= 0 || j >= 0) && obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                // Iterative Calls
+                int down = 0;
+                int right = 0;
+                if (i > 0) {
+                    down = dp[i - 1][j];
+                }
+                if (j > 0) {
+                    right = dp[i][j - 1];
+                }
+                dp[i][j] = down + right;
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    /**
      * Approach II : Using Memoization Approach
      *
      * TC: O(M x N)
@@ -7,7 +49,7 @@ class Solution {
      *
      * Time Limit Exceeded (42 / 42 testcases passed)
      */
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    public int uniquePathsWithObstaclesMemoization(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         if (obstacleGrid[0][0] == 1) {
