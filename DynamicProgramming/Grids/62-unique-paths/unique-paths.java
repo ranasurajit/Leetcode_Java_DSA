17d96ea1bc8ebc668a1aa315dfcb64d80604120f
Surajit Rana <surajit1602@gmail.com>
2025-04-24 16:05:36 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (11.43%) - LeetSync

--- DynamicProgramming/Grids/62-unique-paths/unique-paths.java
+++ DynamicProgramming/Grids/62-unique-paths/unique-paths.java
@@ -1,24 +1,48 @@
 class Solution {
     /**
-     * Approach III : Using Tabulation Approach
+     * Approach IV : Using Space Optimization Approach
      *
      * TC: O(M x N)
-     * SC: O(M x N), Memo Space
+     * SC: O(2 x N) ~ O(N)
      */
     public int uniquePaths(int m, int n) {
         // m and n are the states here
         // Initialization
-        int[][] dp = new int[m][n];
+        int[] prev = new int[n]; // SC: O(N)
+        Arrays.fill(prev, 1);
+        int[] current = new int[n]; // SC: O(N)
+        current[0] = 1;
+        // Iterative Calls
+        for (int i = 1; i < m; i++) { // TC: O(M)
+            for (int j = 1; j < n; j++) { // TC: O(N)
+                current[j] = prev[j] + current[j - 1];
+            }
+            prev = current.clone();
+        }
+        // Return total ways at (m, n)
+        return prev[n - 1];
+    }
+
+    /**
+     * Approach III : Using Tabulation Approach
+     *
+     * TC: O(M x N + (M + N)) ~ O(M x N)
+     * SC: O(M x N), Memo Space
+     */
+    public int uniquePathsTabulation(int m, int n) {
+        // m and n are the states here
+        // Initialization
+        int[][] dp = new int[m][n]; // SC: O(M x N)
         dp[0][0] = 1;
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++) { // TC: O(M)
             dp[i][0] = 1;
         }
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++) { // TC: O(N)
             dp[0][j] = 1;
         }
         // Iterative Calls
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int i = 1; i < m; i++) { // TC: O(M)
+            for (int j = 1; j < n; j++) { // TC: O(N)
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
