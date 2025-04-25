0c7bd259d8a51c5d8358f29587f833b07988b3ca
Surajit Rana <surajit1602@gmail.com>
2025-04-25 10:11:27 +0530

    Time: 33 ms (66.06%) | Memory: 63 MB (64.22%) - LeetSync

--- Arrays/PrefixSuffix/2915-count-of-interesting-subarrays/count-of-interesting-subarrays.java
+++ Arrays/PrefixSuffix/2915-count-of-interesting-subarrays/count-of-interesting-subarrays.java
@@ -1,5 +1,31 @@
 class Solution {
     /**
+     * Approach III : Using Hashing Approach
+     *
+     * TC: O(N)
+     * SC: O(N)
+     *
+     * Accepted (617 / 617 testcases passed)
+     */
+    public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
+        int n = nums.size();
+        long countSubarrays = 0;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N)
+        map.put(0, 1);
+        int prefixCount = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            prefixCount += nums.get(i) % modulo == k ? 1 : 0;
+            prefixCount = prefixCount % modulo;
+            int key = (prefixCount - k + modulo) % modulo;
+            if (map.containsKey(key)) {
+                countSubarrays += map.get(key);
+            }
+            map.put(prefixCount, map.getOrDefault(prefixCount, 0) + 1);
+        }
+        return countSubarrays;
+    }
+
+    /**
      * Approach II : Using Array Pre-Processing and Hashing Approach
      *
      * TC: O(2 x N) ~ O(N)
@@ -7,7 +33,7 @@ class Solution {
      *
      * Accepted (617 / 617 testcases passed)
      */
-    public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
+    public long countInterestingSubarraysApproachII(List<Integer> nums, int modulo, int k) {
         int n = nums.size();
         long countSubarrays = 0;
         int[] inteArray = new int[n]; // SC: O(N)
