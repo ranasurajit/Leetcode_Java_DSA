4b910d41e1a57029dd58797af24c70068a34dfeb
Surajit Rana <surajit1602@gmail.com>
2025-04-27 11:48:28 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (83.98%) - LeetSync

--- SlidingWindow/FixedWindow/3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.java
+++ SlidingWindow/FixedWindow/3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.java
@@ -1,11 +1,38 @@
 class Solution {
     /**
-     * Approach I : Using Offset Method
+     * Approach II : Using Sliding Window (Fixed Size) Approach
      *
      * TC: O(N)
      * SC: O(1)
      */
     public int countSubarrays(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        int i = 0; // start pointer of sliding window
+        int j = 0; // end pointer of sliding window
+        while (j < n) { // TC: O(N)
+            if (j - i + 1 < 3) {
+                j++;
+            } else {
+                // window size is 3 here
+                if (2 * (nums[i] + nums[j]) == nums[i + 1]) {
+                    count++;
+                }
+                // maintain the size and move to next window
+                i++;
+                j++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Approach I : Using Index Offset Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int countSubarraysApproachI(int[] nums) {
         int n = nums.length;
         int count = 0;
         for (int i = 2; i < n; i++) { // TC: O(N)
