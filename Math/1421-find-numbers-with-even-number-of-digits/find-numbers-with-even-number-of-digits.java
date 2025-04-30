55d0d5215695e54f5af5d9ab1990341b5c13e880
Surajit Rana <surajit1602@gmail.com>
2025-04-30 08:02:56 +0530

    Time: 1 ms (98.76%) | Memory: 42.8 MB (77.06%) - LeetSync

--- Math/1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ Math/1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -1,11 +1,29 @@
 class Solution {
     /**
+     * Approach : Using Math (Logarithm) Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int findNumbers(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            int digits = 1 + (int) Math.log10(nums[i]);
+            if ((digits & 1) == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
      * Approach : Brute-Force Approach
      *
      * TC: O(K x N)
      * SC: O(1)
      */
-    public int findNumbers(int[] nums) {
+    public int findNumbersBruteForce(int[] nums) {
         int n = nums.length;
         int count = 0;
         for (int i = 0; i < n; i++) { // TC: O(N)
