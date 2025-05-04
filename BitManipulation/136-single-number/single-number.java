a8120d7eed2566490cc0bc187b042048010b5b69
Surajit Rana <surajit1602@gmail.com>
2025-05-04 13:20:58 +0530

    Time: 1 ms (99.91%) | Memory: 46.2 MB (59.92%) - LeetSync

--- BitManipulation/136-single-number/single-number.java
+++ BitManipulation/136-single-number/single-number.java
@@ -1,9 +1,15 @@
 class Solution {
+    /**
+     * Approach : Using Bit-Manipulation Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
     public int singleNumber(int[] nums) {
-        int xor = 0;
-        for (int i = 0; i < nums.length; i++) {
-            xor ^= nums[i];
+        int result = 0;
+        for (int item : nums) { // TC: O(N)
+            result ^= item;
         }
-        return xor;
+        return result;
     }
 }
