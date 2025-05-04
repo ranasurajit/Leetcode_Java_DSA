cb4e0c0115fd0b49df7fab2af22715e026ccaca9
Surajit Rana <surajit1602@gmail.com>
2025-05-04 15:11:01 +0530

    Time: 1 ms (63.32%) | Memory: 44.7 MB (78.83%) - LeetSync

--- Arrays/TwoPointers/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ Arrays/TwoPointers/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,11 +1,32 @@
 class Solution {
     /**
-     * Approach : Using Hashing Approach
+     * Approach II : Using Two Pointers Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int removeDuplicates(int[] nums) {
+        int i = 0; // pointer for unique elements
+        int j = 0; // variable pointer to find unique elements
+        while (j < nums.length) { // TC: O(N)
+            if (nums[i] != nums[j]) {
+                nums[i + 1] = nums[j];
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return i + 1;
+    }
+
+    /**
+     * Approach I : Using Hashing Approach
      *
      * TC: O(N x log(N) + N) ~ O(N x log(N))
      * SC: O(N)
      */
-    public int removeDuplicates(int[] nums) {
+    public int removeDuplicatesApproachI(int[] nums) {
         Set<Integer> set = new TreeSet<Integer>(); // SC: O(N)
         for (int item : nums) { // TC: O(N)
             set.add(item); // TC: O(log(N))
