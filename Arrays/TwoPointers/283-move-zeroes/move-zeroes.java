66a6e23dc0b44a250f184b2c951234e178903970
Surajit Rana <surajit1602@gmail.com>
2025-05-04 17:19:24 +0530

    Time: 2 ms (83.99%) | Memory: 46.4 MB (5.71%) - LeetSync

--- Arrays/TwoPointers/283-move-zeroes/move-zeroes.java
+++ Arrays/TwoPointers/283-move-zeroes/move-zeroes.java
@@ -1,11 +1,36 @@
 class Solution {
     /**
+     * Approach II : Using Two Pointers (One Pass) Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int idx = 0; // pointer at non-zero index
+        int zeroIdx = 0; // pointer at zero index
+        while (idx < n) { // TC: O(N)
+            if (nums[idx] == 0) {
+                idx++;
+            } else {
+                // swap elements at idx and zeroIdx
+                int temp = nums[zeroIdx];
+                nums[zeroIdx] = nums[idx];
+                nums[idx] = temp;
+                // move to next position
+                zeroIdx++;
+                idx++;
+            }
+        }
+    }
+
+    /**
      * Approach I : Using Two Pass Approach
      *
      * TC: O(2 x N) ~ O(N)
-     * SC: O(N)
+     * SC: O(1)
      */
-    public void moveZeroes(int[] nums) {
+    public void moveZeroesApproachI(int[] nums) {
         int n = nums.length;
         int index = 0;
         for (int i = 0; i < n; i++) { // TC: O(N)
