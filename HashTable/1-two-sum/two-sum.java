01eae3804d06a56e1b87e3f1779d01d6ef9d542b
Surajit Rana <surajit1602@gmail.com>
2025-05-06 00:11:20 +0530

    Time: 2 ms (98.83%) | Memory: 45.1 MB (46.81%) - LeetSync

--- HashTable/1-two-sum/two-sum.java
+++ HashTable/1-two-sum/two-sum.java
@@ -1,22 +1,15 @@
 class Solution {
-    /**
-     * Using Hashing Approach
-     *
-     * TC: O(N)
-     * SC: O(N)
-     */
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
-        int[] result = new int[] { -1, -1 };
+        // we will store { nums[i], i } in HashMap
         Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N)
-        for (int i = 0; i < n; i++) { // TC: O(N)
+        for (int i = 0; i < n; i++) {
             int diff = target - nums[i];
             if (map.containsKey(diff)) {
-                result[0] = map.get(diff);
-                result[1] = i;
+                return new int[] { map.get(diff), i };
             }
             map.put(nums[i], i);
         }
-        return result;
+        return new int[] { -1, -1 };
     }
 }
