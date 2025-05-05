b8ce8df438de08321b8e2d75d968ea1df4897ec9
Surajit Rana <surajit1602@gmail.com>
2025-05-06 00:02:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (25.98%) - LeetSync

--- Arrays/2D-Arrays/48-rotate-image/rotate-image.java
+++ Arrays/2D-Arrays/48-rotate-image/rotate-image.java
@@ -1,29 +1,48 @@
 class Solution {
+    /**
+     * Approach: Reverse the Rows of the Matrix and Transform It
+     *
+     * TC: O(N ^ 2 + N) ~ O(N ^ 2)
+     * SC: O(1)
+     */
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        // transpose of matrix
+        // reverse the columns
+        reverseRows(matrix, 0, n - 1); // TC: O(N)
+        // transpose the matrix
+        transpose(matrix, n); // TC: O(N ^ 2)
+    }
+
+    /**
+     * Reverse the Rows of the Matrix
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    private void reverseRows(int[][] matrix, int start, int end) {
+        while (start < end) {
+            // swap the rows
+            int[] temp = matrix[end];
+            matrix[end] = matrix[start];
+            matrix[start] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    /**
+     * Transpose the Matrix
+     *
+     * TC: O((N ^ 2) / 2) ~ O(N ^ 2)
+     * SC: O(1)
+     */
+    private void transpose(int[][] matrix, int n) {
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
-        }
-        // rotate all row arrays
-        for (int[] row : matrix) {
-            reverseArray(row);
-        }
-    }
-
-    private void reverseArray(int[] arr) {
-        int low = 0;
-        int high = arr.length - 1;
-        while (low < high) {
-            int temp = arr[high];
-            arr[high] = arr[low];
-            arr[low] = temp;
-            low++;
-            high--;
         }
     }
 }
