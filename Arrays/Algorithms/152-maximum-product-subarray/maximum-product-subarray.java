791e10216cbac4d11a1c72dc45d20f2b419d3189
Surajit Rana <surajit1602@gmail.com>
2025-05-06 00:41:38 +0530

    Time: 1 ms (90.35%) | Memory: 45 MB (57.30%) - LeetSync

--- Arrays/Algorithms/152-maximum-product-subarray/maximum-product-subarray.java
+++ Arrays/Algorithms/152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,11 +1,38 @@
 class Solution {
     /**
-     * Approach I: Brute-Force Approach
+     * Approach II : Optimal Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int maxProd = Integer.MIN_VALUE;
+        int prefix = 1;
+        int suffix = 1;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (prefix == 0) {
+                // reset prefix to 1 to start of with a new subarray
+                prefix = 1;
+            }
+            if (suffix == 0) {
+                // reset suffix to 1 to start of with a new subarray
+                suffix = 1;
+            }
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[n - i - 1];
+            maxProd = Math.max(maxProd, Math.max(prefix, suffix));
+        }
+        return maxProd;
+    }
+
+    /**
+     * Approach I : Brute-Force Approach
      *
      * TC: O(N ^ 2)
      * SC: O(1)
      */
-    public int maxProduct(int[] nums) {
+    public int maxProductBruteForce(int[] nums) {
         int n = nums.length;
         int maxProd = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) { // TC: O(N)
