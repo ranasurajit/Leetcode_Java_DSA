bd0bb5240ac8d8271b4113d130f944e8a9e59b65
Surajit Rana <surajit1602@gmail.com>
2025-05-05 20:58:57 +0530

    Time: 2 ms (18.50%) | Memory: 45.7 MB (67.13%) - LeetSync

---
+++ Arrays/2D-Arrays/73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * Approach: Using Hashing Approach
+     *
+     * TC: O(2 x M x N) ~ O(M x N)
+     * SC: O(1)
+     */
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        Set<Integer> rowset = new HashSet<Integer>();
+        Set<Integer> colset = new HashSet<Integer>();
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                if (matrix[i][j] == 0) {
+                    rowset.add(i);
+                    colset.add(j);
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                if (rowset.contains(i) || colset.contains(j)) {
+                    matrix[i][j] = 0;
+                } 
+            }
+        }
+    }
+}
