774978e59ea5176542cd9c80bf9d0fc242975f80
Surajit Rana <surajit1602@gmail.com>
2025-05-07 00:37:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (52.91%) - LeetSync

--- Recursion/795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
+++ Recursion/795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -1,12 +1,18 @@
 class Solution {
+    private double[] power;
+
     /**
      * Approach : Using Recursion Approach
      *
-     * TC: O(N)
-     * SC: O(N)
+     * TC: O(N x log(N) + N) ~ O(N x log(N))
+     * SC: O(2 x N) ~ O(N)
      */
     public int kthGrammar(int n, int k) {
-        return solveRecursion(n, k);
+        power = new double[n + 1]; // SC: O(N)
+        for (int i = 0; i < n + 1; i++) { // TC: O(N)
+            power[i] = fastPow(2.0, i); // TC: O(log(N))
+        }
+        return solveRecursion(n, k); // TC: O(N), SC: O(N)
     }
 
     /**
@@ -18,13 +24,35 @@ class Solution {
         if (n == 1 && k == 1) {
             return 0;
         }
-        // Hypothesis + Induction
-        int size = (int) Math.pow(2, (n - 1));
+        // Hypothesis
+        // at a level n, size of k = 2 ^ (n - 1)
+        int size = (int) power[n - 1];
         int mid = size / 2;
+        // Induction
         if (k <= mid) {
             return solveRecursion(n - 1, k);
         } else {
             return solveRecursion(n - 1, k - mid) == 0 ? 1 : 0;
         }
+    }
+
+    /**
+     * TC: O(log(N))
+     * SC: O(log(N))
+     */
+    private double fastPow(double x, int n) {
+        if (n == 0 || x == 1) {
+            return 1;
+        }
+        if (n == 1) {
+            return x;
+        }
+        double pow = fastPow(x , n / 2);
+        double ans = pow * pow;
+        if ((n & 1) == 1) {
+            // odd exponent
+            ans = ans * x;
+        }
+        return ans;
     }
 }
