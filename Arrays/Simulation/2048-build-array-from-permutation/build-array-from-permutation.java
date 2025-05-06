eea1d7f253fc0222d39d09d24f0d08d47c18c6a1
Surajit Rana <surajit1602@gmail.com>
2025-05-06 09:56:21 +0530

    Time: 1 ms (98.72%) | Memory: 45.9 MB (7.75%) - LeetSync

--- Arrays/Simulation/2048-build-array-from-permutation/build-array-from-permutation.java
+++ Arrays/Simulation/2048-build-array-from-permutation/build-array-from-permutation.java
@@ -1,11 +1,26 @@
 class Solution {
     /**
+     * Approach I : Using Simulation
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            result[i] = nums[nums[i]];
+        }
+        return result;
+    }
+
+    /**
      * Approach II : Using In Place Operation
      *
      * TC: O(2 x N) ~ O(N)
      * SC: O(1)
      */
-    public int[] buildArray(int[] nums) {
+    public int[] buildArrayInPlace(int[] nums) {
         int n = nums.length;
         for (int i = 0; i < n; i++) { // TC: O(N)
             nums[i] += 1000 * (nums[nums[i]] % 1000);
@@ -14,20 +29,5 @@ class Solution {
             nums[i] = nums[i] / 1000;
         }
         return nums;
-    }
-
-    /**
-     * Approach I : Using Simulation
-     *
-     * TC: O(N)
-     * SC: O(1)
-     */
-    public int[] buildArrayApproachI(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-        for (int i = 0; i < n; i++) { // TC: O(N)
-            result[i] = nums[nums[i]];
-        }
-        return result;
     }
 }
