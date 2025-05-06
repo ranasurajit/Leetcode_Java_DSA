b8e73a37ec5ba661fbbbcf59dc24bb6a1798b39e
Surajit Rana <surajit1602@gmail.com>
2025-05-06 09:54:21 +0530

    Time: 2 ms (7.30%) | Memory: 45.4 MB (63.62%) - LeetSync

--- Arrays/Simulation/2048-build-array-from-permutation/build-array-from-permutation.java
+++ Arrays/Simulation/2048-build-array-from-permutation/build-array-from-permutation.java
@@ -1,11 +1,22 @@
 class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            nums[i] += 1000 * (nums[nums[i]] % 1000);
+        }
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            nums[i] = nums[i] / 1000;
+        }
+        return nums;
+    }
+
     /**
-     * Approach : Using Simulation
+     * Approach I : Using Simulation
      *
      * TC: O(N)
      * SC: O(1)
      */
-    public int[] buildArray(int[] nums) {
+    public int[] buildArrayApproachI(int[] nums) {
         int n = nums.length;
         int[] result = new int[n];
         for (int i = 0; i < n; i++) { // TC: O(N)
