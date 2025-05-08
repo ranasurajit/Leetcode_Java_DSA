88e832715e5ee54ea07f6487bf36d94f42fe2109
Surajit Rana <surajit1602@gmail.com>
2025-05-08 11:41:21 +0530

    Time: 194 ms (81.50%) | Memory: 108.6 MB (73.13%) - LeetSync

---
+++ Graphs/DijkstraAlgorithm/3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
@@ -0,0 +1,51 @@
+class Solution {
+    private static final int[][] directions = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+    /**
+     * Approach : Using Dijkstra's Algorithm Approach
+     *
+     * TC: O(2 x N x M x log(N x M)) ~ O(N x M x log(N x M))
+     * SC: O(2 x N x M) ~ O(N x M)
+     */
+    public int minTimeToReach(int[][] moveTime) {
+        int n = moveTime.length;
+        int m = moveTime[0].length;
+        int[][] minTime = new int[n][m]; // SC: O(N x M)
+        for (int[] t : minTime) {
+            Arrays.fill(t, Integer.MAX_VALUE);
+        }
+        // we will store { time, row, column, moveCount } in Min-Heap
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p, q) -> p[0] - q[0]); // SC: O(N x M)
+        pq.offer(new int[] { 0, 0, 0, 0 });
+        minTime[0][0] = 0;
+        while (!pq.isEmpty()) { // TC: O(N x M)
+            int[] current = pq.poll(); // TC: O(log(N x M))
+            int time = current[0];
+            int row = current[1];
+            int col = current[2];
+            int moveCount = current[3];
+            if (row == n - 1 && col == m - 1) {
+                return minTime[n - 1][m - 1];
+            }
+            for (int[] dir : directions) {
+                int effRow = row + dir[0];
+                int effCol = col + dir[1];
+                if (effRow >= 0 && effRow < n && effCol >= 0 && effCol < m) {
+                    int edgeTime = (moveCount & 1) == 0 ? 1 : 2;
+                    int waitTime = Math.max(0, moveTime[effRow][effCol] - time);
+                    int finalTime = time + waitTime + edgeTime;
+                    if (finalTime < minTime[effRow][effCol]) {
+                        minTime[effRow][effCol] = finalTime;
+                        pq.offer(new int[] { 
+                            finalTime, 
+                            effRow,
+                            effCol,
+                            moveCount + 1
+                        }); // TC: O(log(N x M))
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
