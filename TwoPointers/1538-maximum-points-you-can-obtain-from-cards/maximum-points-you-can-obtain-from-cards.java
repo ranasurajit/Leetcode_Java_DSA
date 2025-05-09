6dc523c214063ff49b6627a926ccacb466bf1b48
Surajit Rana <surajit1602@gmail.com>
2025-05-09 20:21:23 +0530

    Time: 3 ms (33.88%) | Memory: 55.2 MB (41.08%) - LeetSync

---
+++ TwoPointers/1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * Approach : Using Two Pointers Approach
+     *
+     * TC: O(2 x K) ~ O(K)
+     * SC: O(1)
+     */
+    public int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length;
+        int max = 0;
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int i = 0; i < k; i++) { // TC: O(K)
+            leftSum += cardPoints[i];
+        }
+        max = Math.max(max, leftSum);
+        int p = k - 1; // left pointer
+        int q = n - 1; // right pointer
+        while (p >= 0 && q >= 0 && k > 0) { // TC: O(K)
+            leftSum -= cardPoints[p];
+            rightSum += cardPoints[q];
+            p--;
+            q--;
+            k--;
+            max = Math.max(max, (leftSum + rightSum));
+        }
+        return max;
+    }
+}
