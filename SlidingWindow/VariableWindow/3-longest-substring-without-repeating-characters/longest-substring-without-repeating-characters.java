7905e22b12d73160b06e57c1064e1f3bdab9114f
Surajit Rana <surajit1602@gmail.com>
2025-05-09 22:49:47 +0530

    Time: 6 ms (70.65%) | Memory: 44.6 MB (67.77%) - LeetSync

--- SlidingWindow/VariableWindow/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ SlidingWindow/VariableWindow/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,43 +1,35 @@
 class Solution {
     /**
-     * Took two pointers i and j = 0
-     * 
-     * Window size: (j - i + 1)
-     * 
-     * when HashMap size < Window size, j++
-     * when HashMap size = Window size,
-     * compare and store the maximum window size (result), j++
-     * till HashMap size > Window size, then start removing elements from index 'i'
-     * and update its count/remove key from HashMap, j++
-     * 
+     * Approach : Using Sliding Window (Variable Length) Approach
+     *
      * TC: O(N)
      * SC: O(N)
-     *
-     * @param s
-     * @return
      */
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
-        int i = 0; // pointer at the start of sliding window
-        int j = 0; // pointer at the end of sliding window
-        Map<Character, Integer> unique = new HashMap<Character, Integer>(); // SC: O(N)
-        int maxLength = Integer.MIN_VALUE;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>(); // SC: O(N)
+        int i = 0; // start pointer of sliding window
+        int j = 0; // end pointer of sliding window
+        int maxLength = 0;
         while (j < n) { // TC: O(N)
-            unique.put(s.charAt(j), unique.getOrDefault(s.charAt(j), 0) + 1);
-            while (unique.size() != j - i + 1) {
-                // remove the character frequency from index i
-                unique.put(s.charAt(i), unique.getOrDefault(s.charAt(i), 0) - 1);
-                if (unique.get(s.charAt(i)) == 0) {
-                    // remove the key from Map if the frequency becomes zero after decreasing frequency
-                    unique.remove(s.charAt(i));
+            map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
+            while (j - i + 1 > map.size()) {
+                // remove computation from ith index
+                int freq = map.get(s.charAt(i));
+                if (freq == 1) {
+                    map.remove(s.charAt(i));
+                } else {
+                    map.put(s.charAt(i), freq - 1);
                 }
+                // shrink the window
                 i++;
             }
-            if (unique.size() == j - i + 1) {
+            if (j - i + 1 == map.size()) {
+                // we got the desired window size
                 maxLength = Math.max(maxLength, j - i + 1);
             }
             j++;
         }
-        return maxLength == Integer.MIN_VALUE ? 0 : maxLength;
+        return maxLength;
     }
 }
