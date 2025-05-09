3ed2defc2620c5ab4a054a56472a303727bef6c4
Surajit Rana <surajit1602@gmail.com>
2025-05-10 03:02:32 +0530

    Time: 46 ms (38.30%) | Memory: 58.7 MB (46.66%) - LeetSync

---
+++ SlidingWindow/FixedWindow/2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * Approach : Using Sliding Window (Fixed Length) Approach
+     *
+     * TC: O(N)
+     * SC: O(K)
+     */
+    public long maximumSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int i = 0; // start pointer of sliding window
+        int j = 0; // end pointer of sliding window
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(K)
+        long maxSum = (long) Integer.MIN_VALUE;
+        long sum = 0L;
+        while (j < n) { // TC: O(N)
+            sum += nums[j];
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+            if (j - i + 1 < k) {
+                j++;
+            } else if (j - i + 1 == k) {
+                if (map.size() == k) {
+                    maxSum = Math.max(maxSum, sum);
+                }
+                // remove computation from index 'i' to slide to next window
+                sum -= nums[i];
+                int freq = map.get(nums[i]);
+                if (freq == 1) {
+                    map.remove(nums[i]);
+                } else {
+                    map.put(nums[i], map.get(nums[i]) - 1);
+                }
+                // slide to next window
+                i++;
+                j++;
+            }
+        }
+        return maxSum == (long) Integer.MIN_VALUE ? 0L : maxSum;
+    }
+}
