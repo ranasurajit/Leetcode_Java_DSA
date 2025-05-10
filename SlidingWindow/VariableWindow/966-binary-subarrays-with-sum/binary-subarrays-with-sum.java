286fea442340f130d667f69900deb4fbf2bdc75f
Surajit Rana <surajit1602@gmail.com>
2025-05-10 22:16:41 +0530

    Time: 1 ms (100.00%) | Memory: 47.2 MB (98.17%) - LeetSync

---
+++ SlidingWindow/VariableWindow/966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -0,0 +1,85 @@
+class Solution {
+    /**
+     * Approach I : Using Sliding Window (Variable Size) Approach
+     * 
+     * We can find countSubArraysWithSum(<= goal) - We can find countSubArraysWithSum(<= goal - 1)
+     *
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(1)
+     * 
+     * @param nums
+     * @param goal
+     * @return
+     */
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int n = nums.length;
+        return countSubArraysWithSumLessThanEqualsK(nums, n, goal) -
+            countSubArraysWithSumLessThanEqualsK(nums, n, goal - 1);
+    }
+
+
+    /**
+     * Using Sliding Window (Variable Size) Approach
+     * 
+     * TC: O(N)
+     * SC: O(1)
+     * 
+     * @param nums
+     * @param n
+     * @param k
+     * @return
+     */
+    private int countSubArraysWithSumLessThanEqualsK(int[] nums, int n, int k) {
+        // as we are looking for sum > 0 so we return 0 if goal < 0
+        if (k < 0) {
+            return 0;
+        }
+        int i = 0; // start pointer of sliding window
+        int j = 0; // end pointer of sliding window
+        int sum = 0;
+        int count = 0;
+        while (j < n) { // TC: O(N)
+            sum += nums[j];
+            while (sum > k) {
+                // remove the computation from index 'i'
+                sum -= nums[i];
+                i++;
+            }
+            count += (j - i + 1);
+            j++;
+        }
+        return count;
+    }
+
+    /**
+     * Approach I : Using Hashing Approach
+     * 
+     * Problem is similar to count sub-arrays with sum = k (goal)
+     * 
+     * TC: O(N)
+     * SC: O(N)
+     * 
+     * @param nums
+     * @param goal
+     * @return
+     */
+    public int numSubarraysWithSumApproachI(int[] nums, int goal) {
+        int n = nums.length;
+        // storing { prefixSum , frequency } in HashMap
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N)
+        map.put(0, 1);
+        int prefixSum = 0;
+        int count = 0;
+        int i = 0;
+        while (i < n) { // TC: O(N)
+            prefixSum += nums[i];
+            int reqSum = prefixSum - goal;
+            if (map.containsKey(reqSum)) {
+                count += map.get(reqSum);
+            }
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+            i++;
+        }
+        return count;
+    }
+}
