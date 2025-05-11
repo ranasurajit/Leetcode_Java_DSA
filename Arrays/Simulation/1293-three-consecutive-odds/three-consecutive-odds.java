4005933729ef193bfc56bcd7be2f432cba15482d
Surajit Rana <surajit1602@gmail.com>
2025-05-11 11:49:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (43.42%) - LeetSync

--- Arrays/Simulation/1293-three-consecutive-odds/three-consecutive-odds.java
+++ Arrays/Simulation/1293-three-consecutive-odds/three-consecutive-odds.java
@@ -1,11 +1,33 @@
 class Solution {
     /**
-     * Approach : Using Simulation
+     * Approach II : Using Simulation + Math
+     * Intuition: Product of odd numbers is always odd
      *
      * TC: O(N)
      * SC: O(1)
      */
     public boolean threeConsecutiveOdds(int[] arr) {
+        int n = arr.length;
+        if (n < 3) {
+            return false;
+        }
+        int product = 1;
+        for (int i = 0; i < n - 2; i++) { // TC: O(N)
+            product = arr[i] * arr[i + 1] * arr[i + 2];
+            if ((product & 1) == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Approach I : Using Simulation
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public boolean threeConsecutiveOddsSimulation(int[] arr) {
         int n = arr.length;
         if (n < 3) {
             return false;
