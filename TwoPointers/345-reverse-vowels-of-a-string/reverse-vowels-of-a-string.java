d751ae68dba399ce40a6d20f85fe59b5c9eaab7b
Surajit Rana <surajit1602@gmail.com>
2025-05-12 23:16:20 +0530

    Time: 2 ms (98.56%) | Memory: 45.1 MB (59.35%) - LeetSync

--- TwoPointers/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ TwoPointers/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,13 +1,15 @@
 class Solution {
     /**
+     * Approach : Two Pointers Approach
+     *
      * TC: O(N)
-     * SC: O(N)
+     * SC: O(1)
      */
     public String reverseVowels(String s) {
-        int n = s.length();
-        int p = 0; // pointer at the beginning of String 's'
-        int q = n - 1; // pointer at the end of String 's'
-        char[] chars = s.toCharArray(); // SC: O(N)
+        char[] chars = s.toCharArray();
+        int n = chars.length;
+        int p = 0;
+        int q = n - 1;
         while (p < q) { // TC: O(N)
             while (p < n && !isVowel(chars[p])) {
                 p++;
@@ -15,20 +17,25 @@ class Solution {
             while (q > 0 && !isVowel(chars[q])) {
                 q--;
             }
+            // at this point both the pointers, point to vowels
+            // swap
             if (p < q) {
-                // swap the chars between index 'p' and 'q'
                 char temp = chars[q];
                 chars[q] = chars[p];
                 chars[p] = temp;
-                p++;
-                q--;
             }
+            p++;
+            q--;
         }
         return String.valueOf(chars);
     }
 
-    private boolean isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-            c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    /**
+     * TC: O(1)
+     * SC: O(1)
+     */
+    private boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
     }
 }
