9f5c65183f5cc5baf0868bfaeee787cb79e07f7b
Surajit Rana <surajit1602@gmail.com>
2025-05-13 00:41:28 +0530

    Time: 3 ms (98.66%) | Memory: 42.7 MB (94.34%) - LeetSync

--- TwoPointers/151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ TwoPointers/151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,21 +1,29 @@
 class Solution {
+    /**
+     * Approach : Using Two Pointers Approach
+     * 
+     * TC: O(N)
+     * SC: O(N)
+     * 
+     * @param s
+     * @return
+     */
     public String reverseWords(String s) {
-        char[] ch = s.toCharArray();
+        char[] ch = s.toCharArray(); // SC: O(N)
         int n = ch.length;
+        // reverse the entire string
         reverse(ch, 0, n - 1);
-        int i = 0;
+        int i = 0; // pointer to iterate the entire String
+        // pointers to swap the words
         int left = 0;
         int right = 0;
-        while (i < n) {
+        while (i < n) { // TC: O(N)
             while (i < n && ch[i] != ' ') {
-                ch[right] = ch[i];
-                i++;
-                right++;
+                ch[right++] = ch[i++];
             }
-            // at this point i points to a space
-            // reverse string between left and right
+            // at this point 'i' and 'right' pointers points to a space
+            // reverse word from [left...right]
             if (left < right) {
-                // reverse
                 reverse(ch, left, right - 1);
                 if (right < n) {
                     ch[right] = ' ';
@@ -28,13 +36,21 @@ class Solution {
         return String.valueOf(ch).substring(0, right - 1);
     }
 
-    private void reverse(char[] ch, int p, int q) {
-        while (p < q) {
-            char temp = ch[q];
-            ch[q] = ch[p];
-            ch[p] = temp;
-            p++;
-            q--;
+    /**
+     * TC: O(N / 2) ~ O(N)
+     * SC: O(1)
+     * 
+     * @param ch
+     * @param a
+     * @param b
+     */
+    private void reverse(char[] ch, int a, int b) {
+        while (a < b) { // TC: O(N / 2)
+            char temp = ch[b];
+            ch[b] = ch[a];
+            ch[a] = temp;
+            a++;
+            b--;
         }
     }
 }
