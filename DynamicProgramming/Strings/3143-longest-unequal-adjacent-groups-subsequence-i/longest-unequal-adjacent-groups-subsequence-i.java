f9032b2c037c9edad44b060dce570ab2b87398c3
Surajit Rana <surajit1602@gmail.com>
2025-05-15 11:17:19 +0530

    Time: 6 ms (1.42%) | Memory: 45.6 MB (5.26%) - LeetSync

---
+++ DynamicProgramming/Strings/3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.java
@@ -0,0 +1,94 @@
+class Solution {
+    /**
+     * Approach II : Using Memoization Approach
+     *
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(2 x N) ~ O(N)
+     *
+     * Accepted (382 / 382 testcases passed)
+     */
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        int n = words.length;
+        Map<Integer, Map<Integer, List<Integer>>> memo = 
+            new HashMap<Integer, Map<Integer, List<Integer>>>(); // SC: O(N)
+        List<Integer> indices = solveMemoization(0, -1, n, groups, memo); // TC: O(N), SC: O(N)
+        List<String> result = new ArrayList<String>();
+        for (Integer it : indices) { // TC: O(N)
+            result.add(words[it]);
+        }
+        return result;
+    }
+
+    /**
+     * Using Memoization Approach
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    private List<Integer> solveMemoization(int idx, int prevGroup, int n, int[] groups,
+        Map<Integer, Map<Integer, List<Integer>>> memo) {
+        // Base Case
+        if (idx == n) {
+            return new ArrayList<Integer>();
+        }
+        // Memoization Check
+        if (memo.containsKey(idx) && memo.get(idx).containsKey(prevGroup)) {
+            return memo.get(idx).get(prevGroup);
+        }
+        // Recursion Calls
+        // we can take or not take
+        // not take
+        List<Integer> skip = solveMemoization(idx + 1, prevGroup, n, groups, memo);
+        List<Integer> take = new ArrayList<Integer>();
+        if (prevGroup == -1 || prevGroup != groups[idx]) {
+            // take
+            take.add(idx);
+            take.addAll(solveMemoization(idx + 1, groups[idx], n, groups, memo));
+        }
+        List<Integer> result = take.size() > skip.size() ? take : skip;
+        memo.computeIfAbsent(idx, k -> new HashMap<Integer, List<Integer>>()).put(prevGroup, result);
+        return result; 
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(2 ^ N + N) ~ O(2 ^ N)
+     * SC: O(N)
+     *
+     * Time Limit Exceeded (278 / 382 testcases passed)
+     */
+    public List<String> getLongestSubsequenceRecursion(String[] words, int[] groups) {
+        int n = words.length;
+        List<Integer> indices = solveRecursion(0, -1, n, groups); // TC: O(2 ^ N), SC: O(N)
+        List<String> result = new ArrayList<String>();
+        for (Integer it : indices) { // TC: O(N)
+            result.add(words[it]);
+        }
+        return result;
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC: O(2 ^ N)
+     * SC: O(N)
+     */
+    private List<Integer> solveRecursion(int idx, int prevGroup, int n, int[] groups) {
+        // Base Case
+        if (idx == n) {
+            return new ArrayList<Integer>();
+        }
+        // Recursion Calls
+        // we can take or not take
+        // not take
+        List<Integer> skip = solveRecursion(idx + 1, prevGroup, n, groups);
+        List<Integer> take = new ArrayList<Integer>();
+        if (prevGroup == -1 || prevGroup != groups[idx]) {
+            // take
+            take.add(idx);
+            take.addAll(solveRecursion(idx + 1, groups[idx], n, groups));
+        }
+        return take.size() > skip.size() ? take : skip;
+    }
+}
