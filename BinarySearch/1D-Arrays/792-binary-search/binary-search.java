3dcc5ff0b888e80bf4b9ffc2ba7988f5de241685
Surajit Rana <surajit1602@gmail.com>
2025-05-18 21:37:17 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (97.86%) - LeetSync

--- BinarySearch/1D-Arrays/792-binary-search/binary-search.java
+++ BinarySearch/1D-Arrays/792-binary-search/binary-search.java
@@ -1,23 +1,23 @@
 class Solution {
     /**
-     * TC: O(log(N) base 2)
+     * Approach : Using Binary Search
+     *
+     * TC: O(log(N))
      * SC: O(1)
-     * 
-     * @param nums
-     * @param target
-     * @return
      */
     public int search(int[] nums, int target) {
         int n = nums.length;
         int low = 0;
         int high = n - 1;
-        while (low <= high) {                // TC: O(log(N) base 2)
+        while (low <= high) { // TC: O(log(N))
             int mid = low + (high - low) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
+                // search space is after mid index
                 low = mid + 1;
             } else {
+                // search space is before mid index
                 high = mid - 1;
             }
         }
