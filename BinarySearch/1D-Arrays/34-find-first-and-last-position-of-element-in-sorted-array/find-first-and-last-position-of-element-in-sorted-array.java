505a2adf8b36120a6348d601205fa236ee865a79
Surajit Rana <surajit1602@gmail.com>
2025-05-18 23:49:59 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (67.29%) - LeetSync

--- BinarySearch/1D-Arrays/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ BinarySearch/1D-Arrays/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,42 +1,36 @@
 class Solution {
     /**
+     * Approach : Using Binary Search Approach
+     *
      * TC: O(2 x log(N)) ~ O(log(N))
      * SC: O(1)
-     * 
-     * @param nums
-     * @param target
-     * @return
      */
     public int[] searchRange(int[] nums, int target) {
-        if (nums.length == 0) {
+        int n = nums.length;
+        if (n == 0) {
             return new int[] { -1, -1 };
         }
-        int lBound = lowerBound(nums, target);
-        if (lBound == -1) {
+        int lBound = lowerBound(nums, n, target); // TC: O(log(N))
+        if (lBound == n || nums[lBound] != target) {
             return new int[] { -1, -1 };
         }
-        int rBound = upperBound(nums, target);
+        int uBound = upperBound(nums, n, target); // TC: O(log(N))
         return new int[] {
             lBound,
-            rBound - 1
+            uBound - 1
         };
     }
 
     /**
+     * Using Binary Search Approach
+     *
      * TC: O(log(N))
      * SC: O(1)
-     * 
-     * Lower Bound => nums[i] >= target
-     * 
-     * @param nums
-     * @param target
-     * @return
      */
-    private int lowerBound(int[] nums, int target) {
-        int n = nums.length;
+    int lowerBound(int[] nums, int n, int target) {
         int low = 0;
         int high = n - 1;
-        while (low <= high) {
+        while (low <= high) { // TC: O(log(N))
             int mid = low + (high - low) / 2;
             if (nums[mid] >= target) {
                 high = mid - 1;
@@ -44,27 +38,19 @@ class Solution {
                 low = mid + 1;
             }
         }
-        if (low >= n || nums[low] != target) {
-            return -1;
-        }
         return low;
     }
 
     /**
+     * Using Binary Search Approach
+     *
      * TC: O(log(N))
      * SC: O(1)
-     * 
-     * Upper Bound => nums[i] > target
-     * 
-     * @param nums
-     * @param target
-     * @return
      */
-    private int upperBound(int[] nums, int target) {
-        int n = nums.length;
+    int upperBound(int[] nums, int n, int target) {
         int low = 0;
         int high = n - 1;
-        while (low <= high) {
+        while (low <= high) { // TC: O(log(N))
             int mid = low + (high - low) / 2;
             if (nums[mid] > target) {
                 high = mid - 1;
@@ -73,76 +59,5 @@ class Solution {
             }
         }
         return low;
-    }
-
-    /**
-     * TC: O(2 x log(N)) ~ O(log(N))
-     * SC: O(1)
-     * 
-     * @param nums
-     * @param target
-     * @return
-     */
-    public int[] searchRangeWithoutBound(int[] nums, int target) {
-        return new int[] {
-            searchMinRange(nums, target),
-            searchMaxRange(nums, target)
-        };
-    }
-
-    /**
-     * TC: O(log(N))
-     * SC: O(1)
-     * 
-     * @param nums
-     * @param target
-     * @return
-     */
-    private int searchMinRange(int[] nums, int target) {
-        int n = nums.length;
-        int low = 0;
-        int high = n - 1;
-        int minRange = -1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] > target) {
-                high = mid - 1;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                // nums[mid] = target
-                minRange = mid;
-                high = mid - 1;
-            }
-        }
-        return minRange;
-    }
-
-    /**
-     * TC: O(log(N))
-     * SC: O(1)
-     * 
-     * @param nums
-     * @param target
-     * @return
-     */
-    private int searchMaxRange(int[] nums, int target) {
-        int n = nums.length;
-        int low = 0;
-        int high = n - 1;
-        int maxRange = -1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] > target) {
-                high = mid - 1;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                // nums[mid] = target
-                maxRange = mid;
-                low = mid + 1;
-            }
-        }
-        return maxRange;
     }
 }
