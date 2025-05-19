f33d680afcf1a418ce0553fbdf54c4a9b1efa4a9
Surajit Rana <surajit1602@gmail.com>
2025-05-19 23:33:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (86.87%) - LeetSync

--- BinarySearch/1D-Arrays/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ BinarySearch/1D-Arrays/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,22 +1,44 @@
 class Solution {
     /**
-     * Using Binary Search
-     *
+     * Approach : Using Binary Search Approach
+     * 
      * TC: O(log(N))
      * SC: O(1)
      */
     public int findMin(int[] nums) {
         int n = nums.length;
+        int minIndex = getMinIndex(nums, n); // TC: O(log(N))
+        return nums[minIndex];
+    }
+
+    /**
+     * Using Binary Search Approach
+     * 
+     * TC: O(log(N))
+     * SC: O(1)
+     */
+    private int getMinIndex(int[] nums, int n) {
         int low = 0;
         int high = n - 1;
-        while (low < high) { // TC: O(log(N))
+        while (low <= high) { // TC: O(log(N))
+            if (nums[low] <= nums[high]) {
+                // array 'nums' is sorted and 0 times rotated
+                return low;
+            }
             int mid = low + (high - low) / 2;
-            if (nums[mid] > nums[high]) {
+            int prev = (mid - 1 + n) % n;
+            int next = (mid + 1) % n;
+            if (nums[mid] <= nums[prev] && nums[mid] <= nums[next]) {
+                // minimum value found
+                return mid;
+            } else if (nums[mid] >= nums[low]) {
+                // left part is sorted so, minimum value lies in right portion
                 low = mid + 1;
-            } else {
-                high = mid;
+            } else if (nums[mid] <= nums[high]) {
+                // right part is sorted so, minimum value lies in left portion
+                high = mid - 1;
             }
         }
-        return nums[low];
+        return 0;
     }
 }
