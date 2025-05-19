62eb5fa0f809af7c3bc8b2de72feba4dd85fa10a
Surajit Rana <surajit1602@gmail.com>
2025-05-20 01:07:52 +0530

    Time: 0 ms (100.00%) | Memory: 50.6 MB (20.70%) - LeetSync

--- BinarySearch/1D-Arrays/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ BinarySearch/1D-Arrays/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,40 +1,46 @@
 class Solution {
-
     /**
+     * Approach : Using Binary Search Approach
+     * 
      * TC: O(log(N))
      * SC: O(1)
      */
     public int singleNonDuplicate(int[] nums) {
         int n = nums.length;
+        // handle array of size 1
         if (n == 1) {
             return nums[0];
         }
+        // handle extreme ends
         if (nums[0] != nums[1]) {
             return nums[0];
         }
-        if (nums[n - 2] != nums[n - 1]) {
+        if (nums[n - 1] != nums[n - 2]) {
             return nums[n - 1];
         }
-        int low = 2;
+        // Apply Binary Search on the range nums [1...(n - 2)]
+        int low = 1;
         int high = n - 2;
-        while (low <= high) {
+        while (low <= high) { // TC: O(log(N))
             int mid = low + (high - low) / 2;
             if (nums[mid] == nums[mid - 1]) {
-                mid = mid - 1;
+                mid--;
             }
-            /**
-                * Non-repeating element would be found in even index only
-                * any duplicate elements starts from even index unless single
-                * element breaks this. After single element, the repeating 
-                * element starts from odd index
-                */
-            if (mid % 2 == 0) {
-                if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+            if ((mid & 1) == 0) {
+                /**
+                 * when mid is pointing to even index there are two possibilities
+                 * 1. It is pointing to start of duplicate number
+                 * 2. It is pointing to the single element
+                 */
+                if (nums[mid] != nums[mid + 1] && nums[mid] > nums[mid - 1]) {
+                    // this is our single element
                     return nums[mid];
                 } else {
+                    // as (mid + 1) will point to the duplicate element
                     low = mid + 2;
                 }
             } else {
+                // mid is pointing to odd index so our single element lies in the left side
                 high = mid - 1;
             }
         }
