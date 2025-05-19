f7753b01ce5c8cda6d836651a0e96b863828d481
Surajit Rana <surajit1602@gmail.com>
2025-05-19 23:51:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (38.65%) - LeetSync

--- BinarySearch/1D-Arrays/81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ BinarySearch/1D-Arrays/81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,28 +1,38 @@
 class Solution {
+    /**
+     * Approach : Using Binary Search Approach
+     * 
+     * TC: O(log(N))
+     * SC: O(1)
+     */
     public boolean search(int[] nums, int target) {
         int n = nums.length;
         int low = 0;
         int high = n - 1;
-        while (low <= high) {
-            while (low < high && nums[low] == nums[low + 1]) {
+        while (low <= high) { // TC: O(log(N))
+            // eliminating duplicates at left
+            while (low < n - 1 && nums[low] == nums[low + 1]) {
                 low++;
             }
-            while (low < high && nums[high] == nums[high - 1]) {
+            // eliminating duplicates at right
+            while (high > 0 && nums[high] == nums[high - 1]) {
                 high--;
             }
             int mid = low + (high - low) / 2;
             if (nums[mid] == target) {
                 return true;
-            } else if (nums[low] <= nums[mid]) {
-                // left side is sorted
+            } else if (nums[mid] >= nums[low]) {
+                // left part is sorted
                 if (target >= nums[low] && target <= nums[mid]) {
+                    // target lies in left
                     high = mid - 1;
                 } else {
                     low = mid + 1;
                 }
             } else if (nums[mid] <= nums[high]) {
-                // right side is sorted
+                // right part is sorted
                 if (target >= nums[mid] && target <= nums[high]) {
+                    // target lies in right
                     low = mid + 1;
                 } else {
                     high = mid - 1;
