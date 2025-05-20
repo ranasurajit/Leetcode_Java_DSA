dade13d29c6bfea9e66d6253275945fb42de4f43
Surajit Rana <surajit1602@gmail.com>
2025-05-20 23:51:06 +0530

    Time: 5 ms (99.52%) | Memory: 46.1 MB (57.73%) - LeetSync

--- BinarySearch/2D-Arrays/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ BinarySearch/2D-Arrays/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,11 +1,43 @@
 class Solution {
     /**
-     * Approach : Using Binary Search On Rows Approach
+     * Approach II : Using Binary Search Approach (Eliminate Rows and Columns)
+     *
+     * TC: O(M + N)
+     * SC: O(1)
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int row = 0;
+        int col = n - 1;
+        /**
+         * we point the starting point at (0, n - 1) as 
+         * row-wise the value decreases from right to left and
+         * increases from top to bottom so, it will be easy to
+         * eliminate either column (reduce column index) and 
+         * row (increase row index)
+         */
+        while (row < m && col >= 0) { // TC: O(M + N)
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                // target cannot be present in the current column
+                col--;
+            } else if (matrix[row][col] < target) {
+                // target cannot be present in the current row
+                row++;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Approach I : Using Binary Search On Rows Approach
      *
      * TC: O(M x log(N))
      * SC: O(1)
      */
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public boolean searchMatrixBetter(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
         for (int i = 0; i < m; i++) { // TC: O(M)
