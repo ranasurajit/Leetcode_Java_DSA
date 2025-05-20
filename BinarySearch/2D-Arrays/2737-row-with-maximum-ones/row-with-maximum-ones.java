c31674316d504ab59dc3d6d623ed388c11f5ead1
Surajit Rana <surajit1602@gmail.com>
2025-05-20 21:43:48 +0530

    Time: 4 ms (76.68%) | Memory: 46.6 MB (70.08%) - LeetSync

---
+++ BinarySearch/2D-Arrays/2737-row-with-maximum-ones/row-with-maximum-ones.java
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * Appraoch : Using Simulation Approach
+     *
+     * TC: O(M x N)
+     * SC: O(1)
+     */
+    public int[] rowAndMaximumOnes(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int max1s = 0;
+        int rowIndex = 0;
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            int count1s = 0;
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                if (mat[i][j] == 1) {
+                    count1s++;
+                }
+            }
+            if (max1s < count1s) {
+                max1s = count1s;
+                rowIndex = i;
+            }
+        }
+        return new int[] { rowIndex, max1s };
+    }
+}
