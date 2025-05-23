6edc860e919fa73d6cd1215b64f048e1a4ce9e77
Surajit Rana <surajit1602@gmail.com>
2025-05-23 21:39:59 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (44.32%) - LeetSync

--- BinarySearch/BinarySearchOnAnswers/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ BinarySearch/BinarySearchOnAnswers/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,11 +1,75 @@
 class Solution {
     /**
-     * Approach : Using Brute-Force (Two Pointers) Approach
+     * Approach II : Using Better (Two Pointers) Approach
      *
      * TC: O(M + N)
      * SC: O(M + N)
      */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m1 = nums1.length;
+        int m2 = nums2.length;
+
+        int n = m1 + m2;
+        int second = n / 2;
+        int first = second - 1;
+        int median1 = 0;
+        int median2 = 0;
+
+        int p = 0; // pointer at the start of array 'nums1'
+        int q = 0; // pointer at the start of array 'nums2'
+        int r = 0; // count of the resultant array index
+
+        while (p < m1 && q < m2) { // TC: O(M + N)
+            if (nums1[p] < nums2[q]) {
+                if (r == first) {
+                    median1 = nums1[p];
+                }
+                if (r == second) {
+                    median2 = nums1[p];
+                }
+                r++;
+                p++;
+            } else {
+                if (r == first) {
+                    median1 = nums2[q];
+                }
+                if (r == second) {
+                    median2 = nums2[q];
+                }
+                r++;
+                q++;
+            }
+        }
+        while (p < m1) {
+            if (r == first) {
+                median1 = nums1[p];
+            }
+            if (r == second) {
+                median2 = nums1[p];
+            }
+            r++;
+            p++;
+        }
+        while (q < m2) {
+            if (r == first) {
+                median1 = nums2[q];
+            }
+            if (r == second) {
+                median2 = nums2[q];
+            }
+            r++;
+            q++;
+        }
+        return ((m1 + m2) & 1) == 0 ? (double) (median1 + median2) / 2.0 : (double) median2;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Two Pointers) Approach
+     *
+     * TC: O(M + N)
+     * SC: O(M + N)
+     */
+    public double findMedianSortedArraysApproachI(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
 
