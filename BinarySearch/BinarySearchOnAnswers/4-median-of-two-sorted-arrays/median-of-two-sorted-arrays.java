b46d2933c2712dcbe082af27cd306dbad740b2ac
Surajit Rana <surajit1602@gmail.com>
2025-05-24 01:07:05 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (11.76%) - LeetSync

--- BinarySearch/BinarySearchOnAnswers/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ BinarySearch/BinarySearchOnAnswers/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,11 +1,54 @@
 class Solution {
     /**
+     * Approach III : Using Binary Search Approach
+     *
+     * TC: O(log(Min(N1, N2)))
+     * SC: O(1)
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        // ensuring that nums1 is always the smaller size array than nums2
+        if (n1 > n2) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int low = 0;   // min count of elements from nums1
+        int high = n1; // max count of elements from nums1
+        int left = (n1 + n2 + 1) / 2;
+        while (low <= high) { // TC: O(log(Min(N1, N2)))
+            int mid1 = low + (high - low) / 2;
+            // finding all the range values
+            int mid2 = left - mid1;
+            int l1 = (mid1 > 0) ? nums1[mid1 - 1] : Integer.MIN_VALUE;
+            int l2 = (mid2 > 0) ? nums2[mid2 - 1] : Integer.MIN_VALUE;
+            int r1 = (mid1 < n1) ? nums1[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2 < n2) ? nums2[mid2] : Integer.MAX_VALUE;
+
+            // Applying Binary Search conditions
+            if (l1 <= r2 && l2 <= r1) {
+                if (((n1 + n2) & 1) == 0) {
+                    // even length
+                    return (double) (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                } else {
+                    // odd length
+                    return (double) Math.max(l1, l2);
+                }
+            } else if (l1 > r2) {
+                high = mid1 - 1;
+            } else if (l2 > r1) {
+                low = mid1 + 1;
+            }
+        }
+        return 0.0;
+    }
+
+    /**
      * Approach II : Using Better (Two Pointers) Approach
      *
      * TC: O(M + N)
-     * SC: O(M + N)
+     * SC: O(1)
      */
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    public double findMedianSortedArraysApproachII(int[] nums1, int[] nums2) {
         int m1 = nums1.length;
         int m2 = nums2.length;
 
