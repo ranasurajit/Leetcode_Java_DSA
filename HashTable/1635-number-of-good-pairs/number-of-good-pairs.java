c3401756cdcef78064432822bbb15c6f584911ab
Surajit Rana <surajit1602@gmail.com>
2025-05-24 19:32:51 +0530

    Time: 1 ms (87.87%) | Memory: 41.5 MB (16.92%) - LeetSync

--- HashTable/1635-number-of-good-pairs/number-of-good-pairs.java
+++ HashTable/1635-number-of-good-pairs/number-of-good-pairs.java
@@ -1,11 +1,27 @@
 class Solution {
     /**
-     * Approach : Using Hashing Approach
+     * Approach II : Using Hashing Approach (Single Pass Approach)
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public int numIdenticalPairs(int[] nums) {
+        Map<Integer, Integer> freqMap = new HashMap<Integer, Integer>(); // SC: O(N)
+        int count = 0;
+        for (int num : nums) { // TC: O(N)
+            count += freqMap.getOrDefault(num, 0);
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+        return count;
+    }
+
+    /**
+     * Approach I : Using Hashing Approach
      *
      * TC: O(2 x N) ~ O(N)
      * SC: O(N)
      */
-    public int numIdenticalPairs(int[] nums) {
+    public int numIdenticalPairsApproachI(int[] nums) {
         Map<Integer, Integer> freqMap = new HashMap<Integer, Integer>(); // SC: O(N)
         for (int num : nums) { // TC: O(N)
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
