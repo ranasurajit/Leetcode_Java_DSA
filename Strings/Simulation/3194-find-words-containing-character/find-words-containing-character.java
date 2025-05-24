34ad9dcd368a4d507d85ad5b51d56bedc24ea229
Surajit Rana <surajit1602@gmail.com>
2025-05-24 11:30:38 +0530

    Time: 1 ms (100.00%) | Memory: 45.4 MB (7.51%) - LeetSync

--- Strings/Simulation/3194-find-words-containing-character/find-words-containing-character.java
+++ Strings/Simulation/3194-find-words-containing-character/find-words-containing-character.java
@@ -1,11 +1,32 @@
 class Solution {
     /**
+     * Approach II : Using Character Comparison Approach
+     *
+     * TC: O(N x L), where L = MaxLength(words[i])
+     * SC: O(1)
+     */
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        int n = words.length;
+        List<Integer> indices = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            String word = words[i];
+            for (int j = 0; j < word.length(); j++) { // TC: O(L)
+                if (word.charAt(j) == x) {
+                    indices.add(i);
+                    break;
+                }
+            }
+        }
+        return indices;
+    }
+
+    /**
      * Approach I : Using String Simulation Approach
      *
      * TC: O(N ^ 2)
      * SC: O(1)
      */
-    public List<Integer> findWordsContaining(String[] words, char x) {
+    public List<Integer> findWordsContainingApproachI(String[] words, char x) {
         int n = words.length;
         List<Integer> indices = new ArrayList<Integer>();
         for (int i = 0; i < n; i++) { // TC: O(N)
