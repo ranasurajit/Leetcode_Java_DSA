ecc9ad9dd9a6b3fdaf4257258c9c13e860a8e06e
Surajit Rana <surajit1602@gmail.com>
2025-05-24 17:02:12 +0530

    Time: 2 ms (98.59%) | Memory: 42 MB (56.86%) - LeetSync

--- HashTable/1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ HashTable/1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,11 +1,30 @@
 class Solution {
     /**
-     * Approach : Using Hashing Approach
+     * Approach II : Using Hashing Approach (Cleaner Approach)
      *
      * TC: O(2 x N) ~ O(N)
      * SC: O(2 x N) ~ O(N)
      */
     public boolean uniqueOccurrences(int[] arr) {
+        int n = arr.length;
+        Map<Integer, Integer> freqMap = new HashMap<Integer, Integer>(); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            freqMap.put(arr[i], freqMap.getOrDefault(arr[i], 0) + 1);
+        }
+        Set<Integer> freqSet = new HashSet<Integer>(); // SC: O(N)
+        for (Integer key : freqMap.keySet()) { // TC: O(N)
+            freqSet.add(freqMap.get(key));
+        }
+        return freqSet.size() == freqMap.size();
+    }
+
+    /**
+     * Approach I : Using Hashing Approach
+     *
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(2 x N) ~ O(N)
+     */
+    public boolean uniqueOccurrencesApproachI(int[] arr) {
         int n = arr.length;
         Map<Integer, Integer> freqMap = new HashMap<Integer, Integer>(); // SC: O(N)
         for (int i = 0; i < n; i++) { // TC: O(N)
