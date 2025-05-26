509192050d7adfcda478363c2837f3b4a6802499
Surajit Rana <surajit1602@gmail.com>
2025-05-27 01:31:15 +0530

    Time: 48 ms (27.51%) | Memory: 79.2 MB (99.43%) - LeetSync

--- HashTable/3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
+++ HashTable/3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
@@ -1,29 +1,36 @@
 class Solution {
     /**
-     * Using Hashing Approach
-     *
-     * TC: O(Q)
-     * SC: O(2 x Q) ~ O(Q)
+     * Approach : Using Hashing (2 HashMaps) Approach
+     * 
+     * TC: O(N)
+     * SC: O(N)
      */
     public int[] queryResults(int limit, int[][] queries) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(Q)
-        Map<Integer, Integer> colorMap = new HashMap<Integer, Integer>(); // SC: O(Q)
-        int q = queries.length;
-        int[] result = new int[q];
-        int count = 0;
-        for (int[] query : queries) { // TC: O(Q)
-            if (colorMap.containsKey(query[0])) {
-                int color = colorMap.get(query[0]);
-                int freq = map.get(color);
+        int n = queries.length;
+        // we will store Map with data { ballIndex, color }
+        Map<Integer, Integer> ballMap = new HashMap<Integer, Integer>(); // SC: O(N)
+        Map<Integer, Integer> colorMap = new HashMap<Integer, Integer>(); // SC: O(N)
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            int[] query = queries[i];
+            if (ballMap.containsKey(query[0])) {
+                int color = ballMap.get(query[0]);
+                int freq = colorMap.get(color);
                 if (freq == 1) {
-                    map.remove(color);
+                    colorMap.remove(color);
                 } else {
-                    map.put(color, freq - 1);
+                    colorMap.put(color, freq - 1);
                 }
             }
-            map.put(query[1], map.getOrDefault(query[1], 0) + 1);
-            colorMap.put(query[0], query[1]);
-            result[count++] = map.size();
+            ballMap.put(query[0], query[1]); // marking ball (query[0]) with color (query[1])
+            colorMap.put(query[1], colorMap.getOrDefault(query[1], 0) + 1);
+            /**
+             * The minimum value of both Map and Set will decide cancel out two scanarios
+             * 1. If same ball is repainted with a color then colro count will 
+             * increase but distinct colors remain the same
+             * 2. If same color is used to paint multiple balls, then we need only distinct colors
+             */
+            result[i] = colorMap.size();
         }
         return result;
     }
