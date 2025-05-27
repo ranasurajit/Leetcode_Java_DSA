eda64b0f12694ab454f53d5f26b1c181f9777a05
Surajit Rana <surajit1602@gmail.com>
2025-05-28 00:53:37 +0530

    Time: 33 ms (29.41%) | Memory: 55.2 MB (31.35%) - LeetSync

---
+++ HashTable/817-design-hashmap/design-hashmap.java
@@ -0,0 +1,52 @@
+/**
+ * Approach : Using an Array Approach
+ *
+ * TC: O(1)
+ * SC: O(1)
+ */
+class MyHashMap {
+
+    int[] nums = null;
+    int size = (int) 1e6 + 1;
+
+    /**
+     * TC: O(1)
+     * SC: O(1e6 + 1) ~ O(1)
+     */
+    public MyHashMap() {
+        nums = new int[size];
+        Arrays.fill(nums, -1);
+    }
+    
+    /**
+     * TC: O(1)
+     * SC: O(1)
+     */
+    public void put(int key, int value) {
+        nums[key] = value;
+    }
+    
+    /**
+     * TC: O(1)
+     * SC: O(1)
+     */
+    public int get(int key) {
+        return nums[key];
+    }
+
+    /**
+     * TC: O(1)
+     * SC: O(1)
+     */
+    public void remove(int key) {
+        nums[key] = -1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
