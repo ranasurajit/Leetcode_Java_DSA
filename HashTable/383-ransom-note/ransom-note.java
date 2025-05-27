ea2af7f8afffc508154e12269b5a1964d5556481
Surajit Rana <surajit1602@gmail.com>
2025-05-28 02:40:21 +0530

    Time: 1 ms (99.35%) | Memory: 44.9 MB (69.04%) - LeetSync

--- HashTable/383-ransom-note/ransom-note.java
+++ HashTable/383-ransom-note/ransom-note.java
@@ -1,14 +1,20 @@
 class Solution {
+    /**
+     * Approach : Using Hashing Approach
+     * 
+     * TC: O(M + N)
+     * SC: O(1)
+     */
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
-        for (int i = 0; i < magazine.length(); i++) {
-            map.put(magazine.charAt(i), map.getOrDefault(magazine.charAt(i), 0) + 1);
+        int[] map = new int[26]; // SC: O(26) ~ O(1)
+        for (char ch : ransomNote.toCharArray()) { // TC: O(M)
+            map[ch - 'a']++;
         }
-        for (int i = 0; i < ransomNote.length(); i++) {
-            char c = ransomNote.charAt(i);
-            if (map.containsKey(c) && map.get(c) > 0) {
-                map.put(c, map.get(c) - 1);
-            } else {
+        for (char ch : magazine.toCharArray()) { // TC: O(N)
+            map[ch - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) { // TC: O(26) ~ O(1)
+            if (map[i] > 0) {
                 return false;
             }
         }
