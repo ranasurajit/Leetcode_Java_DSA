4cb5a2d400e6b053d0c0be8f49b28d880b6c05a3
Surajit Rana <surajit1602@gmail.com>
2025-05-28 02:31:03 +0530

    Time: 21 ms (79.04%) | Memory: 59.7 MB (62.02%) - LeetSync

--- HashTable/219-contains-duplicate-ii/contains-duplicate-ii.java
+++ HashTable/219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,14 +1,30 @@
 class Solution {
+    /**
+     * Approach : Using Hashing Approach
+     * 
+     * TC: O(N)
+     * SC: O(N)
+     */
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length; i++) {
-            if (hm.containsKey(nums[i])) {
-                int diff = Math.abs(i - hm.get(nums[i]));
+        int n = nums.length;
+        // we will store {nums[i], i} in the HashMap
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (map.containsKey(nums[i])) {
+                int diff = Math.abs(map.get(nums[i]) - i);
                 if (diff <= k) {
                     return true;
+                } else {
+                    /**
+                     * if diff > k, then we will override the index with a 
+                     * hope that we will encounter the same value as nums[i] 
+                     * with an index - currentIndex <= k
+                     */
+                    map.put(nums[i], i);
                 }
+            } else {
+                 map.put(nums[i], i);
             }
-            hm.put(nums[i], i);
         }
         return false;
     }
