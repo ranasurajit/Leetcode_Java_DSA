72a821b4cf2341dbb5bf8a107d69a5ca2fef5262
Surajit Rana <surajit1602@gmail.com>
2025-05-28 02:16:30 +0530

    Time: 7 ms (86.44%) | Memory: 42.8 MB (24.93%) - LeetSync

--- HashTable/205-isomorphic-strings/isomorphic-strings.java
+++ HashTable/205-isomorphic-strings/isomorphic-strings.java
@@ -1,11 +1,30 @@
 class Solution {
     /**
-     * Approach : Using Hashing Approach
+     * Approach II : Using Hashing Approach (Clean Approach)
+     * 
+     * TC: O(N)
+     * SC: O(2 x 257) ~ O(1)
+     */
+    public boolean isIsomorphic(String s, String t) {
+        int[] sMap = new int[257]; // as ASCII characters are 256
+        int[] tMap = new int[257]; // as ASCII characters are 256
+        for (int i = 0; i < s.length(); i++) {
+            if (sMap[(int) s.charAt(i)] != tMap[(int) t.charAt(i)]) {
+                return false;
+            }
+            sMap[(int) s.charAt(i)] = i + 1;
+            tMap[(int) t.charAt(i)] = i + 1;
+        }
+        return true;
+    }
+
+    /**
+     * Approach I : Using Hashing Approach
      * 
      * TC: O(N)
      * SC: O(2 x N) ~ O(N)
      */
-    public boolean isIsomorphic(String s, String t) {
+    public boolean isIsomorphicApproachI(String s, String t) {
         Map<Character, Character> map = new HashMap<Character, Character>(); // SC: O(N)
         Map<Character, Character> revMap = new HashMap<Character, Character>(); // SC: O(N)
         int n = s.length();
