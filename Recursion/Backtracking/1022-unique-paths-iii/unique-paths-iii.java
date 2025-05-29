ef9bbed23fb361286155bde4705fccb203c8addd
Surajit Rana <surajit1602@gmail.com>
2025-05-30 02:27:21 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (57.40%) - LeetSync

--- Recursion/Backtracking/1022-unique-paths-iii/unique-paths-iii.java
+++ Recursion/Backtracking/1022-unique-paths-iii/unique-paths-iii.java
@@ -1,74 +1,67 @@
 class Solution {
-    private int[][] directions = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
-    private int emptyCells = 0;
+    private static final int[][] directions = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
     private int m;
     private int n;
-    private int result;
+    private int nonObstacleCells = 0;
+    private int result = 0;
 
     /**
-     * TC: O((M x N) + 4 ^ (M x N))
+     * Approach : Using Recursion + Backtracking Approach
+     *
+     * TC: O(3 ^ (M x N) + (M x N)) ~ O(3 ^ (M x N))
      * SC: O(M x N)
-     * 
-     * @param grid
-     * @return
      */
     public int uniquePathsIII(int[][] grid) {
         m = grid.length;
         n = grid[0].length;
-        int startX = 0;
-        int startY = 0;
-
-        boolean[][] visited = new boolean[m][n];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    startX = i;
-                    startY = j;
-                    emptyCells++;
-                } else if (grid[i][j] == 0) {
-                    emptyCells++;
+        int x = -1;
+        int y = -1;
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                if (grid[i][j] == 0) {
+                    nonObstacleCells++;
+                } else if (grid[i][j] == 1) {
+                    nonObstacleCells++;
+                    x = i;
+                    y = j;
                 }
             }
         }
-
         int[] count = { 0 };
-        backtrack(startX, startY, grid, visited, count);
+        solveBacktrack(x, y, grid, count); // TC: O(3 ^ (M x N)), SC: O(M x N)
         return result;
     }
 
     /**
-     * TC: O(4 ^ (M x N))
+     * Using Recursion + Backtracking Approach
+     *
+     * TC: O(3 ^ (M x N))
      * SC: O(M x N)
-     * 
-     * @param i
-     * @param j
-     * @param grid
-     * @param visited
-     * @param count
      */
-    private void backtrack(int i, int j, int[][] grid,
-        boolean[][] visited, int[] count) {
-        // grid cell validation for out of bound coordinates or if visited already
-        if (i < 0 || i >= m || j < 0 || j >= n || visited[i][j] || grid[i][j] == -1) {
+    private void solveBacktrack(int x, int y, int[][] grid, int[] count) {
+        // Base Case
+        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == -1) {
             return;
         }
-        if (grid[i][j] == 2) {
-            if (count[0] == emptyCells) {
+        if (grid[x][y] == 2) {
+            if (count[0] == nonObstacleCells) {
                 result++;
             }
             return;
         }
-        // marking cell (i, j) as visited
-        visited[i][j] = true;
-
-        // exploring the neighbours in all 4 directions
-        for (int[] direction : directions) {
-            int effRow = i + direction[0];
-            int effCol = j + direction[1];
-            backtrack(effRow, effCol, grid, visited, new int[] { count[0] + 1 });
+        // Recursion Calls
+        // modify
+        int temp = grid[x][y];
+        grid[x][y] = -1;
+        count[0]++;
+        // explore
+        for (int[] dir : directions) {
+            int effX = x + dir[0];
+            int effY = y + dir[1];
+            solveBacktrack(effX, effY, grid, count);
         }
-        // marking cell (i, j) as unvisited
-        visited[i][j] = false;
+        // backtrack
+        grid[x][y] = temp;
+        count[0]--;
     }
 }
