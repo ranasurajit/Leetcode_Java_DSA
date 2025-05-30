9b094a4e4ab730ecea668cf12d042a67f731d5dc
Surajit Rana <surajit1602@gmail.com>
2025-05-30 12:48:47 +0530

    Time: 195 ms (5.30%) | Memory: 108.3 MB (5.30%) - LeetSync

--- Graphs/DFS/2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.java
+++ Graphs/DFS/2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.java
@@ -16,17 +16,18 @@ class Solution {
         // initializing distance arrays
         int[] dist1 = new int[n]; // SC: O(N)
         int[] dist2 = new int[n]; // SC: O(N)
+        boolean[] visited1 = new boolean[n]; // SC: O(N)
+        boolean[] visited2 = new boolean[n]; // SC: O(N)
         Arrays.fill(dist1, Integer.MAX_VALUE);
         Arrays.fill(dist2, Integer.MAX_VALUE);
         dist1[node1] = 0;
         dist2[node2] = 0;
 
         // performing DFS traversal to fill the distances
-        boolean[] visited1 = new boolean[n]; // SC: O(N)
-        boolean[] visited2 = new boolean[n]; // SC: O(N)
         dfsGraph(node1, visited1, adj, dist1); // TC: O(N), SC: O(N)
         dfsGraph(node2, visited2, adj, dist2); // TC: O(N), SC: O(N)
         
+        // compare shortest distances
         int minDist = Integer.MAX_VALUE;
         int resultNode = -1;
         for (int i = 0; i < n; i++) { // TC: O(N)
