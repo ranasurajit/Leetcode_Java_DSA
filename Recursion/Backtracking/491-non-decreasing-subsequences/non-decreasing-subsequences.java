cffcb7769ec974a6c07180fe92fcb06779ccbd6f
Surajit Rana <surajit1602@gmail.com>
2025-05-30 22:24:35 +0530

    Time: 17 ms (40.44%) | Memory: 51.3 MB (62.48%) - LeetSync

--- Recursion/Backtracking/491-non-decreasing-subsequences/non-decreasing-subsequences.java
+++ Recursion/Backtracking/491-non-decreasing-subsequences/non-decreasing-subsequences.java
@@ -1,51 +1,44 @@
 class Solution {
     /**
-     * Using Recursion + Backtracking Approach
-     * 
+     * Approach I : Using Recursion + Backtracking Approach
+     *
      * TC: O(2 ^ N)
-     * SC: O(2 x N) ~ O(N)
-     * 
-     * @param nums
-     * @return
+     * SC: O(2 ^ N + 2 x N) ~ O(2 ^ N)
      */
     public List<List<Integer>> findSubsequences(int[] nums) {
         int n = nums.length;
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        Set<List<Integer>> result = new HashSet<List<Integer>>(); // SC: O(2 ^ N)
         List<Integer> current = new ArrayList<Integer>(); // SC: O(N)
-        backtrack(0, nums, n, current, result); // TC: O(2 ^ N), SC: O(N)
-        return result;
+        solveRecursion(0, -1, n, nums, current, result); // TC: O(2 ^ N), SC: O(N)
+        return new ArrayList<List<Integer>>(result);
     }
 
     /**
+     * Using Recursion + Backtracking Approach
+     *
      * TC: O(2 ^ N)
      * SC: O(N)
-     * 
-     * @param index
-     * @param nums
-     * @param n
-     * @param current
-     * @param result
      */
-    private void backtrack(int index, int[] nums, int n,
-            List<Integer> current, List<List<Integer>> result) {
-        if (current.size() > 1) {
-            result.add(new ArrayList<Integer>(current));
-        }
-        /**
-         * Created set to make sure duplicate elements are not included in the
-         * same recursion call instance
-         */
-        Set<Integer> set = new HashSet<Integer>();
-        for (int i = index; i < n; i++) {
-            // include
-            if ((current.isEmpty() || nums[i] >= current.get(current.size() - 1)) &&
-                    !set.contains(nums[i])) {
-                // pick
-                current.add(nums[i]);
-                backtrack(i + 1, nums, n, current, result);
-                current.remove(current.size() - 1); // backtrack (undo)
-                set.add(nums[i]);
+    private void solveRecursion(int index, int prevIndex, int n, int[] nums, List<Integer> current,
+        Set<List<Integer>> result) {
+        // Base Case
+        if (index == n) {
+            if (current.size() > 1) {
+                result.add(new ArrayList<Integer>(current));
             }
+            return;
+        }
+        // we have options to take or skip
+        // skip
+        solveRecursion(index + 1, prevIndex, n, nums, current, result);
+        // take
+        if (prevIndex == -1 || nums[index] >= nums[prevIndex]) {
+            // modify
+            current.add(nums[index]);
+            // explore
+            solveRecursion(index + 1, index, n, nums, current, result);
+            // backtrack
+            current.remove(current.size() - 1);
         }
     }
 }
