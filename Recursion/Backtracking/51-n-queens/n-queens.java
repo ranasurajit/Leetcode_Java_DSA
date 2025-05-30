461957bc2a8d55e735866a51445a121301303304
Surajit Rana <surajit1602@gmail.com>
2025-05-31 01:06:28 +0530

    Time: 6 ms (21.47%) | Memory: 44.9 MB (74.98%) - LeetSync

--- Recursion/Backtracking/51-n-queens/n-queens.java
+++ Recursion/Backtracking/51-n-queens/n-queens.java
@@ -1,11 +1,79 @@
 class Solution {
+    private Set<Integer> rowSet = new HashSet<Integer>(); // SC: O(N)
+    private Set<Integer> diagSet = new HashSet<Integer>(); // SC: O(N)
+    private Set<Integer> antiDiagSet = new HashSet<Integer>(); // SC: O(N)
+
+    /**
+     * Approach II : Using Recursion + Backtracking + Hashing Approach 
+     * (Reducing Complexity of Queen's safe validation)
+     *
+     * TC: O(N x N! + N ^ 2) ~ O(N x N!)
+     * SC: O(N ^ 2 + 4 x N) ~ O(N ^ 2)
+     */
+    public List<List<String>> solveNQueens(int n) {
+        // preparing the board
+        List<String> board = new ArrayList<String>(); // SC: O(N x N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            StringBuilder sb = new StringBuilder(); // SC: O(N)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                sb.append(".");
+            }
+            board.add(sb.toString());
+        }
+        List<List<String>> result = new ArrayList<List<String>>();
+        // calling recursion function
+        solveBacktrackOptimal(0, n, board, result); // TC: O(N x N!), SC: O(N)
+        return result;
+    }
+
+    /**
+     * Using Recursion + Backtracking Approach
+     *
+     * TC: O(N x N!)
+     * SC: O(N)
+     */
+    private void solveBacktrackOptimal(int col, int n, List<String> board, List<List<String>> result) {
+        // Base Case
+        if (col == n) {
+            result.add(new ArrayList<String>(board));
+            return;
+        }
+        // Recursion Calls
+        for (int row = 0; row < n; row++) { // TC: O(N)
+            int rowKey = row;
+            int diagKey = row + col;
+            int antiDiagKey = row - col;
+            if (rowSet.contains(rowKey) || diagSet.contains(diagKey) || 
+                antiDiagSet.contains(antiDiagKey)) {
+                // here placing the Queen is not safe
+                continue;
+            }
+            // add the keys to the respective Sets
+            rowSet.add(rowKey);
+            diagSet.add(diagKey); 
+            antiDiagSet.add(antiDiagKey);
+            // modify
+            StringBuilder s = new StringBuilder(board.get(row));
+            s.setCharAt(col, 'Q');
+            board.set(row, s.toString());
+            // explore
+            solveBacktrackOptimal(col + 1, n, board, result);
+            // backtrack
+            rowSet.remove(rowKey);
+            diagSet.remove(diagKey); 
+            antiDiagSet.remove(antiDiagKey);
+            s.setCharAt(col, '.');
+            board.set(row, s.toString());
+        }
+    }
+
     /**
      * Approach I : Using Recursion + Backtracking Approach
      *
      * TC: O(N ^ 3 x N! + N ^ 2) ~ O(N ^ 3 x N!)
      * SC: O(N ^ 2 + N) ~ O(N ^ 2)
      */
-    public List<List<String>> solveNQueens(int n) {
+    public List<List<String>> solveNQueensApproachI(int n) {
         // preparing the board
         List<String> board = new ArrayList<String>(); // SC: O(N x N)
         for (int i = 0; i < n; i++) { // TC: O(N)
