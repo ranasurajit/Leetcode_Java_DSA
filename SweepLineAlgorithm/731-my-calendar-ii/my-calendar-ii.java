f9d54a3b5d27ca6e7bad91e7a1e4f2575f8e335f
Surajit Rana <surajit1602@gmail.com>
2025-06-01 20:16:44 +0530

    Time: 733 ms (11.59%) | Memory: 45.8 MB (57.31%) - LeetSync

--- SweepLineAlgorithm/731-my-calendar-ii/my-calendar-ii.java
+++ SweepLineAlgorithm/731-my-calendar-ii/my-calendar-ii.java
@@ -30,7 +30,13 @@ class MyCalendarTwo {
                  * undo map insertion for interval
                  */
                 eventsMap.put(startTime, eventsMap.get(startTime) - 1); // TC: O(log(Q))
+                if (eventsMap.get(startTime) == 0) {
+                    eventsMap.remove(startTime);
+                }
                 eventsMap.put(endTime, eventsMap.get(endTime) + 1); // TC: O(log(Q))
+                if (eventsMap.get(endTime) == 0) {
+                    eventsMap.remove(endTime);
+                }
                 return false;
             }
         }
