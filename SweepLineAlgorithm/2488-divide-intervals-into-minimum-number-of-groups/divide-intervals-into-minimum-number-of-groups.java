839b7e7c62f225fa29e8733f3ce2364e536284a6
Surajit Rana <surajit1602@gmail.com>
2025-06-01 21:06:12 +0530

    Time: 334 ms (5.52%) | Memory: 101.8 MB (9.21%) - LeetSync

--- SweepLineAlgorithm/2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.java
+++ SweepLineAlgorithm/2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.java
@@ -1,41 +1,32 @@
 class Solution {
     /**
-     * Using Sweep-Line Algorithm
+     * Approach : Using Line Sweep Algorithm
      *
-     * TC: O(N x log(N))
-     * SC: O(N)
-     * 
-     * @param intervals
-     * @return
+     * TC: O(2 x N x log(N) + 2 x N x log(2 x N)) ~ O(N x log(N))
+     * SC: O(2 x N) ~ O(N)
      */
     public int minGroups(int[][] intervals) {
-        TreeMap<Integer, Integer> events = new TreeMap<Integer, Integer>();
-        // minimum number of groups is the maximum number of overlapping intervals
-        return getMaximumOverlaps(intervals, events);
-    }
-
-    /**
-     * TC: O(N x log(N))
-     * SC: O(1)
-     * 
-     * @param intervals
-     * @param events
-     * @return
-     */
-    private int getMaximumOverlaps(int[][] intervals, 
-        TreeMap<Integer, Integer> events) {
-        for (int[] interval : intervals) {                    // TC: O(N)
-            events.put(interval[0],
-                events.getOrDefault(interval[0], 0) + 1);     // TC: O(log(N))
-            events.put(interval[1] + 1, 
-                events.getOrDefault(interval[1] + 1, 0) - 1); // TC: O(log(N))
+        Map<Integer, Integer> eventsMap = new TreeMap<Integer, Integer>(); // SC: O(2 x N)
+        for (int[] time : intervals) { // TC: O(N)
+            int start = time[0];
+            int end = time[1];
+            eventsMap.put(start, eventsMap.getOrDefault(start, 0) + 1); // TC: O(log(N))
+            /**
+             * as endTime is included as [startTime, endTime]
+             * is full-open interval i.e. startTime <= x <= endTime
+             */
+            eventsMap.put(end + 1, eventsMap.getOrDefault(end + 1, 0) - 1); // TC: O(log(N))
         }
-        int maxGroups = 0;
-        int currentGroups = 0;
-        for (Integer key : events.keySet()) {                 // TC: O(2 x N)
-            currentGroups += events.get(key);                 // TC: O(log(N))
-            maxGroups = Math.max(maxGroups, currentGroups);
+        int currentOverlaps = 0;
+        int maxOverlaps = 0;
+        for (Integer key : eventsMap.keySet()) { // TC: O(2 x N)
+            currentOverlaps += eventsMap.get(key); // TC: O(log(2 x N))
+            maxOverlaps = Math.max(maxOverlaps, currentOverlaps);
         }
-        return maxGroups;
+        /**
+         * maximum overlaps will tell how many minimum groups are needed
+         * so that no two intervals that are in the same group intersect each other
+         */
+        return maxOverlaps;
     }
 }
