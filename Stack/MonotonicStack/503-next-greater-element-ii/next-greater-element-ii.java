b7f14ec92578daf6f8241b585b56b1374b66f903
Surajit Rana <surajit1602@gmail.com>
2025-06-03 18:52:43 +0530

    Time: 18 ms (28.97%) | Memory: 45.8 MB (94.35%) - LeetSync

--- Stack/MonotonicStack/503-next-greater-element-ii/next-greater-element-ii.java
+++ Stack/MonotonicStack/503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,11 +1,34 @@
 class Solution {
     /**
-     * Approach : Using Stack Approach
+     * Approach II : Using Stack Approach (Cleaner Approach)
+     * 
+     * TC: O(3 x N) ~ O(N)
+     * SC: O(2 x N) ~ O(N)
+     */
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[2 * n]; // SC: O(N)
+        Stack<Integer> st = new Stack<Integer>(); // SC: O(N)
+        for (int i = 2 * n - 1; i >= 0; i--) { // TC: O(2 x N)
+            while (!st.isEmpty() && st.peek() <= nums[i % n]) {
+                st.pop();
+            }
+            result[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(nums[i % n]);
+        }
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            nums[i] = result[i];
+        }
+        return nums;
+    }
+
+    /**
+     * Approach I : Using Stack Approach
      * 
      * TC: O(2 x M + N) ~ O(2 x M + 2 x M) ~ O(M)
      * SC: O(3 x N) ~ O(6 x M) ~ O(M)
      */
-    public int[] nextGreaterElements(int[] nums) {
+    public int[] nextGreaterElementsApproachI(int[] nums) {
         int m = nums.length;
         int n = 2 * m;
         int[] circularNums = new int[n]; // SC: O(N)
