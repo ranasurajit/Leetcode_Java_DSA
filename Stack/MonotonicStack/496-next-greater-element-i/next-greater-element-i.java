79b1e23af56b839fc90e3341fe32055952f3fd2a
Surajit Rana <surajit1602@gmail.com>
2025-06-03 18:06:32 +0530

    Time: 4 ms (37.65%) | Memory: 44.8 MB (8.13%) - LeetSync

--- Stack/MonotonicStack/496-next-greater-element-i/next-greater-element-i.java
+++ Stack/MonotonicStack/496-next-greater-element-i/next-greater-element-i.java
@@ -1,36 +1,30 @@
 class Solution {
     /**
-     * Using Stack to pre-compute the Next Greater Elements
+     * Approach : Using Stack Approach
      * 
      * TC: O(M + N)
-     * SC: O(3 x N) ~ O(N)
-    */
+     * SC: O(3 x M) ~ O(M)
+     */
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int[] result = new int[m];
-        // SC: O(N)
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        /**
-          * we are going to pre-compute next greater element
-          * for 'nums2' array
-          */
-        Stack<Integer> st = new Stack<Integer>(); // SC: O(N)
-        int n = nums2.length;
-        int[] nge = new int[n];            // SC: O(N)
-        /** 
-         * we need to store the array 'nums2' 
-         * elements into HashMap to find the indices
-         * in O(1)
-         */
-        for (int i = n - 1; i >= 0; i--) { // TC: O(N)
-            map.put(nums2[i], i);
-            while (!st.isEmpty() && st.peek() <= nums2[i]) {
-                st.pop();
+        int n = nums1.length;
+        int m = nums2.length;
+        int[] nge = new int[m]; // SC: O(M)
+        Stack<Integer> st = new Stack<Integer>(); // SC: O(M)
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(M)
+        for (int i = m - 1; i >= 0; i--) { // TC: O(M)
+            if (st.isEmpty()) {
+                nge[i] = -1;
+            } else {
+                while (!st.isEmpty() && st.peek() <= nums2[i]) {
+                    st.pop();
+                }
+                nge[i] = st.isEmpty() ? -1 : st.peek();
             }
-            nge[i] = st.isEmpty() ? -1 : st.peek();
-            st.add(nums2[i]);
+            st.push(nums2[i]);
+            map.put(nums2[i], i);
         }
-        for (int i = 0; i < m; i++) {      // TC: O(M)
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) { // TC: O(N)
             result[i] = nge[map.get(nums1[i])];
         }
         return result;
