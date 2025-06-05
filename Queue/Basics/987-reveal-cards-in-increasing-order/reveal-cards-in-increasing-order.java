d748c62e7a8fe8b677ae99e928cf8d7d69b23182
Surajit Rana <surajit1602@gmail.com>
2025-06-06 01:23:10 +0530

    Time: 4 ms (94.66%) | Memory: 43.9 MB (48.96%) - LeetSync

--- Queue/Basics/987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
+++ Queue/Basics/987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
@@ -1,11 +1,38 @@
 class Solution {
     /**
-     * Approach : Using Two Pointers and Sorting Approach
+     * Approach II : Using Two Pointers, Queues and Sorting Approach
+     *
+     * TC: O(N x log(N) + 2 x N) ~ O(N x log(N))
+     * SC: O(N)
+     */
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int n = deck.length;
+        int[] result = new int[n];
+        Arrays.sort(deck); // TC: O(N x log(N))
+        // we will be storing indices in the Queue
+        Queue<Integer> queue = new LinkedList<Integer>(); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            queue.offer(i);
+        }
+        int i = 0; // pointer at array 'deck'
+        while (i < n) { // TC: O(N)
+            int idx = queue.poll();
+            result[idx] = deck[i];
+            if (!queue.isEmpty()) {
+                queue.offer(queue.poll());
+            }
+            i++;
+        }
+        return result;
+    }
+
+    /**
+     * Approach I : Using Two Pointers and Sorting Approach
      *
      * TC: O(2 x N x log(N)) ~ O(N x log(N))
      * SC: O(1)
      */
-    public int[] deckRevealedIncreasing(int[] deck) {
+    public int[] deckRevealedIncreasingTwoPointers(int[] deck) {
         int n = deck.length;
         int[] result = new int[n];
         Arrays.sort(deck); // TC: O(N x log(N))
