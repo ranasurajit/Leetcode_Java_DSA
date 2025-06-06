cf94594abb3f098af05a36bdd3d9c21a89d7abbd
Surajit Rana <surajit1602@gmail.com>
2025-06-07 02:19:03 +0530

    Time: 35 ms (75.77%) | Memory: 60.2 MB (16.92%) - LeetSync

--- Queue/ArrayDeque/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ Queue/ArrayDeque/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,30 +1,80 @@
 class Solution {
+    /**
+     * Approach II : Using Sliding Window + Deque Approach
+     *
+     * TC: O(N)
+     * SC: O(2 x N) ~ O(N)
+     *
+     * Accepted (63 / 63 testcases passed)
+     */
     public int longestSubarray(int[] nums, int limit) {
         int n = nums.length;
-        int result = 0;
-        int left = 0;
-        ArrayDeque<Integer> incDeque = new ArrayDeque<Integer>();
-        ArrayDeque<Integer> decDeque = new ArrayDeque<Integer>();
-        for (int right = 0; right < n; right++) {
-            while (!incDeque.isEmpty() && nums[right] < incDeque.getLast()) {
-                incDeque.removeLast();
+
+        /**
+         * we will use two Deque's to store minimum and maximum value's indices
+         * at the beginning of it in any variable size window
+         */
+        Deque<Integer> minDeque = new ArrayDeque<Integer>(); // SC: O(N)
+        Deque<Integer> maxDeque = new ArrayDeque<Integer>(); // SC: O(N)
+
+        int i = 0; // start pointer of sliding window
+        int j = 0; // end pointer of sliding window
+        int maxLength = 0;
+
+        while (j < n) { // TC: O(N)
+            while (!minDeque.isEmpty() && nums[j] < nums[minDeque.peekLast()]) {
+                minDeque.pollLast();
             }
-            while (!decDeque.isEmpty() && nums[right] > decDeque.getLast()) {
-                decDeque.removeLast();
+            while (!maxDeque.isEmpty() && nums[j] > nums[maxDeque.peekLast()]) {
+                maxDeque.pollLast();
             }
-            incDeque.addLast(nums[right]);
-            decDeque.addLast(nums[right]);
-            while (decDeque.getFirst() - incDeque.getFirst() > limit) {
-                if (incDeque.getFirst() == nums[left]) {
-                    incDeque.removeFirst();
+            minDeque.addLast(j); // TC: O(1)
+            maxDeque.addLast(j); // TC: O(1)
+            while (nums[maxDeque.peekFirst()] - nums[minDeque.peekFirst()] > limit) {
+                // remove computation from index i
+                if (minDeque.peekFirst() == i) {
+                    minDeque.pollFirst(); // TC: O(1)
                 }
-                if (decDeque.getFirst() == nums[left]) {
-                    decDeque.removeFirst();
+                if (maxDeque.peekFirst() == i) {
+                    maxDeque.pollFirst(); // TC: O(1)
                 }
-                left++;
+                i++;
             }
-            result = Math.max(result, (right - left + 1));
+            maxLength = Math.max(maxLength, j - i + 1);
+            j++;
         }
-        return result;
+        return maxLength;
+    }
+
+    /**
+     * Approach I : Using Sliding Window + PriorityQueue Approach
+     *
+     * TC: O(2 x N x log(N) + 2 x N x N) ~ O(N ^ 2)
+     * SC: O(2 x N) ~ O(N)
+     *
+     * Time Limit Exceeded (62 / 63 testcases passed)
+     */
+    public int longestSubarrayUsingHeaps(int[] nums, int limit) {
+        int n = nums.length;
+        PriorityQueue<Integer> minPQ = new PriorityQueue<Integer>(); // SC: O(N)
+        PriorityQueue<Integer> maxPQ = new PriorityQueue<Integer>((p, q) -> q - p); // SC: O(N)
+        int i = 0; // start pointer of sliding window
+        int j = 0; // end pointer of sliding window
+        int maxLength = 0;
+        while (j < n) { // TC: O(N)
+            minPQ.offer(nums[j]); // TC: O(log(N))
+            maxPQ.offer(nums[j]); // TC: O(log(N))
+            while (maxPQ.peek() - minPQ.peek() > limit) {
+                // remove computation from index i
+                minPQ.remove(nums[i]); // TC: O(N)
+                maxPQ.remove(nums[i]); // TC: O(N)
+                i++;
+            }
+            if (maxPQ.peek() - minPQ.peek() <= limit) {
+                maxLength = Math.max(maxLength, j - i + 1);
+            }
+            j++;
+        }
+        return maxLength;
     }
 }
