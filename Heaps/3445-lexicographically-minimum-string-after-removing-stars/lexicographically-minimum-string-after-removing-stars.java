ea16e70c385914fd29f1ac367056eca04254430f
Surajit Rana <surajit1602@gmail.com>
2025-06-07 15:30:43 +0530

    Time: 183 ms (66.06%) | Memory: 46.4 MB (85.45%) - LeetSync

---
+++ Heaps/3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.java
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * Approach : Using Heaps (PriorityQueue) Approach
+     *
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(2 x N) ~ O(N)
+     */
+    public String clearStars(String s) {
+        int n = s.length();
+        char[] chars = s.toCharArray();
+        // we will be storing { char[i] - 'a', index } in the Heap
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p, q) -> {
+            if (p[0] == q[0]) {
+                return q[1] - p[1];
+            }
+            return p[0] - q[0];
+        }); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (chars[i] != '*') {
+                pq.offer(new int[] { chars[i] - 'a', i });
+            } else {
+                int idx = pq.poll()[1];
+                chars[idx] = '$';
+                chars[i] = '$';
+            }
+        }
+        StringBuilder sb = new StringBuilder(); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (chars[i] != '$') {
+                sb.append(chars[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
