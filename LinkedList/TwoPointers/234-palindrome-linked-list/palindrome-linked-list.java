3160c40c5bd66c95c579d6a0fb7cc99084426d01
Surajit Rana <surajit1602@gmail.com>
2025-06-11 23:26:45 +0530

    Time: 7 ms (38.40%) | Memory: 68.3 MB (51.81%) - LeetSync

--- LinkedList/TwoPointers/234-palindrome-linked-list/palindrome-linked-list.java
+++ LinkedList/TwoPointers/234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,43 +9,77 @@
  * }
  */
 class Solution {
+    /**
+     * Approach : Using Linked-List Traversal and Two Pointers Approach
+     *
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(1)
+     */
     public boolean isPalindrome(ListNode head) {
-        ListNode midList = midLL(head);
-        ListNode revList = revLL(midList.next);
-        ListNode t1 = head;
-        ListNode t2 = revList;
-        while (t1 != null && t2 != null) {
-            if (t1.val != t2.val) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode current = head;
+        int length = 0;
+        while (current != null) { // TC: O(N)
+            length++;
+            current = current.next;
+        }
+        boolean isEven = (length & 1) == 0;
+        ListNode midNode = middleLL(head); // TC: O(N / 2)
+        if (!isEven) {
+            midNode = midNode.next;
+        }
+        ListNode revList = reverseLL(midNode);
+        ListNode current1 = head;
+        ListNode current2 = revList;
+        while (current1 != null && current2 != null) { // TC: O(N / 2)
+            if (current1.val != current2.val) {
                 return false;
             }
-            t1 = t1.next;
-            t2 = t2.next;
+            current1 = current1.next;
+            current2 = current2.next;
         }
         return true;
     }
 
-    private ListNode midLL(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-    }
-
-    private ListNode revLL(ListNode head) {
+    /**
+     * Using Iterative Approach to Reverse the Linked-List
+     * 
+     * TC: O(N)
+     * SC: O(1)
+     */
+    private ListNode reverseLL(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
         ListNode prev = null;
         ListNode current = head;
-        while (current != null) {
+        while (current != null) { // TC: O(N)
             ListNode temp = current.next;
             current.next = prev;
             prev = current;
             current = temp;
         }
         return prev;
+    }
+
+    /**
+     * Using Two Pointers (Fast and Slow Pointers) Approach
+     * 
+     * TC: O(N / 2)
+     * SC: O(1)
+     */
+    private ListNode middleLL(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) { // TC: O(N)
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
 }
