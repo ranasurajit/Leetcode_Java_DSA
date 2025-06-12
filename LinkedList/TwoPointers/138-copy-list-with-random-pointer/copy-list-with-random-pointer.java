5170f4c70f1218961bb1b0f6a4f6bd2aa79a60d2
Surajit Rana <surajit1602@gmail.com>
2025-06-13 01:10:05 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (55.02%) - LeetSync

--- LinkedList/TwoPointers/138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ LinkedList/TwoPointers/138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -15,12 +15,49 @@ class Node {
 
 class Solution {
     /**
-     * Approach I : Using Hashing Approach
+     * Approach II : Using Pointer Linking Approach (Without Extra Space)
+     *
+     * TC: O(3 x N) ~ O(N)
+     * SC: O(1)
+     */
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return head;
+        }
+        // creating cross-chained cloned nodes
+        Node current = head;
+        while (current != null) { // TC: O(N)
+            Node temp = current.next;
+            current.next = new Node(current.val);
+            current.next.next = temp;
+            current = temp;
+        }
+        // copying random pointers
+        current = head;
+        while (current != null && current.next != null) { // TC: O(N)
+            current.next.random = current.random != null ? current.random.next : null;
+            current = current.next.next;
+        }
+        // de-linking the cross-chaining and retaining the next pointers on both original and copied lists
+        Node original = head;
+        Node copied = head.next;
+        Node copiedHead = copied;
+        while (original != null && copied != null) { // TC: O(N)
+            original.next = original.next != null ? original.next.next : null;
+            copied.next = copied.next != null ? copied.next.next : null;
+            original = original.next;
+            copied = copied.next;
+        }
+        return copiedHead;
+    }
+
+    /**
+     * Approach I : Using Hashing Approach (Uses Extra Space)
      *
      * TC: O(2 x N) ~ O(N)
      * SC: O(N)
      */
-    public Node copyRandomList(Node head) {
+    public Node copyRandomListUsingHashing(Node head) {
         if (head == null) {
             return head;
         }
