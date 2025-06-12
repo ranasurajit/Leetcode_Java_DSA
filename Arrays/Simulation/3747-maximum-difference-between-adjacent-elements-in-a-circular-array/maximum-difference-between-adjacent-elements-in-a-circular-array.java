e19a48a1035928813394b6347771ad82fbe33f40
Surajit Rana <surajit1602@gmail.com>
2025-06-12 10:05:51 +0530

    Time: 1 ms (98.36%) | Memory: 43.7 MB (22.30%) - LeetSync

--- Arrays/Simulation/3747-maximum-difference-between-adjacent-elements-in-a-circular-array/maximum-difference-between-adjacent-elements-in-a-circular-array.java
+++ Arrays/Simulation/3747-maximum-difference-between-adjacent-elements-in-a-circular-array/maximum-difference-between-adjacent-elements-in-a-circular-array.java
@@ -1,11 +1,27 @@
 class Solution {
     /**
-     * Approach : Using Simulation + Circular Array Property Approach
+     * Approach II : Using Simulation Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int maxAdjacentDistance(int[] nums) {
+        int n = nums.length;
+        int maxDiff = Math.abs(nums[n - 1] - nums[0]);
+        for (int i = 1; i < n; i++) { // TC: O(N)
+            int currentDiff = Math.abs(nums[i] - nums[i - 1]);
+            maxDiff = Math.max(maxDiff, currentDiff);
+        }
+        return maxDiff;
+    }
+
+    /**
+     * Approach I : Using Simulation + Circular Array Property Approach
      *
      * TC: O(2 x N) ~ O(N)
      * SC: O(1)
      */
-    public int maxAdjacentDistance(int[] nums) {
+    public int maxAdjacentDistanceApproachI(int[] nums) {
         int n = nums.length;
         int maxDiff = Integer.MIN_VALUE;
         for (int i = 1; i < 2 * n; i++) { // TC: O(2 x N)
