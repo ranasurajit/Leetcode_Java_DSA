2d67023d61f168324065472450aabf22152b8014
Surajit Rana <surajit1602@gmail.com>
2025-06-12 12:13:13 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (79.58%) - LeetSync

--- LinkedList/Traversal/328-odd-even-linked-list/odd-even-linked-list.java
+++ LinkedList/Traversal/328-odd-even-linked-list/odd-even-linked-list.java
@@ -10,12 +10,60 @@
  */
 class Solution {
     /**
-     * Approach : Using Traversal of Linked-List Approach
+     * Approach II : Using Traversal of Linked-List Approach
      * 
      * TC: O(N)
      * SC: O(1)
      */
     public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode oddHead = null;
+        ListNode oddTail = null;
+        ListNode evenHead = null;
+        ListNode evenTail = null;
+        ListNode current = head;
+        int count = 1;
+        while (current != null) {
+            if ((count & 1) != 0) {
+                if (oddHead == null) {
+                    oddHead = current;
+                    oddTail = current;
+                } else {
+                    oddTail.next = current;
+                    oddTail = oddTail.next;
+                }
+            } else {
+                if (evenHead == null) {
+                    evenHead = current;
+                    evenTail = current;
+                } else {
+                    evenTail.next = current;
+                    evenTail = evenTail.next;
+                }
+            }
+            current = current.next;
+            count++;
+        }
+        if (evenHead == null) {
+            return oddHead;
+        }
+        if (oddHead == null) {
+            return evenHead;
+        }
+        evenTail.next = null;
+        oddTail.next = evenHead;
+        return oddHead;
+    }
+
+    /**
+     * Approach I : Using Traversal of Linked-List (Extra Space) Approach
+     * 
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public ListNode oddEvenListUsingExtraSpace(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
