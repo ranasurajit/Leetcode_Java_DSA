7b22d2671bbcbdbcbc02b4eac41b7488f839467a
Surajit Rana <surajit1602@gmail.com>
2025-06-14 13:17:23 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (42.02%) - LeetSync

---
+++ BinaryTrees/DFS/101-symmetric-tree/symmetric-tree.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * Approach : Using Recursion + DFS Approach
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public boolean isSymmetric(TreeNode root) {
+        // Base Case
+        if (root == null) {
+            return true;
+        }
+        return isTreeMirrored(root.left, root.right);
+    }
+
+    /**
+     * Using Recursion + DFS Approach
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    private boolean isTreeMirrored(TreeNode leftTree, TreeNode rightTree) {
+        // Base Case
+        if (leftTree == null) {
+            return rightTree == null;
+        }
+        // Recursion Calls
+        return leftTree != null && rightTree != null && leftTree.val == rightTree.val &&
+            isTreeMirrored(leftTree.left, rightTree.right) &&
+            isTreeMirrored(leftTree.right, rightTree.left);
+    }
+}
