7d66e09c945163607712618599c204e4cd1cfa4b
Surajit Rana <surajit1602@gmail.com>
2025-06-15 12:32:58 +0530

    Time: 1 ms (92.38%) | Memory: 41 MB (27.62%) - LeetSync

--- Strings/Simulation/1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
+++ Strings/Simulation/1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
@@ -1,11 +1,57 @@
 class Solution {
     /**
-     * Approach : Using String and Simulation Approach
+     * Approach II : Using StringBuilder and Simulation Approach
+     *
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(3 x N) ~ O(N)
+     */
+    public int maxDiff(int num) {
+        char[] digitChars = String.valueOf(num).toCharArray(); // SC: O(N)
+        int n = digitChars.length;
+        char x1 = 0;
+        char x2 = 0;
+        int minIndex = -1;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (x1 == 0 && digitChars[i] != '9') {
+                x1 = digitChars[i];
+            }
+            if (x2 == 0 && digitChars[i] != '1') {
+                if (digitChars[i] == '0') {
+                    continue;
+                }
+                x2 = digitChars[i];
+                minIndex = i;
+            }
+        }
+        // replace all occurences of x1 with '9' and x2 with '0' or '1' ('0' if minIndex > 0)
+        StringBuilder maxSb = new StringBuilder(); // SC: O(N)
+        StringBuilder minSb = new StringBuilder(); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (digitChars[i] == x1) {
+                maxSb.append('9');
+            } else {
+                maxSb.append(digitChars[i]);
+            }
+            if (digitChars[i] == x2) {
+                if (minIndex > 0) {
+                    minSb.append('0');
+                } else {
+                    minSb.append('1');
+                }
+            } else {
+                minSb.append(digitChars[i]);
+            }
+        }
+        return Integer.valueOf(maxSb.toString()) - Integer.valueOf(minSb.toString());
+    }
+
+    /**
+     * Approach I : Using String and Simulation Approach
      *
      * TC: O(2 x N) ~ O(N)
      * SC: O(2 x N) ~ O(N)
      */
-    public int maxDiff(int num) {
+    public int maxDiffApproachI(int num) {
         char[] digitCharsMax = String.valueOf(num).toCharArray(); // SC: O(N)
         char[] digitCharsMin = digitCharsMax.clone(); // SC: O(N)
         int n = digitCharsMax.length;
@@ -31,7 +77,7 @@ class Solution {
             }
             if (digitCharsMin[i] == x2) {
                 digitCharsMin[i] = minIndex > 0 ? '0' : '1';
-            }
+            } 
         }
         return Integer.valueOf(String.valueOf(digitCharsMax)) - 
             Integer.valueOf(String.valueOf(digitCharsMin));
