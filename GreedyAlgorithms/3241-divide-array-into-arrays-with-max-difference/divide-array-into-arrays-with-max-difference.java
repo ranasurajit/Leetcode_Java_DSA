30e005fd59192625e71baf151431057f77e4aeab
Surajit Rana <surajit1602@gmail.com>
2025-06-18 11:31:29 +0530

    Time: 23 ms (81.15%) | Memory: 61.9 MB (24.08%) - LeetSync

--- GreedyAlgorithms/3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
+++ GreedyAlgorithms/3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
@@ -1,11 +1,30 @@
 class Solution {
     /**
-     * Approach : Using Greedy + Sorting Approach
+     * Approach I : Using Greedy + Sorting Approach (Cleaner Approach)
+     *
+     * TC: O(N x log(N) + N / 3) ~ O(N x log(N))
+     * SC: O(1)
+     */
+    public int[][] divideArray(int[] nums, int k) {
+        int n = nums.length;
+        Arrays.sort(nums); // TC: O(N x log(N))
+        int[][] result = new int[n / 3][3];
+        for (int i = 0; i < n; i+=3) { // TC: O(N / 3)
+            if (nums[i + 2] - nums[i] > k) {
+                return new int[0][0];
+            }
+            result[i / 3] = new int[] { nums[i], nums[i + 1], nums[i + 2] };
+        }
+        return result;
+    }
+
+    /**
+     * Approach I : Using Greedy + Sorting Approach
      *
      * TC: O(N x log(N) + 3 x N) ~ O(N x log(N))
      * SC: O(1)
      */
-    public int[][] divideArray(int[] nums, int k) {
+    public int[][] divideArrayApproachI(int[] nums, int k) {
         int n = nums.length;
         Arrays.sort(nums); // TC: O(N x log(N))
         int[][] result = new int[n / 3][3];
