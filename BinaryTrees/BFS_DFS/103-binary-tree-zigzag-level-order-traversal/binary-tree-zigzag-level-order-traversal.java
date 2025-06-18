e3201ac82160bb8070ec98d83203628a85c6f62f
Surajit Rana <surajit1602@gmail.com>
2025-06-18 14:24:17 +0530

    Time: 1 ms (66.09%) | Memory: 42.4 MB (24.62%) - LeetSync

--- BinaryTrees/BFS_DFS/103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ BinaryTrees/BFS_DFS/103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     /**
-     * Approach : Using BFS Traversal Approach
+     * Approach : Using BFS Traversal Approach (Optimal Approach)
      *
      * TC: O(N)
      * SC: O(N)
@@ -30,15 +30,54 @@ class Solution {
         int level = 0;
         while (!queue.isEmpty()) { // TC: O(N)
             int size = queue.size();
+            Deque<Integer> temp = new ArrayDeque<Integer>();
+            for (int i = 0; i < size; i++) { // TC: O(K)
+                TreeNode current = queue.poll();
+                if ((level & 1) == 0) {
+                    // even levels
+                    temp.addLast(current.val);
+                } else {
+                    // odd levels
+                    temp.addFirst(current.val); // TC: O(K), where K = size
+                }
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+            path.add(new ArrayList<Integer>(temp));
+            level++;
+        }
+        return path;
+    }
+
+    /**
+     * Approach : Using BFS Traversal Approach
+     *
+     * TC: O(N + K ^ 2)
+     * SC: O(N)
+     */
+    public List<List<Integer>> zigzagLevelOrderBFSArrayList(TreeNode root) {
+        List<List<Integer>> path = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return path;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>(); // SC: O(N)
+        queue.offer(root);
+        int level = 0;
+        while (!queue.isEmpty()) { // TC: O(N)
+            int size = queue.size();
             List<Integer> temp = new ArrayList<Integer>();
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) { // TC: O(K)
                 TreeNode current = queue.poll();
                 if ((level & 1) == 0) {
                     // even levels
                     temp.add(current.val);
                 } else {
                     // odd levels
-                    temp.add(0, current.val);
+                    temp.add(0, current.val); // TC: O(K), where K = size
                 }
                 if (current.left != null) {
                     queue.offer(current.left);
