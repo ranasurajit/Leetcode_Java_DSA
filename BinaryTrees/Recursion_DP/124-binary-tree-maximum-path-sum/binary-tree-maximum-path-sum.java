5ab76df19f8e3aacbfbf94b6164bd15b78f8fac4
Surajit Rana <surajit1602@gmail.com>
2025-06-19 23:27:27 +0530

    Time: 4 ms (3.27%) | Memory: 47.6 MB (5.07%) - LeetSync

--- BinaryTrees/Recursion_DP/124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ BinaryTrees/Recursion_DP/124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -15,12 +15,59 @@
  */
 class Solution {
     /**
-     * Approach I : Using Recursion Approach
+     * Approach II : Using Memoization + Recursion Approach
      *
      * TC: O(N)
      * SC: O(N)
      */
     public int maxPathSum(TreeNode root) {
+        int[] maxSum = { Integer.MIN_VALUE };
+        Map<TreeNode, Integer> memo = new HashMap<TreeNode, Integer>();
+        solveMemoization(root, maxSum, memo);
+        return maxSum[0];
+    }
+
+    /**
+     * Using Memoization Approach
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    private int solveMemoization(TreeNode node, int[] maxSum, Map<TreeNode, Integer> memo) {
+        // Base Case
+        if (node == null) {
+            return 0;
+        }
+        // Memoization Calls
+        if (memo.containsKey(node)) {
+            return memo.get(node);
+        }
+        // Hypothesis - Recursion Leap of Faith
+        int leftSum = solveMemoization(node.left, maxSum, memo);
+        int rightSum = solveMemoization(node.right, maxSum, memo);
+        // Induction
+        // compute maxSum assuming if it passed through node else pass the best value to its parent
+        // check if leftSum or rightSum < 0 then do not contribute
+        if (leftSum < 0) {
+            leftSum = 0;
+        }
+        if (rightSum < 0) {
+            rightSum = 0;
+        }
+        maxSum[0] = Math.max(maxSum[0], leftSum + rightSum + node.val);
+        // return the best value to its parent
+        int currentSum = node.val + Math.max(0, Math.max(leftSum, rightSum));
+        memo.put(node, currentSum);
+        return currentSum;
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public int maxPathSumRecursion(TreeNode root) {
         int[] maxSum = { Integer.MIN_VALUE };
         solveRecursion(root, maxSum);
         return maxSum[0];
