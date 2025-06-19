5e6a16a1c095d68b37a319b73849773cfac7188d
Surajit Rana <surajit1602@gmail.com>
2025-06-19 10:21:15 +0530

    Time: 30 ms (96.45%) | Memory: 56.9 MB (54.61%) - LeetSync

--- GreedyAlgorithms/2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.java
+++ GreedyAlgorithms/2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.java
@@ -1,11 +1,34 @@
 class Solution {
     /**
-     * Approach : Using Greedy + Sorting + Simulation Approach
+     * Approach II : Using Sorting + Sliding Window Approach
      *
      * TC: O(N x log(N) + N) ~ O(N x log(N))
      * SC: O(1)
      */
     public int partitionArray(int[] nums, int k) {
+        int n = nums.length;
+        Arrays.sort(nums); // TC: O(N x log(N))
+        int i = 0; // start pointer of sliding window
+        int j = 0; // end pointer of sliding window
+        int count = 0;
+        while (j < n) {    // TC: O(N)
+            while (nums[j] - nums[i] > k) {
+                count++;
+                i = j;
+                break;
+            }
+            j++;
+        }
+        return count + 1;
+    }
+
+    /**
+     * Approach I : Using Greedy + Sorting + Simulation Approach
+     *
+     * TC: O(N x log(N) + N) ~ O(N x log(N))
+     * SC: O(1)
+     */
+    public int partitionArrayGreedy(int[] nums, int k) {
         int n = nums.length;
         Arrays.sort(nums); // TC: O(N x log(N))
         int count = 0;
