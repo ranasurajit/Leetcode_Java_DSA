8e6c609b24c50faee5a85e25d700bec37ede7799
Surajit Rana <surajit1602@gmail.com>
2025-06-19 22:15:23 +0530

    Time: 3 ms (14.94%) | Memory: 46.1 MB (5.02%) - LeetSync

--- BinaryTrees/Recursion_DP/543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ BinaryTrees/Recursion_DP/543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -15,12 +15,57 @@
  */
 class Solution {
     /**
-     * Approach : Using Recursion Approach
+     * Approach II : Using Recursion + Memoization Approach
+     *
+     * (DP Not needed here as each node is visited once)
+     * 
+     * DP helps only if:
+     * You're recomputing results for overlapping subproblems.
+     * You're solving a top-down problem with heavy subtree reuse.
+     *
+     * TC: O(N)
+     * SC: O(2 x N) ~ O(N)
+     */
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] diameter = { 0 }; // need to pass it as pass by reference
+        Map<TreeNode, Integer> memo = new HashMap<TreeNode, Integer>(); // SC: O(N)
+        solveMemoization(root, diameter, memo); // TC: O(N), SC: O(N)
+        return diameter[0];
+    }
+
+    /**
+     * Using Recursion + Memoization Approach
      *
      * TC: O(N)
      * SC: O(N)
      */
-    public int diameterOfBinaryTree(TreeNode root) {
+    private int solveMemoization(TreeNode node, int[] diameter, Map<TreeNode, Integer> memo) {
+        // Base Case
+        if (node == null) {
+            return 0;
+        }
+        // Memoization Check
+        if (memo.containsKey(node)) {
+            return memo.get(node);
+        }
+        // Hypothesis - Recursion Calls will do the magic
+        int leftHeight = solveMemoization(node.left, diameter, memo);
+        int rightHeight = solveMemoization(node.right, diameter, memo);
+        // if diameter passes through this node 'node'
+        diameter[0] = Math.max(diameter[0], leftHeight + rightHeight);
+        // if diameter does not pass through this node 'node' then return the best answer to its parent
+        int nodeHeight = 1 + Math.max(leftHeight, rightHeight);
+        memo.put(node, nodeHeight);
+        return nodeHeight;
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public int diameterOfBinaryTreeRecursion(TreeNode root) {
         int[] diameter = { 0 }; // need to pass it as pass by reference
         solveRecursion(root, diameter);
         return diameter[0];
