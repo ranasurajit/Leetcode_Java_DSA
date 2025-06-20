3b4e7e026431e359f8add8f70c5c1200b933aef4
Surajit Rana <surajit1602@gmail.com>
2025-06-21 02:11:42 +0530

    Time: 1 ms (95.64%) | Memory: 44.5 MB (52.33%) - LeetSync

---
+++ BinaryTrees/Recursion_DP/105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * Approach : Using Recursion Approach
+     *
+     * Intuition: pre-order guarantees that root node is at the beginning
+     * in-order guarantees the left and right sub-tree positions
+     *
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(2 x N) ~ O(N)
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = inorder.length;
+        // we will be storing inorder traversal array in HashMap to figure out index in O(1) complexity
+        Map<Integer, Integer> inorderMap = new HashMap<Integer, Integer>(); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            inorderMap.put(inorder[i], i);
+        }
+        int[] index = { 0 };
+        return solveRecursion(preorder, index, 0, n - 1, inorderMap); // TC: O(N), SC: O(N)
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC: O(N)
+     * SC: O(N) 
+     */
+    private TreeNode solveRecursion(int[] preorder, int[] index, int start, int end,
+        Map<Integer, Integer> inorderMap) {
+        // Base Case
+        if (index[0] == preorder.length) {
+            return null;
+        }
+        if (start > end) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[index[0]]);
+        int idx = inorderMap.get(preorder[index[0]]);
+        index[0]++;
+        root.left = solveRecursion(preorder, index, start, idx - 1, inorderMap);
+        root.right = solveRecursion(preorder, index, idx + 1, end, inorderMap);
+        return root;
+    }
+}
