a40400bcaeaaf48f3277252bfea2038cd2d741b3
Surajit Rana <surajit1602@gmail.com>
2025-06-23 22:06:16 +0530

    Time: 5 ms (100.00%) | Memory: 45 MB (80.28%) - LeetSync

--- BinarySearchTrees/235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ BinarySearchTrees/235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,6 +10,36 @@
 
 class Solution {
     /**
+     * Approach III : Using BST Property Approach
+     * 
+     * TC: O(H)
+     * SC: O(1)
+     * 
+     * where H = height of BST
+     * H = log(N) for balanced BST
+     * H = N for skewed Tree
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // Base Case
+        if (root == null) {
+            return null;
+        }
+        TreeNode current = root;
+        while (current != null) {
+            if (current.val < p.val && current.val < q.val) {
+                // both nodes 'p' and 'q' lie on the right side of 'current' node
+                current = current.right;
+            } else if (current.val > p.val && current.val > q.val) {
+                // both nodes 'p' and 'q' lie on the left side of 'current' node
+                current = current.left;
+            } else {
+                return current;
+            }
+        }
+        return null;
+    }
+
+    /**
      * Approach II : Using Recursion + BST Property Approach
      * 
      * TC: O(H)
@@ -19,7 +49,7 @@ class Solution {
      * H = log(N) for balanced BST
      * H = N for skewed Tree
      */
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    public TreeNode lowestCommonAncestorRecursionBST(TreeNode root, TreeNode p, TreeNode q) {
         // Base Case
         if (root == null) {
             return null;
