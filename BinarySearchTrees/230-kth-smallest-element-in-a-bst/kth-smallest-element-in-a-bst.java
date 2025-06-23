74fb1a0eee7bae62fc91325f50f450ad62895481
Surajit Rana <surajit1602@gmail.com>
2025-06-24 01:14:15 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (9.49%) - LeetSync

--- BinarySearchTrees/230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ BinarySearchTrees/230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -15,12 +15,45 @@
  */
 class Solution {
     /**
-     * Approach : Using DFS Inorder Traversal Based on K depth Approach
+     * Approach II : Using DFS Inorder Traversal K depth Approach (Without Space)
+     *
+     * TC: O(K)
+     * SC: O(K)
+     */
+    public int kthSmallest(TreeNode root, int k) {
+        int[] count = { 0 };
+        int[] result = { -1 };
+        dfsInorderBSTOptimal(root, k, count, result); // TC: O(K), SC: O(K)
+        return result[0];
+    }
+
+    /**
+     * Using DFS Inorder Traversal Based on K depth Approach
+     *
+     * TC: O(K)
+     * SC: O(K)
+     */
+    private void dfsInorderBSTOptimal(TreeNode root, int k, int[] count, int[] result) {
+        // Base Case
+        if (root == null) {
+            return;
+        }
+        dfsInorderBSTOptimal(root.left, k, count, result);
+        count[0]++;
+        if (count[0] == k) {
+            result[0] = root.val;
+            return;
+        }
+        dfsInorderBSTOptimal(root.right, k, count, result);
+    }
+
+    /**
+     * Approach I : Using DFS Inorder Traversal Based on K depth Approach (With Space)
      *
      * TC: O(K)
      * SC: O(2 x K) ~ O(K)
      */
-    public int kthSmallest(TreeNode root, int k) {
+    public int kthSmallestUsingArrayList(TreeNode root, int k) {
         List<Integer> result = new ArrayList<Integer>(); // SC: O(K)
         dfsInorderBST(root, k, result);
         return result.get(k - 1);
