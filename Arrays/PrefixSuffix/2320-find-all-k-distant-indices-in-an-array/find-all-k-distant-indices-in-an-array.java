1adc33fdf19a176228cea20c8e2be62474fc7ffd
Surajit Rana <surajit1602@gmail.com>
2025-06-24 10:46:02 +0530

    Time: 2 ms (97.25%) | Memory: 44.8 MB (71.90%) - LeetSync

--- Arrays/PrefixSuffix/2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.java
+++ Arrays/PrefixSuffix/2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.java
@@ -1,11 +1,33 @@
 class Solution {
     /**
-     * Approach : Using Array Pre-Processing Approach
+     * Approach II : Using Two Pointers Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+        int n = nums.length;
+        List<Integer> result = new ArrayList<Integer>();
+        int right = 0; // unknown minimum index
+        for (int j = 0; j < n; j++) { // TC: O(N)
+            if (nums[j] == key) {
+                int left = Math.max(right, j - k);
+                right = Math.min(n - 1, j + k) + 1;
+                for (int i = left; i < right; i++) {
+                    result.add(i);
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Approach I : Using Array Pre-Processing Approach
      *
      * TC: O(3 x N) ~ O(N)
      * SC: O(2 x N) ~ O(N)
      */
-    public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+    public List<Integer> findKDistantIndicesWithExtraSpace(int[] nums, int key, int k) {
         int n = nums.length;
         List<Integer> result = new ArrayList<Integer>();
         int[] prefixIndex = new int[n]; // SC: O(N)
