42272e2a62786a211ba1645ed15b5817d3dcd998
Surajit Rana <surajit1602@gmail.com>
2025-06-28 12:17:25 +0530

    Time: 5 ms (96.06%) | Memory: 44.8 MB (27.82%) - LeetSync

--- Heaps/2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.java
+++ Heaps/2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.java
@@ -1,31 +1,35 @@
 class Solution {
+    /**
+     * Approach : Using Heaps Approach
+     *
+     * TC: O(N x log(K)) + O(K) + O(K) ~ O(N x log(K))
+     * SC: O(K) + O(K) ~ O(K)
+     */
     public int[] maxSubsequence(int[] nums, int k) {
         int n = nums.length;
+        if (k == n) {
+            return nums;
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p, q) -> p[0] - q[0]); // SC: O(K)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (pq.size() < k) {
+                pq.offer(new int[] { nums[i], i }); // TC: O(log(K))
+            } else {
+                if (nums[i] > pq.peek()[0]) {
+                    pq.poll();
+                    pq.offer(new int[] { nums[i], i }); // TC: O(log(K))
+                }
+            }
+        }
+        PriorityQueue<int[]> pqSorted = new PriorityQueue<int[]>((p, q) -> p[1] - q[1]); // SC: O(K)
+        while (!pq.isEmpty()) { // TC: O(K)
+            pqSorted.offer(pq.poll());
+        }
         int[] result = new int[k];
-        PriorityQueue<Pair> pq = new PriorityQueue<Pair>((p, q) -> q.value - p.value);
-        for (int i = 0; i < n; i++) {
-            pq.offer(new Pair(nums[i], i));
-        }
-        PriorityQueue<Pair> sorted = new PriorityQueue<Pair>((p, q) -> p.index - q.index);
-        while (!pq.isEmpty() && k > 0) {
-            Pair current = pq.poll();
-            sorted.offer(current);
-            k--;
-        }
         int index = 0;
-        while (!sorted.isEmpty()) {
-            Pair current = sorted.poll();
-            result[index++] = current.value;
+        while (!pqSorted.isEmpty()) { // TC: O(K)
+            result[index++] = pqSorted.poll()[0];
         }
         return result;
-    }
-
-    class Pair {
-        int value;
-        int index;
-        public Pair(int value, int index) {
-            this.value = value;
-            this.index = index;
-        }
     }
 }
