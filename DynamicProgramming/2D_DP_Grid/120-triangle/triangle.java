3827a05c3600ae7129c9a8c5b07639c2e7a639c4
Surajit Rana <surajit1602@gmail.com>
2025-06-28 18:03:28 +0530

    Time: 1 ms (99.72%) | Memory: 44.1 MB (76.50%) - LeetSync

---
+++ DynamicProgramming/2D_DP_Grid/120-triangle/triangle.java
@@ -0,0 +1,77 @@
+class Solution {
+    /**
+     * Approach II : Using Memoization Approach
+     *
+     * TC: O(N x M)
+     * SC: O(N x M) + O(N)
+     *     - O(N x M) - memoization memory
+     *     - O(N) - recursion stack
+     *
+     * Accepted (45 / 45 testcases passed)
+     */
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        // we will start from index 0 till we reach the last row (n - 1)
+        int[][] memo = new int[n][triangle.get(n - 1).size()]; // SC: O(N x M)
+        for (int[] mem : memo) {
+            Arrays.fill(mem, -1);
+        }
+        return solveMemoization(0, 0, n - 1, triangle, memo);
+    }
+
+    /**
+     * Using Memoization Approach
+     *
+     * TC: O(2 ^ N)
+     * SC: O(N)
+     */
+    private int solveMemoization(int row, int col, int n, List<List<Integer>> triangle, int[][] memo) {
+        // Base Case
+        if (row == n) {
+            return triangle.get(row).get(col);
+        }
+        // Memoization Check
+        if (memo[row][col] != -1) {
+            return memo[row][col];
+        }
+        // Recursion Calls
+        int sum = triangle.get(row).get(col);
+        // from any index we can go down in col -> (col) and (col + 1)
+        int downSum = sum + solveMemoization(row + 1, col, n, triangle, memo);
+        int diagSum = sum + solveMemoization(row + 1, col + 1, n, triangle, memo);
+        return memo[row][col] = Math.min(downSum, diagSum);
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(2 ^ N)
+     * SC: O(N) - recursion stack
+     *
+     * Time Limit Exceeded (42 / 45 testcases passed)
+     */
+    public int minimumTotalRecursion(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        // we will start from index 0 till we reach the last row (n - 1)
+        return solveRecursion(0, 0, n - 1, triangle);
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC: O(2 ^ N)
+     * SC: O(N)
+     */
+    private int solveRecursion(int row, int col, int n, List<List<Integer>> triangle) {
+        // Base Case
+        if (row == n) {
+            return triangle.get(row).get(col);
+        }
+        // Recursion Calls
+        int sum = triangle.get(row).get(col);
+        // from any index we can go down in col -> (col) and (col + 1)
+        int downSum = sum + solveRecursion(row + 1, col, n, triangle);
+        int diagSum = sum + solveRecursion(row + 1, col + 1, n, triangle);
+        return Math.min(downSum, diagSum);
+    }
+}
