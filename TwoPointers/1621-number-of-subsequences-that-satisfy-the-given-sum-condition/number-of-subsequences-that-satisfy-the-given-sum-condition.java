57abe52521f6314d0df9c73fa17bfe83ffc58165
Surajit Rana <surajit1602@gmail.com>
2025-06-29 15:53:08 +0530

    Time: 28 ms (52.80%) | Memory: 55.2 MB (53.38%) - LeetSync

---
+++ TwoPointers/1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -0,0 +1,120 @@
+class Solution {
+    private static final int MOD = (int) 1e9 + 7;
+
+    /**
+     * Approach III : Using Two Pointers + Sorting + Array Pre-processing (Cleaner) Approach
+     *
+     * TC: O(N x log(N)) + O(N) ~ O(N x log(N))
+     * SC: O(N)
+     */
+    public int numSubseq(int[] nums, int target) {
+        int n = nums.length;
+        Arrays.sort(nums); // TC: O(N x log(N))
+        int[] power = new int[n]; // SC: O(N)
+        power[0] = 1; // 2 ^ 0 value
+        for (int i = 1; i < n; i++) { // TC: O(N)
+            power[i] = (2 * power[i - 1]) % MOD;
+        }
+        int count = 0;
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) { // TC: O(N)
+            if (nums[left] + nums[right] <= target) {
+                int diff = right - left;
+                count = (count % MOD + power[diff]) % MOD;
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Approach II : Using Binary Search + Sorting + Array Pre-processing Approach
+     *
+     * TC: O(N x log(N)) + O(N) + O(N x log(N)) ~ O(N x log(N))
+     * SC: O(N)
+     */
+    public int numSubseqBinarySearch(int[] nums, int target) {
+        int n = nums.length;
+        Arrays.sort(nums); // TC: O(N x log(N))
+        int[] power = new int[n]; // SC: O(N)
+        power[0] = 1; // 2 ^ 0 value
+        for (int i = 1; i < n; i++) { // TC: O(N)
+            power[i] = (2 * power[i - 1]) % MOD;
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            int min = nums[i];
+            int max = target - nums[i];
+            if (max >= min) {
+                int diff = upperBound(nums, n, max) - i - 1; // TC: O(log(N))
+                count = (count % MOD + power[diff] % MOD) % MOD;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Using Binary Search Approach
+     *
+     * TC: O(log(N))
+     * SC: O(1)
+     */
+    private int upperBound(int[] nums, int n, int x) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > x) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(2 ^ N)
+     * SC: O(N)
+     *
+     * Time Limit Exceeded (15 / 63 testcases passed)
+     */
+    public int numSubseqRecursion(int[] nums, int target) {
+        int n = nums.length;
+        Arrays.sort(nums); // TC: O(N x log(N))
+        int[] count = { 0 };
+        List<Integer> current = new ArrayList<Integer>();
+        solveRecursion(0, n, nums, current, target, count);
+        return count[0];
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC: O(2 ^ N)
+     * SC: O(N)
+     */
+    private void solveRecursion(int idx, int n, int[] nums, List<Integer> current,
+        int target, int[] count) {
+        // Base Case
+        if (idx == n) {
+            if (current.size() > 0 && current.get(0) + current.get(current.size() - 1) <= target) {
+                count[0] = ((count[0] % MOD) + 1) % MOD;
+            }
+            return;
+        }
+        // Recursive Calls
+        // take or not take
+        // take
+        current.add(nums[idx]);
+        solveRecursion(idx + 1, n, nums, current, target, count);
+        // not take
+        current.remove(current.size() - 1);
+        solveRecursion(idx + 1, n, nums, current, target, count);
+    }
+}
