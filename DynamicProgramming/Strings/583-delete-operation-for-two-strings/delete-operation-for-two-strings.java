33d09989ed5d31504b7ac08580a4fcf86dd6f24d
Surajit Rana <surajit1602@gmail.com>
2025-07-06 00:33:44 +0530

    Time: 7 ms (71.87%) | Memory: 45.7 MB (5.05%) - LeetSync

--- DynamicProgramming/Strings/583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ DynamicProgramming/Strings/583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,5 +1,41 @@
 class Solution {
     /**
+     * Approach II : Using Space Optimization Approach
+     * 
+     * TC: O(M x N)
+     * SC: O(N) + O(N)
+     * 
+     * - O(N) - prev and current array memory
+     * 
+     * Accepted (1306 / 1306 testcases passed)
+     */
+    public int minDistance(String word1, String word2) {
+        /**
+         * we can convert this problem into LCS problem as
+         * number of steps needed to delete from word1 and 
+         * word2 = word1.length() - length(LCS) + 
+         * word2.length() - length(LCS)
+         */
+        int m = word1.length();
+        int n = word2.length();
+        // Initialization
+        int[] prev = new int[n + 1]; // SC: O(N)
+        // Iterative Calls
+        for (int i = 1; i < m + 1; i++) {     // TC: O(M)
+            int[] current = new int[n + 1]; // SC: O(N)
+            for (int j = 1; j < n + 1; j++) { // TC: O(N)
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    current[j] = 1 + prev[j - 1];
+                } else {
+                    current[j] = Math.max(prev[j], current[j - 1]);
+                }
+            }
+            prev = current.clone();
+        }
+        return m + n - 2 * prev[n];
+    }
+
+    /**
      * Approach I : Using Tabulation Approach
      * 
      * TC: O(M x N)
@@ -7,9 +43,9 @@ class Solution {
      * 
      * - O(M x N) - dp array memory
      * 
-     * Accepted (1111 / 1111 testcases passed)
+     * Accepted (1306 / 1306 testcases passed)
      */
-    public int minDistance(String word1, String word2) {
+    public int minDistanceTabulation(String word1, String word2) {
         /**
          * we can convert this problem into LCS problem as
          * number of steps needed to delete from word1 and 
