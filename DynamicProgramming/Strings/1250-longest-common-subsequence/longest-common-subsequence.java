a8b08f2ea6da7429af9a6337356e8cee83e6502e
Surajit Rana <surajit1602@gmail.com>
2025-07-05 18:42:40 +0530

    Time: 24 ms (31.74%) | Memory: 51 MB (42.55%) - LeetSync

--- DynamicProgramming/Strings/1250-longest-common-subsequence/longest-common-subsequence.java
+++ DynamicProgramming/Strings/1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,132 +1,82 @@
 class Solution {
     /**
-     * Approach IV : Using Space Optimization
-     *
-     * TC: O(M x N)
-     * SC: O(2 x N) ~ O(N)
+     * Approach II : Using Memoization (Top-Down DP) Approach
      * 
-     * @param text1
-     * @param text2
-     * @return
+     * TC: O(M x N) + O(M x N) ~ O(M x N)
+     * SC: O(M x N) + O(M + N)
+     * 
+     * - O(M x N) - memoization memory
+     * - O(M + N) - recursion stack
+     *
+     * Accepted (47 / 47 testcases passed)
      */
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
-        int[] current = new int[n + 1];
-        int[] prev = new int[n + 1];
-        // iterative call - convert (dp[i] as current and dp[i - 1] as prev)
-        for (int i = 1; i < m + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    current[j] = 1 + prev[j - 1];
-                } else {
-                    current[j] = Math.max(prev[j], current[j - 1]);
-                }
-            }
-            prev = current.clone();
+        int[][] memo = new int[m + 1][n + 1]; // SC: O(M x N)
+        for (int[] mem : memo) {  // TC: O(M)
+            Arrays.fill(mem, -1); // TC: O(N)
         }
-        return prev[n];
+        return solveMemoization(text1, text2, m - 1, n - 1, memo); // TC: O(M x N), SC: O(M + N)
     }
 
     /**
-     * Approach III : Using Tabulation
+     * Using Memoization Approach
      *
      * TC: O(M x N)
-     * SC: O(M x N)
-     * 
-     * @param text1
-     * @param text2
-     * @return
+     * SC: O(M + N)
      */
-    public int longestCommonSubsequenceTabulation(String text1, String text2) {
-        int m = text1.length();
-        int n = text2.length();
-        int[][] dp = new int[m + 1][n + 1];
-        // initialization
-        for (int i = 0; i < m + 1; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 0;
-                }
-            }
-        }
-        // iterative call
-        for (int i = 1; i < m + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
-        return dp[m][n];
-    }
-
-    /**
-     * Approach II : Using Memoization
-     *
-     * TC: O(M x N)
-     * SC: O((M x N) + (M + N))
-     * 
-     * @param text1
-     * @param text2
-     * @return
-     */
-    public int longestCommonSubsequenceMemoization(String text1, String text2) {
-        int m = text1.length();
-        int n = text2.length();
-        int[][] memo = new int[m + 1][n + 1];
-        for (int[] memoItem : memo) {
-            Arrays.fill(memoItem, -1);
-        }
-        return solveMemoization(text1, text2, m, n, memo);
-    }
-
-    private int solveMemoization(String text1, String text2,
-        int m, int n, int[][] memo) {
-        // Base case
-        if (m == 0 || n == 0) {
+    private int solveMemoization(String text1, String text2, int m, int n, int[][] memo) {
+        // Base Case
+        if (m < 0 || n < 0) {
             return 0;
         }
         // Memoization Check
         if (memo[m][n] != -1) {
             return memo[m][n];
         }
-        // Recursive Calls
-        if (text1.charAt(m - 1) == text2.charAt(n - 1)) {
-            return memo[m][n] = 
-                1 + solveMemoization(text1, text2, m - 1, n - 1, memo);
+        // Recursion Calls
+        if (text1.charAt(m) == text2.charAt(n)) {
+            return memo[m][n] = 1 + solveMemoization(text1, text2, m - 1, n - 1, memo);
         } else {
-            return memo[m][n] = 
-                Math.max(solveMemoization(text1, text2, m - 1, n, memo),
-                         solveMemoization(text1, text2, m, n - 1, memo));
+            return memo[m][n] = Math.max(solveMemoization(text1, text2, m - 1, n, memo),
+                solveMemoization(text1, text2, m, n - 1, memo));
         }
     }
 
     /**
-     * Approach I : Using Recursion
+     * Approach I : Using Recursion Approach
      *
      * TC: O(2 ^ (M + N))
      * SC: O(M + N)
+     *
+     * - O(M + N) - recursion stack
+     *
+     * Time Limit Exceeded (17 / 47 testcases passed)
      */
     public int longestCommonSubsequenceRecursion(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
-        return solveRecursion(text1, text2, m, n);
+        return solveRecursion(text1, text2, m - 1, n - 1);
     }
 
+    /**
+     * Using Recursion Approach
+     *
+     * TC: O(2 ^ (M + N))
+     * SC: O(M + N)
+     */
     private int solveRecursion(String text1, String text2, int m, int n) {
-        // Base case
-        if (m == 0 || n == 0) {
+        // Base Case
+        if (m < 0 || n < 0) {
             return 0;
         }
-        // Recursive Calls
-        if (text1.charAt(m - 1) == text2.charAt(n - 1)) {
+        // Recursion Calls
+        if (text1.charAt(m) == text2.charAt(n)) {
             return 1 + solveRecursion(text1, text2, m - 1, n - 1);
         } else {
             return Math.max(solveRecursion(text1, text2, m - 1, n),
-                            solveRecursion(text1, text2, m, n - 1));
+                solveRecursion(text1, text2, m, n - 1));
         }
     }
 }
