34091d23907f44258b4ced72ff88d555067a05b0
Surajit Rana <surajit1602@gmail.com>
2025-07-05 18:50:25 +0530

    Time: 18 ms (90.16%) | Memory: 51 MB (42.55%) - LeetSync

--- DynamicProgramming/Strings/1250-longest-common-subsequence/longest-common-subsequence.java
+++ DynamicProgramming/Strings/1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,5 +1,32 @@
 class Solution {
     /**
+     * Approach III : Using Tabulation (Bottom-Up DP) Approach
+     * 
+     * TC: O(M x N)
+     * SC: O(M x N)
+     * 
+     * - O(M x N) - dp array memory
+     *
+     * Accepted (47 / 47 testcases passed)
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        // Initialization
+        int[][] dp = new int[m + 1][n + 1]; // SC: O(M x N)
+        for (int i = 1; i < m + 1; i++) { // TC: O(M)
+            for (int j = 1; j < n + 1; j++) { // TC: O(N)
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    /**
      * Approach II : Using Memoization (Top-Down DP) Approach
      * 
      * TC: O(M x N) + O(M x N) ~ O(M x N)
@@ -10,7 +37,7 @@ class Solution {
      *
      * Accepted (47 / 47 testcases passed)
      */
-    public int longestCommonSubsequence(String text1, String text2) {
+    public int longestCommonSubsequenceMemoization(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
         int[][] memo = new int[m + 1][n + 1]; // SC: O(M x N)
