e57852ed600a2db730007bcf91991ff2fe4ab856
Surajit Rana <surajit1602@gmail.com>
2025-07-06 00:31:21 +0530

    Time: 6 ms (93.79%) | Memory: 44.8 MB (52.30%) - LeetSync

---
+++ DynamicProgramming/Strings/583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * Approach I : Using Tabulation Approach
+     * 
+     * TC: O(M x N)
+     * SC: O(M x N)
+     * 
+     * - O(M x N) - dp array memory
+     * 
+     * Accepted (1111 / 1111 testcases passed)
+     */
+    public int minDistance(String word1, String word2) {
+        /**
+         * we can convert this problem into LCS problem as
+         * number of steps needed to delete from word1 and 
+         * word2 = word1.length() - length(LCS) + 
+         * word2.length() - length(LCS)
+         */
+        int m = word1.length();
+        int n = word2.length();
+        // Initialization
+        int[][] dp = new int[m + 1][n + 1]; // SC: O(M x N)
+        // Iterative Calls
+        for (int i = 1; i < m + 1; i++) {     // TC: O(M)
+            for (int j = 1; j < n + 1; j++) { // TC: O(N)
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return m + n - 2 * dp[m][n];
+    }
+}
