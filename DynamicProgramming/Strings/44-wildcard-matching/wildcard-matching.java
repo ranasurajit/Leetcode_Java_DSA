4074613c09356fd4bbeec15490ddf8ba0375cc5c
Surajit Rana <surajit1602@gmail.com>
2025-07-07 00:24:27 +0530

    Time: 15 ms (84.36%) | Memory: 48.4 MB (31.69%) - LeetSync

---
+++ DynamicProgramming/Strings/44-wildcard-matching/wildcard-matching.java
@@ -0,0 +1,132 @@
+class Solution {
+    /**
+     * Approach II : Using Memoization (Top-Down DP) Approach
+     * 
+     * TC: O(M x N) + O(M x N) ~ O(M x N)
+     * SC: O(M x N) + O(M + N)
+     * 
+     * - O(M x N) - memoization memory
+     * - O(M + N) - recursion stack
+     *
+     * Accepted (1811 / 1811 testcases passed)
+     */
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        int[][] memo = new int[m + 1][n + 1]; // SC: O(M x N)
+        for (int[] mem : memo) { // TC: O(M)
+            Arrays.fill(mem, -1); // TC: O(N)
+        }
+        return solveMemoization(s, p, m - 1, n - 1, memo); // TC: O(M x N), SC: O(M + N)
+    }
+
+    /**
+     * Using Memoization Approach
+     *
+     * TC: O(M x N)
+     * SC: O(M + N)
+     */
+    private boolean solveMemoization(String s, String p, int m, int n, int[][] memo) {
+        // Base Case
+        if (m < 0 && n < 0) {
+            // we exhaused both so there is certainly a Match found in Strings s and p
+            return true;
+        }
+        if (n < 0) {
+            // we still have few characters in String s left so we don't have a Match
+            return false;
+        }
+        if (m < 0) {
+            // we still have characters in String p left so Match will happen if all characters left is *
+            for (int k = 0; k <= n; k++) {
+                if (p.charAt(k) != '*') {
+                    return false;
+                }
+            }
+            return true;
+        }
+        // Memoization Check
+        if (memo[m][n] != -1) {
+            return memo[m][n] == 1;
+        }
+        // Recursion Calls
+        if (s.charAt(m) == p.charAt(n) || p.charAt(n) == '?') {
+            // we have a match and we can further check in both Strings shrinking their size
+            boolean result = solveMemoization(s, p, m - 1, n - 1, memo);
+            memo[m][n] = result ? 1 : 0;
+            return result;
+        }
+        if (p.charAt(n) == '*') {
+            /**
+             * case 1: we can think as if * denotes nothing i.e. '' and shrink n index
+             * case 2: we can think as if * denotes character of any length so we can 
+             * shrink m keeping n index intact
+             */
+            boolean result =
+                solveMemoization(s, p, m, n - 1, memo) || solveMemoization(s, p, m - 1, n, memo);
+            memo[m][n] = result ? 1 : 0;
+            return result;
+        }
+        // default return
+        memo[m][n] = 0;
+        return false;
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(2 ^ (M + N))
+     * SC: O(M + N)
+     *
+     * - O(M + N) - recursion stack
+     *
+     * Time Limit Exceeded (1639 / 1811 testcases passed)
+     */
+    public boolean isMatchRecursion(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        return solveRecursion(s, p, m - 1, n - 1); // TC: O(2 ^ (M + N)), SC: O(M + N)
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC: O(2 ^ (M + N))
+     * SC: O(M + N)
+     */
+    private boolean solveRecursion(String s, String p, int m, int n) {
+        // Base Case
+        if (m < 0 && n < 0) {
+            // we exhaused both so there is certainly a Match found in Strings s and p
+            return true;
+        }
+        if (n < 0) {
+            // we still have few characters in String s left so we don't have a Match
+            return false;
+        }
+        if (m < 0) {
+            // we still have characters in String p left so Match will happen if all characters left is *
+            for (int k = 0; k <= n; k++) {
+                if (p.charAt(k) != '*') {
+                    return false;
+                }
+            }
+            return true;
+        }
+        // Recursion Calls
+        if (s.charAt(m) == p.charAt(n) || p.charAt(n) == '?') {
+            // we have a match and we can further check in both Strings shrinking their size
+            return solveRecursion(s, p, m - 1, n - 1);
+        }
+        if (p.charAt(n) == '*') {
+            /**
+             * case 1: we can think as if * denotes nothing i.e. '' and shrink n index
+             * case 2: we can think as if * denotes character of any length so we can 
+             * shrink m keeping n index intact
+             */
+            return solveRecursion(s, p, m, n - 1) || solveRecursion(s, p, m - 1, n);
+        }
+        // default return
+        return false;
+    }
+}
