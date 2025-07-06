09f84966e1790cf8a9b355238e4e6461b7abe2cb
Surajit Rana <surajit1602@gmail.com>
2025-07-06 15:24:10 +0530

    Time: 27 ms (44.53%) | Memory: 45.2 MB (87.55%) - LeetSync

--- DynamicProgramming/Strings/1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ DynamicProgramming/Strings/1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,5 +1,43 @@
 class Solution {
     /**
+     * Approach II : Using Space Optimization (Optimized DP) Approach
+     * 
+     * TC: O(N x N) + O(N) ~ O(N x N)
+     * SC: O(N) + O(N) ~ O(N)
+     * 
+     * - O(N) - prev and current array memory
+     *
+     * Accepted (58 / 58 testcases passed)
+     */
+    public int minInsertions(String s) {
+        int n = s.length();
+        String revStr = reverse(s, n); // TC: O(N)
+        /**
+         * this problem can be reduced to calculating length of 
+         * Longest Common Subsequence (LCS) and then returning
+         * n - LCS.
+         * Intuition: We need to insert characters that are not
+         * common between String s and reverse of String s
+         */
+        // Using Space Optimization Approach
+        // Initialization
+        int[] prev = new int[n + 1]; // SC: O(N)
+        // Iterative Calls
+        for (int i = 1; i < n + 1; i++) { // TC: O(N)
+            int[] current = new int[n + 1]; // SC: O(N)
+            for (int j = 1; j < n + 1; j++) { // TC: O(N)
+                if (s.charAt(i - 1) == revStr.charAt(j - 1)) {
+                    current[j] = 1 + prev[j - 1];
+                } else {
+                    current[j] = Math.max(prev[j], current[j - 1]);
+                }
+            }
+            prev = current.clone();
+        }
+        return n - prev[n];
+    }
+
+    /**
      * Approach I : Using Tabulation (Bottom-Up DP) Approach
      * 
      * TC: O(N x N) + O(N) ~ O(N x N)
@@ -7,9 +45,9 @@ class Solution {
      * 
      * - O(N x N) - dp array memory
      *
-     * Accepted (47 / 47 testcases passed)
+     * Accepted (58 / 58 testcases passed)
      */
-    public int minInsertions(String s) {
+    public int minInsertionsTabulation(String s) {
         int n = s.length();
         String revStr = reverse(s, n); // TC: O(N)
         /**
