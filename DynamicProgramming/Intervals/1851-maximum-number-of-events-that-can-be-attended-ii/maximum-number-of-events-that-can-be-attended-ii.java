01b543fd2eccc0422dffcf545f0911734986bad7
Surajit Rana <surajit1602@gmail.com>
2025-07-08 19:49:34 +0530

    Time: 104 ms (50.00%) | Memory: 136.7 MB (28.36%) - LeetSync

--- DynamicProgramming/Intervals/1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.java
+++ DynamicProgramming/Intervals/1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,29 +1,32 @@
 class Solution {
     /**
-     * Approach II : Using Memoization Approach
+     * Approach II : Using Memoization (Top-Down DP) Approach
      *
-     * TC: O(2 ^ N) + O(N x log(N)) ~ O(2 ^ N)
-     * SC: O(N)
+     * TC: O(N x K) + O(N x K x log(N))
+     * SC: O(N x K) + O(N)
      *
-     * Time Limit Exceeded (64 / 69 testcases passed)
+     * - O(N x K) - memoization array memory
+     * - O(N) - recursion stack
+     *
+     * Accepted (69 / 69 testcases passed)
      */
     public int maxValue(int[][] events, int k) {
         int n = events.length;
-        Arrays.sort(events, (int[] a, int[] b) -> a[0] - b[0]); // TC: O(N x log(N))
-        int[][] memo = new int[n + 1][k + 1];
-        for (int[] mem : memo) {
-            Arrays.fill(mem, -1);
+        Arrays.sort(events, (a, b) -> a[0] - b[0]); // TC: O(N x log(N))
+        int[][] memo = new int[n + 1][k + 1]; // SC: O(N x K)
+        for (int[] mem : memo) { // TC: O(N)
+            Arrays.fill(mem, -1); // TC: O(K)
         }
-        return solveMemoization(0, n, k, events, memo); // TC: O(2 ^ N), SC: O(N)
+        return solveMemoization(0, n, events, k, memo); // TC: O(N x K x log(N)), SC: O(N)
     }
 
     /**
      * Using Memoization Approach
      *
-     * TC: O(2 ^ N)
+     * TC: O(N x K x log(N))
      * SC: O(N)
      */
-    private int solveMemoization(int idx, int n, int k, int[][] events, int[][] memo) {
+    private int solveMemoization(int idx, int n, int[][] events, int k, int[][] memo) {
         // Base Case
         if (idx == n || k == 0) {
             return 0;
@@ -33,23 +36,60 @@ class Solution {
             return memo[idx][k];
         }
         // Recursion Calls
-        // we can choose to pick or skip
-        int skip = solveMemoization(idx + 1, n, k, events, memo);
-        int nextIdx = findNextNonOverlappingInterval(events, idx + 1, events[idx][1]); // TC: O(log(N))
-        int pick = events[idx][2] + solveMemoization(nextIdx, n, k - 1, events, memo);
+        // we have option to pick or skip an event at index 'idx'
+        // skip
+        int skip = solveMemoization(idx + 1, n, events, k, memo);
+        int nextValidIndex = getNextValidEventIndex(idx + 1, n, events[idx][1], events); // TC: O(log(N))
+        int pick = events[idx][2] + solveMemoization(nextValidIndex, n, events, k - 1, memo);
         return memo[idx][k] = Math.max(pick, skip);
     }
 
     /**
-     * Using Binary Search to find next non-overlapping index
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(2 ^ N x log(N)) + O(N x log(N)) ~ O(2 ^ N x log(N))
+     * SC: O(N)
+     *
+     * - O(N) - recursion stack
+     *
+     * Time Limit Exceeded (64 / 69 testcases passed)
+     */
+    public int maxValueRecursion(int[][] events, int k) {
+        int n = events.length;
+        Arrays.sort(events, (a, b) -> a[0] - b[0]); // TC: O(N x log(N))
+        return solveRecursion(0, n, events, k); // TC: O(2 ^ N x log(N)), SC: O(N)
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC: O(2 ^ N x log(N))
+     * SC: O(N)
+     */
+    private int solveRecursion(int idx, int n, int[][] events, int k) {
+        // Base Case
+        if (idx == n || k == 0) {
+            return 0;
+        }
+        // Recursion Calls
+        // we have option to pick or skip an event at index 'idx'
+        // skip
+        int skip = solveRecursion(idx + 1, n, events, k);
+        int nextValidIndex = getNextValidEventIndex(idx + 1, n, events[idx][1], events); // TC: O(log(N))
+        int pick = events[idx][2] + solveRecursion(nextValidIndex, n, events, k - 1);
+        return Math.max(pick, skip);
+    }
+
+    /**
+     * Approach : Using Binary Search
      *
      * TC: O(log(N))
      * SC: O(1)
      */
-    private int findNextNonOverlappingInterval(int[][] events, int start, int endTime) {
+    private int getNextValidEventIndex(int start, int n, int endTime, int[][] events) {
         int low = start;
-        int high = events.length - 1;
-        while (low <= high) {
+        int high = n - 1;
+        while (low <= high) { // TC: O(log(N))
             int mid = low + (high - low) / 2;
             if (events[mid][0] > endTime) {
                 high = mid - 1;
@@ -58,44 +98,5 @@ class Solution {
             }
         }
         return low;
-    }
-
-    /**
-     * Approach I : Using Recursion Approach
-     *
-     * TC: O(2 ^ N) + O(N x log(N)) ~ O(2 ^ N)
-     * SC: O(N)
-     *
-     * Time Limit Exceeded (64 / 69 testcases passed)
-     */
-    public int maxValueRecursion(int[][] events, int k) {
-        int n = events.length;
-        Arrays.sort(events, (int[] a, int[] b) -> a[1] - b[1]); // TC: O(N x log(N))
-        return solveRecursion(0, -1, n, k, events); // TC: O(2 ^ N), SC: O(N)
-    }
-
-    /**
-     * Using Recursion Approach
-     *
-     * TC: O(2 ^ N)
-     * SC: O(N)
-     */
-    private int solveRecursion(int idx, int prevIndex, int n, int k, int[][] events) {
-        // Base Case
-        if (idx == n || k == 0) {
-            return 0;
-        }
-        // Recursion Calls
-        int pick = 0;
-        int skip = 0;
-        if (prevIndex == -1 || events[idx][0] > events[prevIndex][1]) {
-            // we can choose to pick or skip
-            pick = events[idx][2] + solveRecursion(idx + 1, idx, n, k - 1, events);
-            skip = solveRecursion(idx + 1, prevIndex, n, k, events);
-        } else {
-            // we cannot choose to pick anyway
-            skip = solveRecursion(idx + 1, prevIndex, n, k, events);
-        }
-        return Math.max(pick, skip);
     }
 }
