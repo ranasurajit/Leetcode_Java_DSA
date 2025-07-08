93e740dc92edc3bb8cfa777b78c5b79b01e55e3e
Surajit Rana <surajit1602@gmail.com>
2025-07-08 22:38:14 +0530

    Time: 382 ms (10.92%) | Memory: 79.9 MB (6.84%) - LeetSync

--- DynamicProgramming/LIS/300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ DynamicProgramming/LIS/300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,104 +1,50 @@
 class Solution {
     /**
-     * Approach IV : Using Space Optimization Approach
+     * Approach II : Using Memoization (Top-Down DP) Approach
+     * 
+     * TC: O(N x N) + O(N x N) ~ O(N x N)
+     * SC: O(N x N) + O(N)
+     * 
+     * - O(N x N) - memoization memory
+     * - O(N) - recursion stack
      *
-     * TC: O(N x N)
-     * SC: O(2 x N) ~ O(N)
-     *
-     * Accepted (55 / 55 testcases passed) - Beats < 25%
+     * Accepted (55 / 55 testcases passed)
      */
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[] next = new int[n + 1];
-        int[] current = new int[n + 1];
-        // Initialization - not needed as base case returns 0
-        // Iterative Calls
-        for (int i = n - 1; i >= 0; i--) { // TC: O(N)
-            for (int j = i - 1; j >= -1; j--) { // TC: O(N)
-                int nottake = next[j + 1];
-                int take = 0;
-                // Take - Check if prevIndex = -1 or nums[index] > nums[prevIndex] for LIS
-                if (j == -1 || nums[i] > nums[j]) {
-                    // Take - prevIndex becomes index and 1 is contributed to length
-                    take = 1 + next[i + 1];
-                }
-                current[j + 1] = Math.max(take, nottake);
-            }
-            next = current.clone();
+        int[][] memo = new int[n + 1][n + 1]; // SC: O(N x N)
+        for (int[] mem : memo) {  // TC: O(N)
+            Arrays.fill(mem, -1); // TC: O(N)
         }
-        return next[0];
-    }
-
-    /**
-     * Approach III : Using Tabulation Approach
-     *
-     * TC: O(N x N)
-     * SC: O(N x N)
-     *
-     * Accepted (55 / 55 testcases passed) - Beats < 25%
-     */
-    public int lengthOfLISTabulation(int[] nums) {
-        int n = nums.length;
-        int[][] dp = new int[n + 1][n + 1]; // states are index and prevIndex
-        // Initialization - not needed as base case returns 0
-        // Iterative Calls
-        for (int i = n - 1; i >= 0; i--) { // TC: O(N)
-            for (int j = i - 1; j >= -1; j--) { // TC: O(N)
-                int nottake = dp[i + 1][j + 1];
-                int take = 0;
-                // Take - Check if prevIndex = -1 or nums[index] > nums[prevIndex] for LIS
-                if (j == -1 || nums[i] > nums[j]) {
-                    // Take - prevIndex becomes index and 1 is contributed to length
-                    take = 1 + dp[i + 1][i + 1];
-                }
-                dp[i][j + 1] = Math.max(take, nottake);
-            }
-        }
-        return dp[0][0];
-    }
-
-    /**
-     * Approach II : Using Memoization Approach
-     *
-     * TC: O(N x N)
-     * SC: O(N x N + N)
-     *
-     * Accepted (55 / 55 testcases passed) - Beats < 10%
-     */
-    public int lengthOfLISMemoization(int[] nums) {
-        int n = nums.length;
-        int[][] memo = new int[n][n + 1]; // states are index and prevIndex
-        for (int[] mem : memo) {
-            Arrays.fill(mem, -1);
-        }
-        return solveMemoization(0, -1, nums, n, memo);
+        return solveMemoization(0, -1, n, nums, memo); // TC: O(N x N), SC: O(N)
     }
 
     /**
      * Using Memoization Approach
      *
      * TC: O(N x N)
-     * SC: O(N x N + N)
+     * SC: O(N)
      */
-    private int solveMemoization(int index, int prevIndex, int[] nums, int n, int[][] memo) {
+    private int solveMemoization(int idx, int prevIdx, int n, int[] nums, int[][] memo) {
         // Base Case
-        if (index == n) {
+        if (idx == n) {
             return 0;
         }
-        // Memoization Check - shifting of coordinates for prevIndex to accomodate -1 to n
-        if (memo[index][prevIndex + 1] != -1) {
-            return memo[index][prevIndex + 1];
+        // Memoization Check
+        if (memo[idx][prevIdx + 1] != -1) {
+            return memo[idx][prevIdx + 1];
         }
-        // Recursion Calls 
-        // Not Take - prevIndex remains same in this case and no contribution to length
-        int nottake = 0 + solveMemoization(index + 1, prevIndex, nums, n, memo);
-        int take = 0;
-        // Take - Check if prevIndex = -1 or nums[index] > nums[prevIndex] for LIS
-        if (prevIndex == -1 || nums[index] > nums[prevIndex]) {
-             // Take - prevIndex becomes index and 1 is contributed to length
-            take = 1 + solveMemoization(index + 1, index, nums, n, memo);
+        // Recursion Calls
+        // we can opt to pick or skip
+        // skip
+        int skip = solveMemoization(idx + 1, prevIdx, n, nums, memo);
+        // pick
+        int pick = 0;
+        if (prevIdx == -1 || nums[idx] > nums[prevIdx]) {
+            // since we picked so we increase the length of LIS by 1
+            pick =  1 + solveMemoization(idx + 1, idx, n, nums, memo);
         }
-        return memo[index][prevIndex + 1] = Math.max(take, nottake);
+        return memo[idx][prevIdx + 1] = Math.max(pick, skip);
     }
 
     /**
@@ -107,33 +53,36 @@ class Solution {
      * TC: O(2 ^ N)
      * SC: O(N)
      *
+     * - O(N) - recursion stack
+     *
      * Time Limit Exceeded (22 / 55 testcases passed)
      */
     public int lengthOfLISRecursion(int[] nums) {
         int n = nums.length;
-        return solveRecursion(0, -1, nums, n);
+        return solveRecursion(0, -1, n, nums); // TC: O(2 ^ N)), SC: O(N)
     }
 
     /**
      * Using Recursion Approach
      *
-     * TC: O(2 ^ N)
+     * TC: O(2 ^ N))
      * SC: O(N)
      */
-    private int solveRecursion(int index, int prevIndex, int[] nums, int n) {
+    private int solveRecursion(int idx, int prevIdx, int n, int[] nums) {
         // Base Case
-        if (index == n) {
+        if (idx == n) {
             return 0;
         }
-        // Recursion Calls 
-        // Not Take - prevIndex remains same in this case and no contribution to length
-        int nottake = 0 + solveRecursion(index + 1, prevIndex, nums, n);
-        int take = 0;
-        // Take - Check if prevIndex = -1 or nums[index] > nums[prevIndex] for LIS
-        if (prevIndex == -1 || nums[index] > nums[prevIndex]) {
-             // Take - prevIndex becomes index and 1 is contributed to length
-            take = 1 + solveRecursion(index + 1, index, nums, n);
+        // Recursion Calls
+        // we can opt to pick or skip
+        // skip
+        int skip = solveRecursion(idx + 1, prevIdx, n, nums);
+        // pick
+        int pick = 0;
+        if (prevIdx == -1 || nums[idx] > nums[prevIdx]) {
+            // since we picked so we increase the length of LIS by 1
+            pick =  1 + solveRecursion(idx + 1, idx, n, nums);
         }
-        return Math.max(take, nottake);
+        return Math.max(pick, skip);
     }
 }
