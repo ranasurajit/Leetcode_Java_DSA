eebe3b23f9a5ff20175e7b964093dee1cb54addd
Surajit Rana <surajit1602@gmail.com>
2025-07-12 22:54:13 +0530

    Time: 6 ms (45.82%) | Memory: 46.1 MB (54.01%) - LeetSync

--- Graphs/TopologicalSort/210-course-schedule-ii/course-schedule-ii.java
+++ Graphs/TopologicalSort/210-course-schedule-ii/course-schedule-ii.java
@@ -1,62 +1,87 @@
 class Solution {
     /**
-     * Using Topological Sort Approach
-     *
-     * TC: O(V + E)
-     * SC: O(V + E)
+     * Approach I : Using DFS Approach
+     * 
+     * TC: O(E) + O(V + E) + O(2 x V + E) + O(V) ~ O(V + E)
+     * SC: O(V + E) + O(V) + O(V) + O(V) ~ O(V + E)
      */
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        // create adjacency list
-        Map<Integer, ArrayList<Integer>> adj =
-            new HashMap<Integer, ArrayList<Integer>>(); // SC: O(V + E)
-        for (int[] edge : prerequisites) {
+        // creating Adjacency List
+        Map<Integer, ArrayList<Integer>> adj = new HashMap<Integer, ArrayList<Integer>>(); // SC: O(V + E)
+        for (int[] edge : prerequisites) { // TC: O(E)
             adj.computeIfAbsent(edge[1], k -> new ArrayList<Integer>()).add(edge[0]);
         }
+        /**
+         * we can finish all courses if and only if there's no cyclic dependency, 
+         * so we need to check, if this Directed Graph is cyclic in nature
+         */
+        // using DFS Approach
         boolean hasCycle = doesGraphHasCycle(adj, numCourses); // TC: O(V + E), SC: O(V)
         if (hasCycle) {
             return new int[] {};
         }
-        Stack<Integer> st = new Stack<Integer>();       // SC: O(V)
-        boolean[] visited = new boolean[numCourses];    // SC: O(V)
-        for (int i = 0; i < numCourses; i++) {          // TC: O(V)
-            if (!visited[i]) {
-                dfsGraph(i, adj, visited, st);          // TC: O(V + E), SC: O(V)
+        Stack<Integer> st = new Stack<Integer>(); // SC: O(V)
+        boolean[] visited = new boolean[numCourses]; // SC: O(V)
+        for (int i = 0; i < numCourses; i++) { // TC: O(V)
+            if (!visited[i]) { // TC: O(V + E), SC: O(V)
+                dfsTopoGraph(i, visited, adj, st);
             }
         }
-        int[] topoSort = new int[numCourses];
+        int[] order = new int[numCourses];
         int index = 0;
-        System.out.println(st.size());
-        while (!st.isEmpty()) {                         // TC: O(V)
-            topoSort[index++] = st.pop();
+        while (!st.isEmpty()) { // TC: O(V)
+            order[index++] = st.pop();
         }
-        return topoSort;
+        return order;
     }
 
     /**
+     * Using DFS Approach
+     * 
+     * TC: O(V + E)
+     * SC: O(V)
+     */
+    private void dfsTopoGraph(int u, boolean[] visited, Map<Integer, ArrayList<Integer>> adj,
+        Stack<Integer> st) {
+        visited[u] = true;
+        for (Integer v : adj.getOrDefault(u, new ArrayList<Integer>())) {
+            if (!visited[v]) {
+                dfsTopoGraph(v, visited, adj, st);
+            }
+        }
+        st.push(u);
+    }
+
+    /**
+     * Using DFS Approach
+     * 
      * TC: O(2 x V + E) ~ O(V + E)
-     * SC: O(3 x V) ~ O(V)
+     * SC: O(V) + O(V) ~ O(V)
      */
     private boolean doesGraphHasCycle(Map<Integer, ArrayList<Integer>> adj, int n) {
-        boolean[] visited = new boolean[n];             // SC: O(V)
-        boolean[] inRecursion = new boolean[n];         // SC: O(V)
-        for (int i = 0; i < n; i++) {                   // TC: O(V)
-            if (!visited[i] && dfsGraphCycle(i, adj, visited, inRecursion)) {
-                return true;                            // TC: O(V + E), SC: O(V)
+        boolean[] visited = new boolean[n]; // SC: O(V)
+        boolean[] inRecursion = new boolean[n]; // SC: O(V)
+        for (int i = 0; i < n; i++) { // TC: O(V)
+            if (!visited[i] && hasCycle(i, visited, inRecursion, adj)) { // TC: O(V + E), SC: O(V)
+                // this indicates that there is a cycle in graph
+                return true;
             }
         }
         return false;
     }
 
     /**
+     * Using DFS Approach
+     * 
      * TC: O(V + E)
      * SC: O(V)
      */
-    private boolean dfsGraphCycle(int u, Map<Integer, ArrayList<Integer>> adj,
-        boolean[] visited, boolean[] inRecursion) {
+    private boolean hasCycle(int u, boolean[] visited, boolean[] inRecursion,
+        Map<Integer, ArrayList<Integer>> adj) {
         visited[u] = true;
         inRecursion[u] = true;
         for (Integer v : adj.getOrDefault(u, new ArrayList<Integer>())) {
-            if (!visited[v] && dfsGraphCycle(v, adj, visited, inRecursion)) {
+            if (!visited[v] && hasCycle(v, visited, inRecursion, adj)) {
                 return true;
             } else if (inRecursion[v]) {
                 return true;
@@ -64,20 +89,5 @@ class Solution {
         }
         inRecursion[u] = false;
         return false;
-    }
-
-    /**
-     * TC: O(V + E)
-     * SC: O(V)
-     */
-    private void dfsGraph(int u, Map<Integer, ArrayList<Integer>> adj,
-        boolean[] visited, Stack<Integer> st) {
-        visited[u] = true;
-        for (Integer v : adj.getOrDefault(u, new ArrayList<Integer>())) {
-            if (!visited[v]) {
-                dfsGraph(v, adj, visited, st);
-            }
-        }
-        st.push(u);
     }
 }
