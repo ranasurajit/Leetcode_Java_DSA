8bd2fc0eb736bbd3705f28e40f8cc610cb01e904
Surajit Rana <surajit1602@gmail.com>
2025-07-13 12:15:52 +0530

    Time: 26 ms (66.18%) | Memory: 58 MB (46.85%) - LeetSync

---
+++ GreedyAlgorithms/2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * Approach : Using Greedy (Sorting + Two Pointers) Approach
+     *
+     * TC: O(M x log(M) + N x log(N) + Min(M, N))
+     * SC: O(1)
+     */
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        int m = players.length;
+        int n = trainers.length;
+        Arrays.sort(players);  // TC: O(M x log(M))
+        Arrays.sort(trainers); // TC: O(N x log(N))
+        int p = m - 1; // pointer at the end of array 'players'
+        int t = n - 1; // pointer at the end of array 'trainers'
+        int count = 0;
+        while (p >= 0 && t >= 0) { // TC: O(Min(M, N))
+            if (trainers[t] < players[p]) {
+                p--;
+            } else {
+                // trainers[t] >= players[p]
+                p--;
+                t--;
+                count++;
+            }
+        }
+        return count;
+    }
+}
