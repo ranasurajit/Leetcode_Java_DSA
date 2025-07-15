8b2feff544c4307f1a840e7e6f297cfe15e7b205
Surajit Rana <surajit1602@gmail.com>
2025-07-15 09:55:49 +0530

    Time: 1 ms (99.12%) | Memory: 41.9 MB (80.13%) - LeetSync

--- Strings/Simulation/3396-valid-word/valid-word.java
+++ Strings/Simulation/3396-valid-word/valid-word.java
@@ -1,11 +1,39 @@
 class Solution {
     /**
-     * Approach : Using String Simulation Approach
+     * Approach II : Using String Simulation Approach
      *
      * TC: O(N)
      * SC: O(1)
      */
     public boolean isValid(String word) {
+        int n = word.length();
+        if (n < 3) {
+            return false;
+        }
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (Character.isLetter(word.charAt(i))) {
+                char ch = Character.toLowerCase(word.charAt(i));
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            } else if (!Character.isDigit(word.charAt(i))) {
+                return false;
+            }
+        }
+        return hasVowel && hasConsonant;
+    }
+
+    /**
+     * Approach I : Using String Simulation Approach
+     *
+     * TC: O(N)
+     * SC: O(5) ~ O(1)
+     */
+    public boolean isValidApproachI(String word) {
         int n = word.length();
         if (n < 3) {
             return false;
