9829dd56e894f781c6258c5ca21defd6fd786cea
Surajit Rana <surajit1602@gmail.com>
2025-07-17 07:53:09 +0530

    Time: 29 ms (17.31%) | Memory: 55.8 MB (26.32%) - LeetSync

--- Graphs/TopologicalSort/820-find-eventual-safe-states/find-eventual-safe-states.java
+++ Graphs/TopologicalSort/820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,11 +1,69 @@
 class Solution {
     /**
+     * Approach II : Using BFS (Kahn's Algorithm) Approach
+     * 
+     * TC: O(V + E) + O(V) + O(V + E) + O(V) ~ O(V + E)
+     * SC: O(V + E) + O(V) + O(V) + O(V) ~ O(V + E)
+     */
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+        /**
+         * As we need to exclude the nodes which are stuck in cycle
+         * so, for BFS approach we need to add nodes with indegree 0
+         * in the Queue, but few nodes cannot go back to find all the
+         * nodes in the cycle so we need to reverse the directions of
+         * edges and form a new adjacency list with node indegrees
+         */
+        Map<Integer, ArrayList<Integer>> adj = new HashMap<Integer, ArrayList<Integer>>(); // SC: O(V + E)
+        int[] indegrees = new int[n]; // SC: O(V)
+        for (int u = 0; u < n; u++) { // TC: O(V)
+            for (int v : graph[u]) {  // TC: O(E)
+                adj.computeIfAbsent(v, k -> new ArrayList<Integer>()).add(u);
+                indegrees[u]++;
+            }
+        }
+        /**
+         * We will apply Kahn's Algorithm (BFS), so we need a Queue and we need to push
+         * all the nodes having indegree = 0 in the Queue
+         */
+        Queue<Integer> queue = new LinkedList<Integer>(); // SC: O(V)
+        for (int i = 0; i < n; i++) { // TC: O(V)
+            if (indegrees[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        /**
+         * store all safe nodes where the nodes get popped out of Queue 
+         * (as nodes involve in cycle cannot come out of Queue as their
+         * indegrees cannot be zero)
+         */
+        boolean[] isSafeNode = new boolean[n]; // SC: O(V)
+        while (!queue.isEmpty()) { // TC: O(V)
+            Integer u = queue.poll();
+            isSafeNode[u] = true;
+            for (Integer v : adj.getOrDefault(u, new ArrayList<Integer>())) { // TC: O(E)
+                indegrees[v]--;
+                if (indegrees[v] == 0) {
+                    queue.offer(v);
+                }
+            }
+        }
+        List<Integer> safeNodes = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) { // TC: O(V)
+            if (isSafeNode[i]) {
+                safeNodes.add(i);
+            }
+        }
+        return safeNodes;
+    }
+
+    /**
      * Approach I : Using DFS Approach
      * 
      * TC: O(2 x V + E) + O(V) ~ O(V + E)
      * SC: O(V) + O(V) ~ O(V)
      */
-    public List<Integer> eventualSafeNodes(int[][] graph) {
+    public List<Integer> eventualSafeNodesDFS(int[][] graph) {
         int n = graph.length;
         /**
          * A node which is a part of any cycle cannot only end up in a terminal node
