7b864743c369f028e1d1aae18e317854b06a5903
Surajit Rana <surajit1602@gmail.com>
2025-07-21 10:21:07 +0530

    Time: 42 ms (32.32%) | Memory: 46 MB (14.90%) - LeetSync

--- Strings/Simulation/1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
+++ Strings/Simulation/1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
@@ -1,11 +1,28 @@
 class Solution {
     /**
-     * Approach : Using String Simulation Approach
+     * Approach II : Using String Simulation (Cleaner) Approach
      *
      * TC: O(N)
      * SC: O(N)
      */
     public String makeFancyString(String s) {
+        StringBuilder sb = new StringBuilder(); // SC: O(N)
+        for (char ch : s.toCharArray()) { // TC: O(N)
+            int n = sb.length();
+            if (n < 2 || !(ch == sb.charAt(n - 2) && ch == sb.charAt(n - 1))) {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Approach I : Using String Simulation Approach
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public String makeFancyStringSimulation(String s) {
         int n = s.length();
         if (n < 3) {
             return s;
