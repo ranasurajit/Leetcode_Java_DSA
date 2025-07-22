120534dff9f10130cc4f466775cf7a8401d3a6b0
Surajit Rana <surajit1602@gmail.com>
2025-07-22 23:19:24 +0530

    Time: 10 ms (79.63%) | Memory: 48.8 MB (24.72%) - LeetSync

---
+++ GreedyAlgorithms/455-assign-cookies/assign-cookies.java
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * Approach : Using Greedy + Two Pointers Approach
+     *
+     * TC: O(M x log(M)) + O(N x log(N)) + O(Min(M, N))
+     * SC: O(1)
+     */
+    public int findContentChildren(int[] g, int[] s) {
+        int m = g.length;
+        int n = s.length;
+        /**
+         * We will sort the greed factor 'g' and cookie size 's' and check if
+         * maximum value greed factor of child is satisfied with maximum value
+         * of cookie size
+         */
+        Arrays.sort(g); // TC: O(M x log(M))
+        Arrays.sort(s); // TC: O(N x log(N))
+        // Using Two Pointers Approach
+        int p = m - 1; // pointer at the end of array 'g'
+        int q = n - 1; // pointer at the end of array 's'
+        int contentedChildCount = 0;
+        while (p >= 0 && q >= 0) { // TC: O(Min(M, N))
+            if (g[p] <= s[q]) {
+                contentedChildCount++;
+                p--;
+                q--;
+            } else if (s[q] < g[p]) {
+                p--;
+            }
+        }
+        return contentedChildCount;
+    }
+}
