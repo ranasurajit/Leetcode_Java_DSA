26849da1a26b6298f26a3b4a84f12c32cc8df317
Surajit Rana <surajit1602@gmail.com>
2025-07-23 23:10:39 +0530

    Time: 16 ms (94.11%) | Memory: 55 MB (49.79%) - LeetSync

---
+++ GreedyAlgorithms/917-boats-to-save-people/boats-to-save-people.java
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * Approach : Using Greedy Approach
+     *
+     * TC: O(N x log(N)) + O(N) ~ O(N x log(N))
+     * SC: O(1)
+     */
+    public int numRescueBoats(int[] people, int limit) {
+        int n = people.length;
+        Arrays.sort(people); // TC: O(N x log(N))
+        int p = 0;
+        int q = n - 1;
+        int ships = 0;
+        while (p <= q) { // TC: O(N)
+            if (people[p] + people[q] <= limit) {
+                ships++;
+                p++;
+                q--;
+            } else if (people[q] <= limit) {
+                ships++;
+                q--;
+            }
+        }
+        return ships;
+    }
+}
