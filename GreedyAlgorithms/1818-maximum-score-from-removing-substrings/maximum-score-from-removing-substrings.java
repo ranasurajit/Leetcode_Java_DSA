f78dcad0659ed31bbb4a43d26b1891b92e743d3e
Surajit Rana <surajit1602@gmail.com>
2025-07-23 11:05:36 +0530

    Time: 55 ms (84.93%) | Memory: 46.2 MB (45.21%) - LeetSync

--- GreedyAlgorithms/1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
+++ GreedyAlgorithms/1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -1,11 +1,53 @@
 class Solution {
     /**
-     * Approach I : Using Greedy Algorithm + Stack Approach
+     * Approach II : Using Greedy Algorithm (Without Stack) Approach
      *
      * TC: O(2 x N) ~ O(N)
      * SC: O(N), same memory will be reused
      */
     public int maximumGain(String s, int x, int y) {
+        int maxScore = 0;
+        StringBuilder sb = new StringBuilder(s);
+        if (x > y) {
+            // we should remove "ab" first and then "ba"
+            maxScore += scoreAfterSubstringsRemoval(sb, 'a', 'b', x); // TC: O(N), SC: O(1)
+            maxScore += scoreAfterSubstringsRemoval(sb, 'b', 'a', y); // TC: O(N), SC: O(1)
+        } else {
+            // we should remove "ba" first and then "ab"
+            maxScore += scoreAfterSubstringsRemoval(sb, 'b', 'a', y); // TC: O(N), SC: O(1)
+            maxScore += scoreAfterSubstringsRemoval(sb, 'a', 'b', x); // TC: O(N), SC: O(1)
+        }
+        return maxScore;
+    }
+
+    /**
+     * Using Inplace Manipulation of StringBuilder Approach to Remove Substrings
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    private int scoreAfterSubstringsRemoval(StringBuilder sb, char first, char second, int points) {
+        int maxScore = 0;
+        int n = sb.length();
+        int writeIdx = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            sb.setCharAt(writeIdx++, sb.charAt(i));
+            if (writeIdx > 1 && sb.charAt(writeIdx - 2) == first && sb.charAt(writeIdx - 1) == second) {
+                writeIdx -= 2; // remove the last two characters
+                maxScore += points;
+            }
+        }
+        sb.setLength(writeIdx);
+        return maxScore;
+    }
+
+    /**
+     * Approach I : Using Greedy Algorithm + Stack Approach
+     *
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(N), same memory will be reused
+     */
+    public int maximumGainUsingStack(String s, int x, int y) {
         int[] maxScore = { 0 };
         char[] chars = s.toCharArray();
         char[] subAB = { 'a', 'b' };
