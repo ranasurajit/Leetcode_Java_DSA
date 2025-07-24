99db4df4cf6fa05f8d8727f19148b4c3f46c293a
Surajit Rana <surajit1602@gmail.com>
2025-07-24 11:03:46 +0530

    Time: 1 ms (99.43%) | Memory: 45.3 MB (40.65%) - LeetSync

--- GreedyAlgorithms/45-jump-game-ii/jump-game-ii.java
+++ GreedyAlgorithms/45-jump-game-ii/jump-game-ii.java
@@ -1,5 +1,30 @@
 class Solution {
     /**
+     * Approach III : Using Greedy Approach
+     * 
+     * TC: O(N)
+     * SC: O(1)
+     *
+     * Accepted (110 / 110 testcases passed)
+     */
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int right = 0;
+        int jumps = 0;
+        while (right < n - 1) { // TC: O(N)
+            int farthest = 0;
+            for (int i = left; i <= right; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
+            }
+            left = right + 1;
+            right = farthest;
+            jumps++;
+        }
+        return jumps;
+    }
+
+    /**
      * Approach II : Using Memoization (Top-Down DP) Approach
      * 
      * TC: O(N x N)
@@ -10,7 +35,7 @@ class Solution {
      * 
      * Accepted (110 / 110 testcases passed)
      */
-    public int jump(int[] nums) {
+    public int jumpMemoization(int[] nums) {
         int n = nums.length;
         int[] memo = new int[n]; // SC: O(N)
         Arrays.fill(memo, -1);
