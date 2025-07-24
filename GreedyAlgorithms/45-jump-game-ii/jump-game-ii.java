70fd6ffba4ea4f75dfd6d776bc03b8cca7c03a67
Surajit Rana <surajit1602@gmail.com>
2025-07-24 10:43:50 +0530

    Time: 133 ms (5.05%) | Memory: 46.3 MB (6.19%) - LeetSync

--- GreedyAlgorithms/45-jump-game-ii/jump-game-ii.java
+++ GreedyAlgorithms/45-jump-game-ii/jump-game-ii.java
@@ -1,25 +1,80 @@
 class Solution {
     /**
-     * Approach : Using Greedy Approach
-     *
-     * TC: O(N)
-     * SC: O(1)
+     * Approach II : Using Memoization (Top-Down DP) Approach
      * 
+     * TC: O(N x N)
+     * SC: O(N x N) + O(N)
+     * 
+     * - O(N x N) - memoization array memory
+     * - O(N) - recursion stack
+     * 
+     * Accepted (110 / 110 testcases passed)
      */
     public int jump(int[] nums) {
         int n = nums.length;
-        int low = 0;
-        int high = 0;
-        int jumps = 0;
-        while (high < n - 1) { // TC: O(N)
-            int farthest = 0;
-            for (int i = low; i <= high; i++) {
-                farthest = Math.max(farthest, nums[i] + i);
-            }
-            low = high + 1;
-            high = farthest;
-            jumps++;
+        int[] memo = new int[n]; // SC: O(N)
+        Arrays.fill(memo, -1);
+        return solveMemoization(0, n, nums, memo); // TC: O(N x N), SC: O(N)
+    }
+
+    /**
+     * Using Memoization Approach
+     * 
+     * TC: O(N x N)
+     * SC: O(N)
+     */
+    private int solveMemoization(int idx, int n, int[] nums, int[] memo) {
+        // Base Case
+        if (idx >= n - 1) {
+            return 0;
         }
-        return jumps;
+        // Memoization Check
+        if (memo[idx] != -1) {
+            return memo[idx];
+        }
+        // Recursion Calls
+        int jumps = nums[idx];
+        int minJumps = n;
+        for (int i = 1; i <= jumps; i++) { // TC: O(N)
+            if (idx + i < n) {
+                minJumps = Math.min(minJumps, 1 + solveMemoization(idx + i, n, nums, memo));
+            }
+        }
+        return memo[idx] = minJumps;
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     * 
+     * TC: O(N x 2 ^ N)
+     * SC: O(N)
+     *
+     * Time Limit Exceeded (74 / 110 testcases passed)
+     */
+    public int jumpRecursion(int[] nums) {
+        int n = nums.length;
+        return solveRecursion(0, n, nums); // TC: O(N x 2 ^ N), SC: O(N)
+    }
+
+    /**
+     * Using Recursion Approach
+     * 
+     * TC: O(N x 2 ^ N)
+     * SC: O(N)
+     */
+    private int solveRecursion(int idx, int n, int[] nums) {
+        // Base Case
+        if (idx >= n - 1) {
+            return 0;
+        }
+        // Recursion Calls
+        int jumps = nums[idx];
+        int minJumps = n;
+        for (int i = 1; i <= jumps; i++) { // TC: O(N)
+            if (idx + i < n) {
+                minJumps = Math.min(minJumps, 1 + solveRecursion(idx + i, n, nums));
+            }
+        }
+        return minJumps;
     }
 }
