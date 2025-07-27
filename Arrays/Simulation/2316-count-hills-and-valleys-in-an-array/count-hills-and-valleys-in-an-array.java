5962cdaf778f5381c6592459b4b040df43c0b7aa
Surajit Rana <surajit1602@gmail.com>
2025-07-27 12:51:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (7.79%) - LeetSync

--- Arrays/Simulation/2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
+++ Arrays/Simulation/2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
@@ -1,11 +1,35 @@
 class Solution {
     /**
-     * Approach : Using Simulation Approach
+     * Approach II : Using Simulation Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int countHillValley(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        int prev = nums[0];
+        for (int i = 1; i < n - 1; i++) { // TC: O(N)
+            if (nums[i] == nums[i + 1]) {
+                // skipping duplicates
+                continue;
+            }
+            if ((nums[i] > prev && nums[i] > nums[i + 1]) ||
+                (nums[i] < prev && nums[i] < nums[i + 1])) {
+                count++;
+            }
+            prev = nums[i];
+        }
+        return count;
+    }
+
+    /**
+     * Approach I : Using Simulation Approach
      *
      * TC: O(N) + O(N) ~ O(N)
      * SC: O(N)
      */
-    public int countHillValley(int[] nums) {
+    public int countHillValleyUsingSpace(int[] nums) {
         int n = nums.length;
         List<Integer> list = new ArrayList<Integer>(); // SC: O(N)
         list.add(nums[0]);
