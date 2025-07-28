a3b5a470361950e041984e08941269d4a44eecc9
Surajit Rana <surajit1602@gmail.com>
2025-07-28 19:48:16 +0530

    Time: 3 ms (74.26%) | Memory: 46.1 MB (32.11%) - LeetSync

--- GreedyAlgorithms/135-candy/candy.java
+++ GreedyAlgorithms/135-candy/candy.java
@@ -1,11 +1,45 @@
 class Solution {
     /**
+     * Approach II : Using Greedy + Array Pre-processing (Better Approach / Less Memory) Approach
+     *
+     * TC: O(N) + O(N) ~ O(N)
+     * SC: O(N)
+     */
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        // pre-processing candy allocation from left to right
+        int[] leftAlloc = new int[n];      // SC: O(N)
+        leftAlloc[0] = 1;
+        for (int i = 1; i < n; i++) {      // TC: O(N)
+            if (ratings[i] > ratings[i - 1]) {
+                leftAlloc[i] = 1 + leftAlloc[i - 1];
+            } else {
+                leftAlloc[i] = 1;
+            }
+        }
+        // we will be computing sum and right allocation on the fly
+        int current = 1;
+        int right = 1;
+        int totalCandies = Math.max(1, leftAlloc[n - 1]);
+        for (int i = n - 2; i >= 0; i--) { // TC: O(N)
+            if (ratings[i] > ratings[i + 1]) {
+                current = right + 1;
+            } else {
+                current = 1;
+            }
+            totalCandies += Math.max(current, leftAlloc[i]);
+            right = current;
+        }
+        return totalCandies;
+    }
+
+    /**
      * Approach I : Using Greedy + Array Pre-processing Approach
      *
      * TC: O(N) + O(N) + O(N) ~ O(N)
      * SC: O(N) + O(N) ~ O(N)
      */
-    public int candy(int[] ratings) {
+    public int candyBruteForce(int[] ratings) {
         int n = ratings.length;
         // pre-processing candy allocation from left to right
         int[] leftAlloc = new int[n];      // SC: O(N)
