3272014f7e78c412d630ab27b9feba31b9243a44
Surajit Rana <surajit1602@gmail.com>
2025-07-28 14:41:17 +0530

    Time: 45 ms (28.15%) | Memory: 62.4 MB (5.54%) - LeetSync

--- DynamicProgramming/Subsequences/2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ DynamicProgramming/Subsequences/2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,11 +1,57 @@
 class Solution {
     /**
-     * Approach : Using Recursion Approach
+     * Approach II : Using Memoization (Top-Down DP) Approach
+     * 
+     * TC: O(N x R) + O(N x R) ~ O(N x R)
+     * SC: O(N x R) + O(N)
+     */
+    public int countMaxOrSubsets(int[] nums) {
+        int n = nums.length;
+        /**
+         * maximum possible bitwise OR of nums will be bitwise OR of all elements
+         */
+        int maxOR = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            maxOR |= nums[i];
+        }
+        // Using Recursion + Memoization to find the number of subsets that matches maxOR
+        // states are idx and currentXOR ranges from (0 to N - 1) and (0 to maxOR)
+        int[][] memo = new int[n][maxOR + 1]; // SC: O(N x R)
+        for (int[] mem : memo) {  // TC: O(N)
+            Arrays.fill(mem, -1); // TC: O(R)
+        }
+        return solveMemoization(0, n, 0, nums, maxOR, memo); // TC: O(N x R), SC: O(N)
+    }
+
+    /**
+     * Using Memoization Approach
+     * 
+     * TC: O(N x R)
+     * SC: O(N)
+     */
+    private int solveMemoization(int idx, int n, int currentOR, int[] nums, int maxOR, int[][] memo) {
+        // Base Case
+        if (idx == n) {
+            return currentOR == maxOR ? 1 : 0;
+        }
+        // Memoization Check
+        if (memo[idx][currentOR] != -1) {
+            return memo[idx][currentOR];
+        }
+        // Recursion Calls
+        // we can choose to pick or skip the nums[idx]
+        int pick = solveMemoization(idx + 1, n, (currentOR | nums[idx]), nums, maxOR, memo);
+        int skip = solveMemoization(idx + 1, n, currentOR, nums, maxOR, memo);
+        return memo[idx][currentOR] = pick + skip;
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
      *
      * TC: O(2 ^ N) + O(N) ~ O(2 ^ N)
      * SC: O(N)
      */
-    public int countMaxOrSubsets(int[] nums) {
+    public int countMaxOrSubsetsRecursion(int[] nums) {
         int n = nums.length;
         /**
          * maximum possible bitwise OR of nums will be bitwise OR of all elements
