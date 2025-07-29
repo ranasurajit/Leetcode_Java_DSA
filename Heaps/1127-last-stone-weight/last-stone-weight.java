cdfdb1f1f43bc3fc3cdd3fbfcc277a913506b6a0
Surajit Rana <surajit1602@gmail.com>
2025-07-29 20:00:20 +0530

    Time: 2 ms (12.64%) | Memory: 41.7 MB (7.85%) - LeetSync

--- Heaps/1127-last-stone-weight/last-stone-weight.java
+++ Heaps/1127-last-stone-weight/last-stone-weight.java
@@ -1,20 +1,29 @@
 class Solution {
+    /**
+     * Approach : Using Max Heaps (PriorityQueues) Approach
+     *
+     * TC: O(N x log(N)) + O(N x log(N)) ~ O(N x log(N))
+     * SC: O(N)
+     */
     public int lastStoneWeight(int[] stones) {
         int n = stones.length;
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((p, q) -> q - p);
-        for (int i = 0; i < n; i++) {
-            pq.offer(stones[i]);
+        /**
+         * To smash the heaviest two stones, we will be using a Max-Heap 
+         * to store the stones[i] and poll two heaviest and offer the 
+         * remaining result after smashing them
+         */
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((p, q) -> q - p); // SC: O(N)
+        for (int i = 0; i < n; i++) {     // TC: O(N)
+            pq.offer(stones[i]);          // TC: O(log(N))
         }
-        while (pq.size() > 1) {
-            Integer high = pq.poll();
-            Integer low = pq.poll();
-            if (high - low > 0) {
-                pq.offer(high - low);
+        while (pq.size() > 1) {           // TC: O(N)
+            // the loop should run till we have only one stone left
+            int first = pq.poll();
+            int second = pq.poll();
+            if (first - second > 0) {
+                pq.offer(first - second); // TC: O(log(N))
             }
         }
-        if (pq.size() == 1) {
-            return pq.poll();
-        }
-        return 0;
+        return pq.isEmpty() ? 0 : pq.peek();
     }
 }
