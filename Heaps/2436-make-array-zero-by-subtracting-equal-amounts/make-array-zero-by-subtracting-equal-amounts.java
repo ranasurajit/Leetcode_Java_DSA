9cb977e842dd597b59b30998d86634e3e8eb96f0
Surajit Rana <surajit1602@gmail.com>
2025-07-29 23:24:34 +0530

    Time: 6 ms (5.20%) | Memory: 41.4 MB (41.49%) - LeetSync

---
+++ Heaps/2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * Approach : Using Min Heaps (PriorityQueues) Approach
+     *
+     * TC: O(N x log(N)) + O(N x N x log(N)) ~ O((N ^ 2 x log(N))
+     * SC: O(N)
+     */
+    public int minimumOperations(int[] nums) {
+        int n = nums.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(); // SC: O(N)
+        int sum = 0;
+        for (int i = 0; i < n; i++) {     // TC: O(N)
+            if (nums[i] > 0) {
+                pq.offer(nums[i]);        // TC: O(log(N))
+            }
+            sum += nums[i];
+        }
+        int count = 0;
+        while (!pq.isEmpty()) {           // TC: O(N)
+            int x = pq.poll();
+            pq.clear();
+            count++;
+            for (int i = 0; i < n; i++) { // TC: O(N)
+                nums[i] -= x;
+                if (nums[i] > 0) {
+                    pq.offer(nums[i]);    // TC: O(log(N))
+                }
+            }
+        }
+        return count;
+    }
+}
