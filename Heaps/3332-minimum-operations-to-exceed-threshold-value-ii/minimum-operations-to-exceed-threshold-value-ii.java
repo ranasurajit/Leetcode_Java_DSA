ee87e3a6e68fc28e3c1b28dfe38db07d66c305b6
Surajit Rana <surajit1602@gmail.com>
2025-08-01 00:11:46 +0530

    Time: 156 ms (11.50%) | Memory: 70.7 MB (52.21%) - LeetSync

--- Heaps/3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.java
+++ Heaps/3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.java
@@ -1,28 +1,25 @@
 class Solution {
     /**
-     * Using Greedy Approach - Min-Heap (PriorityQueue)
+     * Approach : Using Min-Heap (PriorityQueues) Approach
      *
-     * TC: O(4 x N x log(N)) ~ O(N x log(N))
+     * TC: O(N x log(N)) + O(N x log(N)) ~ O(N x log(N))
      * SC: O(N)
      */
     public int minOperations(int[] nums, int k) {
         int n = nums.length;
-        int operations = 0;
-        // min-heap needed to sort the elements during insertion
+        // we will be adding elements from array 'nums' in Min-Heap
         PriorityQueue<Long> pq = new PriorityQueue<Long>(); // SC: O(N)
-        for (int i = 0; i < n; i++) {              // TC: O(N)
-            pq.offer((long) nums[i]);
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            pq.offer((long) nums[i]); // TC: O(log(N))
         }
-        long x, y;
-        while (pq.peek() < k) {                    // TC: O(N)
-            x = pq.poll();                         // TC: O(log(N))
-            // check if PriorityQueue becomes empty after polling
-            if (!pq.isEmpty()) {
-                y = pq.poll();                     // TC: O(log(N))
-                pq.offer(x * 2 + y);               // TC: O(log(N))
+        int operations = 0;
+        while (!pq.isEmpty() && pq.peek() < k) { // TC: O(N)
+            if (pq.size() >= 2 && pq.peek() < k) {
+                long x = pq.poll();
+                long y = pq.poll();
+                pq.offer((2 * x) + y); // // TC: O(log(N))
                 operations++;
             } else {
-                // the PriorityQueue had only 1 element
                 break;
             }
         }
