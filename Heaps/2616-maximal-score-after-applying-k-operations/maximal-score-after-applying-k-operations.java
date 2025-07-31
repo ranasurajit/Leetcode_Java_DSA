aeee1ec8f05e2edf00e4470e2b878d509d8e78c7
Surajit Rana <surajit1602@gmail.com>
2025-07-31 23:32:53 +0530

    Time: 125 ms (66.03%) | Memory: 62.9 MB (86.35%) - LeetSync

--- Heaps/2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
+++ Heaps/2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
@@ -1,21 +1,22 @@
 class Solution {
     /**
-     * Using PriorityQueue
-     * TC: O(Nlog(N) + Klog(N))
+     * Approach : Using Max-Heap (PriorityQueues) Approach
+     *
+     * TC: O(N x log(N)) + O(K x log(N)) ~ O((N + K) x log(N))
      * SC: O(N)
      */
     public long maxKelements(int[] nums, int k) {
         int n = nums.length;
-        // storing in max-heap TC: O(Nlog(N)), SC: O(N)
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((p, q) -> q - p); 
-        for (int it : nums) { // TC: O(N)
-            pq.offer(it);
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a, b) -> b - a); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            pq.offer(nums[i]);        // TC: O(log(N))
         }
         long score = 0L;
-        while (k-- > 0) { // TC: O(K)
-            int top = pq.poll(); // TC: O(log(N))
-            score += top;
-            pq.offer((int) Math.ceil(top / 3.0)); // TC: O(log(N))
+        while (!pq.isEmpty() && k > 0) { // TC: O(K)
+            long current = (long) pq.poll();
+            score += current;
+            pq.offer((int) Math.ceil(current / 3.0)); // TC: O(log(N))
+            k--;
         }
         return score;
     }
