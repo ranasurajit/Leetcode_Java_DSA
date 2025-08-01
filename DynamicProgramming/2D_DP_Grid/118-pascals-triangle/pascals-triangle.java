829fb7239d50d53aa9a984e4a852ec7b6b9e6dd3
Surajit Rana <surajit1602@gmail.com>
2025-08-01 10:58:54 +0530

    Time: 2 ms (3.52%) | Memory: 42.1 MB (59.76%) - LeetSync

---
+++ DynamicProgramming/2D_DP_Grid/118-pascals-triangle/pascals-triangle.java
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * Approach : Using Array Simulation Approach
+     *
+     * TC: O(N ^ 2)
+     * SC: O(1)
+     */
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+        for (int i = 1; i <= numRows; i++) { // TC: O(N x (N + 1)) / 2
+            triangle.add(new ArrayList<Integer>());
+            for (int j = 0; j < i; j++) {
+                triangle.get(i - 1).add(1);
+                if (j == 0 || j == i - 1) {
+                    continue;
+                }
+                triangle.get(i - 1).set(j, triangle.get(i - 2).get(j - 1) + triangle.get(i - 2).get(j));
+            }
+        }
+        return triangle;
+    }
+}
