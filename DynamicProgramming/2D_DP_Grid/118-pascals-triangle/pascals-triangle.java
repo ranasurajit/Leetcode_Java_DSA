71699340cd8094abc14d841841bd34192340ffb1
Surajit Rana <surajit1602@gmail.com>
2025-08-01 11:49:57 +0530

    Time: 1 ms (82.66%) | Memory: 42 MB (59.76%) - LeetSync

--- DynamicProgramming/2D_DP_Grid/118-pascals-triangle/pascals-triangle.java
+++ DynamicProgramming/2D_DP_Grid/118-pascals-triangle/pascals-triangle.java
@@ -1,11 +1,38 @@
 class Solution {
     /**
-     * Approach I : Using Array Simulation Approach
+     * Approach II : Using Dynamic Programming (Tabulation) Approach
      *
      * TC: O(N ^ 2)
      * SC: O(1)
      */
     public List<List<Integer>> generate(int numRows) {
+        // Initialization
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+        triangle.add(new ArrayList<Integer>());
+        triangle.get(0).add(1);
+        // Iterative Calls
+        for (int row = 1; row < numRows; row++) { // TC: O(N x (N + 1)) / 2
+            List<Integer> current = new ArrayList<Integer>();
+            List<Integer> prev = triangle.get(row - 1);
+            // first cell is 1 always
+            current.add(1);
+            for (int cell = 1; cell < row; cell++) {
+                current.add(prev.get(cell - 1) + prev.get(cell));
+            }
+            // last cell is 1 always
+            current.add(1);
+            triangle.add(current);
+        }
+        return triangle;
+    }
+
+    /**
+     * Approach I : Using Array Simulation Approach
+     *
+     * TC: O(N ^ 2)
+     * SC: O(1)
+     */
+    public List<List<Integer>> generateBruteForce(int numRows) {
         List<List<Integer>> triangle = new ArrayList<List<Integer>>();
         for (int i = 1; i <= numRows; i++) { // TC: O(N x (N + 1)) / 2
             triangle.add(new ArrayList<Integer>());
