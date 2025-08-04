925dd46df3afe761b8809423f19cd157105d22a3
Surajit Rana <surajit1602@gmail.com>
2025-08-04 10:53:10 +0530

    Time: 50 ms (59.78%) | Memory: 55.9 MB (13.66%) - LeetSync

--- SlidingWindow/VariableWindow/940-fruit-into-baskets/fruit-into-baskets.java
+++ SlidingWindow/VariableWindow/940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,43 +1,30 @@
 class Solution {
     /**
-     * Took two pointers i and j = 0
-     * 
-     * Window size: (j - i + 1)
-     * 
-     * when HashMap size < k i.e. 2, j++
-     * when HashMap size <= k i.e. 2,
-     * compare and store the maximum window size (result), j++
-     * till HashMap size > k i.e. 2, then start removing elements from index 'i'
-     * and update its count/remove key from HashMap, j++
-     * 
-     * TC: O(N)
-     * SC: O(1)
+     * Approach : Using Sliding Window (Variable Size) Approach
      *
-     * @param fruits
-     * @return
+     * TC: O(N)
+     * SC: O(2) ~ O(1) as we have only two baskets (HashMap size)
      */
     public int totalFruit(int[] fruits) {
         int n = fruits.length;
-        int i = 0; // pointer for start index of sliding window
-        int j = 0; // pointer for end index of sliding window
-        Map<Integer, Integer> hm = new HashMap<Integer, Integer>(); // O(2) ~ O(1)
-        int k = 2;
-        int total = Integer.MIN_VALUE;
-        while (j < n) { // O(N)
-            hm.put(fruits[j], hm.getOrDefault(fruits[j], 0) + 1);
-            while (hm.size() > k) {
-                // remove the index ith element from HashMap
-                hm.put(fruits[i], hm.getOrDefault(fruits[i], 0) - 1);
-                if (hm.get(fruits[i]) == 0) {
-                    hm.remove(fruits[i]);
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(2)
+        int i = 0;
+        int j = 0;
+        int maxTrees = 0;
+        while (j < n) { // TC: O(N)
+            map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
+            if (map.size() > 2) {
+                // we need to remove computation from index 'i'
+                map.put(fruits[i], map.getOrDefault(fruits[i], 0) - 1);
+                if (map.get(fruits[i]) == 0) {
+                    map.remove(fruits[i]);
                 }
                 i++;
             }
-            if (hm.size() <= k) {
-                total = Math.max(total, j - i + 1);
-            }
+            // at this point maximum two baskets are present
+            maxTrees = Math.max(maxTrees, j - i + 1);
             j++;
         }
-        return total == Integer.MIN_VALUE ? 1 : total;
+        return maxTrees;
     }
 }
