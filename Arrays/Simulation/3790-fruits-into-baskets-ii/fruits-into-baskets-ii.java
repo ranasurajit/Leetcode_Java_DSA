8157e9297ac3f69db7092c9dc0e578dbe7a6833f
Surajit Rana <surajit1602@gmail.com>
2025-08-05 10:31:21 +0530

    Time: 3 ms (19.49%) | Memory: 44.8 MB (15.23%) - LeetSync

--- Arrays/Simulation/3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
+++ Arrays/Simulation/3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
@@ -1,28 +1,26 @@
 class Solution {
     /**
-     * Approach : Using Sorting + Two Pointers Approach
+     * Approach : Using Array Simulation Approach
      *
-     * TC: O(N x log(N)) + O(N x log(N)) + O(N) ~ O(N x log(N))
+     * TC: O(N x N)
      * SC: O(1)
      */
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
         int n = fruits.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        int countUnplaced = 0;
+        for (int i = 0; i < n; i++) {     // TC: O(N)
+            int notset = 1;
+            for (int j = 0; j < n; j++) { // TC: O(N)
                 if (fruits[i] == -1 || baskets[j] == -1) {
                     continue;
                 }
                 if (fruits[i] <= baskets[j]) {
                     fruits[i] = -1;
                     baskets[j] = -1;
+                    notset = 0;
                 }
             }
-        }
-        int countUnplaced = 0;
-        for (int i = 0; i < n; i++) {
-            if (fruits[i] != -1) {
-                countUnplaced++;
-            }
+            countUnplaced += notset;
         }
         return countUnplaced;
     }
