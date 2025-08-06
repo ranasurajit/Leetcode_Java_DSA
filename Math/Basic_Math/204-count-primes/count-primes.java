01d6dea686105efd96a91aa126a934067e46b5f3
Surajit Rana <surajit1602@gmail.com>
2025-08-06 22:47:06 +0530

    Time: 115 ms (21.68%) | Memory: 64.8 MB (6.12%) - LeetSync

--- Math/Basic_Math/204-count-primes/count-primes.java
+++ Math/Basic_Math/204-count-primes/count-primes.java
@@ -6,12 +6,13 @@ class Solution {
      * SC: O(N)
      */
     public int countPrimes(int n) {
+        if (n < 2) {
+            return 0;
+        }
         int[] primes = new int[n + 1]; // SC: O(N)
         Arrays.fill(primes, 1);
         primes[0] = 0;
-        if (n > 1) {
-            primes[1] = 0;
-        }
+        primes[1] = 0;
         for (int i = 2; i * i <= n; i++) { // TC: O(N x log(log(N)))
             if (primes[i] == 1) {
                 for (int j = i * i; j <= n; j += i) {
