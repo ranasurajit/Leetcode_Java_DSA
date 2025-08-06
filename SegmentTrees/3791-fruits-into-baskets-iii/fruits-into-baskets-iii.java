76e633b5e2efe65853c2f967560a12b3d6e37143
Surajit Rana <surajit1602@gmail.com>
2025-08-06 11:26:42 +0530

    Time: 51 ms (86.49%) | Memory: 65.2 MB (45.05%) - LeetSync

---
+++ SegmentTrees/3791-fruits-into-baskets-iii/fruits-into-baskets-iii.java
@@ -0,0 +1,80 @@
+class Solution {
+    /**
+     * Approach II : Using Segment Trees Approach
+     *
+     * TC: O(N x log(N))
+     * SC: O(4 x N) ~ O(N)
+     *
+     * Accepted (740 / 740 testcases passed)
+     */
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        int[] segmentTree = new int[4 * n];
+        // Build segment tree
+        build(0, 0, n - 1, baskets, segmentTree);
+        int unplaced = 0;
+        for (int fruit : fruits) {
+            if (!querySegmentTree(0, 0, n - 1, segmentTree, fruit)) {
+                unplaced++;
+            }
+        }
+        return unplaced;
+    }
+
+    private void build(int i, int l, int r, int[] baskets, int[] segmentTree) {
+        if (l == r) {
+            segmentTree[i] = baskets[l];
+            return;
+        }
+        int m = (l + r) / 2;
+        build(2 * i + 1, l, m, baskets, segmentTree);
+        build(2 * i + 2, m + 1, r, baskets, segmentTree);
+        segmentTree[i] = Math.max(segmentTree[2 * i + 1], segmentTree[2 * i + 2]);
+    }
+
+    private boolean querySegmentTree(int i, int l, int r, int[] segmentTree, int val) {
+        if (segmentTree[i] < val) {
+            return false; // No basket in this segment
+        }
+        if (l == r) {
+            segmentTree[i] = -1; // Mark basket as used
+            return true;
+        }
+        int mid = (l + r) / 2;
+        boolean placed;
+        if (segmentTree[2 * i + 1] >= val) {
+            placed = querySegmentTree(2 * i + 1, l, mid, segmentTree, val);
+        } else {
+            placed = querySegmentTree(2 * i + 2, mid + 1, r, segmentTree, val);
+        }
+        segmentTree[i] = Math.max(segmentTree[2 * i + 1], segmentTree[2 * i + 2]);
+        return placed;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Simulation) Approach
+     *
+     * TC: O(N x N)
+     * SC: O(1)
+     *
+     * Time Limit Exceeded (737 / 740 testcases passed)
+     */
+    public int numOfUnplacedFruitsBruteForce(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        int countUnplaced = 0;
+        for (int i = 0; i < n; i++) {     // TC: O(N)
+            int unset = 1;
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                if (fruits[i] <= baskets[j]) {
+                    unset = 0;
+                    fruits[i] = 0;
+                    baskets[j] = 0;
+                    // early break
+                    break;
+                }
+            }
+            countUnplaced += unset;
+        }
+        return countUnplaced;
+    }
+}
