e5b8fd3beab017d797322ae0c7105560eb9a5a89
Surajit Rana <surajit1602@gmail.com>
2025-08-08 00:43:24 +0530

    Time: 1 ms (99.55%) | Memory: 46.8 MB (33.36%) - LeetSync

--- BitManipulation/338-counting-bits/counting-bits.java
+++ BitManipulation/338-counting-bits/counting-bits.java
@@ -25,14 +25,18 @@ class Solution {
      * TC: O(32)
      * SC: O(1)
      */
-    private int countSetBitsBruteForce(int p) {
-        int count = 0;
-        for (int i = 0; i < 32; i++) { // TC: O(32)
-            if (((p >> i) & 1) == 1) {
-                count++;
-            }
+    /**
+     * Approach I : Using Bit-Manipulation (Brute-Force) Approach
+     * 
+     * TC: O(32 x N) ~ O(N)
+     * SC: O(1)
+     */
+    public int[] countBitsBruteForce(int n) {
+        int[] bitCounts = new int[n + 1];
+        for (int i = 0; i <= n; i++) { // TC: O(N)
+            bitCounts[i] = countSetBits(i); // TC: O(32)
         }
-        return count;
+        return bitCounts;
     }
 
     /**
