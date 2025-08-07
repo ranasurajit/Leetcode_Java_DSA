be6e2db423177602986ee2d270da95fd450d0437
Surajit Rana <surajit1602@gmail.com>
2025-08-08 00:39:16 +0530

    Time: 2 ms (93.90%) | Memory: 46.9 MB (20.28%) - LeetSync

--- BitManipulation/338-counting-bits/counting-bits.java
+++ BitManipulation/338-counting-bits/counting-bits.java
@@ -1,16 +1,38 @@
 class Solution {
     /**
-     * Approach : Using Bit-Manipulation Approach
+     * Approach II : Using Bit-Manipulation (Optimal) Approach
      * 
-     * TC: O(32 x N) ~ O(N)
+     * TC: O(N)
      * SC: O(1)
      */
     public int[] countBits(int n) {
         int[] bitCounts = new int[n + 1];
-        for (int i = 0; i <= n; i++) { // TC: O(N)
-            bitCounts[i] = countSetBits(i); // TC: O(32)
+        // bitCounts in i = 0 = 0;
+        for (int i = 1; i <= n; i++) { // TC: O(N)
+            /**
+             * if i = even, then number of set bits in i = number of set bits in (i / 2)
+             * if i = odd, then number of set bits int i = number of set bits in (i - 1) + 1
+             * (as i - 1 is even and we add 1 to (i - 1) to form i)
+             */
+            bitCounts[i] = (i & 1) == 0 ? bitCounts[i / 2] : bitCounts[i - 1] + 1;
         }
         return bitCounts;
+    }
+
+    /**
+     * Using Bit-Manipulation Approach
+     * 
+     * TC: O(32)
+     * SC: O(1)
+     */
+    private int countSetBitsBruteForce(int p) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) { // TC: O(32)
+            if (((p >> i) & 1) == 1) {
+                count++;
+            }
+        }
+        return count;
     }
 
     /**
