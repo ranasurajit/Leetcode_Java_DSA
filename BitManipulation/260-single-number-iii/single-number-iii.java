6fce614214f5e1d7236b6442ddbbb41153afd75f
Surajit Rana <surajit1602@gmail.com>
2025-08-09 00:22:43 +0530

    Time: 1 ms (99.69%) | Memory: 45.9 MB (30.30%) - LeetSync

--- BitManipulation/260-single-number-iii/single-number-iii.java
+++ BitManipulation/260-single-number-iii/single-number-iii.java
@@ -1,26 +1,31 @@
 class Solution {
     /**
      * Approach : Using Bit-Manipulation Approach
-     *
-     * TC: O(2 x N)
+     * 
+     * TC: O(N) + O(N) ~ O(N)
      * SC: O(1)
      */
     public int[] singleNumber(int[] nums) {
         int xor = 0;
-        for (int item : nums) { // TC: O(N)
-            xor ^= item;
+        for (int num : nums) { // TC: O(N)
+            xor ^= num;
         }
-        // find the right most set bit
-        int rightMostSetBit = xor & -xor;
-        int first = 0;
-        int second = 0;
-        for (int item : nums) { // TC: O(N)
-            if ((rightMostSetBit & item)== 0) {
-                first ^= item;
+        /**
+         * xor will be having xor of two unique numbers, now we need
+         * to separate them out using buckets and since they are unique
+         * so they would be having atleast 1 bit difference, so we 
+         * need to find the right most set bit
+         */
+        int rightMostSetBit = (xor & -xor);
+        int bucket1 = 0;
+        int bucket2 = 0;
+        for (int num : nums) { // TC: O(N)
+            if ((num & rightMostSetBit) != 0) {
+                bucket1 ^= num;
             } else {
-                second ^= item;
+                bucket2 ^= num;
             }
         }
-        return new int[] { first, second };
+        return new int[] { bucket1, bucket2 };
     }
 }
