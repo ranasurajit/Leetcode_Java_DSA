83b5559a30a2891c5684f42f486c358b2c21cd7b
Surajit Rana <surajit1602@gmail.com>
2025-08-10 00:00:07 +0530

    Time: 1 ms (49.33%) | Memory: 42.7 MB (65.47%) - LeetSync

--- BitManipulation/78-subsets/subsets.java
+++ BitManipulation/78-subsets/subsets.java
@@ -1,22 +1,45 @@
 class Solution {
     /**
-     * Approach : Using Recursion Approach
+     * Approach II : Using PowerSet Approach
      *
-     * TC: O(2 ^ N)
-     * SC: O(N) + O(N) ~ O(N)
+     * TC: O(N x 2 ^ N)
+     * SC: O(N)
      */
     public List<List<Integer>> subsets(int[] nums) {
         int n = nums.length;
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        int combinations = (int) Math.pow(2, n); // number of possible subsets
+        for (int i = 0; i < combinations; i++) { // TC: O(2 ^ N)
+            List<Integer> current = new ArrayList<Integer>(); // SC: O(N)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                if (((i >> j) & 1) == 1) {
+                    // jth bit is set in Integer i
+                    current.add(nums[j]);
+                }
+            }
+            result.add(current);
+        }
+        return result;
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(N x 2 ^ N)
+     * SC: O(N) + O(N) ~ O(N)
+     */
+    public List<List<Integer>> subsetsRecursion(int[] nums) {
+        int n = nums.length;
         List<Integer> current = new ArrayList<Integer>(); // SC: O(N)
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        solveRecursion(0, n, nums, current, result); // TC: O(2 ^ N), SC: O(N)
+        solveRecursion(0, n, nums, current, result); // TC: O(N x 2 ^ N), SC: O(N)
         return result;
     }
 
     /**
      * Using Recursion Approach
      *
-     * TC: O(2 ^ N)
+     * TC: O(N x 2 ^ N)
      * SC: O(N)
      */
     private void solveRecursion(int idx, int n, int[] nums, List<Integer> current,
