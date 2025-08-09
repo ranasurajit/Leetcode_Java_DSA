c41423de51c93689733f8204a341198a324f385e
Surajit Rana <surajit1602@gmail.com>
2025-08-09 10:46:49 +0530

    Time: 1 ms (35.75%) | Memory: 40.9 MB (47.93%) - LeetSync

--- BitManipulation/231-power-of-two/power-of-two.java
+++ BitManipulation/231-power-of-two/power-of-two.java
@@ -1,58 +1,26 @@
 class Solution {
     /**
-     * Approach III : Using Bit-Manipulation (Shortcut) Approach
-     * 
-     * TC: O(1)
+     * Approach : Using Bit-Manipulation Approach
+     *
+     * 16 is a Power of Two - Binary -> 10000
+     * 32 is a Power of Two - Binary -> 100000
+     * 1048576 is a Power of Two - Binary -> 100000000000000000000
+     *
+     * so it has exactly 1 set bit
+     *
+     * TC: O(32) ~ O(1)
      * SC: O(1)
      */
     public boolean isPowerOfTwo(int n) {
         if (n <= 0) {
             return false;
         }
-        /**
-         * if n is a power of 2 then it will be form 00010000 
-         * and (n - 1) will be 00001000 so (n & (n - 1)) = 0
-         */
-        return (n & (n - 1)) == 0;
-    }
-
-    /**
-     * Approach II : Using Bit-Manipulation (Cleaner) Approach
-     * 
-     * TC: O(32)
-     * SC: O(1)
-     */
-    public boolean isPowerOfTwoApproachII(int n) {
-        if (n < 0) {
-            return false;
-        }
         int countSetBits = 0;
-        while (n > 0) { // TC: O(32)
-            countSetBits += (n & 1);
-            n = n >> 1;
-        }
-        // for a number to be power of 2 it should have exactly 1 set bit
-        return countSetBits == 1;
-    }
-
-    /**
-     * Approach I : Using Bit-Manipulation Approach
-     * 
-     * TC: O(32) ~ O(1)
-     * SC: O(1)
-     */
-    public boolean isPowerOfTwoApproachI(int n) {
-        if (n < 0) {
-            return false;
-        }
-        int countSetBits = 0;
-        for (int i = 0; i < 32; i++) { // TC: O(32)
-            int ithBit = ((n >> i) & 1);
-            if (ithBit == 1) {
+        for (int i = 0; i < 32; i++) { // TC: O(32) ~ O(1)
+            if (((n >> i) & 1) == 1) {
                 countSetBits++;
             }
         }
-        // for a number to be power of 2 it should have exactly 1 set bit
         return countSetBits == 1;
     }
 }
