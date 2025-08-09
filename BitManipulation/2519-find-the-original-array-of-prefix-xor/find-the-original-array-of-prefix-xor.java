eee4e4c6ec65c7d7db954ed85db0a91dd6b51dd1
Surajit Rana <surajit1602@gmail.com>
2025-08-10 00:42:49 +0530

    Time: 2 ms (84.82%) | Memory: 57 MB (48.68%) - LeetSync

---
+++ BitManipulation/2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.java
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * Approach : Using Bit-Manipulation Approach
+     * 
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int[] findArray(int[] pref) {
+        int n = pref.length;
+        int cummXOR = 0;
+        int[] arr = new int[n];
+        arr[0] = pref[0];
+        for (int i = 1; i < n; i++) { // TC: O(N)
+            arr[i] = pref[i] ^ pref[i - 1];
+        }
+        return arr;
+    }
+}
