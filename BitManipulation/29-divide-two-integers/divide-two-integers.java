547949d3cc09f86170144c497d0fa98d9d781c8c
Surajit Rana <surajit1602@gmail.com>
2025-08-09 23:20:54 +0530

    Time: 1 ms (69.36%) | Memory: 41 MB (46.44%) - LeetSync

--- BitManipulation/29-divide-two-integers/divide-two-integers.java
+++ BitManipulation/29-divide-two-integers/divide-two-integers.java
@@ -1,13 +1,54 @@
 class Solution {
     /**
-     * Approach : Using Math + Simulation Approach
+     * Approach II : Using Math + Simulation (Optimal) Approach
+     *
+     * TC: O(log(D)) x O(log(D))
+     * SC: O(1)
+     *
+     * where D = dividend (in worst case divisor = 1)
+     */
+    public int divide(int dividend, int divisor) {
+        int neg = ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) ? -1 : 1;
+        if (Math.abs(divisor) == 1) {
+            long res = (long) dividend;
+            res = neg * Math.abs(res);
+            if (res > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            } else if (res < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+            return (int) res;
+        }
+        long dvd = dividend < 0 ? -1 * (long) dividend : (long) dividend;
+        long dvs = divisor < 0 ? -1 * (long) divisor : (long) divisor;
+        long sum = 0L;
+        long result = 0L;
+        while (dvd >= dvs) { // TC: O(log(D))
+            int pow = 0;
+            while (dvs * (long) Math.pow(2, pow + 1) <= dvd) { // TC: O(log(D))
+                pow++;
+            }
+            result += (long) Math.pow(2, pow);
+            dvd -= dvs * (long) Math.pow(2, pow);
+        }
+        result = result * neg;
+        if (result > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        } else if (result < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+        return (int) result;
+    }
+
+    /**
+     * Approach I : Using Math + Simulation Approach
      *
      * TC: O(D)
      * SC: O(1)
      *
      * where D = dividend (in worst case divisor = 1)
      */
-    public int divide(int dividend, int divisor) {
+    public int divideBruteForce(int dividend, int divisor) {
         int neg = ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) ? -1 : 1;
         if (Math.abs(divisor) == 1) {
             long res = (long) dividend;
