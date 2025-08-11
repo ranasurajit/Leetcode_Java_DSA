fb6da5ddd25f6019398af7f9e23efed8f116f0dd
Surajit Rana <surajit1602@gmail.com>
2025-08-11 22:57:55 +0530

    Time: 2 ms (100.00%) | Memory: 44.7 MB (68.24%) - LeetSync

--- Strings/Dictionary/13-roman-to-integer/roman-to-integer.java
+++ Strings/Dictionary/13-roman-to-integer/roman-to-integer.java
@@ -1,11 +1,65 @@
 class Solution {
     /**
-     * Approach I : Using Hashing Approach
+     * Approach II : Using String Simulation Approach
      *
      * TC: O(N)
      * SC: O(1)
      */
     public int romanToInt(String s) {
+        int n = s.length();
+        int result = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            char ch = s.charAt(i);
+            if (ch == 'I') {
+                result += 1;
+            } else if (ch == 'V') {
+                result += 5;
+                if (i > 0 && s.charAt(i - 1) == 'I') {
+                    // negating -1 twice as it may be added previously
+                    result -= 2;
+                }
+            } else if (ch == 'X') {
+                result += 10;
+                if (i > 0 && s.charAt(i - 1) == 'I') {
+                    // negating -1 twice as it may be added previously
+                    result -= 2;
+                }
+            } else if (ch == 'L') {
+                result += 50;
+                if (i > 0 && s.charAt(i - 1) == 'X') {
+                    // negating -10 twice as it may be added previously
+                    result -= 20;
+                }
+            } else if (ch == 'C') {
+                result += 100;
+                if (i > 0 && s.charAt(i - 1) == 'X') {
+                    // negating -10 twice as it may be added previously
+                    result -= 20;
+                }
+            } else if (ch == 'D') {
+                result += 500;
+                if (i > 0 && s.charAt(i - 1) == 'C') {
+                    // negating -100 twice as it may be added previously
+                    result -= 200;
+                }
+            } else if (ch == 'M') {
+                result += 1000;
+                if (i > 0 && s.charAt(i - 1) == 'C') {
+                    // negating -100 twice as it may be added previously
+                    result -= 200;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Approach I : Using Hashing Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int romanToIntUsingHashing(String s) {
         int n = s.length();
         Map<Character, Integer> map = getMappings(); // TC: O(7), SC: O(7)
         int result = map.get(s.charAt(0));
