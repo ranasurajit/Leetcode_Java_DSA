8674db3211c2114c836a3162b0d7f7de2dd37e5a
Surajit Rana <surajit1602@gmail.com>
2025-08-12 23:05:51 +0530

    Time: 95 ms (34.08%) | Memory: 76.8 MB (37.95%) - LeetSync

--- SegmentTrees/307-range-sum-query-mutable/range-sum-query-mutable.java
+++ SegmentTrees/307-range-sum-query-mutable/range-sum-query-mutable.java
@@ -1,82 +1,58 @@
 class NumArray {
-    private int[] segTree;
-    private int[] nums;
     private int n;
+    private int[] nums;
+    private long[] segTree;
 
     /**
-     * Using Segment Tree Approach
+     * Approach : Using Segment Tree Approach
      *
-     * TC: O(N)
-     * SC: O(4 x N) + O(log(N)) ~ O(N)
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(4 x N) + O(log(N)) ~ O(N) + O(log(N))
      */
     public NumArray(int[] nums) {
-        this.n = nums.length;
+        n = nums.length;
         this.nums = nums;
-        segTree = new int[4 * n]; // SC: O(4 x N)
-        buildSegmentTree(0, 0, n - 1);
+        segTree = new long[4 * n]; // SC: O(4 x N)
+        buildSegmentTree(0, 0, n - 1); // TC: O(N), SC: O(log(N)) 
     }
 
     /**
-     * Using Segment Tree Approach
-     *
-     * TC: O(N)
-     * SC: O(log(N))
-     */
-    private void buildSegmentTree(int idx, int left, int right) {
-        // Base Case
-        if (left == right) {
-            segTree[idx] = nums[left];
-            return;
-        }
-        // Recursion Leap of Faith
-        int mid = (left + right) / 2;
-        buildSegmentTree(2 * idx + 1, left, mid);
-        buildSegmentTree(2 * idx + 2, mid + 1, right);
-        segTree[idx] = segTree[2 * idx + 1] + segTree[2 * idx + 2];
-    }
- 
-    /**
-     * Using Segment Tree Approach
+     * Approach : Using Segment Tree Approach
      *
      * TC: O(log(N))
      * SC: O(log(N))
      */
     public void update(int index, int val) {
-        updateSegmentTree(0, index, val, 0, n - 1);
+        updateSegmentTree(index, val, 0, 0, n - 1);
     }
 
     /**
-     * Using Segment Tree Approach
-     *
-     * TC: O(log(N))
-     * SC: O(log(N))
-     */
-    private void updateSegmentTree(int segIdx, int index, int val, int left, int right) {
-        // Base Case
-        if (left == right) {
-            segTree[segIdx] = val;
-            return;
-        }
-        // Recursion Calls
-        int mid = (left + right) / 2;
-        if (index <= mid) {
-            // recursively look at left sub-tree
-            updateSegmentTree(2 * segIdx + 1, index, val, left, mid);
-        } else {
-            // recursively look at right sub-tree
-            updateSegmentTree(2 * segIdx + 2, index, val, mid + 1, right);
-        }
-        segTree[segIdx] = segTree[2 * segIdx + 1] + segTree[2 * segIdx + 2];
-    }
-    
-    /**
-     * Using Segment Tree Approach
+     * Approach : Using Segment Tree Approach
      *
      * TC: O(log(N))
      * SC: O(log(N))
      */
     public int sumRange(int left, int right) {
-        return getRangeFromSegmentTree(0, 0, n - 1, left, right);
+        return getRangeQuerySum(left, right, 0, 0, n - 1);
+    }
+
+    /**
+     * Using Segment Tree Approach
+     *
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(log(N))
+     */
+    private void buildSegmentTree(int segIdx, int left, int right) {
+        // Base Case
+        if (left == right) {
+            segTree[segIdx] = nums[left];
+            return;
+        }
+        // Recursion Leap of Faith
+        int mid = left + (right - left) / 2;
+        buildSegmentTree(segIdx * 2 + 1, left, mid);
+        buildSegmentTree(segIdx * 2 + 2, mid + 1, right);
+        segTree[segIdx] = segTree[2 * segIdx + 1] + segTree[2 * segIdx + 2];
     }
 
     /**
@@ -85,19 +61,42 @@ class NumArray {
      * TC: O(log(N))
      * SC: O(log(N))
      */
-    private int getRangeFromSegmentTree(int segIdx, int left, int right, int l, int r) {
-        // case 1 - out of bounds check
-        if (l > right || r < left) {
+    private void updateSegmentTree(int index, int val, int segIdx, int left, int right) {
+        // Base Case
+        if (left == right) {
+            segTree[segIdx] = val;
+            return;
+        }
+        // Recursion Case
+        int mid = left + (right - left) / 2;
+        if (index <= mid) {
+            updateSegmentTree(index, val, 2 * segIdx + 1, left, mid);
+        } else {
+            updateSegmentTree(index, val, 2 * segIdx + 2, mid + 1, right);
+        }
+        segTree[segIdx] = segTree[2 * segIdx + 1] + segTree[2 * segIdx + 2];
+    }
+
+    /**
+     * Using Segment Tree Approach
+     *
+     * TC: O(log(N))
+     * SC: O(log(N))
+     */
+    private int getRangeQuerySum(int start, int end, int segIdx, int left, int right) {
+        // Base Case
+        if (left > end || right < start) {
+            // out of bounds
             return 0;
         }
-        // case 2 - within the exact range
-        if (l <= left && r >= right) {
-            return segTree[segIdx];
+        if (left >= start && right <= end) {
+            return (int) segTree[segIdx];
         }
-        // case 3 - when there is an overlap
-        int mid = (left + right) / 2;
-        return getRangeFromSegmentTree(2 * segIdx + 1, left, mid, l, r) + 
-            getRangeFromSegmentTree(2 * segIdx + 2, mid + 1, right, l, r);
+        // Recursion Calls
+        // partial overlap
+        int mid = left + (right - left) / 2;
+        return getRangeQuerySum(start, end, 2 * segIdx + 1, left, mid) +
+            getRangeQuerySum(start, end, 2 * segIdx + 2, mid + 1, right);
     }
 }
 
