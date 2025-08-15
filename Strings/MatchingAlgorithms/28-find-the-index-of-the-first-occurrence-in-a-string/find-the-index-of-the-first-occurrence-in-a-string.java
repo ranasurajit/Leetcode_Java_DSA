4a152d1788742842027030c78dc2b4d6b85931fd
Surajit Rana <surajit1602@gmail.com>
2025-08-15 19:49:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (78.99%) - LeetSync

--- Strings/MatchingAlgorithms/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ Strings/MatchingAlgorithms/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,11 +1,71 @@
 class Solution {
     /**
+     * Approach III : Using KMP Algorithm Approach
+     *
+     * TC: O(M + N)
+     * SC: O(N)
+     */
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length();
+        int n = needle.length();
+        int[] lps = computeLPS(needle, n); // SC: O(N) - longest prefix suffix array on pattern String
+        // now using Two Pointers Approach
+        int i = 0; // pointer at the start of String 'haystack'
+        int j = 0; // pointer at the start of String 'needle'
+        while (i < m && j < n) { // TC: O(M + N)
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+                if (j == n) {
+                    // we found a match at index 'i - j'
+                    return i - j;
+                }
+            } else {
+                // we need to fallback j to lps[j - 1] and i will continue its journey
+                if (j > 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Using Computation of Longest Prefix Suffix Array Approach
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    private int[] computeLPS(String s, int n) {
+        int[] lps = new int[n];
+        int i = 1;
+        int len = 0;
+        while (i < n) {
+            if (s.charAt(len) == s.charAt(i)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                // we need to fallback len to lps[len - 1]
+                if (len > 0) {
+                    len = lps[len - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+
+    /**
      * Approach II : Using Two Pointers Approach
      *
      * TC: O(M x N)
      * SC: O(1)
      */
-    public int strStr(String haystack, String needle) {
+    public int strStrUsingTwoPointers(String haystack, String needle) {
         int m = haystack.length();
         int n = needle.length();
         if (n > m || (n == m && !haystack.equals(needle))) {
@@ -30,7 +90,7 @@ class Solution {
      * TC: O(M x N)
      * SC: O(N)
      */
-    public int strStrBruteForce(String haystack, String needle) {
+    public int strStrUsingBruteForce(String haystack, String needle) {
         int m = haystack.length();
         int n = needle.length();
         if (n > m || (n == m && !haystack.equals(needle))) {
