bc0be67a3d1acf3e06091128a6d3bf9c2cb8e088
Surajit Rana <surajit1602@gmail.com>
2025-08-15 19:27:28 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (78.99%) - LeetSync

--- Strings/MatchingAlgorithms/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ Strings/MatchingAlgorithms/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,11 +1,36 @@
 class Solution {
     /**
-     * Approach I : Using String Simulation Approach
+     * Approach II : Using Two Pointers Approach
+     *
+     * TC: O(M x N)
+     * SC: O(1)
+     */
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length();
+        int n = needle.length();
+        if (n > m || (n == m && !haystack.equals(needle))) {
+            return -1;
+        }
+        for (int i = 0; i < m - n + 1; i++) { // TC: O(M - N + 1)
+            int j = 0;
+            while (j < n && haystack.charAt(i + j) == needle.charAt(j)) { // TC: O(N)
+                j++;
+            }
+            if (j == n) {
+                // we found the match
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (String Simulation) Approach
      *
      * TC: O(M x N)
      * SC: O(N)
      */
-    public int strStr(String haystack, String needle) {
+    public int strStrBruteForce(String haystack, String needle) {
         int m = haystack.length();
         int n = needle.length();
         if (n > m || (n == m && !haystack.equals(needle))) {
