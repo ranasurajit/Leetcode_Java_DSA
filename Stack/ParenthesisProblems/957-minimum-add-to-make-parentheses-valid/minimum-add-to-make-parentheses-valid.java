a23e726c8475f673721731d3d385ef78d4838b48
Surajit Rana <surajit1602@gmail.com>
2025-08-15 17:03:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (29.03%) - LeetSync

--- Stack/ParenthesisProblems/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ Stack/ParenthesisProblems/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,11 +1,39 @@
 class Solution {
     /**
+     * Approach II : Using String Simulation Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int minAddToMakeValid(String s) {
+        int n = s.length();
+        int open = 0;
+        int close = 0;
+        int i = 0;
+        int count = 0;
+        while (i < n) { // TC: O(N)
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                open++;
+            } else {
+                if (open > 0) {
+                    open--;
+                } else {
+                    close++;
+                }
+            }
+            i++;
+        }
+        return open + close;
+    }
+
+    /**
      * Approach I : Using Stack Approach
      *
      * TC: O(N)
      * SC: O(N)
      */
-    public int minAddToMakeValid(String s) {
+    public int minAddToMakeValidUsingStack(String s) {
         int n = s.length();
         Stack<Character> st = new Stack<Character>(); // TC: O(N)
         int i = 0;
