6981cc6e21ee90188c7cbfa11962daac79db419b
Surajit Rana <surajit1602@gmail.com>
2025-08-16 11:45:20 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (44.60%) - LeetSync

--- Math/Basic_Math/1448-maximum-69-number/maximum-69-number.java
+++ Math/Basic_Math/1448-maximum-69-number/maximum-69-number.java
@@ -1,11 +1,35 @@
 class Solution {
     /**
-     * Approach : Using Math + String Simulation Approach
+     * Approach II : Using Math Approach (No Extra Space)
+     *
+     * TC: O(log10(N))
+     * SC: O(1)
+     */
+    public int maximum69Number(int num) {
+        int n = num;
+        int position = 0;
+        int maxPos = -1;
+        while (n > 0) {
+            int rem = n % 10;
+            n = n / 10;
+            if (rem == 6) {
+                maxPos = Math.max(maxPos, position); 
+            }
+            position++;
+        }
+        if (maxPos == -1) {
+            return num;
+        }
+        return num + (9 - 6) * (int) Math.pow(10, maxPos);
+    }
+
+    /**
+     * Approach I : Using Math + String Simulation Approach (Extra Space)
      *
      * TC: O(log10(N))
      * SC: O(log10(N))
      */
-    public int maximum69Number (int num) {
+    public int maximum69NumberUsingStringSimulation(int num) {
         char[] digits = String.valueOf(num).toCharArray(); // SC: O(log10(N))
         for (int i = 0; i < digits.length; i++) { // TC: O(log10(N))
             if (digits[i] == '6') {
