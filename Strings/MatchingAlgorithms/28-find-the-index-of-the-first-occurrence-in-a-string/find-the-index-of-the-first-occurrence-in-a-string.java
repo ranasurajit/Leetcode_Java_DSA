b3ff6fddf6078cd386a4ad36b97907ad3c9a6a10
Surajit Rana <surajit1602@gmail.com>
2025-08-18 16:27:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (89.97%) - LeetSync

--- Strings/MatchingAlgorithms/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ Strings/MatchingAlgorithms/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,27 +1,33 @@
 class Solution {
     /**
-     * Approach III : Using KMP Algorithm Approach
+     * Approach I : Using KMP Algorithm Approach
      *
-     * TC: O(M + N)
+     * TC: O(N + M)
      * SC: O(N)
      */
     public int strStr(String haystack, String needle) {
         int m = haystack.length();
         int n = needle.length();
-        int[] lps = computeLPS(needle, n); // SC: O(N) - longest prefix suffix array on pattern String
-        // now using Two Pointers Approach
+        /**
+         * we will compute LPS (Longest Prefix Suffix) on the pattern 
+         * String i.e. on needle
+         */
+        int[] lps = computeLPS(needle, n); // TC: O(N), SC: O(N)
+        /**
+         * we will use Two Pointers Approach to find the pattern 
+         * i.e. needle in String 'haystack'
+         */
         int i = 0; // pointer at the start of String 'haystack'
         int j = 0; // pointer at the start of String 'needle'
-        while (i < m && j < n) { // TC: O(M + N)
+        while (i < m) { // TC: O(M)
             if (haystack.charAt(i) == needle.charAt(j)) {
                 i++;
                 j++;
                 if (j == n) {
-                    // we found a match at index 'i - j'
                     return i - j;
                 }
             } else {
-                // we need to fallback j to lps[j - 1] and i will continue its journey
+                // we will fallback j to lps[j - 1]
                 if (j > 0) {
                     j = lps[j - 1];
                 } else {
@@ -33,74 +39,30 @@ class Solution {
     }
 
     /**
-     * Using Computation of Longest Prefix Suffix Array Approach
+     * Using LPS (Longest Prefix Suffix) Approach
      *
      * TC: O(N)
      * SC: O(N)
      */
     private int[] computeLPS(String s, int n) {
-        int[] lps = new int[n];
-        int i = 1;
+        int[] lps = new int[n]; // SC: O(N)
         int len = 0;
-        while (i < n) {
-            if (s.charAt(len) == s.charAt(i)) {
+        int i = 1;
+        while (i < n) { // TC: O(N)
+            if (s.charAt(i) == s.charAt(len)) {
                 len++;
                 lps[i] = len;
                 i++;
             } else {
-                // we need to fallback len to lps[len - 1]
+                // we will fallback 'len' to lps[len - 1]
                 if (len > 0) {
                     len = lps[len - 1];
                 } else {
+                    lps[i] = 0;
                     i++;
                 }
             }
         }
         return lps;
-    }
-
-    /**
-     * Approach II : Using Two Pointers Approach
-     *
-     * TC: O(M x N)
-     * SC: O(1)
-     */
-    public int strStrUsingTwoPointers(String haystack, String needle) {
-        int m = haystack.length();
-        int n = needle.length();
-        if (n > m || (n == m && !haystack.equals(needle))) {
-            return -1;
-        }
-        for (int i = 0; i < m - n + 1; i++) { // TC: O(M - N + 1)
-            int j = 0;
-            while (j < n && haystack.charAt(i + j) == needle.charAt(j)) { // TC: O(N)
-                j++;
-            }
-            if (j == n) {
-                // we found the match
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    /**
-     * Approach I : Using Brute-Force (String Simulation) Approach
-     *
-     * TC: O(M x N)
-     * SC: O(N)
-     */
-    public int strStrUsingBruteForce(String haystack, String needle) {
-        int m = haystack.length();
-        int n = needle.length();
-        if (n > m || (n == m && !haystack.equals(needle))) {
-            return -1;
-        }
-        for (int i = 0; i < m - n + 1; i++) { // TC: O(M - N + 1)
-            if (haystack.substring(i, i + n).equals(needle)) { // TC: O(N)
-                return i;
-            }
-        }
-        return -1;
     }
 }
