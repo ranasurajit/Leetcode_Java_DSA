795db65a095a2b70674f1a304198648f9f543669
Surajit Rana <surajit1602@gmail.com>
2025-08-20 11:24:42 +0530

    Time: 6 ms (75.00%) | Memory: 54.8 MB (85.77%) - LeetSync

--- DynamicProgramming/2D_DP_Grid/1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ DynamicProgramming/2D_DP_Grid/1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -1,5 +1,35 @@
 class Solution {
     /**
+     * Approach III : Using Tabulation (Bottom-Up DP) Approach
+     * 
+     * TC: O(M x N)
+     * SC: O(M x N)
+     * 
+     * - O(M x N) - dp array memory
+     *
+     * Accepted (32 / 32 testcases passed)
+     */
+    public int countSquares(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int count = 0;
+        int[][] dp = new int[m][n]; // SC: O(M x N)
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                if (i == 0 || j == 0) {
+                    dp[i][j] = matrix[i][j];
+                } else {
+                    if (matrix[i][j] == 1) {
+                        dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
+                    }
+                }
+                count += dp[i][j];
+            }
+        }
+        return count;
+    }
+
+    /**
      * Approach II : Using Memoization (Top-Down DP) Approach
      *
      * TC: O(M x N) x O(M x N)
@@ -7,7 +37,7 @@ class Solution {
      *
      * Accepted (32 / 32 testcases passed)
      */
-    public int countSquares(int[][] matrix) {
+    public int countSquaresMemoization(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
         int count = 0;
