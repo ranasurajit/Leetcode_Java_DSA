35aeeae94f8ecc348d7f6da2c66dd771a99fd446
Surajit Rana <surajit1602@gmail.com>
2025-08-21 11:59:23 +0530

    Time: 20 ms (10.27%) | Memory: 45.5 MB (63.36%) - LeetSync

--- Stack/MonotonicStack/1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.java
+++ Stack/MonotonicStack/1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.java
@@ -1,11 +1,49 @@
 class Solution {
     /**
+     * Approach II : Using Monotonic Stack + Array Pre-Processing Approach
+     *
+     * TC: O(M x 2 x N) ~ O(M x N)
+     * SC: O(3 x N) ~ O(N)
+     */
+    public int numSubmat(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] heights = new int[n];   // SC: O(N)
+        int count = 0;
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                if (mat[i][j] == 1) {
+                    heights[j] += 1;
+                } else {
+                    heights[j] = 0;
+                }
+            }
+            Stack<Integer> st = new Stack<Integer>(); // SC: O(N)
+            int[] sum = new int[n]; // SC: O(N)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                while (!st.isEmpty() && heights[st.peek()] >= heights[j]) {
+                    st.pop();
+                }
+                if (st.isEmpty()) {
+                    sum[j] = heights[j] * (j + 1);
+                } else {
+                    int prev = st.peek();
+                    sum[j] = sum[prev] + heights[j] * (j - prev);
+                }
+                st.push(j);
+                count += sum[j];
+            }
+        }
+        return count;
+    }
+
+    /**
      * Approach I : Using Simulation + Array Pre-Processing Approach
      *
      * TC: O(M x N x N)
      * SC: O(N)
      */
-    public int numSubmat(int[][] mat) {
+    public int numSubmatArrayPreProcess(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
         int[] heights = new int[n];   // SC: O(N)
