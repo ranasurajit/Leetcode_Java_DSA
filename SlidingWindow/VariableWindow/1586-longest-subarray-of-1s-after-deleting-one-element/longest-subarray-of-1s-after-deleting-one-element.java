699b16e67197b760a2085a98c5bc26197a1a1771
Surajit Rana <surajit1602@gmail.com>
2025-08-24 09:54:51 +0530

    Time: 5 ms (5.33%) | Memory: 57.7 MB (6.82%) - LeetSync

---
+++ SlidingWindow/VariableWindow/1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * Approach : Using Sliding Window (Variable Size) Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int longestSubarray(int[] nums) {
+        int n = nums.length;
+        int count1s = 0;
+        int count0s = 0;
+        int i = 0; // start pointer of sliding window
+        int j = 0; // end pointer of sliding window
+        int maxLength = 0;
+        while (j < n) { // TC: O(N)
+            count1s += (nums[j] == 1) ? 1 : 0;
+            count0s += (nums[j] == 0) ? 1 : 0;
+            while (count0s > 1) {
+                // remove computation from index 'i'
+                count1s -= (nums[i] == 1) ? 1 : 0;
+                count0s -= (nums[i] == 0) ? 1 : 0;
+                i++;
+            }
+            if (count0s <= 1) {
+                maxLength = Math.max(maxLength, j - i + 1);
+            }
+            j++;
+        }
+        return maxLength - 1;
+    }
+}
