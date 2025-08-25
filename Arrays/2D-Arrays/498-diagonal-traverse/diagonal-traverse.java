1d085cb6b26c03624486cdf5add74e6215429a55
Surajit Rana <surajit1602@gmail.com>
2025-08-25 10:50:38 +0530

    Time: 9 ms (20.13%) | Memory: 47.3 MB (56.76%) - LeetSync

--- Arrays/2D-Arrays/498-diagonal-traverse/diagonal-traverse.java
+++ Arrays/2D-Arrays/498-diagonal-traverse/diagonal-traverse.java
@@ -1,27 +1,32 @@
 class Solution {
+    /**
+     * Approach : Using Simulation Approach
+     *
+     * TC: O(M x N) + O(M x N) ~ O(M x N)
+     * SC: O(M x N) + O(M x N) ~ O(M x N)
+     */
     public int[] findDiagonalOrder(int[][] mat) {
-        int rows = mat.length;
-        int cols = mat[0].length;
-        int size = rows * cols;
-        int[] result = new int[size];
-        Map<Integer, ArrayList<Integer>> hm = new HashMap<Integer, ArrayList<Integer>>();
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (!hm.containsKey(i + j)) {
-                    hm.put((i + j), new ArrayList<Integer>());
-                }
-                if ((i + j) % 2 == 0) {
-                    hm.get(i + j).add(0, mat[i][j]);
-                } else {
-                    hm.get(i + j).add(mat[i][j]);
-                }
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] result = new int[m * n]; // SC: O(M x N)
+        Map<Integer, ArrayList<Integer>> map =
+            new HashMap<Integer, ArrayList<Integer>>(); // SC: O(M x N)
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                map.computeIfAbsent(i + j, k -> new ArrayList<Integer>()).add(mat[i][j]);
             }
         }
-        int count = 0;
-        for (Integer key : hm.keySet()) {
-            for (Integer it : hm.get(key)) {
-                result[count] = it;
-                count++;
+        int index = 0;
+        for (int i = 0; i <= m + n - 2; i++) { // TC: O(M x N)
+            ArrayList<Integer> list = map.get(i);
+            if ((i & 1) == 0) {
+                for (int j = list.size() - 1; j >= 0; j--) {
+                    result[index++] = list.get(j);
+                }
+            } else {
+                for (int j = 0; j < list.size(); j++) {
+                    result[index++] = list.get(j);
+                }
             }
         }
         return result;
