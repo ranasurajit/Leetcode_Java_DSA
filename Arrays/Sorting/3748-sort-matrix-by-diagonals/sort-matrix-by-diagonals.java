bea3d8fe5927a54ddb1b40cdf9c0199625a334bb
Surajit Rana <surajit1602@gmail.com>
2025-08-28 11:16:47 +0530

    Time: 5 ms (69.27%) | Memory: 45.5 MB (78.44%) - LeetSync

--- Arrays/Sorting/3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.java
+++ Arrays/Sorting/3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.java
@@ -1,11 +1,44 @@
 class Solution {
     /**
-     * Approach : Using Array Simulation + Hashing Approach
+     * Approach II : Using Array Simulation Approach
+     *
+     * TC: O(2 x N x N) + O(N x N x log(N)) + O(2 x N x N) + O(N x N x log(N)) ~ O(N ^ 2 x log(N))
+     * SC: O(N)
+     */
+    public int[][] sortMatrix(int[][] grid) {
+        int n = grid.length;
+        // handling bottom-left triangle
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            List<Integer> temp = new ArrayList<Integer>(); // SC: O(N)
+            for (int j = 0; i + j < n; j++) { // TC: O(N)
+                temp.add(grid[i + j][j]);
+            }
+            temp.sort(Collections.reverseOrder()); // TC: O(N x log(N))
+            for (int j = 0; i + j < n; j++) { // TC: O(N)
+                grid[i + j][j] = temp.get(j);
+            }
+        }
+        // handling top-right triangle
+        for (int j = 1; j < n; j++) { // TC: O(N)
+            List<Integer> temp = new ArrayList<Integer>(); // SC: O(N) - reused
+            for (int i = 0; j + i < n; i++) { // TC: O(N)
+                temp.add(grid[i][j + i]);
+            }
+            Collections.sort(temp); // TC: O(N x log(N))
+            for (int i = 0; j + i < n; i++) { // TC: O(N)
+                grid[i][j + i] = temp.get(i);
+            }
+        }
+        return grid;
+    }
+
+    /**
+     * Approach I : Using Array Simulation + Hashing Approach
      *
      * TC: O(N x N) + O(2 x N x N x log(N)) + O(N x N x N) ~ O(N ^ 3)
      * SC: O(2 x N) ~ O(N)
      */
-    public int[][] sortMatrix(int[][] grid) {
+    public int[][] sortMatrixUsingHashingSort(int[][] grid) {
         int n = grid.length;
         Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>(); // SC: O(2 x N)
         for (int i = 0; i < n; i++) {     // TC: O(N)
