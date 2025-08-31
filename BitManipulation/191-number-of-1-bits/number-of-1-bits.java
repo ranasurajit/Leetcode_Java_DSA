a854f4b16a702747086668d23baa249b7ac22fbf
Surajit Rana <surajit1602@gmail.com>
2025-08-31 15:04:32 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (30.98%) - LeetSync

--- BitManipulation/191-number-of-1-bits/number-of-1-bits.java
+++ BitManipulation/191-number-of-1-bits/number-of-1-bits.java
@@ -1,11 +1,26 @@
 class Solution {
     /**
-     * Approach : Using Bit-Manipulation Approach
+     * Approach II: Brian Kernighan's Algorithm Approach
      *
      * TC: O(32) ~ O(1)
      * SC: O(1)
      */
     public int hammingWeight(int n) {
+        int countSetBits = 0;
+        while (n > 0) {
+            n = (n & (n - 1));
+            countSetBits++;
+        }
+        return countSetBits;
+    }
+    
+    /**
+     * Approach I : Using Bit-Manipulation Approach
+     *
+     * TC: O(32) ~ O(1)
+     * SC: O(1)
+     */
+    public int hammingWeightBitManipulation(int n) {
         int countSetBits = 0;
         for (int i = 31; i >= 0; i--) { // TC: O(32)
             if (((n >> i) & 1) == 1) {
