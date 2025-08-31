60ba295dcda9832ce80340d322b9c5279fc0dacd
Surajit Rana <surajit1602@gmail.com>
2025-08-31 14:31:30 +0530

    Time: 238 ms (49.67%) | Memory: 41.2 MB (56.80%) - LeetSync

--- Recursion/Backtracking/37-sudoku-solver/sudoku-solver.java
+++ Recursion/Backtracking/37-sudoku-solver/sudoku-solver.java
@@ -2,32 +2,35 @@ class Solution {
     /**
      * Approach : Using Recursion + Backtracking Approach
      *
-     * TC: O(9 ^ M) as each cell has 9 options to be filled and M = count(empty cells) <<< 81
+     * TC: O(9 ^ M)
      * SC: O(M)
+     *
+     * where M = empty cells (.) and each empty cell has 9 possibilities to be filled
      */
     public void solveSudoku(char[][] board) {
-        solveBacktrack(board);
+        solvedSudokuRecursion(board);
     }
 
     /**
      * Using Recursion + Backtracking Approach
      *
-     * TC: O(9 ^ M) as each cell has 9 options to be filled and M = count(empty cells) <<< 81
+     * TC: O(9 ^ M)
      * SC: O(M)
+     *
+     * where M = empty cells (.) and each empty cell has 9 possibilities to be filled
      */
-    private boolean solveBacktrack(char[][] board) {
+    private boolean solvedSudokuRecursion(char[][] board) {
         for (int i = 0; i < 9; i++) { // TC: O(9)
             for (int j = 0; j < 9; j++) { // TC: O(9)
                 if (board[i][j] == '.') {
-                    // here at this cell (i, j) we need to find all possibilities by choosing 1 to 9
+                    // explore setting characters '1' to '9' in board[i][j]
                     for (char ch = '1'; ch <= '9'; ch++) { // TC: O(9)
-                        if (isValidBoard(board, i, j, ch)) { // TC: O(9)
-                            board[i][j] = ch;
-                            if (solveBacktrack(board)) {
+                        if (isValidSudoku(board, i, j, ch)) {
+                            board[i][j] = ch; // modify
+                            if (solvedSudokuRecursion(board)) { // explore
                                 return true;
                             } else {
-                                // backtrack
-                                board[i][j] = '.';
+                                board[i][j] = '.'; // backtrack
                             }
                         }
                     }
@@ -41,24 +44,30 @@ class Solution {
     /**
      * Using Simulation Approach
      *
-     * TC: O(9)
+     * TC: O(3 x 9) ~ O(1)
      * SC: O(1)
      */
-    private boolean isValidBoard(char[][] board, int row, int col, char ch) {
-        for (int i = 0; i < 9; i++) {
-            // validate in same row
-            if (board[row][i] == ch) {
+    private boolean isValidSudoku(char[][] board, int i, int j, char ch) {
+        // validate rows
+        for (int row = 0; row < 9; row++) { // TC: O(9)
+            if (board[row][j] == ch) {
                 return false;
             }
-            // validate in same column
+        }
+        // validate columns
+        for (int col = 0; col < 9; col++) { // TC: O(9)
             if (board[i][col] == ch) {
                 return false;
             }
-            // validate in 3 x 3 grid
-            int effRow = 3 * (row / 3) + (i / 3);
-            int effCol = 3 * (col / 3) + (i % 3);
-            if (board[effRow][effCol] == ch) {
-                return false;
+        }
+        // validate 3 x 3 sub-boxes
+        int rowoffset = i >= 6 ? 6: (i >= 3 ? 3 : 0);
+        int coloffset = j >= 6 ? 6: (j >= 3 ? 3 : 0);
+        for (int r = rowoffset; r < rowoffset + 3; r++) { // TC: O(3)
+            for (int c = coloffset; c < coloffset + 3; c++) { // TC: O(3)
+                if (board[r][c] == ch) {
+                    return false;
+                }
             }
         }
         return true;
